8548588554c3e1bc87651c2b6340b52fb820b228
Jiri Olsa <jolsa@kernel.org>
2022-04-25 14:07:06 +0200

    tetragon: Enable full copy support in retkprobe
    Enabling full copy support in retkprobe by adding data_hep
    map which is used by data events and new function called
    by tail call that sends the extra data messages.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_generic_retkprobe.c
+++ bpf/process/bpf_generic_retkprobe.c
@@ -8,6 +8,8 @@
 #include "bpf_events.h"
 #include "retprobe_map.h"
 #include "types/basic.h"
+#include "data_event.h"
+#include "full_copy.h"
 
 #define MAX_FILENAME 8096
 
@@ -24,6 +26,13 @@ struct bpf_map_def __attribute__((section("maps"), used)) retkprobe_calls = {
 	.type = BPF_MAP_TYPE_PROG_ARRAY,
 	.key_size = sizeof(__u32),
 	.value_size = sizeof(__u32),
+	.max_entries = 1,
+};
+
+struct bpf_map_def __attribute__((section("maps"), used)) data_heap = {
+	.type = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(struct msg_data),
 	.max_entries = 1,
 };
 
@@ -46,6 +55,7 @@ generic_kprobe_event(struct pt_regs *ctx)
 		return 0;
 
 	e->thread_id = retprobe_map_get_key(ctx);
+	full_copy_init(e);
 
 	if (!retprobe_map_get(e->thread_id, &info))
 		return 0;
@@ -57,7 +67,8 @@ generic_kprobe_event(struct pt_regs *ctx)
 				      (unsigned long)ctx->ax, 0, 0);
 	switch (do_copy) {
 	case char_buf:
-		size += __copy_char_buf(size, info.ptr, ctx->ax, e, false);
+		size += __copy_char_buf(size, info.ptr, ctx->ax, e,
+					info.fullCopy);
 		break;
 	case char_iovec:
 		size += __copy_char_iovec(size, info.ptr, info.cnt, ctx->ax, e,
@@ -97,6 +108,18 @@ generic_kprobe_event(struct pt_regs *ctx)
 		     : [total] "+r"(total)
 		     :);
 	e->common.size = total;
+
+	if (e->full_copy.cnt) {
+		tail_call(ctx, &retkprobe_calls, 0);
+		return 2;
+	}
+
 	perf_event_output(ctx, &tcpmon_map, BPF_F_CURRENT_CPU, e, total);
 	return 0;
+}
+
+__attribute__((section(("kprobe/0")), used)) int
+generic_retkprobe_full_copy(void *ctx)
+{
+	return full_copy(ctx, &process_call_heap, &data_heap);
 }
