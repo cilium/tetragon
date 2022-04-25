9283bb017bc91a9d26f615a3d3cb1518689afbc4
Jiri Olsa <jolsa@kernel.org>
2022-04-25 22:51:26 +0200

    tetragon: Add char_iovec fullCopy test
    Adding test to check on new 'fullCopy' spec flag,
    which can be used at the char_iovec argument.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -2091,3 +2091,169 @@ spec:
 
 	runKprobeFullCopy_char_buf(t, configHook, checker, fdw, fdr, &buffer)
 }
+
+func runKprobe_char_iovec_fullCopy(t *testing.T, configHook string,
+	checker ec.MultiEventChecker, fdw, fdr int, buffer []byte) {
+	var doneWG, readyWG sync.WaitGroup
+	defer doneWG.Wait()
+
+	ctx, cancel := context.WithTimeout(context.Background(), cmdWaitTime)
+	defer cancel()
+
+	testConfigHook := []byte(configHook)
+	err := ioutil.WriteFile(testConfigFile, testConfigHook, 0644)
+	if err != nil {
+		t.Fatalf("writeFile(%s): err %s", testConfigFile, err)
+	}
+
+	obs, err := observer.GetDefaultObserverWithWatchers(t, observer.WithConfig(testConfigFile),
+		observer.WithLib(tetragonLib), observer.WithNumPages(64))
+	if err != nil {
+		t.Fatalf("GetDefaultObserverWithWatchers error: %s", err)
+	}
+	observer.LoopEvents(ctx, t, &doneWG, &readyWG, obs)
+	readyWG.Wait()
+
+	var iovw = make([][]byte, 4)
+	var iovr = make([][]byte, 7)
+
+	// write 200000B
+	iovw[0] = make([]byte, 10000)
+	iovw[1] = make([]byte, 40000)
+	iovw[2] = make([]byte, 50000)
+	iovw[3] = make([]byte, 100000)
+
+	copy(iovw[0], buffer)
+	copy(iovw[1], buffer[10000:])
+	copy(iovw[2], buffer[50000:])
+	copy(iovw[3], buffer[100000:])
+
+	_, err = unix.Writev(fdw, iovw)
+	assert.NoError(t, err)
+
+	syscall.Fsync(fdw)
+
+	// read 210000B
+	iovr[0] = make([]byte, 10000)
+	iovr[1] = make([]byte, 40000)
+	iovr[2] = make([]byte, 50000)
+	iovr[3] = make([]byte, 10000)
+	iovr[4] = make([]byte, 40000)
+	iovr[5] = make([]byte, 50000)
+	iovr[6] = make([]byte, 10000)
+
+	_, err = unix.Readv(fdr, iovr)
+	assert.NoError(t, err)
+
+	err = observer.JsonTestCheck(t, checker)
+	assert.NoError(t, err)
+}
+
+func TestKprobe_char_iovec_fullCopy(t *testing.T) {
+	fdw, fdr, _ := createTestFile(t)
+	pidStr := strconv.Itoa(int(observer.GetMyPid()))
+
+	configHook := `
+apiVersion: hubble-enterprise.io/v1
+metadata:
+  name: "sys_write_writev"
+spec:
+  kprobes:
+  - call: "__x64_sys_writev"
+    syscall: true
+    args:
+    - index: 0
+      type: "int"
+    - index: 1
+      type: "char_iovec"
+      sizeArgIndex: 3
+      fullCopy: true
+    - index: 2
+      type: "int"
+    selectors:
+    - matchPIDs:
+      - operator: In
+        followForks: true
+        values:
+        - ` + pidStr + `
+      matchArgs:
+      - index: 0
+        operator: "Equal"
+        values:
+        - ` + fmt.Sprint(fdw)
+
+	size := 200000
+	buffer := make([]byte, size)
+
+	for i := 0; i < size; i++ {
+		buffer[i] = 'A' + byte(i%26)
+	}
+
+	kpChecker := ec.NewProcessKprobeChecker().
+		WithFunctionName(sm.Full("__x64_sys_writev")).
+		WithArgs(ec.NewKprobeArgumentListMatcher().
+			WithOperator(lc.Ordered).
+			WithValues(
+				ec.NewKprobeArgumentChecker().WithIntArg(int32(fdw)),
+				ec.NewKprobeArgumentChecker().WithBytesArg(bc.Full(buffer)),
+				ec.NewKprobeArgumentChecker().WithSizeArg(4),
+			))
+	checker := ec.NewUnorderedEventChecker(kpChecker)
+
+	runKprobe_char_iovec_fullCopy(t, configHook, checker, fdw, fdr, buffer)
+}
+
+func TestKprobe_char_iovec_fullCopy_returnCopy(t *testing.T) {
+	fdw, fdr, _ := createTestFile(t)
+	pidStr := strconv.Itoa(int(observer.GetMyPid()))
+
+	configHook := `
+apiVersion: hubble-enterprise.io/v1
+metadata:
+  name: "sys_write_read"
+spec:
+  kprobes:
+  - call: "__x64_sys_readv"
+    syscall: true
+    args:
+    - index: 0
+      type: "int"
+    - index: 1
+      type: "char_iovec"
+      returnCopy: true
+      sizeArgIndex: 3
+      fullCopy: true
+    - index: 2
+      type: "size_t"
+    selectors:
+    - matchPIDs:
+      - operator: In
+        followForks: true
+        values:
+        - ` + pidStr + `
+      matchArgs:
+      - index: 0
+        operator: "Equal"
+        values:
+        - ` + fmt.Sprint(fdr)
+
+	size := 200000
+	buffer := make([]byte, size)
+
+	for i := 0; i < size; i++ {
+		buffer[i] = 'A' + byte(i%26)
+	}
+
+	kpChecker := ec.NewProcessKprobeChecker().
+		WithFunctionName(sm.Full("__x64_sys_readv")).
+		WithArgs(ec.NewKprobeArgumentListMatcher().
+			WithOperator(lc.Ordered).
+			WithValues(
+				ec.NewKprobeArgumentChecker().WithIntArg(int32(fdr)),
+				ec.NewKprobeArgumentChecker().WithBytesArg(bc.Full(buffer)),
+				ec.NewKprobeArgumentChecker().WithSizeArg(7),
+			))
+	checker := ec.NewUnorderedEventChecker(kpChecker)
+
+	runKprobe_char_iovec_fullCopy(t, configHook, checker, fdw, fdr, buffer)
+}
