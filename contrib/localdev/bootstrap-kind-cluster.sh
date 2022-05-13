7fb1084db5b6933e957e4b7db16a32e2c398fd85
William Findlay <will@isovalent.com>
2022-05-13 15:46:11 -0400

    workflow: add kind scripts to contrib/localdev
    This PR introduces contrib/localdev and adds two helper scripts to make it easy to
    bootstrap a new kind cluster and install the desired version of Tetragon into that
    cluster.
    
    Signed-off-by: William Findlay <will@isovalent.com>

---
+++ contrib/localdev/bootstrap-kind-cluster.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+error() {
+    echo $@ 1>&2
+    exit 1
+}
+
+set -eu
+
+PROJECT_ROOT="$(realpath $(dirname "${BASH_SOURCE[0]}")/../..)"
+cd "$PROJECT_ROOT"
+source contrib/localdev/conf
+
+if ! command -v kind &>/dev/null; then
+    error "kind is not in \$PATH! Bailing out!"
+fi
+
+FORCE=0
+
+usage() {
+	echo "usage: bootstrap-kind-cluster.sh [OPTIONS]" 1>&2
+	echo "OPTIONS:" 1>&2
+    echo "    -f,--force  force bootstrapping even if cluster already exists" 1>&2
+}
+
+while [ $# -ge 1 ]; do
+	if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+        FORCE=1
+		shift 1
+    else
+        usage
+        exit 1
+	fi
+done
+
+bootstrap_cluster() {
+    if ! kind get clusters | grep "$CLUSTER_NAME" &>/dev/null; then
+        echo "Creating a new cluster \"$CLUSTER_NAME\"..." 1>&2
+        kind create cluster --name "$CLUSTER_NAME" --wait=2m
+    else
+        if [ "$FORCE" != 1 ]; then
+            echo "Cluster already exists... Exiting... (Re-run with -f to force.)" 1>&2
+            exit 0
+        else
+            echo "Recreating cluster..." 1>&2
+            kind delete cluster --name "$CLUSTER_NAME"
+            kind create cluster --name "$CLUSTER_NAME" --wait=5m
+        fi
+    fi
+
+    kubectl cluster-info --context "kind-$CLUSTER_NAME"
+}
+
+bootstrap_cluster
