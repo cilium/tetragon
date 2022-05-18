cb28e1bae72cb08afc30195a90654af06807f00d
Kornilios Kourtis <kornilios@isovalent.com>
2022-05-18 13:31:30 +0200

    contrib: move C test progs in tester-progs
    We have a lot of different C programs under contrib/.  Move them all to
    a single directory.
    
    [commit: 10b6e9dd1b2f622bbca97edd20e91ac608eb6f9b]
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/sensors/tracing/kprobe_filterchange_test.go
+++ pkg/sensors/tracing/kprobe_filterchange_test.go
@@ -32,7 +32,7 @@ func TestKprobeNSChanges(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), cmdWaitTime)
 	defer cancel()
 
-	testBin := testContribPath("namespace-tester/test_ns")
+	testBin := testContribPath("tester-progs/namespace-tester")
 	testCmd := exec.CommandContext(ctx, testBin)
 	testPipes, err := testutils.NewCmdBufferedPipes(testCmd)
 	if err != nil {
@@ -107,7 +107,7 @@ func TestKprobeCapChanges(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), cmdWaitTime)
 	defer cancel()
 
-	testBin := testContribPath("capabilities-tester/test_caps")
+	testBin := testContribPath("tester-progs/capabilities-tester")
 	testCmd := exec.CommandContext(ctx, testBin)
 	testPipes, err := testutils.NewCmdBufferedPipes(testCmd)
 	if err != nil {
