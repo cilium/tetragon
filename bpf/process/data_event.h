19f42f0d7240d6e99951fc8d77a23a6e47b129f3
Jiri Olsa <jolsa@kernel.org>
2022-05-31 12:28:02 +0200

    tetragon: Add data_event_str function
    Adding data_event_str function that works in a same way
    as data_event_bytes but reads full string instead of data
    from pointer with size.
    
    The string is sent as data event the same way as for
    data_event_bytes interface.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/data_event.h
+++ bpf/process/data_event.h
@@ -58,6 +58,63 @@ static long do_bytes(void *ctx, struct msg_data *msg, unsigned long arg,
 	return bytes - rd_bytes;
 }
 
+static inline __attribute__((always_inline)) long
+__do_str(void *ctx, struct msg_data *msg, unsigned long arg,
+	 size_t bytes __maybe_unused)
+{
+	size_t size, max = sizeof(msg->arg) - 1;
+	int err;
+
+	/* Code movement from clang forces us to inline bounds checks here */
+	asm volatile("%[max] &= 0x7fff;\n"
+		     "if %[max] < 32736 goto +1\n;"
+		     "%[max] = 32736;\n"
+		     :
+		     : [max] "+r"(max)
+		     :);
+
+	err = probe_read_str(&msg->arg[0], max, (char *)arg);
+	if (err < 0)
+		return err;
+
+	/* cut out the zero byte */
+	err -= 1;
+
+	msg->common.size = offsetof(struct msg_data, arg) + err;
+
+	size = err + offsetof(struct msg_data, arg);
+
+	/* Code movement from clang forces us to inline bounds checks here */
+	asm volatile("%[size] &= 0x7fff;\n" : : [size] "+r"(size) :);
+	perf_event_output(ctx, &tcpmon_map, BPF_F_CURRENT_CPU, msg, size);
+	return err == max ? 0 : 1;
+}
+
+static long do_str(void *ctx, struct msg_data *msg, unsigned long arg,
+		   size_t bytes __maybe_unused)
+{
+	size_t rd_bytes = 0;
+	int err, i;
+
+#ifdef __LARGE_BPF_PROG
+#define __CNT 10
+#else
+#define __CNT 2
+#pragma unroll
+#endif
+	for (i = 0; i < __CNT; i++) {
+		err = __do_str(ctx, msg, arg + rd_bytes, bytes - rd_bytes);
+		if (err < 0)
+			return err;
+		if (err == 1)
+			return 0;
+	}
+#undef __CNT
+
+	/* we have no idea what's string leftover */
+	return -1;
+}
+
 static inline __attribute__((always_inline)) int data_event(
 	void *ctx, struct data_event_desc *desc, unsigned long uptr,
 	size_t size, struct bpf_map_def *heap,
@@ -95,4 +152,11 @@ data_event_bytes(void *ctx, struct data_event_desc *desc, unsigned long uptr,
 		 size_t size, struct bpf_map_def *heap)
 {
 	return data_event(ctx, desc, uptr, size, heap, do_bytes);
+}
+
+static inline __attribute__((always_inline)) size_t
+data_event_str(void *ctx, struct data_event_desc *desc, unsigned long uptr,
+	       struct bpf_map_def *heap)
+{
+	return data_event(ctx, desc, uptr, -1, heap, do_str);
 }
