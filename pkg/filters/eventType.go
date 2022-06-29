1755bc3a3440b9e54893d5a72dc5e8a70cde53a6
John Fastabend <john.fastabend@gmail.com>
2022-06-29 13:53:29 -0700

    tetragon: remove filter codegen
    Use reflect pkg to do this without having to generate any specific
    code.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- pkg/filters/eventType.go
+++ pkg/filters/eventType.go
@@ -10,7 +10,6 @@ import (
 	v1 "github.com/cilium/hubble/pkg/api/v1"
 	hubbleFilters "github.com/cilium/hubble/pkg/filters"
 	"github.com/cilium/tetragon/api/v1/tetragon"
-	codegen "github.com/cilium/tetragon/api/v1/tetragon/codegen/filters"
 )
 
 func filterByEventType(types []reflect.Type) hubbleFilters.FilterFunc {
@@ -36,11 +35,7 @@ func (f *EventTypeFilter) OnBuildFilter(_ context.Context, ff *tetragon.Filter) 
 		var types []reflect.Type
 
 		for _, s := range ff.EventSet {
-			opCode, err := codegen.OpCodeForEventType(s)
-			if err != nil {
-				return nil, err
-			}
-			types = append(types, opCode)
+			types = append(types, reflect.TypeOf(s))
 		}
 		fs = append(fs, filterByEventType(types))
 	}
