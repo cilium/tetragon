8926bb4ca247d8491353c76d9920db32c4725581
Djalal Harouni <tixxdz@gmail.com>
2022-07-01 20:05:47 +0200

    pkg:api: add cgroup operations
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/api/ops/ops.go
+++ pkg/api/ops/ops.go
@@ -21,8 +21,25 @@ const (
 
 	MSG_OP_DATA = 24
 
+	MSG_OP_CGROUP = 25
+
 	// just for testing
 	MSG_OP_TEST = 254
+)
+
+const (
+	MSG_OP_CGROUP_UNDEF       = iota
+	MSG_OP_CGROUP_MKDIR       = 1
+	MSG_OP_CGROUP_RMDIR       = 2
+	MSG_OP_CGROUP_RELEASE     = 3
+	MSG_OP_CGROUP_ATTACH_TASK = 10
+)
+
+const (
+	CGROUP_UNTRACKED    = iota // Cgroup was created but we did not track it
+	CGROUP_NEW          = 1    // Cgroup was just created
+	CGROUP_RUNNING      = 2    // Cgroup from new => running (fork,exec task inside)
+	CGROUP_RUNNING_PROC = 3    // Cgroups that were generated from pids of procfs
 )
 
 type OpCode int
@@ -46,6 +63,30 @@ func (op OpCode) String() string {
 		14:  "GenericTracepoint",
 		23:  "Clone",
 		24:  "Data",
+		25:  "Cgroup",
 		254: "Test",
 	}[op]
+}
+
+type CgroupOpCode int
+
+func (op CgroupOpCode) String() string {
+	return [...]string{
+		0:  "Undef",
+		1:  "CgroupMkdir",
+		2:  "CgroupRmdir",
+		3:  "CgroupRelease",
+		10: "CgroupAttachTask",
+	}[op]
+}
+
+type CgroupState int
+
+func (st CgroupState) String() string {
+	return [...]string{
+		0: "Untracked",
+		1: "New",
+		2: "Running",
+		3: "RunningProc",
+	}[st]
 }
