0a9558ae1b4e752400bb3c188f983db0aad67d0e
Djalal Harouni <tixxdz@gmail.com>
2022-07-11 16:49:04 +0200

    pkg: add TetragonConf struct to store and share runtime config with bpf
    TetragonConf struct will be stored in a bpf map `tg_conf_map` and will be
    shared between userspace and bpf.
    
    Information will be obtained by Tetragon userspace during startup then stored
    into the map, then bpf cgroups programs will be probed to assert and complete
    that information in order to discover Tetragon runtime and cgroup configurations.
    
    This allows Tetragon to work in namespaced and non-namespaced environments where
    obtaining cgroup information is difficult.
    
    Signed-off-by: Djalal Harouni <djalal@isovalent.com>

---
+++ pkg/api/confapi/confapi.go
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+package confapi
+
+type TetragonConf struct {
+	Mode        uint32 // Deployment mode
+	PID         uint32 // Tetragon PID
+	NSPID       uint32 // Tetragon PID in namespace
+	TgCgrpLevel uint32 // Tetragon cgroup level
+	TgCgrpId    uint64 // Tetragon cgroup ID
+	CgrpFsMagic uint64 // Cgroupv1 or cgroupv2
+}
