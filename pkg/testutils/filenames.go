9fa82276dd7027960027f094482d366bcb6429b0
Kornilios Kourtis <kornilios@isovalent.com>
2022-07-22 14:59:59 +0200

    testing: fix "file already closed" error messages
    In the previous PR we threaded the ctx from the test cases to the
    loadObserver and loadExporter functions. This means that we can now use
    the Close() method to cleanup the export files used in our tests.
    
    This wil fix an error condition that caused messages such as:
    fix time="2022-07-22T11:23:08Z" level=warning msg="Failed to JSON encode" error="write /tmp/tetragon.gotest.TestKprobeObjectWriteReadHostNs.3694345681.json: file already closed"
    to appear.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/testutils/filenames.go
+++ pkg/testutils/filenames.go
@@ -10,25 +10,51 @@ import (
 	"testing"
 )
 
-type exportFile struct {
-	f     *os.File
-	fname string
-	keep  bool
+// t.Name() -> ExportFile
+var exportFiles = make(map[string]*ExportFile, 1)
+var exportFilesLock sync.Mutex
+
+type ExportFile struct {
+	*os.File
+	t     *testing.T
+	fName string // file name
+	keep  bool   // should we keep the file at the end?
 }
 
-// t.Name() -> file
-var exportFiles = map[string]exportFile{}
-var exportFilesLock sync.Mutex
+func (f *ExportFile) Close() error {
+	exportFilesLock.Lock()
+	defer exportFilesLock.Unlock()
+
+	tName := f.t.Name()
+	ef, ok := exportFiles[tName]
+	if !ok {
+		f.t.Logf("could not find ourself in exportFiles: testName=%s fname=%s", tName, f.fName)
+		return f.File.Close()
+	}
+	if ef != f {
+		f.t.Logf("Unexpected file %+v vs %+v", ef, f)
+	}
+	defer delete(exportFiles, tName)
+
+	err := f.File.Close()
+	if f.keep {
+		f.t.Logf("keeping export file for %s (%s)", tName, ef.fName)
+	} else {
+		f.t.Logf("deleting export file for %s (%s)", tName, ef.fName)
+		os.Remove(f.fName)
+	}
+	return err
+}
 
 // CreateExportFile creates an export file for a test.
 // a callback will be registered at t.Cleanup() for closing the file, and removing the file
-func CreateExportFile(t *testing.T) *os.File {
+func CreateExportFile(t *testing.T) *ExportFile {
 	exportFilesLock.Lock()
 	defer exportFilesLock.Unlock()
 
 	testName := t.Name()
 	if _, ok := exportFiles[testName]; ok {
-		t.Fatalf("unexpected error: t.Name() %s is not unique", testName)
+		t.Fatalf("unexpected error: t.Name() %s already exists", testName)
 	}
 
 	fname := fmt.Sprintf("tetragon.gotest.%s.*.json", testName)
@@ -38,32 +64,15 @@ func CreateExportFile(t *testing.T) *os.File {
 	}
 	os.Chmod(f.Name(), 0644)
 
-	exportFiles[testName] = exportFile{
-		f:     f,
-		fname: f.Name(),
+	ret := &ExportFile{
+		File:  f,
+		t:     t,
+		fName: f.Name(),
 		keep:  false,
 	}
 
-	t.Cleanup(func() {
-		tname := testName
-		exportFilesLock.Lock()
-		defer exportFilesLock.Unlock()
-		ef, ok := exportFiles[testName]
-		if !ok {
-			t.Fatalf("file for test %s does not exist", tname)
-		}
-
-		ef.f.Close()
-		if ef.keep {
-			t.Logf("keeping export file for %s (%s)", tname, ef.fname)
-		} else {
-			t.Logf("deleting export file for %s (%s)", tname, ef.fname)
-			os.Remove(ef.fname)
-		}
-		delete(exportFiles, tname)
-	})
-
-	return f
+	exportFiles[testName] = ret
+	return ret
 }
 
 // GetExportFilename return export filename for test
@@ -75,7 +84,7 @@ func GetExportFilename(t *testing.T) string {
 	if !ok {
 		t.Fatalf("file for test %s does not exist", testName)
 	}
-	return ef.fname
+	return ef.fName
 }
 
 // KeepExportFile marks export file to be kept
