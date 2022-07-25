b203b011031c40881dc57e56d0d802fa3c5fe48d
Jiri Olsa <jolsa@kernel.org>
2022-07-25 14:56:44 +0200

    tetragon: Add util to bench package
    Adding util object that carries some other helper objects.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ pkg/bench/cpuusage.go
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package bench
+
+import (
+	"fmt"
+	"log"
+	"syscall"
+	"time"
+)
+
+type CPUUsage struct {
+	SystemTime      time.Duration
+	UserTime        time.Duration
+	MaxRss          int64
+	ContextSwitches int64
+}
+
+type CPUUsageTarget int
+
+const (
+	CPU_USAGE_ALL_THREADS = syscall.RUSAGE_SELF
+	CPU_USAGE_THIS_THREAD = syscall.RUSAGE_THREAD
+)
+
+func CPUUsageFromRusage(rusage *syscall.Rusage) (cpuUsage CPUUsage) {
+	cpuUsage.UserTime = timevalToDuration(rusage.Utime)
+	cpuUsage.SystemTime = timevalToDuration(rusage.Stime)
+	cpuUsage.MaxRss = rusage.Maxrss
+	cpuUsage.ContextSwitches = rusage.Nivcsw + rusage.Nvcsw
+	return
+}
+
+func GetCPUUsage(tgt CPUUsageTarget) CPUUsage {
+	var rusage syscall.Rusage
+	if err := syscall.Getrusage(int(tgt), &rusage); err != nil {
+		log.Printf("Getrusage failed: %v", err)
+		return CPUUsage{}
+	}
+	return CPUUsageFromRusage(&rusage)
+}
+
+func timevalToDuration(tv syscall.Timeval) time.Duration {
+	return time.Duration(tv.Sec)*time.Second +
+		time.Duration(tv.Usec)*time.Microsecond
+}
+
+func (cu CPUUsage) Sub(cu2 CPUUsage) CPUUsage {
+	cu.UserTime -= cu2.UserTime
+	cu.SystemTime -= cu2.SystemTime
+	cu.ContextSwitches -= cu2.ContextSwitches
+	return cu
+}
+
+func (cu CPUUsage) Add(cu2 CPUUsage) CPUUsage {
+	cu.UserTime += cu2.UserTime
+	cu.SystemTime += cu2.SystemTime
+	cu.ContextSwitches += cu2.ContextSwitches
+	return cu
+}
+
+func (cu CPUUsage) String() string {
+	return fmt.Sprintf("system=%s, user=%s, rss=%d, ctxsw=%d", cu.SystemTime, cu.UserTime, cu.MaxRss, cu.ContextSwitches)
+}
