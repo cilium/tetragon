23f3206841b8015a727a99470a5cbc7e6b003e6d
Djalal Harouni <tixxdz@gmail.com>
2022-07-26 18:51:46 +0200

    tetragon: add tetragon_conf struct to store envrionment config
    This is a preparation patch that adds:
    
    - tetragon_conf struct that stores Tetragon runtime or deployment
      configuration, it allows to know if this deployment is a k8s
      deployment? what is the cgroup configuration of Tetragon, etc.
    
    - tg_conf bpf map to store and pass the tetragon_conf struct between
      userspace and kernel space.
    
    Signed-off-by: Djalal Harouni <djalal@isovalent.com>

---
+++ bpf/lib/environ_conf.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright Authors of Tetragon */
+
+#ifndef __ENVIRON_CONF_
+#define __ENVIRON_CONF_
+
+/* Tetragon running configuration */
+struct tetragon_conf {
+	__u32 mode; /* Tetragon deployment mode */
+	__u32 pid; /* Tetragon pid */
+	__u32 nspid; /* Tetragon pid in namespace */
+	__u32 tg_cgrp_level; /* Tetragon cgroup level */
+	__u64 tg_cgrpid; /* Tetragon current cgroup ID to avoid filtering blocking itself */
+	__u64 cgrp_fs_magic; /* Cgroupv1 or Cgroupv2 */
+};
+
+struct bpf_map_def __attribute__((section("maps"), used)) tg_conf_map = {
+	.type = BPF_MAP_TYPE_HASH,
+	.key_size = sizeof(__s32),
+	.value_size = sizeof(struct tetragon_conf),
+	.max_entries = 1,
+};
+
+#endif // __ENVIRON_CONFIG__
