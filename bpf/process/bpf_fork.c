503811f540e5409725432bc41977e71351459463
Djalal Harouni <tixxdz@gmail.com>
2022-07-27 10:33:34 +0200

    bpf:cgroup: set the tracking Cgroup ID of tasks during fork
    When a task forks we will query the cgroup tracking level, if it has
    a higher level then we use the Tetragon cgroup level and assign it to
    the task execve_map_value in the execve_map. This way all forked childs
    will belong to the same tracked cgroup.
    
    If it has a lower cgroup tracking level, this is probably a service, pod,
    container being created and in this case we assign the current task cgroup
    id as the tracking Cgroup id.
    
    This will allow later Tetragon to track all processes that have started
    inside a container/pod easily, where they will be assigned a cgroup tracking
    ID that should not change during their life cycle even if the task's cgroup
    config changes.
    
    Signed-off-by: Djalal Harouni <djalal@isovalent.com>

--- bpf/process/bpf_fork.c
+++ bpf/process/bpf_fork.c
@@ -5,7 +5,9 @@
 #include "api.h"
 
 #include "hubble_msg.h"
+#include "bpf_cgroup.h"
 #include "bpf_events.h"
+#include "environ_conf.h"
 #include "bpf_process_event.h"
 
 char _license[] __attribute__((section("license"), used)) = "GPL";
@@ -19,7 +21,11 @@ event_wake_up_new_task(struct pt_regs *ctx)
 {
 	struct execve_map_value *curr, *parent;
 	struct task_struct *task;
-	u32 pid = 0;
+	struct cgroup *cgrp;
+	struct cgroup_tracking_value *cgrp_data;
+	struct tetragon_conf *config;
+	u32 pid = 0, level = 0, hierarchy_id = 0, tracking_level = 0;
+	int zero = 0;
 
 	probe_read(&task, sizeof(task), &ctx->di);
 	if (!task)
@@ -29,6 +35,43 @@ event_wake_up_new_task(struct pt_regs *ctx)
 	curr = execve_map_get(pid);
 	if (!curr)
 		return 0;
+
+	/* Set the tracking cgroup id only if it was not set,
+	 * this avoids thread granularity cgroupv1 mess up.
+	 */
+	if (curr->cgrpid_tracker == 0) {
+		cgrp = get_task_cgroup(task);
+		level = get_cgroup_level(cgrp);
+
+		/* Cgroup Tracking level */
+		config = map_lookup_elem(&tg_conf_map, &zero);
+		if (config && config->tg_cgrp_level > 0) {
+			if (level <= config->tg_cgrp_level) {
+				/* Set this as the tracking cgroup of the task since it is before the
+				 * tracked level. This means this is probably a Pod or Container level
+				 * Anything below will be attached to this tracker
+				 */
+				curr->cgrpid_tracker = get_cgroup_id(cgrp);
+				tracking_level = level;
+			} else {
+				/* Set the ancestor as the tracking cgroup */
+				curr->cgrpid_tracker = get_ancestor_cgroup_id(cgrp, CGROUP_SUPER_MAGIC, config->tg_cgrp_level);
+				tracking_level = config->tg_cgrp_level;
+			}
+
+			cgrp_data = map_lookup_elem(&tg_cgrps_tracking_map, &curr->cgrpid_tracker);
+			if (cgrp_data) {
+				/* Convert to cgroup running now as we are able to track it */
+				cgrp_data->state = CGROUP_RUNNING;
+			} else {
+				/* This was never tracked let's push it here */
+				hierarchy_id = get_cgroup_hierarchy_id(cgrp);
+				cgrp_data = __get_cgrp_tracking_val_heap(CGROUP_RUNNING, hierarchy_id, tracking_level);
+				if (cgrp_data)
+					map_update_elem(&tg_cgrps_tracking_map, &curr->cgrpid_tracker, cgrp_data, BPF_ANY);
+			}
+		}
+	}
 
 	/* generate an EVENT_COMMON_FLAG_CLONE event only once per process */
 	if (curr->key.ktime != 0)
