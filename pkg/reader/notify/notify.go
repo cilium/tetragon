89618bfc5998a389baa0d3c48291bdbcc9f2d575
William Findlay <will@isovalent.com>
2022-07-27 11:16:38 -0400

    notify: add an Event interface
    Signed-off-by: William Findlay <will@isovalent.com>

--- pkg/reader/notify/notify.go
+++ pkg/reader/notify/notify.go
@@ -5,3 +5,9 @@ import "github.com/cilium/tetragon/api/v1/tetragon"
 type Message interface {
 	HandleMessage() *tetragon.GetEventsResponse
 }
+
+type Event interface {
+	GetProcess() *tetragon.Process
+	SetProcess(*tetragon.Process)
+	Encapsulate() tetragon.IsGetEventsResponse_Event
+}
