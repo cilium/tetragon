3df4024d6f4670a211ba2c7cc1df6abe5e841bba
William Findlay <will@isovalent.com>
2022-07-27 12:08:34 -0400

    tetragon: fold execcache into eventcache
    There's no need to have two caches here and unifying them will greatly simplify the code
    and reduce the possibility to make errors such as the one fixed in 54856c907.
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- pkg/grpc/exec/exec.go
+++ pkg/grpc/exec/exec.go
@@ -10,7 +10,6 @@ import (
 	"github.com/cilium/tetragon/pkg/api/processapi"
 	tetragonAPI "github.com/cilium/tetragon/pkg/api/processapi"
 	"github.com/cilium/tetragon/pkg/eventcache"
-	"github.com/cilium/tetragon/pkg/execcache"
 	"github.com/cilium/tetragon/pkg/ktime"
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/cilium/tetragon/pkg/metrics/errormetrics"
@@ -75,9 +74,9 @@ func (msg *MsgExecveEventUnix) HandleMessage() *tetragon.GetEventsResponse {
 	case ops.MSG_OP_EXECVE:
 		proc := process.AddExecEvent(&msg.MsgExecveEventUnix)
 		procEvent := GetProcessExec(proc)
-		ec := execcache.Get()
+		ec := eventcache.Get()
 		if ec != nil && ec.Needed(procEvent.Process) {
-			ec.Add(proc, procEvent, ktime.ToProto(msg.Common.Ktime), &msg.MsgExecveEventUnix)
+			ec.Add(proc, procEvent, ktime.ToProto(msg.Common.Ktime), msg)
 		} else {
 			procEvent.Process = proc.GetProcessCopy()
 			res = &tetragon.GetEventsResponse{
@@ -90,6 +89,10 @@ func (msg *MsgExecveEventUnix) HandleMessage() *tetragon.GetEventsResponse {
 		logger.GetLogger().WithField("message", msg).Warn("HandleExecveMessage: Unhandled event")
 	}
 	return res
+}
+
+func (msg *MsgExecveEventUnix) GetNsPid() uint32 {
+	return msg.Process.NSPID
 }
 
 type MsgCloneEventUnix struct {
