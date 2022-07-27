4e1e504e29b5e73c3f94a3e5dc50024f7af7f942
Djalal Harouni <tixxdz@gmail.com>
2022-07-27 12:35:38 +0200

    pkg:cgroups: add Cgroup Mode detection
    Add the Cgroups Mode detection that will report if Cgroups environment is:
    1. Unified
    2. Legacy
    3. Hybrid
    
    This allows to optimize how we handle cgroups from bpf side, but also how
    we call helpers.
    
    Reference: https://systemd.io/CGROUP_DELEGATION/ , however this is not
    systemd specific as it should run with other init systems if they setup
    cgroupfs at the right place.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/cgroups/cgroups.go
+++ pkg/cgroups/cgroups.go
@@ -23,6 +23,10 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 
 	"github.com/cilium/tetragon/pkg/defaults"
 	"github.com/cilium/tetragon/pkg/logger"
@@ -30,14 +34,26 @@ import (
 )
 
 const (
-	/* Arbitrary values we only use this to track processes
-	 * not to enforce nor set cgroup values
+	/* Arbitrary values we only use this to track Tetragon
+	 * process, not to enforce nor set cgroup values
 	 */
 	SYSTEMD_V2_HIERARCHY = 1
 	CPUSET_HIERARCHY     = 2
 	PIDS_HIERARCHY       = 3
 	MEMORY_HIERARCHY     = 4
 	CGROUPV2_HIERARCHY   = 20
+)
+
+const (
+	/* Cgroup Mode:
+	 * https://systemd.io/CGROUP_DELEGATION/
+	 * But this should work also for non-systemd environments: where
+	 * only legacy or unified are available.
+	 */
+	CGROUP_UNDEF   = 0
+	CGROUP_LEGACY  = 1
+	CGROUP_HYBRID  = 2
+	CGROUP_UNIFIED = 3
 )
 
 var (
@@ -52,7 +68,28 @@ var (
 		MEMORY_HIERARCHY:     ":memory:",
 		CGROUPV2_HIERARCHY:   "0::",
 	}
+
+	cgroupModesStr = map[int]string{
+		CGROUP_UNDEF:   "undefined",
+		CGROUP_LEGACY:  "Legacy mode (Cgroupv1)",
+		CGROUP_HYBRID:  "Hybrid mode (Cgroupv1 and Cgroupv2)",
+		CGROUP_UNIFIED: "Unified mode (Cgroupv2)",
+	}
+
+	readCgroupMode sync.Once
+	cgroupMode     int
+	cgroupPath     string
 )
+
+func CgroupFsMagicStr(magic uint64) string {
+	if magic == unix.CGROUP2_SUPER_MAGIC {
+		return "Cgroupv2"
+	} else if magic == unix.CGROUP_SUPER_MAGIC {
+		return "Cgroupv1"
+	}
+
+	return ""
+}
 
 func getValidCgroupPath(hierarchy_idx int, cgroup string) string {
 	_, ok := matchedHierarchies[hierarchy_idx]
@@ -124,6 +161,74 @@ func MigratePidToSameCgrp(pid uint32) error {
 	logger.GetLogger().WithError(err).Warn("Unable to migrate Tetragon pid to its own cgroup")
 
 	return err
+}
+
+func detectCgroupMode(cgroupfs string) (int, error) {
+	var st syscall.Statfs_t
+
+	if err := syscall.Statfs(cgroupfs, &st); err != nil {
+		return CGROUP_UNDEF, err
+	}
+
+	if st.Type == unix.CGROUP2_SUPER_MAGIC {
+		return CGROUP_UNIFIED, nil
+	} else if st.Type == unix.TMPFS_MAGIC {
+		err := syscall.Statfs(filepath.Join(cgroupfs, "unified"), &st)
+		if err == nil && st.Type == unix.CGROUP2_SUPER_MAGIC {
+			return CGROUP_HYBRID, nil
+		}
+		return CGROUP_LEGACY, nil
+	}
+
+	err := fmt.Errorf("cgroupfs: '%s' is with the wrong fs type: %d", cgroupfs, st.Type)
+	logger.GetLogger().WithError(err).WithField("cgroupfs", cgroupfs).Debug("could not detect Cgroup Mode")
+	return CGROUP_UNDEF, err
+}
+
+func getCgroupMode() error {
+	readCgroupMode.Do(func() {
+		var err error
+		cgroupPath = defaultCgroupRoot
+		cgroupMode, err = detectCgroupMode(cgroupPath)
+		if err != nil {
+			cgroupMode, err = detectCgroupMode(defaults.Cgroup2Dir)
+			if err == nil {
+				cgroupPath = defaults.Cgroup2Dir
+			}
+		}
+		if cgroupMode != CGROUP_UNDEF {
+			str, ok := cgroupModesStr[cgroupMode]
+			if ok {
+				logger.GetLogger().WithField("cgroupfs", cgroupPath).Infof("Running under Cgroup: %s", str)
+			}
+		}
+	})
+
+	if cgroupMode == CGROUP_UNDEF {
+		return fmt.Errorf("could not detect Cgroup Mode")
+	}
+
+	return nil
+}
+
+// Return the Cgroupfs v1 or v2 that will be used by bpf programs
+func GetBpfCgroupFS() (uint64, error) {
+	err := getCgroupMode()
+	if err != nil {
+		return CGROUP_UNDEF, err
+	}
+
+	switch cgroupMode {
+	case CGROUP_LEGACY, CGROUP_HYBRID:
+		/* In both legacy or Hybrid modes we switch to Cgroupv1 from bpf side. */
+		logger.GetLogger().WithField("cgroupfs", cgroupPath).Info("Cgroup BPF helpers will run in raw Cgroup mode")
+		return unix.CGROUP_SUPER_MAGIC, nil
+	case CGROUP_UNIFIED:
+		logger.GetLogger().WithField("cgroupfs", cgroupPath).Info("Cgroup BPF helpers will run in Cgroupv2 mode or fallback to raw Cgroup on errors")
+		return unix.CGROUP2_SUPER_MAGIC, nil
+	}
+
+	return CGROUP_UNDEF, fmt.Errorf("could not detect Cgroup Mode")
 }
 
 func CgroupNameFromCStr(cstr []byte) string {
