2560eb4e2f54988d894efcf8dfdf72164c1f35ba
William Findlay <will@isovalent.com>
2022-07-27 15:39:31 -0400

    eventchecker: add GetRemainingChecks() for MultiEventChecker types
    These functions enable grabbing the remaining unmatched event checks for a given checker
    before a call to .Finalize(). We can use this to print unmatched checks at the end of
    tests.
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- api/v1/tetragon/codegen/eventchecker/eventchecker.pb.go
+++ api/v1/tetragon/codegen/eventchecker/eventchecker.pb.go
@@ -112,6 +112,11 @@ func (checker *OrderedEventChecker) GetChecks() []EventChecker {
 	return checker.checks
 }
 
+// GetRemainingChecks returns this checker's list of remaining checks
+func (checker *OrderedEventChecker) GetRemainingChecks() []EventChecker {
+	return checker.checks[checker.idx:]
+}
+
 // UnorderedEventChecker checks a series of events in arbitrary order
 type UnorderedEventChecker struct {
 	pendingChecks *list.List
@@ -207,6 +212,19 @@ func (checker *UnorderedEventChecker) GetChecks() []EventChecker {
 	var checks []EventChecker
 
 	for e := checker.allChecks.Front(); e != nil; e = e.Next() {
+		if check, ok := e.Value.(EventChecker); ok {
+			checks = append(checks, check)
+		}
+	}
+
+	return checks
+}
+
+// GetRemainingChecks returns this checker's list of remaining checks
+func (checker *UnorderedEventChecker) GetRemainingChecks() []EventChecker {
+	var checks []EventChecker
+
+	for e := checker.pendingChecks.Front(); e != nil; e = e.Next() {
 		if check, ok := e.Value.(EventChecker); ok {
 			checks = append(checks, check)
 		}
