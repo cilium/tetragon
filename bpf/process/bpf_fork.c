ac1e1b7655282d2641c0624ef595f8f6670a572a
Djalal Harouni <tixxdz@gmail.com>
2022-07-28 12:34:48 +0200

    bpf: move __set_task_cgrpid_tracker() to bpf_cgroup.h
    Move __set_task_cgrpid_tracker() definition to bpf_cgroup.h
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/process/bpf_fork.c
+++ bpf/process/bpf_fork.c
@@ -16,61 +16,6 @@ int _version __attribute__((section(("version")), used)) =
 	VMLINUX_KERNEL_VERSION;
 #endif
 
-static inline __attribute__((always_inline)) int
-__set_task_cgrpid_tracker(struct tetragon_conf *conf, struct task_struct *task,
-			  struct execve_map_value *execve_val)
-{
-	struct cgroup *cgrp;
-	struct cgroup_tracking_value *cgrp_data;
-	u32 level = 0, hierarchy_id = 0, tracking_level = 0, flags = 0;
-
-	if (unlikely(!execve_val))
-		return 0;
-
-	probe_read(&flags, sizeof(flags), _(&task->flags));
-	if (flags & PF_KTHREAD)
-		return 0;
-
-	/* Set the tracking cgroup id only if it was not set,
-	 * this avoids thread granularity cgroupv1 mess up.
-	 */
-	if (execve_val->cgrpid_tracker != 0)
-		return 0;
-
-	cgrp = get_task_cgroup(task);
-	level = get_cgroup_level(cgrp);
-
-	if (level <= conf->tg_cgrp_level) {
-		/* Set this as the tracking cgroup of the task since it is before the
-		 * tracked level. This means this is probably a Pod or Container level
-		 * Anything below will be attached to this tracker
-		 */
-		execve_val->cgrpid_tracker = get_cgroup_id(cgrp);
-		tracking_level = level;
-	} else {
-		/* Set the ancestor as the tracking cgroup */
-		execve_val->cgrpid_tracker = get_ancestor_cgroup_id(cgrp, conf->cgrp_fs_magic,
-								    conf->tg_cgrp_level);
-		tracking_level = conf->tg_cgrp_level;
-	}
-
-	cgrp_data = map_lookup_elem(&tg_cgrps_tracking_map, &execve_val->cgrpid_tracker);
-	if (!cgrp_data) {
-		/* This was never tracked let's push it here */
-		hierarchy_id = get_cgroup_hierarchy_id(cgrp);
-		cgrp_data = __get_cgrp_tracking_val_heap(CGROUP_RUNNING, hierarchy_id,
-							 tracking_level);
-		if (cgrp_data)
-			map_update_elem(&tg_cgrps_tracking_map, &execve_val->cgrpid_tracker,
-					cgrp_data, BPF_ANY);
-	} else if (cgrp_data->state != CGROUP_RUNNING) {
-		/* Convert to cgroup running now as we are able to track it */
-		cgrp_data->state = CGROUP_RUNNING;
-	}
-
-	return 0;
-}
-
 __attribute__((section("kprobe/wake_up_new_task"), used)) int
 event_wake_up_new_task(struct pt_regs *ctx)
 {
