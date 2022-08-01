cilium/tetragon

commit 931b70f2c9878ba985ba6b589827bea17da6ec33

Tetragon: Cilium developers happily contribute Tetragon
Cilium developers are happy to contribute Tetragon, Cilium's new component
enabling powerful, eBPF-based Security Observability and Runtime
Enforcement.

Tetragon detects and is able to respond in real time to security-significant
events, such as

* Process execution events
* Changes to privileges and capabilities
* I/O activity including network & file access

When used in a Kubernetes environment, Tetragon is Kubernetes-aware - that
is, it understands Kubernetes identities such as namespaces, pods and
so-on - so that security event detection can be configured in relation
to individual workloads.

For more details and examples please see README.

Thanks!

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/eventcache/eventcache.go
@@ -1,0 +1,145 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package eventcache
+
+import (
+	"time"
+
+	"github.com/cilium/tetragon/api/v1/tetragon"
+	codegen "github.com/cilium/tetragon/api/v1/tetragon/codegen/eventcache"
+	"github.com/cilium/tetragon/pkg/dns"
+	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/metrics"
+	"github.com/cilium/tetragon/pkg/process"
+	"github.com/cilium/tetragon/pkg/reader/node"
+	"github.com/cilium/tetragon/pkg/server"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// garbage collection states
+const (
+	threeStrikes = 3
+)
+
+// garbage collection run interval
+const (
+	eventRetryTimer = time.Second * 10
+)
+
+type eventObj interface {
+	GetProcess() *tetragon.Process
+}
+
+var (
+	nodeName string
+)
+
+type cacheObj struct {
+	internal  *process.ProcessInternal
+	event     eventObj
+	timestamp *timestamppb.Timestamp
+	color     int
+	msg       interface{}
+}
+
+type Cache struct {
+	objsChan chan cacheObj
+	cache    []cacheObj
+	dns      *dns.Cache
+	server   *server.Server
+}
+
+func (ec *Cache) handleNetEvents() {
+	tmp := ec.cache[:0]
+	for _, e := range ec.cache {
+		/* Ensure we actually have a dockerID, we use this for testing reasons
+		 * mostly. It is nice though if we ever hit this case to just post it.
+		 */
+		endpoint := process.GetProcessEndpoint(e.event.GetProcess())
+		if e.event.GetProcess().GetDocker() != "" {
+			/* If the Pod is nil because process event is incomplete lets
+			 * wait and hopefully it is eventually updated from handleProcEvents.
+			 */
+
+			if endpoint == nil || e.event.GetProcess().Pod == nil {
+				e.color++
+				if e.color < threeStrikes {
+					tmp = append(tmp, e)
+					continue
+				}
+				metrics.EventCacheCount.WithLabelValues(string(metrics.EventCacheEndpointRetryFailed)).Inc()
+			}
+		}
+
+		processedEvent, err := codegen.DoHandleEvent(e.event, e.internal, []string{}, nodeName, e.timestamp)
+		if err == nil {
+			ec.server.NotifyListeners(e.msg, processedEvent)
+		} else {
+			logger.GetLogger().WithField("event", e.event).WithError(err).Warn("Error while handling event")
+		}
+	}
+	ec.cache = tmp
+}
+
+func (ec *Cache) loop() {
+	ticker := time.NewTicker(eventRetryTimer)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			/* Every 'eventRetryTimer' walk the slice of events pending pod info. If
+			 * an event hasn't completed its podInfo after two iterations send the
+			 * event anyways.
+			 */
+			ec.handleNetEvents()
+			metrics.ExecveMapSize.WithLabelValues("netCache", "0").Set(float64(len(ec.cache)))
+
+		case event := <-ec.objsChan:
+			metrics.EventCacheCount.WithLabelValues(string(metrics.EventCacheNetworkCount)).Inc()
+			ec.cache = append(ec.cache, event)
+		}
+	}
+}
+
+// We handle two race conditions here one where the event races with
+// an TETRAGON execve event and the other -- much more common -- where we
+// race with K8s watcher
+// case 1 (execve race):
+//  Its possible to receive this TETRAGON event before the process event cache
+//  has been populated with a TETRAGON execve event. In this case we need to
+//  cache the event until the process cache is populated.
+// case 2 (k8s watcher race):
+//  Its possible to receive an event before the k8s watcher receives the
+//  podInfo event and populates the local cache. If we expect podInfo,
+//  indicated by having a nonZero dockerID we cache the event until the
+//  podInfo arrives.
+func (ec *Cache) Needed(proc *tetragon.Process) bool {
+	if proc == nil {
+		return true
+	}
+	if proc.Docker != "" && proc.Pod == nil {
+		return true
+	}
+	return false
+}
+
+func (ec *Cache) Add(internal *process.ProcessInternal,
+	e eventObj,
+	t *timestamppb.Timestamp,
+	msg interface{}) {
+	ec.objsChan <- cacheObj{internal: internal, event: e, timestamp: t, msg: msg}
+}
+
+func New(s *server.Server, dns *dns.Cache) *Cache {
+	ec := &Cache{
+		objsChan: make(chan cacheObj),
+		cache:    make([]cacheObj, 0),
+		dns:      dns,
+		server:   s,
+	}
+	nodeName = node.GetNodeNameForExport()
+	go ec.loop()
+	return ec
+}

cmd/protoc-gen-go-tetragon/eventcache/eventcache.go
@@ -1,0 +1,74 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package eventcache
+
+import (
+	"fmt"
+
+	"github.com/cilium/tetragon/cmd/protoc-gen-go-tetragon/common"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+// getEventsResponse generates a new GetEventsResponse_<EVENT_TYPE>
+func doGetEventsResponse(g *protogen.GeneratedFile, eventType string) string {
+	tetragonGER := common.TetragonApiIdent(g, "GetEventsResponse")
+	subtype := common.TetragonApiIdent(g, fmt.Sprintf("GetEventsResponse_%s", eventType))
+
+	return tetragonGER + `{
+        Event: &` + subtype + `{` + eventType + `: e},
+        NodeName: nodeName,
+        Time: timestamp,
+    }`
+}
+
+func generateDoHandleEvents(g *protogen.GeneratedFile, f *protogen.File) error {
+	tetragonProcessInternal := common.GoIdent(g, "github.com/cilium/tetragon/pkg/process", "ProcessInternal")
+	tetragonGER := common.TetragonApiIdent(g, "GetEventsResponse")
+	timestamp := common.GoIdent(g, "google.golang.org/protobuf/types/known/timestamppb", "Timestamp")
+
+	mErrorCount := common.GoIdent(g, "github.com/cilium/tetragon/pkg/metrics", "ErrorCount")
+	mInfoFailed := common.GoIdent(g, "github.com/cilium/tetragon/pkg/metrics", "EventCacheProcessInfoFailed")
+	mProcessInfoErrors := common.GoIdent(g, "github.com/cilium/tetragon/pkg/metrics", "ProcessInfoErrors")
+
+	g.P(`func DoHandleEvent(event eventObj, internal *` + tetragonProcessInternal + `, labels []string, nodeName string, timestamp *` + timestamp + `) (*` + tetragonGER + `, error) {
+        switch e := event.(type) {`)
+	for _, msg := range f.Messages {
+		if !common.IsProcessEvent(msg) {
+			continue
+		}
+		g.P(`
+        case *` + g.QualifiedGoIdent(msg.GoIdent) + `:
+            if internal != nil {
+                e.Process = internal.GetProcessCopy()
+            } else {
+                ` + mProcessInfoErrors + `.WithLabelValues("` + msg.GoIdent.GoName + `").Inc()
+                ` + mErrorCount + `.WithLabelValues(string(` + mInfoFailed + `)).Inc()
+            }
+            return &` + doGetEventsResponse(g, msg.GoIdent.GoName) + `, nil`)
+	}
+	g.P(`}
+            return nil, ` + common.FmtErrorf(g, "DoHandleEvent: Unhandled event type %T", "event") + `
+        }`)
+
+	return nil
+}
+
+// Generate generates boilerplate code for the event cache
+func Generate(gen *protogen.Plugin, f *protogen.File) error {
+	g := common.NewGeneratedFile(gen, f, "eventcache")
+
+	tetragonProcess := common.TetragonApiIdent(g, "Process")
+
+	g.P(`
+        type eventObj interface {
+            GetProcess() *` + tetragonProcess + `
+        }
+    `)
+
+	if err := generateDoHandleEvents(g, f); err != nil {
+		return err
+	}
+
+	return nil
+}

commit 5d307ff317422c08dce1e6036bcdceb7ba0e6db5

tetragon: eventcache Needed() is incomplete
Resolve obvious cases for Needed() where we have a process pointer, but
are missing binary and other information. This happens through common
paths for example when exit events are received before their exec event.

To fix just extend the Needed() check to search for Binary name. I
introduced this while preparing initial release and doing some
refactoring.

Fixes: 931b70f2c9878 "Tetragon: Cilium developers happily contribute Tetragon"
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/eventcache/eventcache.go
@@ -165,6 +165,9 @@ func (ec *Cache) Needed(proc *tetragon.Process) bool {
 	if proc.Docker != "" && proc.Pod == nil {
 		return true
 	}
+	if proc.Binary == "" {
+		return true
+	}
 	return false
 }
 