b08cc16931c60c553765ea235f1ffd79b72e9cc1
Kornilios Kourtis <kornilios@gmail.com>
2022-08-11 10:13:14 +0200

    tests/vmtests/fetch-data.sh: improve script
    Make script more flexible. Users can specify independently whether they
    want to download an image and/or a kernel.
    
    Signed-off-by: Kornilios Kourtis <kornilios@gmail.com>

--- tests/vmtests/fetch-data.sh
+++ tests/vmtests/fetch-data.sh
@@ -2,33 +2,60 @@
 
 set -eu -o pipefail
 
-
 OCIORG=quay.io/lvh-images
 ROOTIMG=$OCIORG/root-images
 KERNIMG=$OCIORG/kernel-images
 CONTAINER_ENGINE=docker
-KERNEL_VERS="$@"
 DEST_DIR="tests/vmtests/test-data"
 
-if [ -z "$KERNEL_VERS" ]; then
-	echo "Usage: $0 <kver1> <kver2>"
-	echo "Example: $0 bpf-next 5.10"
-	exit 1
+kernel_vers=""
+base_image=0
+
+usage() {
+	echo "Usage: $0 [-k kernel_ver] [-b]"
+	echo "-k kver: fetch kernel version kver"
+	echo "-b fetch base image"
+	echo "Example: $0 -b -k bpf-next -k 5.10"
+}
+
+while getopts "hk:b" option
+do
+	case $option in
+		h)
+			usage
+			exit 0
+			;;
+		k)
+			kernel_vers="$kernel_vers ${OPTARG}"
+			;;
+		b)
+			base_image=1
+			;;
+	esac
+done
+
+if [ $base_image  = 0 ] && [ -z $kernel_vers ] ; then
+	usage
+	exit 0
 fi
 
 set -x
-rm -rf $DEST_DIR
-mkdir -p $DEST_DIR/images
-$CONTAINER_ENGINE pull $ROOTIMG
-x=$($CONTAINER_ENGINE create $ROOTIMG)
-$CONTAINER_ENGINE cp $x:/data/images/base.qcow2.zst  $DEST_DIR/images/base.qcow2.zst
-zstd --decompress $DEST_DIR/images/base.qcow2.zst
-$CONTAINER_ENGINE rm $x
 
-mkdir -p $DEST_DIR/kernels
-for ver in $KERNEL_VERS; do
+if [ $base_image  = 1 ]; then
+	mkdir -p $DEST_DIR/images
+	$CONTAINER_ENGINE pull $ROOTIMG
+	rm -f $DEST_DIR/images/base.qcow2*
+	x=$($CONTAINER_ENGINE create $ROOTIMG)
+	$CONTAINER_ENGINE cp $x:/data/images/base.qcow2.zst  $DEST_DIR/images/base.qcow2.zst
+	zstd --decompress $DEST_DIR/images/base.qcow2.zst
+	$CONTAINER_ENGINE rm $x
+fi
+
+for ver in $kernel_vers; do
+	mkdir -p $DEST_DIR/kernels
 	img="$KERNIMG:$ver"
 	$CONTAINER_ENGINE pull $img
+	rm -rf $DEST_DIR/kernels/$ver
 	x=$($CONTAINER_ENGINE create $img)
 	$CONTAINER_ENGINE cp $x:/data/kernels/$ver  $DEST_DIR/kernels
 	$CONTAINER_ENGINE rm $x
