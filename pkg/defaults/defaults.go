bac267fe672206661e27245643cdd32e3f80c53a
Djalal Harouni <tixxdz@gmail.com>
2022-08-15 17:32:39 +0200

    pkg: declare cgroupv2 mount point into defaults package
    Explicitly use '/run/' prefix instead of '/var/run' which may fail
    when we operate on file systems from /proc and try to discover the
    real mount point instead of the mount source.
    
    See man page of proc  section: /proc/[pid]/mountinfo , fields 4 and 10
    they can be different. Field 4 is the absolute truth.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -31,6 +31,9 @@ const (
 	// DefaultRunDir is the default run directory for runtime
 	DefaultRunDir = "/var/run/tetragon/"
 
+	// Default Path to where cgroup2 is mounted (Prefix with /run)
+	Cgroup2Dir = "/run/tetragon/cgroup2"
+
 	// DedfaultNetnsDir is the default network namespace directory for runtime
 	DefaultNetnsDir = "/var/run/docker/netns/"
 
