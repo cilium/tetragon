c53772421606cae9bec48d76ecf6425985cdb261
Djalal Harouni <tixxdz@gmail.com>
2022-08-19 16:33:36 +0200

    bpf:cgroup: improve Cgroup detection by checking Cgroup ID and hierarchy
    Improve cgroup attach detection by checking Cgroup ID and current Hierarchy
    ID if both match. This will ensure that current task is really operating
    on the right Cgroup ID that belongs to the right Cgroup hierarchy. Using
    namespaced PID may clash if we have another process with same pid in
    different namespace PID that is doing same thing... anyway with this patch
    this is robust now and impossible to clash.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/cgroup/bpf_cgroup_attach_task.c
+++ bpf/cgroup/bpf_cgroup_attach_task.c
@@ -19,9 +19,9 @@ int _version __attribute__((section(("version")), used)) =
 __attribute__((section(("raw_tracepoint/cgroup_attach_task")), used)) int
 tg_tp_cgrp_attach_task(struct bpf_raw_tracepoint_args *ctx)
 {
-	int level, zero = 0;
+	int level, hierarchy_id, zero = 0;
 	uint64_t cgrpid;
-	uint32_t pid, nspid, tgid;
+	uint32_t pid, tgid;
 	struct cgroup *cgrp;
 	struct tetragon_conf *config;
 	struct cgroup_tracking_value *cgrp_heap;
@@ -47,20 +47,12 @@ tg_tp_cgrp_attach_task(struct bpf_raw_tracepoint_args *ctx)
 	if (likely(pid != tgid))
 		return 0;
 
-	nspid = get_task_pid_vnr();
 	cgrpid = get_cgroup_id(cgrp);
+	hierarchy_id = get_cgroup_hierarchy_id(cgrp);
 
-	/* Make sure we only catch own pid */
-	tgid = (nspid) ? nspid : pid;
-	if (config->nspid != tgid)
+	/* Check if target Cgroup matches and it is same hierarchy ID */
+	if (config->tg_cgrp_hierarchy != hierarchy_id || config->tg_cgrpid != cgrpid)
 		return 0;
-
-	/* TODO:  Make this robust
-	 read /proc/tetragon/cgroup match the hierarchy to this current
-	 task hierarchy see below then match also cgrpids
-	 if (likely(config->cgrpid != id))
-		return 0;
-	*/
 
 	/* Let's initialize tetragon itself in execve_value_map here */
 	curr = execve_map_get(pid);
@@ -68,20 +60,22 @@ tg_tp_cgrp_attach_task(struct bpf_raw_tracepoint_args *ctx)
 		curr->cgrpid_tracker = cgrpid;
 
 	level = get_cgroup_level(cgrp);
-
 	/* Set level to max cgroup nested */
-	if (level > CGROUP_MAX_NESTED_LEVEL)
+	if (level > CGROUP_MAX_NESTED_LEVEL) {
 		level = CGROUP_MAX_NESTED_LEVEL;
+		/* Fix up cgroup ID if we are bellow max nested hierarchies */
+		cgrpid = get_ancestor_cgroup_id(cgrp, config->cgrp_fs_magic, level);
+		config->tg_cgrpid = cgrpid;
 
-	/* Update config */
+		/* Update Tetragon own cgroup id if necessary */
+		if (curr)
+			curr->cgrpid_tracker = cgrpid;
+	}
+
+	/* Update config, pids are used for debugging */
 	config->pid = pid;
+	config->nspid = get_task_pid_vnr();
 	config->tg_cgrp_level = level;
-
-	/* Fix up NSPID in case userspace was in pid namespace */
-	config->nspid = nspid;
-
-	/* TODO this should be set from userspace */
-	config->tg_cgrpid = cgrpid;
 
 	/* Match later all cgroups level where tasks are being migrated to */
 	if (config->tg_cgrp_level > 0 && level <= config->tg_cgrp_level) {
