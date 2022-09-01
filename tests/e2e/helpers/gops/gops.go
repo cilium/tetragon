8c0b32e0b80f8a65de14ce1b53392002e7eb01b7
William Findlay <will@isovalent.com>
2022-09-01 16:14:25 -0400

    tests/e2e: dump gops information at the end of failed tests
    This commit introduces the ability to dump gops debug information at the end of failed
    end-to-end tests. We start by dumping memory statics as well as a heap profile for the
    application. As with metrics, dumps occur every 30 seconds as well as at the end of
    a test, and are discarded unless either the test fails or the user has passed
    --tetragon.keep-export=true when running the test.
    
    We also dump a copy of the Tetragon binary running in each Tetragon pod. Unlike the other
    dumps, this happens only once at the beginning of a test as it is a costlier operation and
    the binary is obviously not expected to change. This binary dump can be used in
    cominbation with the heap dump to run it through `go tool pprof`, generating a call graph
    that summarizes memory usage throughout Tetragon.
    
    Signed-off-by: William Findlay <will@isovalent.com>

---
+++ tests/e2e/helpers/gops/gops.go
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+// Package gops is a simple gops client implementation to dump gops info from end-to-end
+// tests.
+package gops
+
+import (
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/google/gops/signal"
+)
+
+func DumpHeapProfile(addr net.TCPAddr) ([]byte, error) {
+	out, err := cmd(addr, signal.HeapProfile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dump heap profile: %w", err)
+	}
+	return out, nil
+}
+
+func DumpBinary(addr net.TCPAddr) ([]byte, error) {
+	out, err := cmd(addr, signal.BinaryDump)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dump binary: %w", err)
+	}
+	return out, nil
+}
+
+func DumpMemStats(addr net.TCPAddr) ([]byte, error) {
+	out, err := cmd(addr, signal.MemStats)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dump memstats: %w", err)
+	}
+	return out, nil
+}
+
+func cmd(addr net.TCPAddr, sig byte) ([]byte, error) {
+	conn, err := net.DialTCP("tcp", nil, &addr)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := conn.Write([]byte{sig}); err != nil {
+		return nil, err
+	}
+
+	out, err := io.ReadAll(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
--- tests/e2e/runners/runners.go
+++ tests/e2e/runners/runners.go
@@ -175,7 +175,20 @@ func (r *Runner) Init() *Runner {
 		if err != nil {
 			return ctx, fmt.Errorf("failed to get export dir: %w", err)
 		}
+
+		// Start the metrics and gops dumpers
 		helpers.StartMetricsDumper(ctx, exportDir, 30*time.Second)
+		helpers.StartGopsDumper(ctx, exportDir, 30*time.Second)
+
+		// The binary won't change, so dump it now at the beginning
+		if ports, ok := ctx.Value(state.GopsForwardedPorts).(map[string]int); ok {
+			for podName, port := range ports {
+				helpers.DumpBin(port, podName, exportDir)
+			}
+		} else {
+			klog.V(4).Info("failed to retrieve gops portforward, refusing to dump binary")
+		}
+
 		return ctx, nil
 	})
 
