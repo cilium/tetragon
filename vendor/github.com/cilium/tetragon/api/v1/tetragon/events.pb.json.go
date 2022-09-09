e00816fd5ee12ed94a237e0ce0daf626b809709c
William Findlay <will@isovalent.com>
2022-09-09 11:42:48 -0400

    api: implement field filters for GetEventsRequest
    This commit introduces two new field filters for GetEventsRequest. These allow the user to
    optionally specify a set of paths that should be included/excluded from the
    GetEventsResponse where exclusion takes precedence over inclusion. Leaving the filters
    empty will keep the default behaviour of including all fields in the GetEventsResponse.
    Filters are specified using period-separated names as they appear in protobuf JSON (i.e.
    lower snake case).
    
    This new filtering mechanism has several use cases, for example filtering out unwanted
    noisy fields from events in a customer environment or implementing functionality that
    transforms events for further processing (for example an event recorder that filters out
    noise).
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- vendor/github.com/cilium/tetragon/api/v1/tetragon/events.pb.json.go
+++ vendor/github.com/cilium/tetragon/api/v1/tetragon/events.pb.json.go
@@ -24,6 +24,22 @@ func (msg *Filter) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *FieldFilter) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{
+		UseEnumNumbers:  false,
+		EmitUnpopulated: false,
+		UseProtoNames:   true,
+	}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *FieldFilter) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+	}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *GetEventsRequest) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
