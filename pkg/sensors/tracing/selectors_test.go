915f037b5b948702e88c18b7ee4c792ce56260fc
Kornilios Kourtis <kornilios@isovalent.com>
2022-09-15 11:38:06 +0200

    selector_tests: update and expand testing
    - test both tracepoints and kprobes  using reload
    - test InMap operator
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/sensors/tracing/selectors_test.go
+++ pkg/sensors/tracing/selectors_test.go
@@ -3,12 +3,17 @@
 
 package tracing
 
+// NB(kkourt): Function(t *testing.T, ctx context.Context) is the reasonable
+// thing to do here even if revive complains.
+//revive:disable:context-as-argument
+
 import (
 	"context"
 	"fmt"
 	"testing"
 	"time"
 
+	"github.com/cilium/tetragon/pkg/api/tracingapi"
 	"github.com/cilium/tetragon/pkg/grpc/tracing"
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 	"github.com/cilium/tetragon/pkg/logger"
@@ -38,6 +43,84 @@ func tpSpecReload(t *testing.T, tpSensor *sensors.Sensor, tpSpec *v1alpha1.Trace
 	if len(tpSensor.Progs) != 1 {
 		t.Fatalf("unexpected progs size: %d", len(tpSensor.Progs))
 	}
+}
+
+// loadGenericSensorTest loads a tracing sensor for testing
+func loadGenericSensorTest(t *testing.T, ctx context.Context, spec *v1alpha1.TracingPolicySpec) *sensors.Sensor {
+	ret, err := sensors.GetSensorsFromParserPolicy(spec)
+	if err != nil {
+		t.Fatalf("GetSensorsFromParserPolicy failed: %v", err)
+	} else if len(ret) != 1 {
+		t.Fatalf("GetSensorsFromParserPolicy returned unexpected number of sensors (%d)", len(ret))
+	}
+	tpSensor := ret[0]
+	option.Config.HubbleLib = tus.Conf().TetragonLib
+	option.Config.Verbosity = 5
+	tus.LoadSensor(ctx, t, base.GetInitialSensor())
+	tus.LoadSensor(ctx, t, testsensor.GetTestSensor())
+	tus.LoadSensor(ctx, t, tpSensor)
+	return tpSensor
+}
+
+// lseekTestOps retruns a function to perform test lseek operations using the
+// given whence values
+func lseekTestOps(whences []int) func(t *testing.T) {
+	return func(t *testing.T) {
+		for _, whence := range whences {
+			t.Logf("Calling lseek(-1,0,%d)", whence)
+			unix.Seek(-1, 0, whence)
+		}
+	}
+}
+
+type testCase struct {
+	// the test will perform a series of (bogys) lseek cols using the provided values as a whence argumetn
+	lseekOpsVals []int
+	// the expectedArgs are a map from whence values to the frequency they were observed in the events
+	expectedArgs map[uint64]int
+}
+
+// testCases defines the use-cases we want to test
+var testCases = []struct {
+	// specOperator is the operator that will be used in the generated spec
+	specOperator string
+	// specFilterVals is the values that will be used in the generated spec
+	specFilterVals []int
+	// the cases to actually test given the above spec properties
+	tests []testCase
+}{
+	{
+		specOperator:   "Equal",
+		specFilterVals: []int{4443},
+		tests: []testCase{
+			{lseekOpsVals: []int{4444, 4443}, expectedArgs: map[uint64]int{4443: 1}},
+			{lseekOpsVals: []int{4443, 4444, 4443}, expectedArgs: map[uint64]int{4443: 2}},
+		},
+	},
+	{
+		specOperator:   "Equal",
+		specFilterVals: []int{4444},
+		tests: []testCase{
+			{lseekOpsVals: []int{4444, 4443}, expectedArgs: map[uint64]int{4444: 1}},
+			{lseekOpsVals: []int{4443, 4444, 4443}, expectedArgs: map[uint64]int{4444: 1}},
+		},
+	},
+	{
+		specOperator:   "InMap",
+		specFilterVals: []int{4443},
+		tests: []testCase{
+			{lseekOpsVals: []int{4444, 4443}, expectedArgs: map[uint64]int{4443: 1}},
+			{lseekOpsVals: []int{4443, 4444, 4443}, expectedArgs: map[uint64]int{4443: 2}},
+		},
+	},
+	{
+		specOperator:   "InMap",
+		specFilterVals: []int{4444},
+		tests: []testCase{
+			{lseekOpsVals: []int{4444, 4443}, expectedArgs: map[uint64]int{4444: 1}},
+			{lseekOpsVals: []int{4443, 4444, 4443}, expectedArgs: map[uint64]int{4444: 1}},
+		},
+	},
 }
 
 // TestTracepointSelectors tests the tracepoint selectors.
@@ -74,7 +157,7 @@ func TestTracepointSelectors(t *testing.T) {
 	// It will create filters:
 	//  - for our pid, to get more predictable events
 	//  - for the whence values provided as argument (if any)
-	makeSpec := func(t *testing.T, filterWhenceVals []int) *v1alpha1.TracingPolicySpec {
+	makeSpec := func(t *testing.T, filterWhenceVals []int, filterOperator string) *v1alpha1.TracingPolicySpec {
 		mypid := int(observer.GetMyPid())
 		t.Logf("filtering for my pid (%d)", mypid)
 		sels := []v1alpha1.KProbeSelector{{
@@ -93,7 +176,7 @@ func TestTracepointSelectors(t *testing.T) {
 			sels[0].MatchArgs = []v1alpha1.ArgSelector{
 				{
 					Index:    whenceIdx,
-					Operator: "Equal",
+					Operator: filterOperator,
 					Values:   whences,
 				},
 			}
@@ -111,38 +194,10 @@ func TestTracepointSelectors(t *testing.T) {
 		return &spec
 	}
 
-	// loadSensor loads a sensor
-	loadSensor := func(t *testing.T, spec *v1alpha1.TracingPolicySpec) *sensors.Sensor {
-		ret, err := sensors.GetSensorsFromParserPolicy(spec)
-		if err != nil {
-			t.Fatalf("GetSensorsFromParserPolicy failed: %v", err)
-		} else if len(ret) != 1 {
-			t.Fatalf("GetSensorsFromParserPolicy returned unexpected number of sensors (%d)", len(ret))
-		}
-		tpSensor := ret[0]
-		option.Config.HubbleLib = tus.Conf().TetragonLib
-		option.Config.Verbosity = 5
-		tus.LoadSensor(ctx, t, base.GetInitialSensor())
-		tus.LoadSensor(ctx, t, testsensor.GetTestSensor())
-		tus.LoadSensor(ctx, t, tpSensor)
-		return tpSensor
-	}
-
-	// lseekOps retruns a function to perform test lseek operations using the given whence
-	// values
-	lseekOps := func(whences []int) func(t *testing.T) {
-		return func(t *testing.T) {
-			for _, whence := range whences {
-				t.Logf("Calling lseek(-1,0,%d)", whence)
-				unix.Seek(-1, 0, whence)
-			}
-		}
-	}
-
 	// runAndCheck runs perfring test where op is exected and events are collected.
-	// expectedArgs is a counter for the whence values seen by events, and is checked at the end
-	// of the test.
-	runAndCheck := func(t *testing.T, name string, op func(t *testing.T), expectedArgs map[uint64]int) {
+	// expectedArgs is a counter for the whence values seen by events, and is
+	// checked at the end of the test.
+	runAndCheck := func(t *testing.T, ctx context.Context, name string, op func(t *testing.T), expectedArgs map[uint64]int) {
 		ret := make(map[uint64]int)
 		perfring.RunSubTest(t, ctx, name, op, func(ev notify.Message) error {
 			if tpEvent, ok := ev.(*tracing.MsgGenericTracepointUnix); ok {
@@ -172,20 +227,141 @@ func TestTracepointSelectors(t *testing.T) {
 		}
 	}
 
+	tpSensor := loadGenericSensorTest(t, ctx, makeSpec(t, testCases[0].specFilterVals, testCases[0].specOperator))
 	t0 := time.Now()
-	tpSensor := loadSensor(t, makeSpec(t, []int{4443}))
 	loadElapsed := time.Since(t0)
 	t.Logf("loading sensors took: %s\n", loadElapsed)
+	for i, tcs := range testCases {
+		tName := fmt.Sprintf("spec:%s%v", tcs.specOperator, tcs.specFilterVals)
+		t.Run(tName, func(t *testing.T) {
+			t.Logf("%d", i)
+			testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+			spec := makeSpec(t, tcs.specFilterVals, tcs.specOperator)
+			if i == 0 {
+			} else {
+				tpSpecReload(t, tpSensor, &spec.Tracepoints[0])
+			}
 
-	t.Run("initial sensor", func(t *testing.T) {
-		testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
-		runAndCheck(t, "two events", lseekOps([]int{4444, 4443}), map[uint64]int{4443: 1})
-	})
+			for _, tc := range tcs.tests {
+				runAndCheck(t, ctx, fmt.Sprintf("lseekValls:%v", tc.lseekOpsVals), lseekTestOps(tc.lseekOpsVals), tc.expectedArgs)
+			}
+		})
+	}
 
-	t.Run("sensor with filter for 4443", func(t *testing.T) {
-		testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
-		spec := makeSpec(t, []int{4444})
-		tpSpecReload(t, tpSensor, &spec.Tracepoints[0])
-		runAndCheck(t, "one event", lseekOps([]int{4444, 4443}), map[uint64]int{4444: 1})
-	})
+}
+
+func TestKprobeSelectors(t *testing.T) {
+	testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
+	defer cancel()
+
+	makeSpec := func(t *testing.T, filterWhenceVals []int, filterOperator string) *v1alpha1.TracingPolicySpec {
+		mypid := int(observer.GetMyPid())
+		t.Logf("filtering for my pid (%d)", mypid)
+		sels := []v1alpha1.KProbeSelector{{
+			MatchPIDs: []v1alpha1.PIDSelector{{
+				Operator:       "In",
+				IsNamespacePID: false,
+				FollowForks:    true,
+				Values:         []uint32{uint32(mypid)},
+			}}}}
+
+		if len(filterWhenceVals) > 0 {
+			whences := make([]string, len(filterWhenceVals))
+			for i := range filterWhenceVals {
+				whences[i] = fmt.Sprintf("%d", filterWhenceVals[i])
+			}
+			sels[0].MatchArgs = []v1alpha1.ArgSelector{
+				{
+					Index:    2,
+					Operator: filterOperator,
+					Values:   whences,
+				},
+			}
+		}
+
+		spec := v1alpha1.TracingPolicySpec{
+			KProbes: []v1alpha1.KProbeSpec{{
+				Call:    "__x64_sys_lseek",
+				Return:  true,
+				Syscall: true,
+				ReturnArg: v1alpha1.KProbeArg{
+					Type: "int",
+				},
+				Args: []v1alpha1.KProbeArg{{
+					Index: 2,
+					Type:  "int",
+				}},
+				Selectors: sels,
+			}},
+		}
+
+		return &spec
+	}
+
+	// runAndCheck runs perfring test where op is exected and events are collected.
+	// expectedArgs is a counter for the whence values seen by events, and is
+	// checked at the end of the test.
+	runAndCheck := func(t *testing.T, ctx context.Context, name string, op func(t *testing.T), expectedArgs map[uint64]int) {
+		ret := make(map[uint64]int)
+		perfring.RunSubTest(t, ctx, name, op, func(ev notify.Message) error {
+			if kpEvent, ok := ev.(*tracing.MsgGenericKprobeUnix); ok {
+				if kpEvent.FuncName != "__x64_sys_lseek" {
+					return fmt.Errorf("unexpected kprobe event, func:%s", kpEvent.FuncName)
+				}
+				if len(kpEvent.Args) != 2 {
+					return fmt.Errorf("unexpected kprobe arguments: %+v", kpEvent.Args)
+				}
+				whenceArg, ok := kpEvent.Args[0].(tracingapi.MsgGenericKprobeArgInt)
+				if !ok {
+					return fmt.Errorf("unexpected kprobe arguments %+v", kpEvent.Args[0])
+				}
+
+				retArg, ok := kpEvent.Args[1].(tracingapi.MsgGenericKprobeArgInt)
+				if !ok {
+					return fmt.Errorf("unexpected kprobe arguments %+v", kpEvent.Args[0])
+				}
+				if retArg.Value != -9 { // -EBADF
+					return fmt.Errorf("unexpected return value: %+v", retArg)
+				}
+
+				whence := uint64(whenceArg.Value)
+				// the test sensor also uses the same trick: an lseek call with a
+				// bogus whence value. Ignore those events
+				if whence == uint64(testsensor.BogusWhenceVal) {
+					return nil
+				}
+
+				ret[whence] = ret[whence] + 1
+				return nil
+			}
+			return nil
+		})
+		if diff := cmp.Diff(expectedArgs, ret); diff != "" {
+			t.Fatalf("expecting %v but got %v, diff:%s", expectedArgs, ret, diff)
+		}
+	}
+
+	t0 := time.Now()
+	kpSensor := loadGenericSensorTest(t, ctx, makeSpec(t, testCases[0].specFilterVals, testCases[0].specOperator))
+	loadElapsed := time.Since(t0)
+	t.Logf("loading sensors (kpSensor: %p)  took: %s\n", kpSensor, loadElapsed)
+	for i, tcs := range testCases {
+		tName := fmt.Sprintf("spec:%s%v", tcs.specOperator, tcs.specFilterVals)
+		t.Run(tName, func(t *testing.T) {
+			t.Logf("%d", i)
+			testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+			spec := makeSpec(t, tcs.specFilterVals, tcs.specOperator)
+			if i == 0 {
+			} else {
+				if err := ReloadGenericKprobeSelectors(kpSensor, &spec.KProbes[0]); err != nil {
+					t.Fatalf("failed to reload kprobe prog: %s", err)
+				}
+			}
+
+			for _, tc := range tcs.tests {
+				runAndCheck(t, ctx, fmt.Sprintf("lseekValls:%v", tc.lseekOpsVals), lseekTestOps(tc.lseekOpsVals), tc.expectedArgs)
+			}
+		})
+	}
 }
