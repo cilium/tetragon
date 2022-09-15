1a6e259f7af8e2ec3a100291c9df1f60f69e0d2d
Kornilios Kourtis <kornilios@isovalent.com>
2022-09-15 10:23:59 +0200

    sensors: add function for creating bpf fs paths
    This is meant to be used by sensors to create paths for their objects
    under /sys/fs/bpf. Ideally, we would want a directory hierarchy under
    each sensor, as described in https://github.com/cilium/tetragon/issues/408.
    
    This is currently not easy to do, however. Instead, use a function that
    adds "-" instead of "/", leading to a flat hierarchy that has unique
    names. This function can be changed to filepath.Join once everything is
    in place.
    
    The purpose of the function is to document the above intention.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/sensors/path.go
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+package sensors
+
+import (
+	"strings"
+)
+
+// PathJoin creates a path meant for sensor filenames in /sys/fs/bpf.
+//
+// At some point, we would like to have a file hierarchy under /sys/fs/bpf for each sensor.
+// see: https://github.com/cilium/tetragon/issues/408
+//
+// Unfortunately, this requires changes, for properly creating and deleting
+// these directories requires. As an intermediate step, we use this function
+// that uses dashes instead of / to create unique files in flat hierarchy,
+// without needeing to manage directories.
+func PathJoin(elem ...string) string {
+	return strings.Join(elem, "-")
+}
