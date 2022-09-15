8a1bd4258f0aa9668fcd562f5c37f6f11e5cf192
Kornilios Kourtis <kornilios@isovalent.com>
2022-09-15 09:52:27 +0200

    bpf: add {,not}inmap operators for argfilter
    Add the bpf code for filtering args based on values found in maps. We
    use an array of maps that keeps the values that we want to filter by.
    
    So using this operators requires:
     - getting a reference to the inner map from the outer map
     - checking whether the argument is in the inner map
    
    For now, the values in the inner hashmap are ignore; we just check
    whether the argument exists or not.
    
    These operators lift the limitation of op_filter_{neq,eq} where only a
    limited number of values is supported.
    
    We only handle 64- and 32-bit integers in this first implementation.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ bpf/process/argfilter_maps.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright Authors of Cilium */
+
+#ifndef ARGFILTER_MAPS_H__
+#define ARGFILTER_MAPS_H__
+
+#define ARGFILTER_MAPS_OUTER_MAX_ENTRIES 8
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY_OF_MAPS);
+	__uint(max_entries, ARGFILTER_MAPS_OUTER_MAX_ENTRIES);
+	__uint(key_size, sizeof(__u32));
+	__array(
+		values, struct {
+			__uint(type, BPF_MAP_TYPE_HASH);
+			__uint(max_entries, 1);
+			__type(key, __u64);
+			__type(value, __u8);
+		});
+} argfilter_maps SEC(".maps");
+
+#endif // ARGFILTER_MAPS_H__
