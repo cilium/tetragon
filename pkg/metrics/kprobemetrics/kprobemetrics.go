7752fde4c410fe0583df5f519bfc73b328ef98bb
Jiri Olsa <jolsa@kernel.org>
2022-09-19 14:40:33 +0200

    tetragon: Add counter for total merged events
    Adding counter for total merged events.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/metrics/kprobemetrics/kprobemetrics.go
+++ pkg/metrics/kprobemetrics/kprobemetrics.go
@@ -15,6 +15,11 @@ var (
 		Help:        "The total number of failed attempts to merge a kprobe and kretprobe event.",
 		ConstLabels: nil,
 	}, []string{"curr_fn", "curr_type", "prev_fn", "prev_type"})
+	MergeOkTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name:        consts.MetricNamePrefix + "generic_kprobe_merge_ok_total",
+		Help:        "The total number of successful attempts to merge a kprobe and kretprobe event.",
+		ConstLabels: nil,
+	})
 )
 
 // Get a new handle on the mergeErrors metric for a current and previous function
@@ -27,4 +32,8 @@ func GetMergeErrors(currFn, currType, prevFn, prevType string) prometheus.Counte
 // name and probe type
 func MergeErrorsInc(currFn, currType, prevFn, prevType string) {
 	GetMergeErrors(currFn, currType, prevFn, prevType).Inc()
+}
+
+func MergeOkTotalInc() {
+	MergeOkTotal.Inc()
 }
