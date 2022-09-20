41d0d9a43ee00e9e601c072ae759019809e90a8b
Djalal Harouni <tixxdz@gmail.com>
2022-09-20 11:29:18 +0200

    bpf:cgroup: add cgroups tracking structures and maps
    This is a preparation patch that adds related cgroup structures and bpf
    maps:
    
    - cgroup_state enum: to track cgroups states.
      cgroup is being created (NEW), then when a cgroup is started
      it usually means a container process is running now (RUNNING),
      or maybe cgroup info was obtained from proc (RUNNING_PROC)
    
    - cgroup_tracking_value: is the necessary data that is used to track
      cgroups information. Key is the Cgroup ID.
    
    - msg_cgroup_event: is the cgroup event that is sent from bpf to user space about
      cgroup operations. Right now it will contain and handle only cgroup_attach_task
      tracepoint event that is used to detect Tetragon cgroup configurations.
    
    - tg_cgrps_tracking_map is the map where we track cgroups IDs that are under
      or before a cgroup level that is Tetragon cgroup level. This usually include
      containers cgroup levels. In other words it will track cgroups that are between
      cgroup level 1 (first) -> Tetragon cgroup level.
    
    - tg_cgrps_tracking_heap: is a heap used to construct dynamically
      cgroup_tracking_value structures.
    
    - tg_cgrps_msg_heap: is a heap used to construct dynamically
      msg_cgroup_event structures.
    
    Signed-off-by: Djalal Harouni <djalal@isovalent.com>

--- bpf/lib/bpf_cgroup.h
+++ bpf/lib/bpf_cgroup.h
@@ -10,8 +10,29 @@
 
 #define NULL ((void *)0)
 
+#ifndef CGROUP_SUPER_MAGIC
+#define CGROUP_SUPER_MAGIC 0x27e0eb /* Cgroupv1 pseudo FS */
+#endif
+
+#ifndef CGROUP2_SUPER_MAGIC
+#define CGROUP2_SUPER_MAGIC 0x63677270 /* Cgroupv2 pseudo FS */
+#endif
+
 /* Our kernfs node name length, can be made 256? */
 #define KN_NAME_LENGTH 128
+
+/* Max nested cgroups that are tracked. Arbitrary value, nested cgroups
+ * that are at a level greater than 32 will be attached to the cgroup
+ * at level 32.
+ */
+#define CGROUP_MAX_NESTED_LEVEL 32
+
+typedef enum {
+	CGROUP_UNTRACKED = 0, /* Cgroup was created but we did not track it */
+	CGROUP_NEW = 1, /* Cgroup was just created */
+	CGROUP_RUNNING = 2, /* new => running (fork,exec task inside) */
+	CGROUP_RUNNING_PROC = 3, /* Generated from pids of procfs */
+} cgroup_state;
 
 /* Represent old kernfs node with the kernfs_node_id
  * union to read the id in 5.4 kernels and older
@@ -19,6 +40,57 @@
 struct kernfs_node___old {
 	union kernfs_node_id id;
 };
+
+struct cgroup_tracking_value {
+	/* State of cgroup */
+	cgroup_state state;
+
+	/* Unique id for the hierarchy this is mostly for cgroupv1 */
+	__u32 hierarchy_id;
+
+	/* The depth this cgroup is at */
+	__u32 level;
+
+	__u32 pad;
+
+	/* Cgroup kernfs_node name */
+	char name[KN_NAME_LENGTH];
+} __attribute__((packed));
+
+struct msg_cgroup_event {
+	struct msg_common common;
+	struct msg_execve_key parent;
+	__u32 cgrp_op; /* Current cgroup operation */
+	__u32 pid;
+	__u32 nspid;
+	__u32 flags;
+	__u64 ktime;
+	__u64 cgrpid_tracker; /* Cgroup ID that is used as a tracker for the current cgroup */
+	__u64 cgrpid; /* Current cgroup ID */
+	struct cgroup_tracking_value cgrp_data; /* Current cgroup data */
+	char path[PATH_MAP_SIZE]; /* Current cgroup path */
+} __attribute__((packed));
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 32768);
+	__type(key, __u64);
+	__type(value, struct cgroup_tracking_value);
+} tg_cgrps_tracking_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __s32);
+	__type(value, struct cgroup_tracking_value);
+} tg_cgrps_tracking_heap SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, struct msg_cgroup_event);
+} tg_cgrps_msg_heap SEC(".maps");
 
 /**
  * get_cgroup_kn_name() Returns a pointer to the kernfs node name
