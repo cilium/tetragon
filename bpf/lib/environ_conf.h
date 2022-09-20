95c833b90a3c650cd59c69670497b518fab59aa8
Djalal Harouni <tixxdz@gmail.com>
2022-09-20 13:02:34 +0200

    bpf:conf: update tetragon_conf with necessary fields
    This is a preparation patch that adds more fields to tetragon_conf:
    
      - Log level.
      - PID and PIDNS for debugging purpose.
      - Tetragon current cgroup level.
      - Tetragon cgroup ID.
      - Cgroup filesystem magic number.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- bpf/lib/environ_conf.h
+++ bpf/lib/environ_conf.h
@@ -6,9 +6,15 @@
 
 /* Tetragon runtime configuration */
 struct tetragon_conf {
+	__u32 loglevel; /* Tetragon log level */
+	__u32 pid; /* Tetragon pid for debugging purpose */
+	__u32 nspid; /* Tetragon pid in namespace for debugging purpose */
 	__u32 tg_cgrp_hierarchy; /* Tetragon tracked hierarchy ID */
 	__u32 tg_cgrp_subsys_idx; /* Tetragon tracked cgroup subsystem state index at compile time */
-};
+	__u32 tg_cgrp_level; /* Tetragon cgroup level */
+	__u64 tg_cgrpid; /* Tetragon current cgroup ID to avoid filtering blocking itself */
+	__u64 cgrp_fs_magic; /* Cgroupv1 or Cgroupv2 */
+}; // All fields aligned so no 'packed' attribute.
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
