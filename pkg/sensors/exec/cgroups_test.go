3be72f428e9fe267a0e50ce8a11a9470b041deae
Djalal Harouni <tixxdz@gmail.com>
2022-10-12 17:13:15 +0200

    tests:cgroups: ensure that we do not get bpf cgroup events
    Add TestCgroupNoEvents() to ensure that we do not get BPF cgroups
    events.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/sensors/exec/cgroups_test.go
+++ pkg/sensors/exec/cgroups_test.go
@@ -4,19 +4,82 @@ package exec
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/cilium/tetragon/pkg/api/ops"
 	"github.com/cilium/tetragon/pkg/bpf"
+	"github.com/cilium/tetragon/pkg/cgroups"
+	grpcexec "github.com/cilium/tetragon/pkg/grpc/exec"
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/cilium/tetragon/pkg/option"
+	"github.com/cilium/tetragon/pkg/sensors"
 	"github.com/cilium/tetragon/pkg/sensors/base"
 	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
 	"github.com/cilium/tetragon/pkg/testutils"
+	"github.com/cilium/tetragon/pkg/testutils/perfring"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	defaultTimeout = 30 * time.Second
+
+	tetragonCgrpRoot = "tetragon-tests"
+)
+
+func registerSensors(loaded []*sensors.Sensor) {
+	for _, s := range loaded {
+		sensors.RegisterSensorAtInit(s)
+	}
+}
+
+func enableSensors(ctx context.Context, smanager *sensors.Manager, loaded []*sensors.Sensor) error {
+	for _, s := range loaded {
+		if err := smanager.EnableSensor(ctx, s.Name); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func disableSensors(ctx context.Context, smanager *sensors.Manager, loaded []*sensors.Sensor) error {
+	for _, s := range loaded {
+		if err := smanager.DisableSensor(ctx, s.Name); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func cgroupMkdir(t *testing.T, cgroupfsPath string, hierarchy string, dir string) error {
+	path := filepath.Join(cgroupfsPath, hierarchy, dir)
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		t.Logf("test failed to create cgroup directory '%s': %v", path, err)
+	} else {
+		t.Logf("test created cgroup directory '%s' with success", path)
+	}
+	return err
+}
+
+func cgroupRmdir(t *testing.T, cgroupfsPath string, hierarchy string, dir string) error {
+	path := filepath.Join(cgroupfsPath, hierarchy, dir)
+	err := os.RemoveAll(path)
+	if err != nil {
+		t.Logf("test failed to clean cgroup directory '%s': %v", path, err)
+	} else {
+		t.Logf("test cleaned cgroup directory '%s' with success", path)
+	}
+	return err
+}
 
 // Test loading bpf cgroups programs
 func TestLoadCgroupsPrograms(t *testing.T) {
@@ -61,4 +124,100 @@ func TestTgRuntimeConf(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.EqualValues(t, ret, val)
+}
+
+// Test we do not receive any cgroup events from BPF side
+func TestCgroupNoEvents(t *testing.T) {
+	testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	option.Config.HubbleLib = tus.Conf().TetragonLib
+	option.Config.Verbosity = 5
+
+	_, err := observer.GetDefaultObserver(t, ctx, tus.Conf().TetragonLib)
+	if err != nil {
+		t.Fatalf("GetDefaultObserver error: %s", err)
+	}
+
+	loadedSensors := []*sensors.Sensor{
+		testsensor.GetTestSensor(),
+		testsensor.GetCgroupSensor(),
+	}
+
+	registerSensors(loadedSensors)
+
+	mapDir := bpf.MapPrefixPath()
+	smanager, err := sensors.StartSensorManager(mapDir, mapDir, "")
+	if err != nil {
+		t.Fatalf("startSensorController failed: %s", err)
+	}
+	observer.SensorManager = smanager
+	defer func() {
+		err := smanager.StopSensorManager(ctx)
+		if err != nil {
+			fmt.Printf("stopSensorController failed: %s\n", err)
+		}
+	}()
+
+	err = enableSensors(ctx, smanager, loadedSensors)
+	if err != nil {
+		t.Fatalf("enableSensors error: %s", err)
+	}
+
+	defer func() {
+		disableSensors(ctx, smanager, loadedSensors)
+		if err != nil {
+			fmt.Printf("disableSensor failed: %s\n", err)
+		}
+	}()
+
+	val, err := testutils.GetTgRuntimeConf()
+	assert.NoError(t, err)
+
+	// Set Log Level to trace so we receive BPF events
+	val.LogLevel = uint32(logrus.TraceLevel)
+
+	err = testutils.UpdateTgRuntimeConf(mapDir, val)
+	assert.NoError(t, err)
+
+	cgroupFSPath := cgroups.GetCgroupFSPath()
+	assert.NotEmpty(t, cgroupFSPath)
+
+	// Do not use random names so we can predict if directory
+	// failed to be removed by previous tests...
+	dir := fmt.Sprintf("%s/%s", tetragonCgrpRoot, t.Name())
+	cgroupMode := cgroups.GetCgroupMode()
+	assert.NotZero(t, uint32(cgroupMode))
+
+	t.Logf("Test %s is running in '%s'", t.Name(), cgroupMode.String())
+	hierarchy := ""
+	if cgroupMode != cgroups.CGROUP_UNIFIED {
+		// In cgroupv1 tracking
+		hierarchy = cgroups.GetCgrpControllerName()
+	}
+
+	cgroupRmdir(t, cgroupFSPath, hierarchy, tetragonCgrpRoot)
+
+	finalpath := filepath.Join(cgroupFSPath, hierarchy, dir)
+	_, err = os.Stat(finalpath)
+	if err == nil {
+		t.Fatalf("Test %s failed cgroup test hierarchy should not exist '%s'", t.Name(), finalpath)
+	}
+
+	defer cgroupRmdir(t, cgroupFSPath, hierarchy, dir)
+	trigger := func() {
+		err = cgroupMkdir(t, cgroupFSPath, hierarchy, dir)
+		assert.NoError(t, err)
+	}
+
+	events := perfring.RunTestEvents(t, ctx, trigger)
+	for _, ev := range events {
+		if msg, ok := ev.(*grpcexec.MsgCgroupEventUnix); ok {
+			if msg.Common.Op == ops.MSG_OP_CGROUP {
+				op := ops.CgroupOpCode(msg.CgrpOp)
+				t.Fatalf("Test failed received a cgroup.event=%s", op)
+			}
+		}
+	}
 }
