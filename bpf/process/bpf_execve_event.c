17fd045aa9e2409bbd3a9bb13d6dde9586dbad2e
John Fastabend <john.fastabend@gmail.com>
2022-10-19 23:30:18 -0700

    hubble-fgs: clean up squash into prev
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- bpf/process/bpf_execve_event.c
+++ bpf/process/bpf_execve_event.c
@@ -29,13 +29,6 @@ struct {
 } data_heap SEC(".maps");
 #endif
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__uint(max_entries, 1);
-	__type(key, __u32);
-	__type(value, struct inode);
-} inode_heap SEC(".maps");
-
 struct exec_policy_key {
 	__u8 cookie[64];
 };
@@ -171,68 +164,30 @@ event_inode_builder(void *ctx,  struct linux_binprm *bprm, struct msg_process *c
 	struct inode *f_inode;
 	struct file *file;
 	struct fsverity_info *i_verity_info;
-	int is_ovl = 1;
 	struct path f_path;
 	struct dentry *dentry;
 	struct ovl_entry *oentry;
-	unsigned numlower;
 	struct ovl_path lower;
 
 	probe_read(&file, sizeof(file), _(&bprm->file));
-	probe_read(&f_inode, sizeof(f_inode), _(&file->f_inode));
 	probe_read(&f_path, sizeof(struct path), _(&file->f_path));
 	probe_read(&dentry, sizeof(dentry), _(&f_path.dentry));
 	probe_read(&oentry, sizeof(oentry), _(&dentry->d_fsdata));
-	if (!oentry)
-		return;
-	probe_read(&numlower, sizeof(numlower), &oentry->numlower);
-	probe_read(&lower, sizeof(lower), &oentry->lowerstack[0]);
-	probe_read(&dentry, sizeof(dentry), &lower.dentry);
 
-	probe_read(&f_inode, sizeof(f_inode), _(&dentry->d_inode));
-
-	bpf_printk("entry num lower %d\n", numlower);
-	if (is_ovl) {
-		struct ovl_inode *ovl;
-		__u64 offset = offsetof(struct ovl_inode, vfs_inode);
-		__u64 version, flags;
-		struct inode *orig_node;
-		struct inode *ovll;
-		int zero = 0;
-
-		orig_node = map_lookup_elem(&inode_heap, &zero);
-		if (!orig_node)
-			return;
-
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
-
-		ovl = container_of(f_inode, struct ovl_inode, vfs_inode);
-		//ovl = (struct ovl_inode *)((char *)f_inode - offset);
-		//bpf_printk("inode %p - %d = %p\n", (long) f_inode, offset,
-				//(long)f_inode - offset);
-//		if (ovl != 0) {
-//			void *addr = &ovl->lowerdata;
-
-		bpf_printk("original f_inode %p\n", f_inode);
-		probe_read(&ovll, sizeof(struct dentry *), &ovl->lower);
-	//	probe_read(&f_inode, sizeof(f_inode), &ovl->lowerdata);
-		probe_read(orig_node, sizeof(struct inode), ovll);
-		bpf_printk("f_inode %p ovl %p offset %d\n", f_inode, ovl, offset);
-		probe_read(&version, sizeof(version), &ovl->version);
-		probe_read(&flags, sizeof(flags), &ovl->flags);
-		bpf_printk("version %d flags %d ovl %p\n", version, flags, ovl);
-		bpf_printk("ovll %p origi i_no %u\n", ovll, orig_node->i_ino);
-
+	// this could be other fs perhaps so we really should check type
+	if (oentry) {
+		probe_read(&lower, sizeof(lower), &oentry->lowerstack[0]);
+		probe_read(&dentry, sizeof(dentry), &lower.dentry);
+		probe_read(&f_inode, sizeof(f_inode), _(&dentry->d_inode));
+		// probably could need to walk through next lower layer but
+		// haven't found the case yet.
+	} else {
+		probe_read(&f_inode, sizeof(f_inode), _(&file->f_inode));
 	}
 
 	probe_read(&curr->i_ino, sizeof(curr->i_ino), _(&f_inode->i_ino));
-
 	probe_read(&i_verity_info, sizeof(i_verity_info), _(&f_inode->i_verity_info));
 	probe_read(&curr->digest, MAX_DIGEST_SIZE, _(&i_verity_info->file_digest));
-	bpf_printk("curr->digest %p %x %x\n", i_verity_info, curr->digest[1], curr->digest[2]);
-	bpf_printk("i_node %u\n", curr->i_ino);
 }
 
 static inline __attribute__((always_inline)) uint32_t
