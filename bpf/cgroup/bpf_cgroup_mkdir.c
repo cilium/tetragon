ccb736f938b0a3d48d48832ecc0e959107caf7ce
Djalal Harouni <tixxdz@gmail.com>
2022-10-21 12:26:16 +0200

    bpf:cgroup: add bpf programs to track cgroup creation and deletion
    This patch adds:
    
    1. bpf_cgroup_mkdir source that attaches to
       raw_tracepoint/cgroup_mkdir to track new cgroups that are being
       created. Tracking will be from the first cgroup level to
       discovered Tetragon cgroup level.
    
       Tracked cgroups will be saved inside the `tg_cgrps_tracking_map`
       BPF map.
    
    2. bpf_cgroup_rmdir source that attaches to
       raw_tracepoint/cgroup_rmdir when a cgroup is being removed.
       It allows to remove entries from cgroup bpf maps.
    
    3. bpf_cgroup_release source that attaches to
       raw_tracepoint/cgroup_release hook in order to ensure that we
       remove tracked cgroups from the bpf map.
    
       This extra precaution works when cgroup_release tracepoint
       is called to drop the reference on the css which could release
       the cgroup where it is attached.
    
    Those programs will also send events to ring buffer to notify
    user space about cgroup state. Right now we are using this for
    debugging purpose with log level trace.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>
    (cherry picked from commit 1b6d8dfcab24da9e0f443125e46a7b211c5c2e02)

---
+++ bpf/cgroup/bpf_cgroup_mkdir.c
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright Authors of Tetragon */
+
+#include "vmlinux.h"
+#include "api.h"
+
+#include "hubble_msg.h"
+#include "bpf_cgroup.h"
+#include "bpf_events.h"
+#include "bpf_cgroup_events.h"
+
+char _license[] __attribute__((section(("license")), used)) = "GPL";
+#ifdef VMLINUX_KERNEL_VERSION
+int _version __attribute__((section(("version")), used)) =
+	VMLINUX_KERNEL_VERSION;
+#endif
+
+__attribute__((section(("raw_tracepoint/cgroup_mkdir")), used)) int
+tg_tp_cgrp_mkdir(struct bpf_raw_tracepoint_args *ctx)
+{
+	int level, zero = 0;
+	uint64_t cgrpid;
+	struct cgroup *cgrp;
+	struct cgroup_tracking_value *cgrp_heap;
+	struct tetragon_conf *config;
+
+	config = map_lookup_elem(&tg_conf_map, &zero);
+	if (!config || config->tg_cgrp_level == 0)
+		return 0;
+
+	cgrp = (struct cgroup *)ctx->args[0];
+
+	level = get_cgroup_level(cgrp);
+	/* This should never happen as the cgroup hierarchy has already been
+	 * set (e.g., by systemd)
+	 */
+	if (level == 0)
+		return 0;
+
+	cgrpid = get_cgroup_id(cgrp);
+	/* This should never happen unless the bpf helper failed */
+	if (cgrpid == 0)
+		return 0;
+
+	/* We want to track all processes of a container system so that we can
+	 * provide proper identity to events. To do that, we use a certain cgroup
+	 * level. Any cgroups that are created under that level, we ignore.
+	 * That is, if we are monitoring level 5, we do not care about cgroup
+	 * events with level >5.
+	 */
+	if (level <= config->tg_cgrp_level) {
+		cgrp_heap = __init_cgrp_tracking_val_heap(cgrp, CGROUP_NEW);
+		if (!cgrp_heap)
+			return 0;
+
+		/* We track only for now cgroups that are at same or above tetragon
+		 * level (ancestors level)
+		 */
+		map_update_elem(&tg_cgrps_tracking_map, &cgrpid, cgrp_heap,
+				BPF_ANY);
+
+		/* We forward bpf events only under TraceLevel */
+		if (unlikely(config->loglevel == LOG_TRACE_LEVEL))
+			send_cgrp_event(ctx, cgrp_heap, cgrpid,
+					MSG_OP_CGROUP_MKDIR);
+	}
+
+	return 0;
+}
