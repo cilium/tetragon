9a6f7906f6a075c6831f4f5fd6c7f67c57f65e04
Djalal Harouni <tixxdz@gmail.com>
2022-10-21 12:26:16 +0200

    bpf:cgroup: add bpf programs to track cgroup creation and deletion
    This patch adds:
    
    1. bpf_cgroup_mkdir source that attaches to
       raw_tracepoint/cgroup_mkdir to track new cgroups that are being
       created. Tracking will be from the first cgroup level to
       discovered Tetragon cgroup level.
    
       Tracked cgroups will be saved inside the `tg_cgrps_tracking_map`
       BPF map.
    
    2. bpf_cgroup_rmdir source that attaches to
       raw_tracepoint/cgroup_rmdir when a cgroup is being removed.
       It allows to remove entries from cgroup bpf maps.
    
    3. bpf_cgroup_release source that attaches to
       raw_tracepoint/cgroup_release hook in order to ensure that we
       remove tracked cgroups from the bpf map.
    
       This extra precaution works when cgroup_release tracepoint
       is called to drop the reference on the css which could release
       the cgroup where it is attached.
    
    Those programs will also send events to ring buffer to notify
    user space about cgroup state. Right now we are using this for
    debugging purpose with log level trace.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>
    (cherry picked from commit 1b6d8dfcab24da9e0f443125e46a7b211c5c2e02)

---
+++ bpf/cgroup/bpf_cgroup_release.c
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright Authors of Tetragon */
+
+#include "vmlinux.h"
+#include "api.h"
+
+#include "hubble_msg.h"
+#include "bpf_cgroup.h"
+#include "bpf_events.h"
+#include "bpf_cgroup_events.h"
+
+char _license[] __attribute__((section(("license")), used)) = "GPL";
+#ifdef VMLINUX_KERNEL_VERSION
+int _version __attribute__((section(("version")), used)) =
+	VMLINUX_KERNEL_VERSION;
+#endif
+
+/* Ensure to remove tracked cgroups from bpf map */
+__attribute__((section(("raw_tracepoint/cgroup_release")), used)) int
+tg_tp_cgrp_release(struct bpf_raw_tracepoint_args *ctx)
+{
+	int zero = 0;
+	uint64_t cgrpid;
+	struct cgroup *cgrp;
+	struct tetragon_conf *conf;
+	struct cgroup_tracking_value *cgrp_track;
+
+	cgrp = (struct cgroup *)ctx->args[0];
+	cgrpid = get_cgroup_id(cgrp);
+	/* This should never happen unless our helper failed */
+	if (cgrpid == 0)
+		return 0;
+
+	cgrp_track = map_lookup_elem(&tg_cgrps_tracking_map, &cgrpid);
+	/* TODO: check cgroup level if it is under our tracking level
+	 *   then we probably did miss it and should report this.
+	 *   Otherwise the cgroup was never tracked and let's exit.
+	 */
+	if (!cgrp_track)
+		return 0;
+
+	map_delete_elem(&tg_cgrps_tracking_map, &cgrpid);
+
+	conf = map_lookup_elem(&tg_conf_map, &zero);
+	if (!conf)
+		return 0;
+
+	/* We forward bpf events only under TraceLevel */
+	if (unlikely(conf->loglevel == LOG_TRACE_LEVEL))
+		send_cgrp_event(ctx, cgrp_track, cgrpid, MSG_OP_CGROUP_RELEASE);
+
+	return 0;
+}
