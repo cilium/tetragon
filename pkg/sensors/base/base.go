7f48bcd556009d159bf23e4280b178706c79762a
Jiri Olsa <jolsa@kernel.org>
2022-11-10 13:40:10 +0100

    tetragon: Add v6.0 base sensors
    Adding new v6.0 flavor of base sensors. Adding new set of base
    sensors with V60 suffix that load *_v60.o based objects.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/base/base.go
+++ pkg/sensors/base/base.go
@@ -26,6 +26,14 @@ var (
 		"execve",
 	)
 
+	ExecveV60 = program.Builder(
+		"bpf_execve_event_v60.o",
+		"sched/sched_process_exec",
+		"tracepoint/sys_execve",
+		"event_execve",
+		"execve",
+	)
+
 	Exit = program.Builder(
 		"bpf_exit.o",
 		"sched/sched_process_exit",
@@ -45,28 +53,37 @@ var (
 	/* Event Ring map */
 	TCPMonMap    = program.MapBuilder("tcpmon_map", Execve)
 	TCPMonMapV53 = program.MapBuilder("tcpmon_map", ExecveV53)
+	TCPMonMapV60 = program.MapBuilder("tcpmon_map", ExecveV60)
 
 	/* Networking and Process Monitoring maps */
 	ExecveMap    = program.MapBuilder("execve_map", Execve)
 	ExecveMapV53 = program.MapBuilder("execve_map", ExecveV53)
+	ExecveMapV60 = program.MapBuilder("execve_map", ExecveV60)
 
 	ExecveTailCallsMap    = program.MapBuilderPin("execve_calls", "execve_calls", Execve)
 	ExecveTailCallsMapV53 = program.MapBuilderPin("execve_calls", "execve_calls", ExecveV53)
+	ExecveTailCallsMapV60 = program.MapBuilderPin("execve_calls", "execve_calls", ExecveV60)
 
 	/* Policy maps populated from base programs */
 	NamesMap    = program.MapBuilder("names_map", Execve)
 	NamesMapV53 = program.MapBuilder("names_map", ExecveV53)
+	NamesMapV60 = program.MapBuilder("names_map", ExecveV60)
 
 	/* Tetragon runtime configuration */
 	TetragonConfMap    = program.MapBuilder("tg_conf_map", Execve)
 	TetragonConfMapV53 = program.MapBuilder("tg_conf_map", ExecveV53)
+	TetragonConfMapV60 = program.MapBuilder("tg_conf_map", ExecveV60)
 
 	/* Internal statistics for debugging */
 	ExecveStats    = program.MapBuilder("execve_map_stats", Execve)
 	ExecveStatsV53 = program.MapBuilder("execve_map_stats", ExecveV53)
+	ExecveStatsV60 = program.MapBuilder("execve_map_stats", ExecveV60)
 )
 
 func GetExecveMap() *program.Map {
+	if kernels.EnableV60Progs() {
+		return ExecveMapV60
+	}
 	if kernels.EnableLargeProgs() {
 		return ExecveMapV53
 	}
@@ -74,6 +91,9 @@ func GetExecveMap() *program.Map {
 }
 
 func GetExecveMapStats() *program.Map {
+	if kernels.EnableV60Progs() {
+		return ExecveStatsV60
+	}
 	if kernels.EnableLargeProgs() {
 		return ExecveStatsV53
 	}
@@ -81,6 +101,9 @@ func GetExecveMapStats() *program.Map {
 }
 
 func GetTetragonConfMap() *program.Map {
+	if kernels.EnableV60Progs() {
+		return TetragonConfMapV60
+	}
 	if kernels.EnableLargeProgs() {
 		return TetragonConfMapV53
 	}
@@ -92,7 +115,9 @@ func GetDefaultPrograms() []*program.Program {
 		Exit,
 		Fork,
 	}
-	if kernels.EnableLargeProgs() {
+	if kernels.EnableV60Progs() {
+		progs = append(progs, ExecveV60)
+	} else if kernels.EnableLargeProgs() {
 		progs = append(progs, ExecveV53)
 	} else {
 		progs = append(progs, Execve)
@@ -103,7 +128,16 @@ func GetDefaultPrograms() []*program.Program {
 func GetDefaultMaps() []*program.Map {
 	maps := []*program.Map{}
 
-	if kernels.EnableLargeProgs() {
+	if kernels.EnableV60Progs() {
+		maps = append(maps,
+			ExecveMapV60,
+			ExecveStatsV60,
+			ExecveTailCallsMapV60,
+			NamesMapV60,
+			TCPMonMapV60,
+			TetragonConfMapV60,
+		)
+	} else if kernels.EnableLargeProgs() {
 		maps = append(maps,
 			ExecveMapV53,
 			ExecveStatsV53,
