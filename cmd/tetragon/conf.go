347e553adee6231448431fa2fe78fdeda46e182b
Djalal Harouni <tixxdz@gmail.com>
2022-12-20 13:14:38 +0100

    conf: improve how we handle conf
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ cmd/tetragon/conf.go
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cilium/tetragon/pkg/option"
+
+	"github.com/spf13/viper"
+)
+
+func readConfigFile(path string, file string) error {
+	filePath := filepath.Join(path, file)
+	st, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+	if st.Mode().IsRegular() == false {
+		return fmt.Errorf("failed to read config file '%s' not a regular file", file)
+	}
+
+	viper.AddConfigPath(path)
+	err = viper.MergeInConfig()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readConfigDir(path string) error {
+	st, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if st.IsDir() == false {
+		return fmt.Errorf("'%s' is not a directory", path)
+	}
+
+	cm, err := option.ReadDirConfig(path)
+	if err != nil {
+		return err
+	}
+	if err := viper.MergeConfigMap(cm); err != nil {
+		return fmt.Errorf("merge config failed %v", err)
+	}
+
+	return nil
+}
+
+func readConfigSettings(defaultConfDir string, dropInsDir []string) {
+	viper.SetEnvPrefix("tetragon")
+	replacer := strings.NewReplacer("-", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.AutomaticEnv()
+
+	viper.SetConfigName("tetragon")
+	viper.SetConfigType("yaml")
+
+	// Read drop-ins
+	for _, dir := range dropInsDir {
+		readConfigDir(dir)
+	}
+
+	// Look into cwd first, this is needed for quick development only
+	readConfigFile(".", "tetragon.yaml")
+
+	readConfigFile(defaultConfDir, "tetragon.yaml")
+
+	// Read now the passed key config
+	if viper.IsSet(keyConfigDir) {
+		configDir := viper.GetString(keyConfigDir)
+		cm, err := option.ReadDirConfig(configDir)
+		if err != nil {
+			log.WithField(keyConfigDir, configDir).WithError(err).Fatal("Failed to read config from directory")
+		}
+		if err := viper.MergeConfigMap(cm); err != nil {
+			log.WithField(keyConfigDir, configDir).WithError(err).Fatal("Failed to merge config from directory")
+		}
+		log.WithField(keyConfigDir, configDir).Info("Loaded config from directory")
+	}
+}
