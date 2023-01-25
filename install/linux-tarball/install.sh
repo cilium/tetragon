3d04011299c68e4ed10fe8ed371419942aa0da68
Djalal Harouni <tixxdz@gmail.com>
2023-01-25 12:22:33 +0100

    deployment: add linux-tarball deployment resource files
    This adds the linux-tarball deployment resource files with their
    Dockerfile.tarball file to make a docker image with one layer that
    can be dumped to a tar -> tar.gz
    
    Include sane and secure configurations for a non k8s deployment.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ install/linux-tarball/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+SRC_DIR=$(dirname -- "$(readlink -f -- "$0")")
+
+cp -vRf ${SRC_DIR}/usr/local/* /usr/local/
+
+cp -vf /usr/local/lib/tetragon/systemd/tetragon.service /usr/lib/systemd/system/tetragon.service
+
+systemctl daemon-reload
+systemctl enable tetragon
+systemctl start tetragon
+
+echo "Tetragon installed successfully!"
