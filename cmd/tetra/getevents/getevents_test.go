628f1cee7c08c8092f94837808337d51fd142993
Mahe Tardy <mahe.tardy@isovalent.com>
2023-01-27 16:43:04 +0100

    tetra: add tests for getevents fields filter
    Signed-off-by: Mahe Tardy <mahe.tardy@isovalent.com>

--- cmd/tetra/getevents/getevents_test.go
+++ cmd/tetra/getevents/getevents_test.go
@@ -5,8 +5,10 @@ package getevents
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 
+	"github.com/cilium/tetragon/api/v1/tetragon"
 	"github.com/cilium/tetragon/pkg/testutils"
 	"github.com/stretchr/testify/assert"
 )
@@ -62,5 +64,45 @@ func Test_GetEvents_Process(t *testing.T) {
 		cmd.SetArgs([]string{"--process", "docker"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 2, bytes.Count(output, []byte("\n")))
+	})
+}
+
+func Test_GetEvents_FilterFields(t *testing.T) {
+	t.Run("ExcludeParent", func(t *testing.T) {
+		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
+		cmd := New()
+		cmd.SetArgs([]string{"-F", "parent"})
+		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
+		// remove last trailing newline for splitting
+		output = bytes.TrimSpace(output)
+		lines := bytes.Split(output, []byte("\n"))
+		for _, line := range lines {
+			var res tetragon.GetEventsResponse
+			err := json.Unmarshal(line, &res)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.NotEmpty(t, res.GetProcessExec().Process)
+			assert.Empty(t, res.GetProcessExec().Parent)
+		}
+	})
+
+	t.Run("IncludeParent", func(t *testing.T) {
+		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
+		cmd := New()
+		cmd.SetArgs([]string{"-f", "parent"})
+		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
+		// remove last trailing newline for splitting
+		output = bytes.TrimSpace(output)
+		lines := bytes.Split(output, []byte("\n"))
+		for _, line := range lines {
+			var res tetragon.GetEventsResponse
+			err := json.Unmarshal(line, &res)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Empty(t, res.GetProcessExec().Process)
+			assert.NotEmpty(t, res.GetProcessExec().Parent)
+		}
 	})
 }
