d21d92dd8eff498278cecad081f2467307c43673
Kornilios Kourtis <kornilios@isovalent.com>
2023-02-03 10:17:08 +0100

    confmap: do not fail if map does not exist
    This patch modifies the confmap code to not fail if the map does not
    exist. Instead, the map will be created.
    
    While we are at it, it cleanus up the confmap code:
     * removes uses of the now deprecated pkg/bpf
     * removes code duplication, and uses a single function for setting the
       value of the confmap.
    
    This allows is us to run the code that initialized the config map
    earlier in the tetragon init process, which will be used later.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/testutils/confmap.go
+++ pkg/testutils/confmap.go
@@ -5,19 +5,10 @@ package testutils
 
 import (
 	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/cilium/tetragon/pkg/bpf"
 	"github.com/cilium/tetragon/pkg/cgroups"
 	"github.com/cilium/tetragon/pkg/logger"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	"github.com/cilium/tetragon/pkg/sensors/config/confmap"
-	"github.com/sirupsen/logrus"
-)
-
-var (
-	log = logger.GetLogger()
 )
 
 func GetTgRuntimeConf() (*confmap.TetragonConfValue, error) {
@@ -48,46 +39,6 @@ func GetTgRuntimeConf() (*confmap.TetragonConfValue, error) {
 		NSPID:           uint32(nspid),
 		CgrpFsMagic:     cgroupFsMagic,
 	}, nil
-}
-
-// Test `tg_conf_map` BPF MAP with explicit values
-func UpdateTgRuntimeConf(mapDir string, v *confmap.TetragonConfValue) error {
-	configMap := base.GetTetragonConfMap()
-	mapPath := filepath.Join(mapDir, configMap.Name)
-
-	m, err := bpf.OpenMap(mapPath)
-	for i := 0; err != nil; i++ {
-		m, err = bpf.OpenMap(mapPath)
-		if err != nil {
-			time.Sleep(1 * time.Second)
-		}
-		if i > 4 {
-			log.WithField("confmap-update", configMap.Name).WithError(err).Warn("Failed to update TetragonConf map")
-			return err
-		}
-	}
-
-	defer m.Close()
-
-	k := &confmap.TetragonConfKey{Key: 0}
-	err = m.Update(k, v)
-	if err != nil {
-		log.WithField("confmap-update", configMap.Name).WithError(err).Warn("Failed to update TetragonConf map")
-		return err
-	}
-
-	log.WithFields(logrus.Fields{
-		"confmap-update":                configMap.Name,
-		"log.level":                     logrus.Level(v.LogLevel).String(),
-		"cgroup.fs.magic":               cgroups.CgroupFsMagicStr(v.CgrpFsMagic),
-		"cgroup.controller.name":        cgroups.GetCgrpControllerName(),
-		"cgroup.controller.hierarchyID": v.TgCgrpHierarchy,
-		"cgroup.controller.index":       v.TgCgrpSubsysIdx,
-		"cgroup.ID":                     v.TgCgrpId,
-		"NSPID":                         v.NSPID,
-	}).Debug("Updated TetragonConf map successfully")
-
-	return nil
 }
 
 func ReadTgRuntimeConf(mapDir string) (*confmap.TetragonConfValue, error) {
