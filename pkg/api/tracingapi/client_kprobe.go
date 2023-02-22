15d2eaad294202d32a1f39184a5d03eb974dc62a
Kevin Sheldrake <kevin.sheldrake@isovalent.com>
2023-02-22 15:59:46 +0000

    Handle multiple URL and DNS selectors correctly
    Currently we have experimental support for URL and DNS actions, which
    could be used to trigger Thinkst canaries. This experimental support
    incorrectly handles multiple selectors – it simply collects all the URLs
    in the kprobe and all the FQDNs in the kprobe into lists, and when a URL
    or FQDN action fires on it, it triggers everything in the corresponding
    list. This is obviously wrong.
    
    This commit fixes this as follows. Each kprobe stores a table of URLs
    and FQDNs that it references, each entry with its own index. These
    indices are provided in the config to the match actions in the
    selectors, and the BPF program reports the matching index. In user
    space, the URL or FQDN is retrieved from the table using this index.
    
    Note that only one URL and/or FQDN action is permitted per selector. It
    will be possible to enable multiple if necessary, but it is deemed that
    a single trigger should be sufficient to trigger any further triggers.
    
    Signed-off-by: Kevin Sheldrake <kevin.sheldrake@isovalent.com>

--- pkg/api/tracingapi/client_kprobe.go
+++ pkg/api/tracingapi/client_kprobe.go
@@ -18,6 +18,8 @@ const (
 	ActionUnfollowFd = 3
 	ActionOverride   = 4
 	ActionCopyFd     = 5
+	ActionGetUrl     = 6
+	ActionLookupDns  = 7
 )
 
 const (
@@ -43,6 +45,8 @@ type MsgGenericKprobe struct {
 	Id           uint64
 	ThreadId     uint64
 	ActionId     uint64
+	ActionArgId  uint32
+	Pad          uint32
 }
 
 type MsgGenericKprobeArgPath struct {
