8493cb68374bc397575d931dc85e7313aec309d0
Mahe Tardy <mahe.tardy@gmail.com>
2023-03-03 17:41:41 +0000

    kprobe: add tests for addSyscallPrefix
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

---
+++ pkg/sensors/tracing/generickprobe_test.go
@@ -0,0 +1,34 @@
+package tracing
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_addSyscallPrefix(t *testing.T) {
+	symbol := "sys_test"
+	arch := "test64"
+	supportedArchPrefix[arch] = "__test64_"
+	prefixedSymbol := supportedArchPrefix[arch] + symbol
+
+	// adding prefix
+	res, err := addSyscallPrefix(symbol, arch)
+	assert.NoError(t, err)
+	assert.Equal(t, prefixedSymbol, res)
+
+	// doing nothing
+	res, err = addSyscallPrefix(prefixedSymbol, arch)
+	assert.NoError(t, err)
+	assert.Equal(t, prefixedSymbol, res)
+
+	// wrong prefix for current arch
+	res, err = addSyscallPrefix("__x64_"+symbol, arch)
+	assert.Error(t, err)
+	assert.Empty(t, res)
+
+	// not supported arch
+	res, err = addSyscallPrefix(symbol, "unsupported64")
+	assert.Error(t, err)
+	assert.Empty(t, res)
+}
