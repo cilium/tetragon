7ed2513f5a0da98974c3d1f4d46194a65925ed7f
Jiri Olsa <jolsa@kernel.org>
2023-04-04 21:21:12 +0000

    tetragon: Add NoPost action to selector
    Adding new NoPost action to selector to be used like:
    
      matchActions:
      - action: Override
        argError: -2
      - action: NoPost
    
    It will result in the kprobe/tracepoint event not to be sent
    to the user space.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- vendor/github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1/types.go
+++ vendor/github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1/types.go
@@ -242,7 +242,7 @@ type ArgSelector struct {
 }
 
 type ActionSelector struct {
-	// +kubebuilder:validation:Enum=Post;FollowFD;UnfollowFD;Sigkill;CopyFD;Override;GetUrl;DnsLookup
+	// +kubebuilder:validation:Enum=Post;FollowFD;UnfollowFD;Sigkill;CopyFD;Override;GetUrl;DnsLookup;NoPost
 	// Action to execute.
 	Action string `json:"action"`
 	// +kubebuilder:validation:Optional
