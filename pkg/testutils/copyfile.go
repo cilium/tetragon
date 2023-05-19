d61522ad01e5038052c1a9bf41ccab6115171d85
Jiri Olsa <jolsa@kernel.org>
2023-05-19 07:40:50 +0000

    tetragon: Use testutils.CopyFile instead of invoking cp
    It's cleaner to use go functions directly.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ pkg/testutils/copyfile.go
@@ -0,0 +1,27 @@
+package testutils
+
+import (
+	"io"
+	"os"
+	"syscall"
+)
+
+func CopyFile(toPath, fromPath string, perm os.FileMode) error {
+	from, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	to, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+
+	_, err = io.Copy(to, from)
+	if err != nil {
+		syscall.Unlink(toPath)
+	}
+	return err
+}
