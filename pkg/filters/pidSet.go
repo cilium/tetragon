57fdeb52f396f6184ad66a671d7a6a9d3facd664
William Findlay <will@isovalent.com>
2023-05-19 15:11:00 -0400

    filters/pidset: fix bugs and account for children of chilren
    The existing pidSet filter was broken in a few ways. First, we were using a single shared
    global variable to track PIDs. This meant that having multiple pidSet filters active would
    cause them to pollute each other's state. The existing implementation also did not
    properly track children of children, meaning that we would only pay attention to nodes of
    depth < 2 from the parent.
    
    This patch introduces refactors to the pidSet filters that resolve the above issues. Since
    we are maintaining a separate PID cache per filter, we implement the cache as an LRU of
    size 8192 to prevent unbounded memory usage. This should be more than enough for most
    practical contexts.
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- pkg/filters/pidSet.go
+++ pkg/filters/pidSet.go
@@ -18,52 +18,85 @@ import (
 	"context"
 
 	"github.com/cilium/tetragon/api/v1/tetragon"
+	"github.com/cilium/tetragon/pkg/logger"
 	v1 "github.com/cilium/tetragon/pkg/oldhubble/api/v1"
 	hubbleFilters "github.com/cilium/tetragon/pkg/oldhubble/filters"
 )
 
-/* pidSet is the set of pids we are interested in receiving events
- * for. This includes the pids listed in filter and any of their
- * children.
- */
-var pidSet map[uint32]bool
+// We could use an LRU here but we really don't want to evict old entries and risk failing
+// a test that uses this filter. Instead, we take the safer approach from the perspective
+// of testing and opt to grow the map indefinitely and log a warning if the size exceeeds
+// a pre-determined threshold. Since we have protections in place to prevent this filter
+// being used in production, this should be acceptable.
+type ChildCache = map[uint32]struct{}
 
-func filterByPidSet(pids []uint32) hubbleFilters.FilterFunc {
-	return func(ev *v1.Event) bool {
-		process := GetProcess(ev)
-		if process == nil {
-			return false
-		}
-		for _, pid := range pids {
-			if pid == process.Pid.GetValue() {
-				pidSet[pid] = true
-				return true
-			}
-		}
-		parent := GetParent(ev)
-		if parent == nil {
-			return false
-		}
-		if pidSet[parent.Pid.GetValue()] == true {
+func checkPidSetMembership(pid uint32, pidSet []uint32, childCache ChildCache) bool {
+	// Check the original pidSet. The reason for doing this separately is that we never
+	// want to drop the original pidSet from the cache. Keeping this separately in a slice
+	// is an easy way to achieve this.
+	for _, p := range pidSet {
+		if pid == p {
 			return true
 		}
-		for _, pid := range pids {
-			if pid == parent.Pid.GetValue() {
-				pidSet[pid] = true
-				return true
-			}
-		}
+	}
+	// Fall back to childCache to check children.
+	_, ok := childCache[pid]
+	return ok
+}
+
+func doFilterByPidSet(ev *v1.Event, pidSet []uint32, childCache ChildCache, childCacheWarning *int) bool {
+	process := GetProcess(ev)
+	if process == nil {
 		return false
+	}
+
+	// Check the process against our cache
+	pid := process.Pid.GetValue()
+	if checkPidSetMembership(pid, pidSet, childCache) {
+		return true
+	}
+
+	parent := GetParent(ev)
+	if parent == nil {
+		return false
+	}
+
+	// Check the parent against our cache
+	ppid := parent.Pid.GetValue()
+	if checkPidSetMembership(ppid, pidSet, childCache) {
+		// Add our own PID to the children cache so that we can match our future children.
+		childCache[pid] = struct{}{}
+		// If we exceeded the pre-determined warning limit, log a warning message and
+		// double it.
+		if len(childCache) == *childCacheWarning {
+			logger.GetLogger().Warnf("pidSet filter cache has exceeded %d entries. To prevent excess memory usage, consider disabling it.", childCacheWarning)
+			*childCacheWarning *= 2
+		}
+		return true
+	}
+
+	// No matches, return false
+	return false
+}
+
+func filterByPidSet(pidSet []uint32, childCache ChildCache, childCacheWarning int) hubbleFilters.FilterFunc {
+	return func(ev *v1.Event) bool {
+		return doFilterByPidSet(ev, pidSet, childCache, &childCacheWarning)
 	}
 }
 
+// PidSetFilter is a filter that matches on a process and all of its children by their
+// PID, up to maxChildCacheSize number of children.
 type PidSetFilter struct{}
 
 func (f *PidSetFilter) OnBuildFilter(_ context.Context, ff *tetragon.Filter) ([]hubbleFilters.FilterFunc, error) {
-	pidSet = make(map[uint32]bool)
 	var fs []hubbleFilters.FilterFunc
 	if ff.PidSet != nil {
-		fs = append(fs, filterByPidSet(ff.PidSet))
+		childCache := make(ChildCache)
+		childCacheWarning := 8192
+
+		pidSet := ff.PidSet
+		fs = append(fs, filterByPidSet(pidSet, childCache, childCacheWarning))
 	}
 	return fs, nil
 }
