c8b468ac4418a3e8949f30fea99096ed12234ea9
Jiri Olsa <jolsa@kernel.org>
2023-05-19 19:45:25 +0000

    tetragon: Add more thread to TestExitLeader test
    [ upstream commit 0646c56a75284f802829345615647e66934802cb ]
    
    Adding extra threads to the exit-leader test prog.
    
    Spawning 2 threads directly from leader and another 2
    from the newly created threads.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- contrib/tester-progs/exit-leader.c
+++ contrib/tester-progs/exit-leader.c
@@ -5,13 +5,29 @@
 #include <stdlib.h>
 #include <sys/syscall.h>
 
+#define gettid() (int) syscall(SYS_gettid)
+
 static void *worker(void *ctx)
 {
-	int cnt = 3;
+	unsigned long cnt = (unsigned long) ctx;
+	pthread_t th;
+	int err;
+
+	fprintf(stderr, "worker %d:%d cnt %lu\n", getpid(), gettid(), cnt);
+
+	if (cnt == 2 || cnt == 4) {
+		err = pthread_create(&th, NULL, worker, (void *) (cnt + 1));
+		if (err) {
+			perror("pthread_create");
+			return NULL;
+		}
+	}
 
 	while (cnt--) {
 		sleep(1);
 	}
+
+	fprintf(stderr, "exit %d:%d\n", getpid(), gettid());
 	return NULL;
 }
 
@@ -20,10 +36,18 @@ int main(void)
 	pthread_t th;
 	int err;
 
-	err = pthread_create(&th, NULL, worker, NULL);
+	err = pthread_create(&th, NULL, worker, (void *) 2);
 	if (err) {
 		perror("pthread_create");
 		return -1;
 	}
+	err = pthread_create(&th, NULL, worker, (void *) 4);
+	if (err) {
+		perror("pthread_create");
+		return -1;
+	}
+
+	sleep(3);
+	fprintf(stderr, "exit %d:%d\n", getpid(), gettid());
 	syscall(SYS_exit, 0);
 }
