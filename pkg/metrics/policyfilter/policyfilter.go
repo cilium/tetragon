5a74c67741a6d42dcd66c3ff4be88132aa41eb11
Kornilios Kourtis <kornilios@isovalent.com>
2023-05-23 17:07:23 +0200

    policyfilter: add operation metrics
    This patch adds operation metrics to the policfilter operations. We
    maintain a counter using the subsystem (currently pod-handlers or
    rthooks), the operation, and the error (including the case where error
    was nil) as labels.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/metrics/policyfilter/policyfilter.go
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package policfilter
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cilium/tetragon/pkg/metrics/consts"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	PolicyFilterOpMetrics = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name:        consts.MetricNamePrefix + "policyflter_metrics_total",
+		Help:        "Policy filter metrics. For internal use only.",
+		ConstLabels: nil,
+	}, []string{"subsys", "op", "error_type"})
+)
+
+func OpInc(subsys, op string, err error) {
+	PolicyFilterOpMetrics.WithLabelValues(
+		subsys, op,
+		strings.ReplaceAll(fmt.Sprintf("%T", errors.Cause(err)), "*", ""),
+	).Inc()
+}
