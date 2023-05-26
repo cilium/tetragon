035b15ac69ced4c109449c013a64301cbf959d13
Kornilios Kourtis <kornilios@isovalent.com>
2023-05-26 14:29:44 +0200

    tests/e2e: skeleton: wait after starting pod
    Set the wait flag to wait when for the pod to be ready when starting it.
    We 've observed flakes on the skeleton test, so they could be
    potentially caused by starting the test when the pod is not ready yet.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- tests/e2e/tests/skeleton/skeleton_test.go
+++ tests/e2e/tests/skeleton/skeleton_test.go
@@ -107,7 +107,7 @@ func TestSkeletonBasic(t *testing.T) {
 		Assess("Wait for Checker", curlChecker.Wait(30*time.Second)).
 		/* Run the workload */
 		Assess("Run Workload", func(ctx context.Context, _ *testing.T, c *envconf.Config) context.Context {
-			ctx, err := helpers.LoadCRDString(namespace, curlPod, false)(ctx, c)
+			ctx, err := helpers.LoadCRDString(namespace, curlPod, true)(ctx, c)
 			if err != nil {
 				klog.ErrorS(err, "failed to spawn workload")
 				t.Fail()
