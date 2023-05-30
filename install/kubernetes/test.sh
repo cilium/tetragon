357206b421615ad16a46da821582ac0dd00d21d0
Mahe Tardy <mahe.tardy@gmail.com>
2023-05-30 17:16:39 +0200

    helm: fix linter script to be portable
    - use docker to run kubeval instead of local install
    - make the script cd into its current directory so that we can run it
      from anywhere
    - add `set -o pipefail` bash option
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- install/kubernetes/test.sh
+++ install/kubernetes/test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 shopt -s expand_aliases
 
+# make this script executable from anywhere
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 alias helm='docker run --rm -v $(pwd):/apps alpine/helm:3.3.4'
+alias kubeval='docker run --rm -i garethr/kubeval:0.15.0@sha256:6962d8ecbb7839637667f66e6703e6ebaae0c29dfe93a31d9968fba4324c7b8d'
 helm dependency update .
 helm lint . --with-subcharts
 helm template tetragon . | kubeval --strict --additional-schema-locations https://raw.githubusercontent.com/joshuaspence/kubernetes-json-schema/master
