2ad1fddfb20d9570cd8622e20b8c8ea4f44635af
Kornilios Kourtis <kornilios@isovalent.com>
2023-05-31 12:45:03 +0200

    e2e/tests/policyfilter: add pod label filter test
    This patch adds a pod label filter test to the policyfilter e2e test.
    
    Idea is:
    - create a namespace
    - install a namespaced policy with pod label filter in that namespace (on sys_exit)
    - create two pods, one that matches the policy and one that does not
    - check that we only receive events from the first
    
    We use sys_exit here to make it easier to dinguish with the namespace
    test which uses sys_enter.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- tests/e2e/tests/policyfilter/policyfilter_test.go
+++ tests/e2e/tests/policyfilter/policyfilter_test.go
@@ -37,11 +37,19 @@ var (
 var (
 	// for the namespace test, we:
 	//  - create two namespaces and start a pod in each of them
-	//  - install a policy for monitoring syscalls in one of them
+	//  - install a policy for monitoring syscalls in one of them (on sys_enter)
 	//  - check that we get events only from that namespace, and not the other.
 	otherNamespace  = "ns1"
 	policyNamespace = "ns2"
-	testNamespaces  = []string{otherNamespace, policyNamespace}
+
+	// for the pod label filter test, we:
+	//  - create a namespace
+	//  - install a namespaced policy with pod label filter in that namespace (on sys_exit)
+	//  - create two pods, one that matches the policy and one that does not
+	//  - check that we only receive events from the first
+	podlblNamespace = "nslabel"
+
+	testNamespaces = []string{otherNamespace, policyNamespace, podlblNamespace}
 )
 
 func TestMain(m *testing.M) {
@@ -74,7 +82,8 @@ func TestNamespacedPolicy(t *testing.T) {
 			30*time.Second,
 			// allow list
 			[]*tetragon.Filter{{
-				EventSet: []tetragon.EventType{tetragon.EventType_PROCESS_TRACEPOINT},
+				EventSet:  []tetragon.EventType{tetragon.EventType_PROCESS_TRACEPOINT},
+				Namespace: []string{otherNamespace, policyNamespace},
 			}},
 			// deny list
 			[]*tetragon.Filter{},
@@ -99,7 +108,7 @@ func TestNamespacedPolicy(t *testing.T) {
 		Assess("Wait for Checker", checker.Wait(30*time.Second)).
 		Assess("Start pods", func(ctx context.Context, _ *testing.T, c *envconf.Config) context.Context {
 			var err error
-			for _, ns := range testNamespaces {
+			for _, ns := range []string{policyNamespace, otherNamespace} {
 				ctx, err = helpers.LoadCRDString(ns, ubuntuPod, true)(ctx, c)
 				if err != nil {
 					klog.ErrorS(err, "failed to load pod")
@@ -184,4 +193,158 @@ func (nsc *namespaceChecker) FinalCheck(_ *logrus.Logger) error {
 		return nil
 	}
 	return fmt.Errorf("namespace checker failed, had %d matches", nsc.matches)
+}
+
+func TestPodLabelFilters(t *testing.T) {
+	runner.SetupExport(t)
+
+	checker := podlblChecker().WithTimeLimit(30 * time.Second).WithEventLimit(20)
+
+	runEventChecker := features.New("Run Event Checks").
+		Assess("Run Event Checks", checker.CheckWithFilters(
+			30*time.Second,
+			// allow list
+			[]*tetragon.Filter{{
+				EventSet:  []tetragon.EventType{tetragon.EventType_PROCESS_TRACEPOINT},
+				Namespace: []string{podlblNamespace},
+			}},
+			// deny list
+			[]*tetragon.Filter{},
+		)).Feature()
+
+	runWorkload := features.New("Pod label filter test").
+		Assess("Install policy", func(ctx context.Context, _ *testing.T, c *envconf.Config) context.Context {
+			ctx, err := helpers.LoadCRDString(podlblNamespace, podlblPolicy, false)(ctx, c)
+			if err != nil {
+				klog.ErrorS(err, "failed to install policy")
+				t.Fail()
+			}
+			return ctx
+		}).
+		Assess("Wait for policy", func(ctx context.Context, _ *testing.T, cfg *envconf.Config) context.Context {
+			if err := grpc.WaitForTracingPolicy(ctx, "l1-syscalls"); err != nil {
+				klog.ErrorS(err, "failed to wait for policy")
+				t.Fail()
+			}
+			return ctx
+		}).
+		Assess("Wait for Checker", checker.Wait(30*time.Second)).
+		Assess("Start pods", func(ctx context.Context, _ *testing.T, c *envconf.Config) context.Context {
+			var err error
+			for _, pod := range []string{ubuntuPod_l1, ubuntuPod_l2} {
+				ctx, err = helpers.LoadCRDString(podlblNamespace, pod, true)(ctx, c)
+				if err != nil {
+					klog.ErrorS(err, "failed to load pod")
+					t.Fail()
+				}
+
+			}
+			return ctx
+		}).
+		Feature()
+
+	runner.TestInParallel(t, runWorkload, runEventChecker)
+}
+
+const podlblPolicy = `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicyNamespaced
+metadata:
+  name: "l1-syscalls"
+spec:
+  podSelector:
+     matchLabels:
+        app: "ubuntu-l1"
+  tracepoints:
+  - subsystem: "raw_syscalls"
+    event: "sys_exit"
+    args:
+    - index: 4
+      type: "int64"
+`
+
+const ubuntuPod_l1 = `
+kind: Deployment
+apiVersion: apps/v1
+metadata:
+  name: ubuntu-l2
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: "ubuntu-l1"
+  template:
+    metadata:
+      labels:
+        app: "ubuntu-l1"
+    spec:
+      containers:
+      - name: ubuntu
+        image: ubuntu:20.04
+        imagePullPolicy: Always
+        command: ["bash"]
+        args: ["-c", "while sleep 1; do cat /etc/hostname; done"]
+`
+
+const ubuntuPod_l2 = `
+kind: Deployment
+apiVersion: apps/v1
+metadata:
+  name: ubuntu-l1
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: "ubuntu-l2"
+  template:
+    metadata:
+      labels:
+        app: "ubuntu-l2"
+    spec:
+      containers:
+      - name: ubuntu
+        image: ubuntu:20.04
+        imagePullPolicy: Always
+        command: ["bash"]
+        args: ["-c", "while sleep 1; do cat /etc/hostname; done"]
+`
+
+func podlblChecker() *checker.RPCChecker {
+	return checker.NewRPCChecker(&podLabelChecker{}, "policyfilter-pod-label-checker")
+}
+
+type podLabelChecker struct {
+	matches int
+}
+
+func (plc *podLabelChecker) NextEventCheck(event ec.Event, _ *logrus.Logger) (bool, error) {
+	// ignore non-trace point events
+	ev, ok := event.(*tetragon.ProcessTracepoint)
+	if !ok {
+		return false, errors.New("not a tracepoint")
+	}
+
+	// ignore other tracepoints
+	if ev.GetSubsys() != "raw_syscalls" || ev.GetEvent() != "sys_exit" {
+		return false, fmt.Errorf("not raw_syscalls:sys_exit (%s:%s instead)", ev.GetSubsys(), ev.GetEvent())
+	}
+
+	labels := ev.GetProcess().GetPod().GetPodLabels()
+	if labels == nil {
+		return true, fmt.Errorf("event %+v has no labels", ev)
+	}
+
+	if val, ok := labels["app"]; !ok || val != "ubuntu-l1" {
+		return true, fmt.Errorf("event %+v has wrong label labels", ev)
+	}
+
+	plc.matches++
+	return false, nil
+}
+
+func (plc *podLabelChecker) FinalCheck(_ *logrus.Logger) error {
+	if plc.matches > 0 {
+		return nil
+	}
+	return fmt.Errorf("pod-label checker failed, had %d matches", plc.matches)
 }
