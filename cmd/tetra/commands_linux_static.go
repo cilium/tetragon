28c61005a9beb2672acd7b5a61054490d6611246
Kornilios Kourtis <kornilios@isovalent.com>
2023-06-02 11:53:30 +0200

    tetra: remove bugtool from static build
    Previous change added a bpf dependency on bugtool, which means that we
    cannot build it statically anymore. One approach would be to split the
    parts from bugtool that we can build statically and those which we
    cannot, but I bugtool only makes sense to be executed from within the
    pod. So we remove it from the static builds for the release.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- cmd/tetra/commands_linux_static.go
+++ cmd/tetra/commands_linux_static.go
@@ -6,13 +6,11 @@
 package main
 
 import (
-	"github.com/cilium/tetragon/cmd/tetra/bugtool"
 	"github.com/cilium/tetragon/cmd/tetra/tracingpolicy"
 	"github.com/spf13/cobra"
 )
 
 func addCommands(rootCmd *cobra.Command) {
 	addBaseCommands(rootCmd)
-	rootCmd.AddCommand(bugtool.New())
 	rootCmd.AddCommand(tracingpolicy.New())
 }
