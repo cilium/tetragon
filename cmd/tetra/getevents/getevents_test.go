dafb683157c84abeca29ee51dc1960a2a3144da2
Mahe Tardy <mahe.tardy@gmail.com>
2023-06-21 19:07:27 +0000

    tetra: pluralize some getevents cmd flags
    Deprecate old singular flags in the getevents command as they should be
    plural since you can use multiple values for them.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- cmd/tetra/getevents/getevents_test.go
+++ cmd/tetra/getevents/getevents_test.go
@@ -13,11 +13,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_GetEvents_Namespace(t *testing.T) {
+func Test_GetEvents_Namespaces(t *testing.T) {
 	t.Run("FilterNothing", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--namespace", "default"})
+		cmd.SetArgs([]string{"--namespaces", "default"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 3, bytes.Count(output, []byte("\n")))
 	})
@@ -25,17 +25,17 @@ func Test_GetEvents_Namespace(t *testing.T) {
 	t.Run("FilterAll", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--namespace", "doesnotexist"})
+		cmd.SetArgs([]string{"--namespaces", "doesnotexist"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 0, bytes.Count(output, []byte("\n")))
 	})
 }
 
-func Test_GetEvents_Pod(t *testing.T) {
+func Test_GetEvents_Pods(t *testing.T) {
 	t.Run("FilterTie", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--pod", "tiefighter"})
+		cmd.SetArgs([]string{"--pods", "tiefighter"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 1, bytes.Count(output, []byte("\n")))
 	})
@@ -43,17 +43,17 @@ func Test_GetEvents_Pod(t *testing.T) {
 	t.Run("FilterDeathstart", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--pod", "deathstar"})
+		cmd.SetArgs([]string{"--pods", "deathstar"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 2, bytes.Count(output, []byte("\n")))
 	})
 }
 
-func Test_GetEvents_Process(t *testing.T) {
+func Test_GetEvents_Processes(t *testing.T) {
 	t.Run("FilterNetserver", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--process", "netserver"})
+		cmd.SetArgs([]string{"--processes", "netserver"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 1, bytes.Count(output, []byte("\n")))
 	})
@@ -61,7 +61,7 @@ func Test_GetEvents_Process(t *testing.T) {
 	t.Run("FilterDocker", func(t *testing.T) {
 		testutils.MockPipedFile(t, testutils.RepoRootPath("testdata/events.json"))
 		cmd := New()
-		cmd.SetArgs([]string{"--process", "docker"})
+		cmd.SetArgs([]string{"--processes", "docker"})
 		output := testutils.RedirectStdoutExecuteCmd(t, cmd)
 		assert.Equal(t, 2, bytes.Count(output, []byte("\n")))
 	})
