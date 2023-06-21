af2ddf26441f67e84551b60fcb77e764b5f5be47
Jiri Olsa <jolsa@kernel.org>
2023-06-21 10:44:15 +0000

    tetragon: Add bpf_killer bpf program
    Adding bpf_killer bpf program that allows to (when attached to syscall)
    override syscall or kill current process.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ bpf/process/bpf_killer.c
@@ -0,0 +1,28 @@
+#include "bpf_killer.h"
+
+char _license[] __attribute__((section("license"), used)) = "GPL";
+
+#ifdef __MULTI_KPROBE
+#define MAIN "kprobe.multi/killer"
+#else
+#define MAIN "kprobe/killer"
+#endif
+
+__attribute__((section(MAIN), used)) int
+killer(void *ctx)
+{
+	__u64 id = get_current_pid_tgid();
+	struct killer_data *data;
+
+	data = map_lookup_elem(&killer_data, &id);
+	if (!data)
+		return 0;
+
+	if (data->error)
+		override_return(ctx, data->error);
+	if (data->signal)
+		send_signal(data->signal);
+
+	map_delete_elem(&killer_data, &id);
+	return 0;
+}
