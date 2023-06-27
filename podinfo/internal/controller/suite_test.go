cca900809aee16fff1a087decdce90bd3cbe5fba
Prateek Singh <prateeksingh9741@gmail.com>
2023-06-27 15:12:26 +0530

    Cleared the file since the tests will be written using testify, and not ginkgo
    Signed-off-by: Prateek Singh <prateeksingh9741@gmail.com>

--- podinfo/internal/controller/suite_test.go
+++ podinfo/internal/controller/suite_test.go
@@ -16,62 +16,64 @@ limitations under the License.
 
 package controller
 
-import (
-	"path/filepath"
-	"testing"
+// Write tests using Testify instad of Ginkgo
 
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+// import (
+// 	"path/filepath"
+// 	"testing"
 
-	ciliumiov1alpha1 "github.com/cilium/tetragon/podinfo/api/v1alpha1"
-	//+kubebuilder:scaffold:imports
-)
+// 	"k8s.io/client-go/kubernetes/scheme"
+// 	"k8s.io/client-go/rest"
+// 	"sigs.k8s.io/controller-runtime/pkg/client"
+// 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+// 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+// 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+// 	ciliumiov1alpha1 "github.com/cilium/tetragon/podinfo/api/v1alpha1"
+// 	//+kubebuilder:scaffold:imports
+// )
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+// // These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-func TestAPIs(t *testing.T) {
-	RegisterFailHandler(Fail)
+// var cfg *rest.Config
+// var k8sClient client.Client
+// var testEnv *envtest.Environment
 
-	RunSpecs(t, "Controller Suite")
-}
+// func TestAPIs(t *testing.T) {
+// 	RegisterFailHandler(Fail)
 
-var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+// 	RunSpecs(t, "Controller Suite")
+// }
 
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: true,
-	}
+// var _ = BeforeSuite(func() {
+// 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-	var err error
-	// cfg is defined in this file globally.
-	cfg, err = testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+// 	By("bootstrapping test environment")
+// 	testEnv = &envtest.Environment{
+// 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+// 		ErrorIfCRDPathMissing: true,
+// 	}
 
-	err = ciliumiov1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+// 	var err error
+// 	// cfg is defined in this file globally.
+// 	cfg, err = testEnv.Start()
+// 	Expect(err).NotTo(HaveOccurred())
+// 	Expect(cfg).NotTo(BeNil())
 
-	//+kubebuilder:scaffold:scheme
+// 	err = ciliumiov1alpha1.AddToScheme(scheme.Scheme)
+// 	Expect(err).NotTo(HaveOccurred())
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
+// 	//+kubebuilder:scaffold:scheme
 
-})
+// 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+// 	Expect(err).NotTo(HaveOccurred())
+// 	Expect(k8sClient).NotTo(BeNil())
 
-var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
-})
+// })
+
+// var _ = AfterSuite(func() {
+// 	By("tearing down the test environment")
+// 	err := testEnv.Stop()
+// 	Expect(err).NotTo(HaveOccurred())
+// })
