cilium/tetragon

commit 732087036a7180d1647ec5350c7f624e4ed9d36c

tetragon: Allow to override tracepoint argument type
The syscall enter/exit tracepoints exports all arguments as 8 bytes
values (unsigned long). This might lead to problems when interpreting
4 byte integer like with unsigned int for 'fd' values.

Adding a way to force type for given argument with 'Type' field and
support to decode [u]int32 types.

This works on little endian machines only (x86/arm), because we have
8 bytes number and we access its first 4 bytes.

arg with 4 bytes value 0x12345678 is stored in 8 bytes as:

  0: 78 56 34 12 00 00 00 00

and in 4 bytes as:

  0: 78 56 34 12

With big endian we'd need to shift the offset as well and it'd be
more difficult to have generic solution.

Signed-off-by: Jiri Olsa <jolsa@kernel.org>

pkg/sensors/tracing/generictracepoint.go
@@ -110,6 +110,9 @@ type genericTracepointArg struct {
 
 	// bpf generic type
 	genericTypeId int
+
+	// user type overload
+	userType string
 }
 
 // tracepointTable is, for now, an array.
@@ -167,6 +170,10 @@ func (out *genericTracepointArg) setGenericTypeId() (int, error) {
 // getGenericTypeId: returns the generic type Id of a tracepoint argument
 // if such an id cannot be termined, it returns an GenericInvalidType and an error
 func (out *genericTracepointArg) getGenericTypeId() (int, error) {
+
+	if out.userType != "" {
+		return gt.GenericTypeFromString(out.userType), nil
+	}
 
 	if out.format == nil {
 		return gt.GenericInvalidType, errors.New("format is nil")
@@ -245,6 +252,7 @@ func buildGenericTracepointArgs(info *tracepoint.Tracepoint, specArgs []v1alpha1
 			nopTy:         false,
 			format:        &field,
 			genericTypeId: gt.GenericInvalidType,
+			userType:      specArg.Type,
 		})
 	}
 
@@ -634,6 +642,22 @@ func handleGenericTracepoint(r *bytes.Reader) ([]observer.Event, error) {
 
 		case gt.GenericS64Type:
 			var val int64
+			err := binary.Read(r, binary.LittleEndian, &val)
+			if err != nil {
+				logger.GetLogger().WithError(err).Warnf("Size type error sizeof %d", m.Common.Size)
+			}
+			unix.Args = append(unix.Args, val)
+
+		case gt.GenericU32Type:
+			var val uint32
+			err := binary.Read(r, binary.LittleEndian, &val)
+			if err != nil {
+				logger.GetLogger().WithError(err).Warnf("Size type error sizeof %d", m.Common.Size)
+			}
+			unix.Args = append(unix.Args, val)
+
+		case gt.GenericS32Type:
+			var val int32
 			err := binary.Read(r, binary.LittleEndian, &val)
 			if err != nil {
 				logger.GetLogger().WithError(err).Warnf("Size type error sizeof %d", m.Common.Size)

pkg/grpc/tracing/tracing.go
@@ -293,6 +293,14 @@ func (msg *MsgGenericTracepointUnix) HandleMessage() *tetragon.GetEventsResponse
 			tetragonArgs = append(tetragonArgs, &tetragon.KprobeArgument{Arg: &tetragon.KprobeArgument_LongArg{
 				LongArg: v,
 			}})
+		case uint32:
+			tetragonArgs = append(tetragonArgs, &tetragon.KprobeArgument{Arg: &tetragon.KprobeArgument_UintArg{
+				UintArg: v,
+			}})
+		case int32:
+			tetragonArgs = append(tetragonArgs, &tetragon.KprobeArgument{Arg: &tetragon.KprobeArgument_IntArg{
+				IntArg: v,
+			}})
 		case string:
 			tetragonArgs = append(tetragonArgs, &tetragon.KprobeArgument{Arg: &tetragon.KprobeArgument_StringArg{
 				StringArg: v,

pkg/sensors/tracing/tracepoint_test.go
@@ -57,8 +57,8 @@ func TestGenericTracepointSimple(t *testing.T) {
 		Subsystem: "syscalls",
 		Event:     "sys_enter_lseek",
 		Args: []v1alpha1.KProbeArg{
-			{Index: 7}, /* whence */
-			{Index: 5}, /* fd */
+			{Index: 7},                 /* whence */
+			{Index: 5, Type: "sint32"}, /* fd */
 		},
 	}
 
@@ -83,7 +83,7 @@ func TestGenericTracepointSimple(t *testing.T) {
 			WithOperator(lc.Ordered).
 			WithValues(
 				ec.NewKprobeArgumentChecker().WithSizeArg(uint64(whenceBogusValue)),
-				ec.NewKprobeArgumentChecker().WithSizeArg(fdBogusValue), // -1
+				ec.NewKprobeArgumentChecker().WithIntArg(int32(fdBogusValue)), // -1
 			))
 	checker := ec.NewUnorderedEventChecker(tpChecker)
 
@@ -202,7 +202,7 @@ func TestGenericTracepointPidFilterLseek(t *testing.T) {
 }
 
 func TestGenericTracepointArgFilterLseek(t *testing.T) {
-	fd_u := uint64(100)
+	fd_u := int32(100)
 	fd := 100
 	whence_u := uint64(whenceBogusValue)
 	whenceStr := fmt.Sprintf("%d", whenceBogusValue)
@@ -216,7 +216,7 @@ func TestGenericTracepointArgFilterLseek(t *testing.T) {
 				Index: 7, /* whence */
 			},
 			{
-				Index: 5, /* fd */
+				Index: 5, Type: "sint32", /* fd */
 			},
 		},
 		Selectors: []v1alpha1.KProbeSelector{
@@ -250,11 +250,11 @@ func TestGenericTracepointArgFilterLseek(t *testing.T) {
 		if xwhence != whence_u {
 			return fmt.Errorf("unexpected arg val. got:%d expecting:%d", xwhence, whence)
 		}
-		arg1, ok := event.Args[1].GetArg().(*tetragon.KprobeArgument_SizeArg)
+		arg1, ok := event.Args[1].GetArg().(*tetragon.KprobeArgument_IntArg)
 		if !ok {
 			return fmt.Errorf("unexpected first arg: %s", event.Args[1])
 		}
-		xfd := arg1.SizeArg
+		xfd := arg1.IntArg
 		if xfd != fd_u {
 			return fmt.Errorf("unexpected arg val. got:%d expecting:%d", xfd, fd)
 		}
@@ -497,8 +497,8 @@ func TestTracepointCloneThreads(t *testing.T) {
 		Subsystem: "syscalls",
 		Event:     "sys_enter_lseek",
 		Args: []v1alpha1.KProbeArg{
-			{Index: 7}, /* whence */
-			{Index: 5}, /* fd */
+			{Index: 7},                 /* whence */
+			{Index: 5, Type: "sint32"}, /* fd */
 		},
 	}
 
@@ -564,7 +564,7 @@ func TestTracepointCloneThreads(t *testing.T) {
 			WithOperator(lc.Ordered).
 			WithValues(
 				ec.NewKprobeArgumentChecker().WithSizeArg(uint64(whenceBogusValue)),
-				ec.NewKprobeArgumentChecker().WithSizeArg(uint64(uint32(fdBogusValue))), // -1
+				ec.NewKprobeArgumentChecker().WithIntArg(int32(fdBogusValue)), // -1
 			)).WithProcess(child1Checker).WithParent(parentCheck)
 
 	thread1Checker := ec.NewProcessChecker().
@@ -579,7 +579,7 @@ func TestTracepointCloneThreads(t *testing.T) {
 			WithOperator(lc.Ordered).
 			WithValues(
 				ec.NewKprobeArgumentChecker().WithSizeArg(uint64(whenceBogusValue)),
-				ec.NewKprobeArgumentChecker().WithSizeArg(uint64(uint32(fdBogusValue))), // -1
+				ec.NewKprobeArgumentChecker().WithIntArg(int32(fdBogusValue)), // -1
 			)).WithProcess(thread1Checker).WithParent(parentCheck)
 
 	checker := ec.NewUnorderedEventChecker(execCheck, child1TpChecker, thread1TpChecker, exitCheck)

commit cb7f75ed610520a4de1d12c2a6b8a97b67d8c589

tetragon: do not allow users to set const_buf
732087036a7180d1647ec5350c7f624e4ed9d36c introduced override tracepoint
argument types by users.

The const_buf type, however, requires special information for its
decoding that is only set during automatic type detection.

This results in the agent panicking when this happens. Fix this by
disallowing const_buf being defined by users. In addition add a nil
check before the dereference to avoid panics in the same code in the
future if thigns change.

Fixes: 732087036a7180d1647ec5350c7f624e4ed9d36c

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/sensors/tracing/generictracepoint.go
@@ -172,6 +172,11 @@ func (out *genericTracepointArg) setGenericTypeId() (int, error) {
 func (out *genericTracepointArg) getGenericTypeId() (int, error) {
 
 	if out.userType != "" {
+		if out.userType == "const_buf" {
+			// const_buf type depends on the .format.field.Type to decode the result, so
+			// disallow it.
+			return gt.GenericInvalidType, errors.New("const_buf type cannot be user-defined")
+		}
 		return gt.GenericTypeFromString(out.userType), nil
 	}
 
@@ -681,6 +686,10 @@ func handleGenericTracepoint(r *bytes.Reader) ([]observer.Event, error) {
 			}
 
 		case gt.GenericConstBuffer:
+			if out.format == nil {
+				logger.GetLogger().Warn("GenericConstBuffer lacks format. Cannot decode argument")
+				break
+			}
 			if arrTy, ok := out.format.Field.Type.(tracepoint.ArrayTy); ok {
 				intTy, ok := arrTy.Ty.(tracepoint.IntTy)
 				if !ok {