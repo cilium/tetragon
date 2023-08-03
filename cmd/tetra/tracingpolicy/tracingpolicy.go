ccccc2dfa02aefa893e55c18a6630058d99e32ae
Jiri Olsa <jolsa@kernel.org>
2023-08-03 11:57:01 +0000

    tetra: Add avail-list tracingpolicy generate sub command
    Adding new avail-list sub command that allows to generate tracing
    policy with generated functions list.
    
    The list is based on ftrace available_filter_functions file and
    filtered out for regex pattern given in -r/--regex option, like:
    
      # ./tetra tracingpolicy generate avail-list -r ^ksys_
      apiVersion: cilium.io/v1alpha1
      kind: TracingPolicy
      metadata:
        name: "ftrace"
      spec:
        lists:
        - name: "ftrace"
          values:
          - "ksys_dup3"
          - "ksys_fadvise64_64"
          - "ksys_fallocate"
          - "ksys_fchown"
          - "ksys_ioperm"
          - "ksys_lseek"
          - "ksys_mmap_pgoff"
          - "ksys_msgctl.constprop.0"
          - "ksys_msgget"
          - "ksys_msgrcv"
          - "ksys_msgsnd"
          - "ksys_pread64"
          - "ksys_pwrite64"
          - "ksys_read"
          - "ksys_readahead"
          - "ksys_semctl.constprop.0"
          - "ksys_semget"
          - "ksys_semtimedop"
          - "ksys_setsid"
          - "ksys_shmctl.constprop.0"
          - "ksys_shmdt"
          - "ksys_shmget"
          - "ksys_sync"
          - "ksys_sync_file_range"
          - "ksys_sync_helper"
          - "ksys_unshare"
          - "ksys_write"
        kprobes:
          - call: "list:ftrace"
    
    Note it's possible to get all available functions generated,
    but it's not advisable to run such policy ;-)
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- cmd/tetra/tracingpolicy/tracingpolicy.go
+++ cmd/tetra/tracingpolicy/tracingpolicy.go
@@ -23,6 +23,7 @@ import (
 	"github.com/cilium/tetragon/api/v1/tetragon"
 	"github.com/cilium/tetragon/cmd/tetra/common"
 	"github.com/cilium/tetragon/pkg/btf"
+	"github.com/cilium/tetragon/pkg/ftrace"
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/spf13/cobra"
 )
@@ -76,17 +77,19 @@ func New() *cobra.Command {
 	tpListFlags.StringVarP(&tpListOutputFlag, common.KeyOutput, "o", "text", "Output format. text or json")
 
 	var tpGenerateMatchBinary string
+	var tpGenerateRegex string
 	tpGenerateCmd := &cobra.Command{
-		Use:   "generate <all-syscalls|all-syscalls-list|empty>",
+		Use:   "generate <all-syscalls|all-syscalls-list|ftrace-list|empty>",
 		Short: "generate tracing policies",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			generateTracingPolicy(args[0], tpGenerateMatchBinary)
+			generateTracingPolicy(args[0], tpGenerateMatchBinary, tpGenerateRegex)
 		},
 	}
 
 	tpGenerateFlags := tpGenerateCmd.Flags()
 	tpGenerateFlags.StringVarP(&tpGenerateMatchBinary, "match-binary", "m", "", "Add binary to matchBinaries selector")
+	tpGenerateFlags.StringVarP(&tpGenerateRegex, "regex", "r", "", "Use regex to limit the generated symbols")
 
 	tpCmd.AddCommand(tpAddCmd, tpDelCmd, tpListCmd, tpGenerateCmd)
 	return tpCmd
@@ -167,6 +170,43 @@ func generateAllSyscallsList(binary string) {
 	fmt.Printf("%s\n", crd)
 }
 
+func generateFtrace(binary, regex string) {
+	syms, err := ftrace.ReadAvailFuncs(regex)
+	if err != nil {
+		logger.GetLogger().WithError(err).Fatalf("failed to read ftrace functions: %s", err)
+	}
+
+	crd := `apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "ftrace"
+spec:
+  lists:
+  - name: "ftrace"
+    values:`
+
+	for idx := range syms {
+		crd = crd + "\n" + fmt.Sprintf("    - \"%s\"", syms[idx])
+	}
+
+	crd = crd + `
+  kprobes:
+    - call: "list:ftrace"`
+
+	if binary != "" {
+		filter := `
+    selectors:
+    - matchBinaries:
+      - operator: "In"
+        values:
+        - "` + binary + `"`
+
+		crd = crd + filter
+	}
+
+	fmt.Printf("%s\n", crd)
+}
+
 func generateEmpty() {
 	crd := `apiVersion: cilium.io/v1alpha1
 kind: TracingPolicy
@@ -176,12 +216,14 @@ metadata:
 	fmt.Printf("%s\n", crd)
 }
 
-func generateTracingPolicy(cmd, binary string) {
+func generateTracingPolicy(cmd, binary, regex string) {
 	switch cmd {
 	case "all-syscalls":
 		generateAllSyscalls(binary)
 	case "all-syscalls-list":
 		generateAllSyscallsList(binary)
+	case "ftrace-list":
+		generateFtrace(binary, regex)
 	case "empty":
 		generateEmpty()
 	}
