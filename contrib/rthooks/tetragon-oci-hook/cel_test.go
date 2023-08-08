8adb43f4a17f8426e2000d434ce30db2b29278e0
Kornilios Kourtis <kornilios@isovalent.com>
2023-08-08 10:32:53 +0200

    tetragon-oci-hook: add failure flags
    Currently, tetragon-oci-hook will not fail under any circumstances.
    This means that if there is an error (e.g., the hook was unable to
    contact the tetragon agent over gRPC), the container will be created
    normally.
    
    Hence, if the tetragon pod restarts, there might be containers that are
    started without setting up the necessary information for policy
    filtering (namespace and pod-label filters). This introduces a window,
    where policies will not be applied to workloads, which is what the
    tetragon-oci-hook aims to avoid.
    
    It is then desirable for the tetragon-oci-hook to fail and stop the
    containers from running if an error occurs. This, however, means that if
    the tetragon agent was restarted, it will also be stopped from starting
    by the hook. This can similarly affect other system containers.
    
    To address this, we use the OCI hook runtime annotations to distinghish
    between containers that need to fail, and containers that do not need to
    fail. By default, we use a key that corresponds to k8s namespace and we
    do not fail for containers in the kube-system namespace.
    
    We, also, introduce CLI options to make this configurable:
     -annotations-namespace-key: allows users to define the key for the k8s
      namespace in the annotations.
     -fail-allow-namespaces: allows to specify the namespaces which will not fail
     -fail-cel-expr: allows users to specify an arbitrary CEL expression to
      determine whether the hook will fail or not based on the annotations.
    
    Some simple tests for the CEL expressions are added as well.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ contrib/rthooks/tetragon-oci-hook/cel_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	CelAllowKubeSystem = `!("io.kubernetes.pod.namespace" in annotations) || annotations["io.kubernetes.pod.namespace"] != "kube-system"`
+)
+
+func celUserExprNoError(t *testing.T, expr string) *celProg {
+	ret, err := celUserExpr(expr)
+	require.NoError(t, err)
+	return ret
+}
+
+func celAllowNamespacesNoError(t *testing.T, vals []string) *celProg {
+	ret, err := celAllowNamespaces(vals)
+	require.NoError(t, err)
+	return ret
+}
+
+func TestCel(t *testing.T) {
+	type testCase struct {
+		prog        *celProg
+		expectedVal interface{}
+		annotations map[string]string
+	}
+	testCases := []testCase{
+		{prog: celUserExprNoError(t, "true"), expectedVal: true},
+		// expected value true: program will fail because annotations do not include namespace key
+		{prog: celAllowNamespacesNoError(t, []string{"kube-system"}), expectedVal: true},
+		// expected value true: program will fail because namespace is default
+		{prog: celAllowNamespacesNoError(t, []string{"kube-system"}), expectedVal: true, annotations: map[string]string{
+			"io.kubernetes.pod.namespace": "default",
+		}},
+		// expected value false: program will not fail because namespace is kube-system
+		{prog: celAllowNamespacesNoError(t, []string{"root", "kube-system"}), expectedVal: false, annotations: map[string]string{
+			"io.kubernetes.pod.namespace": "kube-system",
+		}},
+	}
+
+	for _, tc := range testCases {
+		ret, err := tc.prog.RunFailCheck(tc.annotations)
+		require.NoError(t, err)
+		require.Equal(t, tc.expectedVal, ret)
+	}
+}
