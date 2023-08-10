318c96f8f5255ae3d1b5ce38d0abcbb17223d2a5
Mahe Tardy <mahe.tardy@gmail.com>
2023-08-10 14:58:49 +0000

    tetra: version cmd default to CLI and add build info
    Version commands are usually expected to return quickly and can be used
    to see if the CLI is well installed and works, I think it's best to put
    the server version behind a flag. We could also adjust the timeout for
    this command in order to return quickly.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,4 +3,61 @@
 
 package version
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 var Version string
+
+type BuildInfo struct {
+	GoVersion string
+	Commit    string
+	Time      string
+	Modified  string
+}
+
+func ReadBuildInfo() *BuildInfo {
+	info := &BuildInfo{}
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok {
+		info.GoVersion = buildInfo.GoVersion
+		// unfortunately, it's not a Go map
+		for _, s := range buildInfo.Settings {
+			if s.Key == "vcs.revision" {
+				info.Commit = s.Value
+				continue
+			}
+			if s.Key == "vcs.time" {
+				info.Time = s.Value
+				continue
+			}
+			if s.Key == "vcs.modified" {
+				info.Modified = s.Value
+				continue
+			}
+		}
+	}
+	return info
+}
+
+func (info BuildInfo) Print() {
+	if info.GoVersion != "" {
+		fmt.Printf("GoVersion: %s\n", info.GoVersion)
+	}
+	if info.Time != "" {
+		fmt.Printf("Date: %s\n", info.Time)
+	}
+	if info.Commit != "" {
+		fmt.Printf("GitCommit: %s\n", info.Commit)
+	}
+	if info.Modified != "" {
+		var state string
+		if info.Modified == "true" {
+			state = "dirty"
+		} else {
+			state = "clean"
+		}
+		fmt.Printf("GitTreeState: %s\n", state)
+	}
+}
