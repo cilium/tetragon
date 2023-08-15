b8f9cc51995b9a7b0563cea3a853f8a22f5cafbe
Prateek Singh <prateeksingh9741@gmail.com>
2023-08-15 21:11:15 +0530

    Updated TetragonPod controller, update logic
    Signed-off-by: Prateek Singh <prateeksingh9741@gmail.com>

--- tetragonpod/controller/tetragonpod_controller.go
+++ tetragonpod/controller/tetragonpod_controller.go
@@ -23,6 +23,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -55,27 +56,150 @@ func (r *TetragonPodReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			r.deleteTetragonPod(ctx, req)
 			return ctrl.Result{}, nil
 		}
+		// Error fetching the pod.
+		l.Error(err, "unable to fetch Pod")
+		return ctrl.Result{}, err
 	}
 
 	// CASE 2: Pod is created or updated.
-	// wait until the pod is running for cleaner logs.
-	if pod.Status.Phase != corev1.PodRunning {
+	// wait until the necessary pod fields are available.
+	if checkIfReady(pod) == false {
 		return ctrl.Result{}, nil
 	}
 
-	// Check if corresponsind tetragonPod already exists.
-	tetragonPod, exists := r.checkIfAlreadyExists(ctx, req)
-	if exists {
-		// CASE 2.1: TetragonPod already exists, update it.
+	// The needed fields are ready, therefore create or update the corresponding tetragonPod resource.
+	// Check if corresponding tetragonPod already exists and has changed.
+	tetragonPod, existsAndChanged := r.checkExistsAndChanged(ctx, req, pod)
+	if existsAndChanged && tetragonPod != nil {
+		// CASE 2.1: TetragonPod already exists, and the pod hsa changed, update it.
 		l.Info("Pod Updated: ", "Name", req.Name, "Namespace", req.Namespace)
 		r.updateTetragonPod(ctx, pod, tetragonPod)
 		return ctrl.Result{}, nil
+	} else if tetragonPod == nil {
+		// pod not found
+		return ctrl.Result{}, nil
 	}
 
-	// CASE 2.2: TetragonPod does not exist, create new.
+	// CASE 3: TetragonPod does not exist, create new.
 	l.Info("Pod Created: ", "Name", req.Name, "Namespace", req.Namespace)
 	r.createTetragonPod(ctx, pod)
 	return ctrl.Result{}, nil
+}
+
+// checkExistsAndChanged checks if the corresponding tetragonPod resource already exists.
+func (r *TetragonPodReconciler) checkExistsAndChanged(ctx context.Context, req ctrl.Request, pod *corev1.Pod) (*ciliumiov1alpha1.TetragonPod, bool) {
+
+	// check if the tetragonPod already exists for that pod.
+	tetragonPod, err := r.getTetragonPod(ctx, req)
+	if err != nil {
+		return nil, false
+	}
+	// checkIfChanged returns true if the pod and tetragonPod do not match.
+	if checkIfChanged(pod, tetragonPod) {
+		return tetragonPod, true
+	}
+	return tetragonPod, false
+
+}
+
+// checkIfChanged returns true if the pod has changed.
+func checkIfChanged(pod *corev1.Pod, tetragonPod *ciliumiov1alpha1.TetragonPod) bool {
+	podStringFields := []string{
+		pod.Name,
+		pod.Namespace,
+		pod.Status.PodIP,
+	}
+
+	podNumFields := []int{
+		len(pod.Status.PodIPs),
+		len(pod.Labels),
+		len(pod.Annotations),
+	}
+
+	tetragonPodStringFields := []string{
+		tetragonPod.Name,
+		tetragonPod.Namespace,
+		tetragonPod.Status.PodIP,
+	}
+
+	tetragonPodNumFields := []int{
+		len(tetragonPod.Status.PodIPs),
+		len(tetragonPod.Labels),
+		len(tetragonPod.Annotations),
+	}
+
+	// check string fields
+	for index := range podStringFields {
+		if podStringFields[index] != tetragonPodStringFields[index] {
+			return true
+		}
+	}
+
+	// check num fields
+	for index := range podNumFields {
+		if podNumFields[index] != tetragonPodNumFields[index] {
+			return true
+		}
+	}
+
+	// check UID field
+	if pod.UID == types.UID("") {
+		return true
+	}
+
+	// check owner reference
+	if ownerReferenceChanged(pod, tetragonPod) {
+		return true
+	}
+
+	return false
+}
+
+// ownerReferenceChanged returns true if the owner of the tetragonPod has changed.
+func ownerReferenceChanged(pod *corev1.Pod, tetragonPod *ciliumiov1alpha1.TetragonPod) bool {
+	ownerReference := tetragonPod.OwnerReferences[0]
+	if ownerReference.Name != pod.Name || ownerReference.UID != pod.UID || ownerReference.Kind != pod.Kind || ownerReference.APIVersion != pod.APIVersion {
+		return true
+	}
+	return false
+}
+
+// checkIfReady checks if the necessary pod fields are available.
+func checkIfReady(pod *corev1.Pod) bool {
+	requiredStringFields := []string{
+		pod.Name,
+		pod.Namespace,
+		pod.Status.PodIP,
+		pod.APIVersion,
+		pod.Kind,
+	}
+
+	requiredNumFields := []int{
+		len(pod.Status.PodIPs),
+		len(pod.Labels),
+		len(pod.Annotations),
+	}
+
+	// check string fields
+	for _, field := range requiredStringFields {
+		if field == "" {
+			return false
+		}
+	}
+
+	// check num fields
+	for _, field := range requiredNumFields {
+		if field == 0 {
+			return false
+		}
+	}
+
+	// check UID field
+	if pod.UID == types.UID("") {
+		return false
+	}
+
+	return true
 }
 
 // deleteTetragonPod deletes the corresponding tetragonPod resource.
@@ -113,11 +237,23 @@ func (r *TetragonPodReconciler) updateTetragonPod(ctx context.Context, pod *core
 	l := log.FromContext(ctx)
 	l.Info("Updating the corresponding tetragonPod resource")
 
-	// get the new IP address of the Pod.
-	newIP := ciliumiov1alpha1.PodIP{IP: pod.Status.PodIP}
-	tetragonPod.Status.PodIP = newIP.IP
-	// add the new IP to the beginning of the array.
-	tetragonPod.Status.PodIPs = append([]ciliumiov1alpha1.PodIP{newIP}, tetragonPod.Status.PodIPs...)
+	// get the updated fields to be mapped to the tetragonPod resource.
+	updatedIP := ciliumiov1alpha1.PodIP{IP: pod.Status.PodIP}
+	updatedPodIPs := append([]ciliumiov1alpha1.PodIP{updatedIP}, tetragonPod.Status.PodIPs...)
+	updatedOwnerReference := metav1.OwnerReference{
+		Name:       pod.Name,
+		UID:        pod.UID,
+		Kind:       pod.Kind,
+		APIVersion: pod.APIVersion,
+	}
+
+	// update the fields of the tetragonPod resource.
+	tetragonPod.Name = pod.Name
+	tetragonPod.Labels = pod.Labels
+	tetragonPod.Annotations = pod.Annotations
+	tetragonPod.Status.PodIP = updatedIP.IP
+	tetragonPod.Status.PodIPs = updatedPodIPs
+	tetragonPod.OwnerReferences = []metav1.OwnerReference{updatedOwnerReference}
 
 	// Update the tetragonPod resource.
 	if err := r.Update(ctx, tetragonPod); err != nil {
@@ -126,17 +262,6 @@ func (r *TetragonPodReconciler) updateTetragonPod(ctx context.Context, pod *core
 	}
 	l.Info("TetragonPod updated successfully")
 	return ctrl.Result{}, nil
-}
-
-// checkIfAlreadyExists checks if the corresponding tetragonPod resource already exists.
-func (r *TetragonPodReconciler) checkIfAlreadyExists(ctx context.Context, req ctrl.Request) (*ciliumiov1alpha1.TetragonPod, bool) {
-
-	// check if the tetragonPod already exists for that pod.
-	tetragonPod, err := r.getTetragonPod(ctx, req)
-	if err != nil {
-		return nil, false
-	}
-	return tetragonPod, true
 }
 
 // createTetragonPod creates a tetragonPod resource.
@@ -199,9 +324,12 @@ func (r *TetragonPodReconciler) getTetragonPod(ctx context.Context, req ctrl.Req
 	tetragonPod := &ciliumiov1alpha1.TetragonPod{}
 	if err := r.Get(ctx, req.NamespacedName, tetragonPod); err != nil {
 		if client.IgnoreNotFound(err) == nil {
-			l.Info("Unable to fetch the tetragonPod: Name=", req.Name, " Namespace=", req.Namespace)
+			l.Info("TetragonPod does not exist: Name=", req.Name, " Namespace=", req.Namespace)
 			return nil, err
 		}
+
+		l.Error(err, "error getting TetragonPod")
+		return nil, err
 	}
 	return tetragonPod, nil
 }
