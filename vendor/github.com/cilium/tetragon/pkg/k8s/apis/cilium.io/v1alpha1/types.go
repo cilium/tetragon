7ab61718cb4d1212e03c6a209a9c9a476278033e
Prateek Singh <prateeksingh9741@gmail.com>
2023-08-18 21:32:26 +0530

    Add logic to register pod Info CRD
    Currently tetragon uses cilium Endoints to get Pod Labels. Pod Info CRD
    is a mapping between Pod IPs and Pod metadata therefore, instead of
    using cilium endpoints, it can use the pod info CRs, hence removing the
    dependency on cilium.
    
    This commit:
    - Added flag to skip PodInfo CRD in the operator, tetragon will use
      endpoints in this case.
    - Integrated  logic to register the PodInfo CRD with the API server,
      similar to tracing Policies CRD.
    
    Signed-off-by: Prateek Singh <prateeksingh9741@gmail.com>
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- vendor/github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1/types.go
+++ vendor/github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1/types.go
@@ -3,6 +3,24 @@
 
 package v1alpha1
 
+import (
+	ciliumio "github.com/cilium/tetragon/pkg/k8s/apis/cilium.io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// PodInfo (PI) is the custom resource that stores pod related information
+
+	// PIPluralName is the plural name of Tetragon Pod Info
+	PIPluralName = "podinfo"
+
+	// PIKindDefinition is the Kind name of the Tetragon Pod Info
+	PIKindDefinition = "PodInfo"
+
+	// PIName is the full name of the Tetragon Pod Info
+	PIName = PIPluralName + "." + ciliumio.GroupName
+)
+
 type KProbeSpec struct {
 	// Name of the function to apply the kprobe spec to.
 	Call string `json:"call"`
@@ -231,4 +249,48 @@ type ListSpec struct {
 	// +kubebuilder:validation:Optional
 	// Pattern for 'generated' lists.
 	Pattern *string `json:"pattern,omitempty"`
+}
+
+type PodInfoSpec struct {
+	// Host networking requested for this pod. Use the host's network namespace.
+	// If this option is set, the ports that will be used must be specified.
+	HostNetwork bool `json:"hostNetwork,omitempty"`
+}
+
+type PodInfoStatus struct {
+	// IP address allocated to the pod. Routable at least within the cluster.
+	// Empty if not yet allocated.
+	PodIP string `json:"podIP,omitempty"`
+
+	// List of Ip addresses allocated to the pod. 0th entry must be same as PodIP.
+	PodIPs []PodIP `json:"podIPs,omitempty"`
+}
+
+type PodIP struct {
+	// IP is an IP address (IPv4 or IPv6) assigned to the pod
+	IP string `json:"IP,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:singular="podinfo",path="podinfo",scope="Namespaced",shortName={}
+
+// PodInfo is the Scheme for the Podinfo API
+type PodInfo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PodInfoSpec   `json:"spec,omitempty"`
+	Status PodInfoStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PodInfoList contains a list of Podinfo
+type PodInfoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PodInfo `json:"items"`
 }
