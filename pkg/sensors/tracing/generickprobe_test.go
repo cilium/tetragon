e637af2376a8a75c345281fbb8606703cfb1a059
Mahe Tardy <mahe.tardy@gmail.com>
2023-08-24 17:52:43 +0000

    pkg/sensors: test that table cleanup is done on sensor unload
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/sensors/tracing/generickprobe_test.go
+++ pkg/sensors/tracing/generickprobe_test.go
@@ -6,6 +6,9 @@ package tracing
 import (
 	"bytes"
 	"testing"
+
+	"github.com/cilium/tetragon/pkg/idtable"
+	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 )
 
 func Fuzz_parseString(f *testing.F) {
@@ -49,4 +52,55 @@ func Test_parseString(t *testing.T) {
 			t.Errorf("got %q, want %q", out, "pizza")
 		}
 	})
+}
+
+func Test_SensorPostUnloadHook(t *testing.T) {
+	if genericKprobeTable.Len() != 0 {
+		t.Errorf("genericKprobeTable expected initial length: 0, got: %d", genericKprobeTable.Len())
+	}
+
+	// we use createGenericKprobeSensor because it's where the PostUnloadHook is
+	// created. It would be technically more correct if it was added just after
+	// insertion in the table in AddKprobe, but this is done by the caller to
+	// have just PostUnloadHook that regroups all the potential multiple kprobes
+	// contained in one sensor.
+	sensor, err := createGenericKprobeSensor("test_sensor", []v1alpha1.KProbeSpec{
+		{
+			Call:    "test_symbol",
+			Syscall: false,
+		},
+	}, 0, "test_policy", nil)
+	if err != nil {
+		t.Errorf("createGenericKprobeSensor err expected: nil, got: %s", err)
+	}
+
+	if genericKprobeTable.Len() != 1 {
+		t.Errorf("genericKprobeTable expected length: 1, got: %d", genericKprobeTable.Len())
+	}
+
+	// the return sensor entry does not expose anything publicly so we cannot
+	// check anything more than the length of the table, hopefully GetEntry on a
+	// non-existing ID will return an error
+	_, err = genericKprobeTable.GetEntry(idtable.EntryID{ID: 0})
+	if err != nil {
+		t.Errorf("genericKprobeTable.GetEntry err expected: nil, got: %s", err)
+	}
+
+	// For testing purposes, we simulate that the sensor was loaded, because
+	// otherwise, Unload will immediately exit when seeing that this sensor was
+	// never loaded
+	sensor.Loaded = true
+
+	// Unload should call the PostUnloadHook that was set in
+	// createGenericKprobeSensor and do the cleanup
+	err = sensor.Unload()
+	if err != nil {
+		t.Errorf("sensor.Unload err expected: nil, got: %s", err)
+	}
+
+	// Table implem detail: the entry still technically exists in the table but
+	// is invalid, thus is not taken into account in the length
+	if genericKprobeTable.Len() != 0 {
+		t.Errorf("genericKprobeTable expected length after cleanup: 0, got: %d", genericKprobeTable.Len())
+	}
 }
