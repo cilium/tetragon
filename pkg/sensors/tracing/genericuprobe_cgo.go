b387c468e60c33ce1e04cd82db5501be999035ba
Mahe Tardy <mahe.tardy@gmail.com>
2023-09-28 10:42:16 +0000

    pkg/sensors: uprobe test fix using dyn export
    Go 1.21 broke dynamic symbol linking export, see more
    - https://github.com/golang/go/issues/62520
    - https://github.com/golang/go/issues/62520#issuecomment-1712181946
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/sensors/tracing/genericuprobe_cgo.go
+++ pkg/sensors/tracing/genericuprobe_cgo.go
@@ -6,6 +6,14 @@
 
 package tracing
 
+// Go 1.21 broke dynamic symbol linking export, see more
+// https://github.com/golang/go/issues/62520
+// https://github.com/golang/go/issues/62520#issuecomment-1712181946
+// For unknown reasons --export-dynamic-symbol=uprobe_test_func doesn't work
+// with CGO comment, maybe for security reasons, see
+// https://pkg.go.dev/cmd/cgo#hdr-Using_cgo_with_the_go_command
+
+// #cgo LDFLAGS: '-Wl,--export-dynamic'
 /*
 int uprobe_test_func(void)
 {
