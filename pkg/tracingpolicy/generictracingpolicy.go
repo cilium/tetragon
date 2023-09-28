7375c2090373bec6057cd26da6b2746b6264594b
Mahe Tardy <mahe.tardy@gmail.com>
2023-09-28 09:13:45 +0000

    pkg/tracingpolicy: apply CRD defaults to policies
    Before we had a discrepancy between using custom resources Tracing
    Policies on k8s and in standalone mode. If you went via the API server,
    the validation and default was applied but going straight via gRPC or
    --tracing-policy flag allowed to circumvent validation and most
    importantly defaulting. Creating potentially different behavior loading
    on k8s and in standalone.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/tracingpolicy/generictracingpolicy.go
+++ pkg/tracingpolicy/generictracingpolicy.go
@@ -15,15 +15,20 @@ import (
 
 	ext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apischema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
+	structuraldefaulting "k8s.io/apiextensions-apiserver/pkg/apiserver/schema/defaulting"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
 	apivalidation "k8s.io/apimachinery/pkg/api/validation"
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/kube-openapi/pkg/validation/validate"
 	"sigs.k8s.io/yaml"
 )
 
+// validatorState is used by the CRD validation process to store the validators
+// structures.
 var validatorState = struct {
 	validators map[schema.GroupVersionKind]*validate.SchemaValidator
 	init       sync.Once
@@ -32,6 +37,89 @@ var validatorState = struct {
 	validators: make(map[schema.GroupVersionKind]*validate.SchemaValidator),
 }
 
+// defaultState is used by to store the structural schemas apply the defaults in
+// the custom resources.
+var defaultState = struct {
+	structuralSchemaTP  *apischema.Structural
+	structuralSchemaTPN *apischema.Structural
+	init                sync.Once
+	initError           error
+}{}
+
+// ApplyCRDDefault uses internal k8s api server machinery and can only process
+// unustructured objects (unfortunately, since it requires to unmarshal and
+// marshal)
+func ApplyCRDDefault(rawPolicy []byte) ([]byte, error) {
+	defaultState.init.Do(func() {
+		// retrieve CRD
+		customTP := client.GetPregeneratedCRD(v1alpha1.TPCRDName)
+		var crvInternalTP ext.CustomResourceDefinition
+		err := extv1.Convert_v1_CustomResourceDefinition_To_apiextensions_CustomResourceDefinition(
+			&customTP,
+			&crvInternalTP,
+			nil,
+		)
+		if err != nil {
+			defaultState.initError = fmt.Errorf("failed to convert TracingPolicy CRD: %w", err)
+			return
+		}
+
+		customTPN := client.GetPregeneratedCRD(v1alpha1.TPNamespacedCRDName)
+		var crvInternalTPN ext.CustomResourceDefinition
+		err = extv1.Convert_v1_CustomResourceDefinition_To_apiextensions_CustomResourceDefinition(
+			&customTPN,
+			&crvInternalTPN,
+			nil,
+		)
+		if err != nil {
+			defaultState.initError = fmt.Errorf("failed to convert TracingPolicyNamespaced CRD: %w", err)
+			return
+		}
+
+		// create a structural schema from the CRD
+		defaultState.structuralSchemaTP, err = apischema.NewStructural(crvInternalTP.Spec.Validation.OpenAPIV3Schema)
+		if err != nil {
+			defaultState.initError = fmt.Errorf("failed to initialize structural for TracingPolicy: %w", err)
+			return
+		}
+		defaultState.structuralSchemaTPN, err = apischema.NewStructural(crvInternalTPN.Spec.Validation.OpenAPIV3Schema)
+		if err != nil {
+			defaultState.initError = fmt.Errorf("failed to initialize structural for TracingPolicyNamespaced: %w", err)
+			return
+		}
+	})
+
+	if defaultState.initError != nil {
+		return nil, fmt.Errorf("failed to initialize default structural schemas: %w", validatorState.initError)
+	}
+
+	// unmarshall into an unstructured object
+	var policyUnstr unstructured.Unstructured
+	err := yaml.UnmarshalStrict(rawPolicy, &policyUnstr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshall policy: %v", err)
+	}
+
+	// apply defaults
+	switch policyUnstr.GetKind() {
+	case v1alpha1.TPKindDefinition:
+		structuraldefaulting.Default(policyUnstr.Object, defaultState.structuralSchemaTP)
+	case v1alpha1.TPNamespacedKindDefinition:
+		structuraldefaulting.Default(policyUnstr.Object, defaultState.structuralSchemaTPN)
+	}
+
+	// marshal defaulted unstructured object into json
+	data, err := policyUnstr.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal defaulted object: %w", err)
+	}
+
+	return data, nil
+
+}
+
+// ValidateCRD validates the metadata of the objects (name, labels,
+// annotations...) and the specification using the custom CRD schemas.
 func ValidateCRD(policy GenericTracingPolicy) (*validate.Result, error) {
 	metaErrors := ValidateCRDMeta(policy)
 
@@ -121,19 +209,19 @@ func (gtp *GenericTracingPolicy) TpInfo() string {
 	return gtp.Metadata.Name
 }
 
-func (gtp GenericTracingPolicy) Namespaced() bool {
-	return gtp.APIVersion == v1alpha1.TPNamespacedKindDefinition
-}
-
 func FromYAML(data string) (TracingPolicy, error) {
-	var k GenericTracingPolicy
-
-	err := yaml.UnmarshalStrict([]byte(data), &k)
+	rawPolicy, err := ApplyCRDDefault([]byte(data))
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling the policy: %w", err)
+		return nil, fmt.Errorf("error applying CRD defaults: %w", err)
 	}
 
-	validationResult, err := ValidateCRD(k)
+	var policy GenericTracingPolicy
+	err = yaml.UnmarshalStrict(rawPolicy, &policy)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal object with defaults: %w", err)
+	}
+
+	validationResult, err := ValidateCRD(policy)
 	if err != nil {
 		return nil, err
 	}
@@ -141,24 +229,23 @@ func FromYAML(data string) (TracingPolicy, error) {
 	// display all validation errors and warnings before maybe returning a validation error
 	for _, e := range validationResult.Errors {
 		logger.GetLogger().WithFields(logrus.Fields{
-			"kind":    k.Kind,
-			"version": k.APIVersion,
-			"name":    k.TpName(),
+			"kind":    policy.Kind,
+			"version": policy.APIVersion,
+			"name":    policy.TpName(),
 		}).WithError(e).Error("Validation error")
 	}
 	for _, e := range validationResult.Warnings {
 		logger.GetLogger().WithFields(logrus.Fields{
-			"kind":    k.Kind,
-			"version": k.APIVersion,
-			"name":    k.TpName(),
+			"kind":    policy.Kind,
+			"version": policy.APIVersion,
+			"name":    policy.TpName(),
 		}).WithError(e).Warn("Validation warning")
 	}
-	fmt.Println("HEY", k.Spec.KProbes[0].Syscall)
 	if len(validationResult.Errors) > 0 {
 		return nil, fmt.Errorf("validation failed: %w", validationResult.AsError())
 	}
 
-	return &k, nil
+	return &policy, nil
 }
 
 func FromFile(path string) (TracingPolicy, error) {
