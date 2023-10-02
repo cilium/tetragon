d2965c52cf856fee1e874c0c1aeae0b5ab6bbcff
Jiri Olsa <jolsa@kernel.org>
2023-10-02 13:20:20 -0400

    tetragon: Update kprobe/tracepoint event action definitions
    Catching up with kprobe/tracepoint event action enum definitions.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/api/tracingapi/client_kprobe.go
+++ pkg/api/tracingapi/client_kprobe.go
@@ -12,16 +12,19 @@ const (
 )
 
 const (
-	ActionPost       = 0
-	ActionFollowFd   = 1
-	ActionSigKill    = 2
-	ActionUnfollowFd = 3
-	ActionOverride   = 4
-	ActionCopyFd     = 5
-	ActionGetUrl     = 6
-	ActionLookupDns  = 7
-	ActionNoPost     = 8
-	ActionSignal     = 9
+	ActionPost         = 0
+	ActionFollowFd     = 1
+	ActionSigKill      = 2
+	ActionUnfollowFd   = 3
+	ActionOverride     = 4
+	ActionCopyFd       = 5
+	ActionGetUrl       = 6
+	ActionLookupDns    = 7
+	ActionNoPost       = 8
+	ActionSignal       = 9
+	ActionTrackSock    = 10
+	ActionUntrackSock  = 11
+	ActionNotifyKiller = 12
 )
 
 const (
