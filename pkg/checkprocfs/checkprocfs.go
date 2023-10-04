ea22c460c350b7f79c7adefceaf8a3a3cb7d2ba1
Kornilios Kourtis <kornilios@isovalent.com>
2023-10-04 10:52:18 +0200

    checkprocfs: check if proc is host proc
    Tetragon requires access to host /proc. In certain environments (e.g.,
    kind) this is not achieved by default. This patch adds a warning by
    comparing the inode number of the pid namespace in the provided /proc
    with the expected hard-coded value.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/checkprocfs/checkprocfs.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package checkprocfs
+
+import (
+	"path/filepath"
+	"syscall"
+
+	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/option"
+	"github.com/sirupsen/logrus"
+)
+
+// Check tries to determine whether the configured procfs is the host's procfs
+func Check() {
+
+	path := filepath.Join(option.Config.ProcFS, "1", "ns", "pid")
+	var stat syscall.Stat_t
+	if err := syscall.Stat(path, &stat); err != nil {
+		logger.GetLogger().WithField("path", path).WithError(err).Info("stat failed")
+	}
+
+	// we compare against the known inode of the host pid namespace:
+	// ...
+	// 	PROC_PID_INIT_INO	= 0xEFFFFFFCU,
+	// ...
+	expectedIno := uint64(0xEFFFFFFC)
+	if stat.Ino != expectedIno {
+		logger.GetLogger().WithFields(logrus.Fields{
+			"path":           path,
+			"inode":          stat.Ino,
+			"expected inode": expectedIno,
+		}).Warn("inode mismatch: procfs does not appear to be host procfs")
+	}
+}
