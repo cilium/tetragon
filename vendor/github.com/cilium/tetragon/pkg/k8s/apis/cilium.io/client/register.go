94477e791f1476573b2642a6fa39fd60658af7ae
Kornilios Kourtis <kornilios@isovalent.com>
2023-10-09 09:57:27 +0200

    pkg/k8s: refactor CRD handling
    This patch refactors CRD handling.
    
    It introduces a new package (crdutils) that includes a CRD type, meant
    to hold all the information for registering a CRD.
    
    Registration code is moved into crdutils, and the caller just needs to
    specify the CRD objects to register.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ vendor/github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/client/register.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package client
+
+import (
+	_ "embed"
+
+	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
+	crdutils "github.com/cilium/tetragon/pkg/k8s/crdutils"
+)
+
+var (
+	//go:embed crds/v1alpha1/cilium.io_tracingpolicies.yaml
+	crdsv1Alpha1TracingPolicies []byte
+
+	TracingPolicyCRD = crdutils.NewCRDBytes(
+		v1alpha1.TPCRDName,
+		v1alpha1.TPName,
+		crdsv1Alpha1TracingPolicies)
+
+	//go:embed crds/v1alpha1/cilium.io_tracingpoliciesnamespaced.yaml
+	crdsv1Alpha1TracingPoliciesNamespaced []byte
+
+	TracingPolicyNamespacedCRD = crdutils.NewCRDBytes(
+		v1alpha1.TPNamespacedCRDName,
+		v1alpha1.TPNamespacedName,
+		crdsv1Alpha1TracingPoliciesNamespaced)
+
+	//go:embed crds/v1alpha1/cilium.io_podinfo.yaml
+	crdsv1Alpha1PodInfo []byte
+
+	PodInfoCRD = crdutils.NewCRDBytes(
+		v1alpha1.PICRDName,
+		v1alpha1.PIName,
+		crdsv1Alpha1PodInfo)
+
+	AllCRDs = []crdutils.CRD{
+		TracingPolicyCRD,
+		TracingPolicyNamespacedCRD,
+		PodInfoCRD,
+	}
+)
