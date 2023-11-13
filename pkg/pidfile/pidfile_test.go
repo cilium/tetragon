0b88d6496c9563431f386c76815523f840ce7048
Djalal Harouni <tixxdz@gmail.com>
2023-11-13 17:43:33 +0100

    tests: pidfile tests
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ pkg/pidfile/pidfile_test.go
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+package pidfile
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/tetragon/pkg/defaults"
+)
+
+func TestCreatePidFile(t *testing.T) {
+	err := os.MkdirAll(defaults.DefaultRunDir, os.ModeDir)
+	require.NoError(t, err)
+
+	pid, err := Create()
+	require.NoError(t, err)
+	require.NotZero(t, pid)
+
+	pid1, err := readPidFile()
+	require.NoError(t, err)
+	require.NotZero(t, pid1)
+
+	require.Equal(t, pid, pid1)
+	err = Delete()
+	require.NoError(t, err)
+
+	pid1, err = readPidFile()
+	require.ErrorIs(t, err, ErrPidFileAccess)
+	require.Zero(t, pid1)
+}
