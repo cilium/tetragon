e2c76cc5b684acf51db244adbe36e34023a4e0e9
Jiri Olsa <jolsa@kernel.org>
2023-12-04 18:06:42 +0000

    tetragon: Add 32bit versions of killer tests
    Adding killer tests for 32bit syscall values.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/killer_amd64_test.go
+++ pkg/sensors/tracing/killer_amd64_test.go
@@ -1,65 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Tetragon
 
+//go:build amd64 && linux
+// +build amd64,linux
+
 package tracing
 
 import (
-	"context"
-	"os"
-	"os/exec"
-	"sync"
 	"syscall"
 	"testing"
 
 	"github.com/cilium/tetragon/api/v1/tetragon"
-	"github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker"
-	ec "github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker"
 	"github.com/cilium/tetragon/pkg/bpf"
-	"github.com/cilium/tetragon/pkg/jsonchecker"
-	lc "github.com/cilium/tetragon/pkg/matchers/listmatcher"
-	"github.com/cilium/tetragon/pkg/observer/observertesthelper"
+	"github.com/cilium/tetragon/pkg/syscallinfo/i386"
 	"github.com/cilium/tetragon/pkg/testutils"
-	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
-	"github.com/stretchr/testify/assert"
+
+	ec "github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker"
+	lc "github.com/cilium/tetragon/pkg/matchers/listmatcher"
 )
 
-func testKiller(t *testing.T, configHook string, test string,
-	checker *eventchecker.UnorderedEventChecker,
-	checkerFunc func(err error, rc int)) {
-
-	var doneWG, readyWG sync.WaitGroup
-	defer doneWG.Wait()
-
-	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
-	defer cancel()
-
-	err := os.WriteFile(testConfigFile, []byte(configHook), 0644)
-	if err != nil {
-		t.Fatalf("writeFile(%s): err %s", testConfigFile, err)
-	}
-
-	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
-	if err != nil {
-		t.Fatalf("GetDefaultObserverWithFile error: %s", err)
-	}
-	observertesthelper.LoopEvents(ctx, t, &doneWG, &readyWG, obs)
-	readyWG.Wait()
-
-	cmd := exec.Command(test)
-	err = cmd.Run()
-
-	checkerFunc(err, cmd.ProcessState.ExitCode())
-
-	err = jsonchecker.JsonTestCheck(t, checker)
-	assert.NoError(t, err)
-}
-
-func TestKillerOverride(t *testing.T) {
+func TestKillerOverride32(t *testing.T) {
 	if !bpf.HasOverrideHelper() {
 		t.Skip("skipping killer test, bpf_override_return helper not available")
 	}
 
-	test := testutils.RepoRootPath("contrib/tester-progs/killer-tester")
+	test := testutils.RepoRootPath("contrib/tester-progs/killer-tester-32")
 	configHook := `
 apiVersion: cilium.io/v1alpha1
 kind: TracingPolicy
@@ -70,7 +35,7 @@ spec:
   - name: "mine"
     type: "syscalls"
     values:
-    - "sys_prctl"
+    - "__ia32_sys_prctl"
   killers:
   - syscalls:
     - "list:mine"
@@ -99,7 +64,7 @@ spec:
 		WithArgs(ec.NewKprobeArgumentListMatcher().
 			WithOperator(lc.Ordered).
 			WithValues(
-				ec.NewKprobeArgumentChecker().WithSizeArg(syscall.SYS_PRCTL),
+				ec.NewKprobeArgumentChecker().WithSizeArg(i386.SYS_PRCTL),
 			)).
 		WithAction(tetragon.KprobeAction_KPROBE_ACTION_NOTIFYKILLER)
 
@@ -114,12 +79,12 @@ spec:
 	testKiller(t, configHook, test, checker, checkerFunc)
 }
 
-func TestKillerSignal(t *testing.T) {
+func TestKillerSignal32(t *testing.T) {
 	if !bpf.HasOverrideHelper() {
 		t.Skip("skipping killer test, bpf_override_return helper not available")
 	}
 
-	test := testutils.RepoRootPath("contrib/tester-progs/killer-tester")
+	test := testutils.RepoRootPath("contrib/tester-progs/killer-tester-32")
 	configHook := `
 apiVersion: cilium.io/v1alpha1
 kind: TracingPolicy
@@ -130,7 +95,7 @@ spec:
   - name: "mine"
     type: "syscalls"
     values:
-    - "sys_prctl"
+    - "__ia32_sys_prctl"
   killers:
   - syscalls:
     - "list:mine"
@@ -159,7 +124,7 @@ spec:
 		WithArgs(ec.NewKprobeArgumentListMatcher().
 			WithOperator(lc.Ordered).
 			WithValues(
-				ec.NewKprobeArgumentChecker().WithSizeArg(syscall.SYS_PRCTL),
+				ec.NewKprobeArgumentChecker().WithSizeArg(i386.SYS_PRCTL),
 			)).
 		WithAction(tetragon.KprobeAction_KPROBE_ACTION_NOTIFYKILLER)
 
@@ -172,50 +137,4 @@ spec:
 	}
 
 	testKiller(t, configHook, test, checker, checkerFunc)
-}
-
-func TestKillerMulti(t *testing.T) {
-	if !bpf.HasOverrideHelper() {
-		t.Skip("skipping killer test, bpf_override_return helper not available")
-	}
-
-	crd := `
-apiVersion: cilium.io/v1alpha1
-kind: TracingPolicy
-metadata:
-  name: "kill-syscalls"
-spec:
-  lists:
-  - name: "mine1"
-    type: "syscalls"
-    values:
-    - "sys_prctl"
-  - name: "mine2"
-    type: "syscalls"
-    values:
-    - "sys_prctl"
-  killers:
-  - syscalls:
-    - "list:mine1"
-  - syscalls:
-    - "list:mine2"
-  tracepoints:
-  - subsystem: "raw_syscalls"
-    event: "sys_enter"
-    args:
-    - index: 4
-      type: "syscall64"
-    selectors:
-    - matchArgs:
-      - index: 0
-        operator: "InMap"
-        values:
-        - "list:mine1"
-      matchActions:
-      - action: "NotifyKiller"
-        argSig: 9 # SIGKILL
-`
-
-	err := checkCrd(t, crd)
-	assert.Error(t, err)
 }
