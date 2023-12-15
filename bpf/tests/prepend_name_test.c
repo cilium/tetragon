a3aeff8fd2897943589b3da7154c5183a725430b
Mahe Tardy <mahe.tardy@gmail.com>
2023-12-15 16:23:44 +0000

    bpf: add unit tests for the prepend_name function
    It looked like this particular function presented some bugs:
    - It would not return -ENAMETOOLONG when the buffer was filled
      "best-effort" because the name was not fitting.
    - It would never fill the first byte of the buffer, thus reducing the
      maximum size written by one.
    - It would not fill "best-effort" correctly. E.g, previously in a buffer
      of 4, "pizza" would have been "/piz" (when ignoring previous bug)
      instead of "izza". Now it tries to write the "end" of the name without
      a wrong "/" char.
    - It would behave unexpectedly when filled with more data than size of
      buffer.
    
    BPF unit tests were put under bpf/tests, as more can be added there in
    the future. The Makefile was updated so that we can share variable
    definition between the bpf/Makefile and the bpf/tests/Makefile file.
    This separation allows us not compile the test program along the rest of
    the BPF progs, and not ship them in the released images/archives.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

---
+++ bpf/tests/prepend_name_test.c
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+//go:build ignore
+
+#include "vmlinux.h"
+
+#include "bpf_tracing.h" // bpf_printk
+
+#include "bpf_task.h"
+#include "process/bpf_process_event.h"
+
+char _license[] __attribute__((section("license"), used)) = "Dual BSD/GPL";
+
+#define TEST_MAX_BUF_LEN 256
+#define NAME_MAX	 255
+
+struct test_prepend_name_state_map_value {
+	char buf[TEST_MAX_BUF_LEN];
+	u64 buflen;
+	char dname[NAME_MAX];
+	char pad;
+	u32 dlen;
+	u32 offset;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, struct test_prepend_name_state_map_value);
+} test_prepend_name_state_map SEC(".maps");
+
+__attribute__((section("raw_tracepoint/test"), used)) int
+test_prepend_name()
+{
+	struct test_prepend_name_state_map_value *ts;
+	int zero = 0;
+
+	ts = map_lookup_elem(&test_prepend_name_state_map, &zero);
+	if (!ts)
+		return 1;
+
+	if (ts->buflen < 0 || ts->buflen > TEST_MAX_BUF_LEN)
+		return 2;
+
+	char *bufptr = ts->buf + ts->buflen;
+
+	ts->dlen &= 255;
+
+	int ret = prepend_name((char *)&ts->buf, &bufptr, (int *)&ts->buflen, ts->dname, ts->dlen);
+
+	ts->offset = bufptr - (char *)&ts->buf;
+
+	return ret;
+}
---
+++ bpf/tests/prepend_name_test.go
@@ -0,0 +1,299 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package bpf
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/cilium/ebpf"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	// those constants must be synchronized with the BPF code
+	MAX_BUF_LEN                 = 256
+	NAME_MAX                    = 255
+	testPrependNameStateMapName = "test_prepend_name_state_map"
+	programName                 = "test_prepend_name"
+)
+
+var (
+	zero uint32
+)
+
+type PrependNameStateMapValue struct {
+	Buf    [MAX_BUF_LEN]byte
+	Buflen uint64
+	Dname  [NAME_MAX]byte
+	_      byte
+	Dlen   uint32
+	Offset uint32
+}
+
+type PrependNameState struct {
+	t      *testing.T
+	Map    *ebpf.Map
+	Values PrependNameStateMapValue
+
+	OutOfDate bool
+}
+
+// Refresh looks up the map value and write it in the internal state.
+func (s *PrependNameState) Refresh() {
+	err := s.Map.Lookup(&zero, &s.Values)
+	if err != nil {
+		s.t.Fatal(err)
+	}
+	s.OutOfDate = false
+}
+
+// BufferToString returns the buffer converted to a string after removing all
+// the 0 bytes "on the right".
+func (s *PrependNameState) BufferToString() string {
+	if s.OutOfDate {
+		s.Refresh()
+	}
+	return string(bytes.TrimRight(s.Values.Buf[s.Values.Offset:], "\x00"))
+}
+
+// Buf returns the up to date buffer from the state.
+func (s *PrependNameState) Buf() [MAX_BUF_LEN]byte {
+	if s.OutOfDate {
+		s.Refresh()
+	}
+	return s.Values.Buf
+}
+
+// ResetStateWithBuflen resets the state, thus the inputs to the prepend_name
+// call to zero byte buffers with a buflen specified as input.
+func (s *PrependNameState) ResetStateWithBuflen(buflen int) error {
+	s.Values = PrependNameStateMapValue{
+		Buf:    [MAX_BUF_LEN]byte{},
+		Buflen: uint64(buflen),
+		Dname:  [NAME_MAX]byte{},
+		Dlen:   0,
+	}
+	return s.Map.Update(&zero, &s.Values, ebpf.UpdateAny)
+}
+
+// UpdateDentry updates the dentry on the input, it also updates the len
+// accordingly.
+func (s *PrependNameState) UpdateDentry(dentry string) error {
+	s.Refresh()
+
+	dentryName := [NAME_MAX]byte{}
+	length := copy(dentryName[:], []byte(dentry))
+	if length != len(dentry) {
+		return fmt.Errorf("dentry buffer is too small for string: %s", dentry)
+	}
+
+	s.Values.Dname = dentryName
+	s.Values.Dlen = uint32(length)
+
+	return s.Map.Update(&zero, &s.Values, ebpf.UpdateAny)
+}
+
+// NewPrependNameState creates a new state connected to the BPF map.
+func NewPrependNameState(t *testing.T, stateMap *ebpf.Map) PrependNameState {
+	return PrependNameState{
+		t:      t,
+		Map:    stateMap,
+		Values: PrependNameStateMapValue{},
+	}
+}
+
+func Test_PrependName(t *testing.T) {
+	// load test program
+	coll, err := ebpf.LoadCollection("objs/prepend_name_test.o")
+	if err != nil {
+		var ve *ebpf.VerifierError
+		if errors.As(err, &ve) {
+			t.Fatalf("verifier error: %+v\n", ve)
+		}
+		t.Fatal(err)
+	}
+	defer coll.Close()
+
+	// get ref to objects
+	prog, ok := coll.Programs[programName]
+	if !ok {
+		t.Fatalf("%s not found", programName)
+	}
+	stateMap := coll.Maps[testPrependNameStateMapName]
+	if stateMap == nil {
+		t.Fatalf("%s not found", testPrependNameStateMapName)
+	}
+
+	state := NewPrependNameState(t, stateMap)
+
+	// runPrependName BPF code
+	runPrependName := func() int {
+		code, err := prog.Run(&ebpf.RunOptions{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		state.OutOfDate = true
+		return int(int32(code))
+	}
+
+	// This part is factorized since it's the setup used in many of the tests below
+	SetupCatBinHelper := func() {
+		err = state.UpdateDentry("cat")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/cat", state.BufferToString())
+
+		err = state.UpdateDentry("bin")
+		assert.NoError(t, err)
+		code = runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/bin/cat", state.BufferToString())
+	}
+
+	t.Run("ExactBufferSize", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/usr/bin/cat"))
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/usr/bin/cat", state.BufferToString())
+	})
+
+	t.Run("FillAllAvailableSpace", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/usr/bin/cat"))
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.NotEqual(t, byte(0), state.Buf()[0])
+		assert.NotEqual(t, byte(0), state.Buf()[len("/usr/bin/cat")-1])
+	})
+
+	t.Run("TooSmallBufferSize", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/usr/bin/cat") - 1)
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, -int(unix.ENAMETOOLONG), code)
+		assert.Equal(t, "usr/bin/cat", state.BufferToString())
+	})
+
+	t.Run("TooBigBufferSize", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/usr/bin/cat") + 1)
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/usr/bin/cat", state.BufferToString())
+		assert.Equal(t, byte(0), state.Buf()[0])
+	})
+
+	t.Run("AlreadyFullBuffer", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/bin/cat"))
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, -int(unix.ENAMETOOLONG), code)
+		assert.Equal(t, "/bin/cat", state.BufferToString())
+	})
+
+	t.Run("TooSmallCutPath", func(t *testing.T) {
+		state.ResetStateWithBuflen(len("/usr/bin/cat") - 2)
+
+		SetupCatBinHelper()
+
+		err = state.UpdateDentry("usr")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, -int(unix.ENAMETOOLONG), code)
+		assert.Equal(t, "sr/bin/cat", state.BufferToString())
+	})
+
+	SetupLongDentry := func() string {
+		// length is 239
+		const longDentry = "pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil_pizza_tomato_mozzarella_basil"
+
+		err = state.UpdateDentry(longDentry)
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/"+longDentry, state.BufferToString())
+		return longDentry
+	}
+
+	t.Run("MaxSizeBufFull", func(t *testing.T) {
+		state.ResetStateWithBuflen(MAX_BUF_LEN)
+
+		longDentry := SetupLongDentry()
+
+		// length is 15, so 239 + 15 + 2 slash chars = 256
+		err = state.UpdateDentry("favorite_recipe")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, 0, code)
+		assert.Equal(t, "/favorite_recipe"+"/"+longDentry, state.BufferToString())
+		assert.Equal(t, MAX_BUF_LEN, len(state.BufferToString()))
+	})
+
+	t.Run("MaxSizeBufTooSmall", func(t *testing.T) {
+		state.ResetStateWithBuflen(MAX_BUF_LEN)
+
+		longDentry := SetupLongDentry()
+
+		// length is 16 with the "s" of "recipes", so 240 + 15 + 2 slash chars = 257
+		err = state.UpdateDentry("favorite_recipes")
+		assert.NoError(t, err)
+		code := runPrependName()
+		assert.Equal(t, -int(unix.ENAMETOOLONG), code)
+		assert.Equal(t, "favorite_recipes"+"/"+longDentry, state.BufferToString())
+		assert.Equal(t, MAX_BUF_LEN, len(state.BufferToString()))
+	})
+
+	t.Run("MaxSizeBufNormalUse", func(t *testing.T) {
+		// simulate a dentry walk on path
+		walkPath := func(path string) {
+			dentries := strings.Split(path, "/")
+			if len(dentries) > 0 && strings.HasPrefix(path, "/") {
+				dentries = dentries[1:]
+			}
+			slices.Reverse(dentries) // walk from local to root
+
+			for _, dentry := range dentries {
+				state.UpdateDentry(dentry)
+
+				code := runPrependName()
+				assert.Equal(t, 0, code)
+			}
+			assert.Equal(t, path, state.BufferToString())
+		}
+
+		state.ResetStateWithBuflen(MAX_BUF_LEN)
+		walkPath("/home/user/.bin/tetragon")
+
+		state.ResetStateWithBuflen(MAX_BUF_LEN)
+		walkPath("/usr/bin/cat")
+	})
+}
