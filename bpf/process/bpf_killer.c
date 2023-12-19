8e54dfa154709866818c5c241a1f754e1bdf2b97
Kornilios Kourtis <kornilios@isovalent.com>
2023-12-19 14:59:21 +0100

    bpf: killer: add an fmod_ret version
    Some kernels do not include CONFIG_BPF_KPROBE_OVERRIDE so it's not
    possible to use bpf_override_return(). Instead, we can use fmod_ret.
    Previously, we were building two versions of the killer program: one
    with multi-kprobe and one without. With this patch, we build a third
    version: one that uses fmod_ret.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- bpf/process/bpf_killer.c
+++ bpf/process/bpf_killer.c
@@ -2,14 +2,8 @@
 
 char _license[] __attribute__((section("license"), used)) = "Dual BSD/GPL";
 
-#ifdef __MULTI_KPROBE
-#define MAIN "kprobe.multi/killer"
-#else
-#define MAIN "kprobe/killer"
-#endif
-
-__attribute__((section(MAIN), used)) int
-killer(void *ctx)
+static inline __attribute__((always_inline)) int
+do_killer(void *ctx)
 {
 	__u64 id = get_current_pid_tgid();
 	struct killer_data *data;
@@ -18,11 +12,42 @@ killer(void *ctx)
 	if (!data)
 		return 0;
 
-	if (data->error)
-		override_return(ctx, data->error);
 	if (data->signal)
 		send_signal(data->signal);
 
 	map_delete_elem(&killer_data, &id);
+	return data->error;
+}
+
+#if defined(__BPF_OVERRIDE_RETURN)
+
+#ifdef __MULTI_KPROBE
+#define MAIN "kprobe.multi/killer"
+#else
+#define MAIN "kprobe/killer"
+#endif
+
+__attribute__((section(MAIN), used)) int
+multi_kprobe_killer(void *ctx)
+{
+	long ret;
+
+	ret = do_killer(ctx);
+	if (ret)
+		override_return(ctx, ret);
+
 	return 0;
 }
+
+#else /* !__BPF_OVERRIDE_RETURN */
+
+/* Putting security_task_prctl in here to pass contrib/verify/verify.sh test,
+ * in normal run the function is set by tetragon dynamically.
+ */
+__attribute__((section("fmod_ret/security_task_prctl"), used)) long
+fmodret_killer(void *ctx)
+{
+	return do_killer(ctx);
+}
+
+#endif
