06b6179ebca7170f26e10ac119d97aa87d82e9e4
Djalal Harouni <tixxdz@gmail.com>
2024-01-09 16:00:09 +0100

    test: add message testing
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ pkg/sensors/tracing/message_test.go
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package tracing
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPolicyMessage(t *testing.T) {
+	msg, err := getPolicyMessage("")
+	require.Empty(t, msg)
+	require.Equal(t, err, ErrMsgSyntaxEmpty)
+
+	msg, err = getPolicyMessage("a")
+	require.Empty(t, msg)
+	require.Equal(t, err, ErrMsgSyntaxShort)
+
+	msg, err = getPolicyMessage("test")
+	require.NoError(t, err)
+	require.Equal(t, msg, "test")
+
+	msg, err = getPolicyMessage(strings.Repeat("a", TpMaxMessageLen+1))
+	require.Equal(t, err, ErrMsgSyntaxLong)
+	require.Equal(t, TpMaxMessageLen, len(msg))
+}
