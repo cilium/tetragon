cc7ed7a0e9db61f82a94f4d8c9756d4a5804b9bd
Philip Schmid <philip.schmid@isovalent.com>
2024-01-10 17:24:04 +0100

    Add masOS compatibility for 'sed' usage in scripts
    Signed-off-by: Philip Schmid <philip.schmid@isovalent.com>

--- api/export-doc.sh
+++ api/export-doc.sh
@@ -15,15 +15,20 @@ cp v1/README.md $TMP_FILE
 
 # cleanup the generated documentation for the website
 # remove the link to the top of the page
-sed -i '/<p align="right"><a href="#top">Top<\/a><\/p>/d' $TMP_FILE
-sed -i '/<a name="top"><\/a>/d' $TMP_FILE
+sed -i'' -e '/<p align="right"><a href="#top">Top<\/a><\/p>/d' $TMP_FILE
+sed -i'' -e '/<a name="top"><\/a>/d' $TMP_FILE
 # remove title
-sed -i '/^# Protocol Documentation/d' $TMP_FILE
+sed -i'' -e '/^# Protocol Documentation/d' $TMP_FILE
 # remove table of content
-sed -i -e '/## Table of Contents/,/##/{//!d}' $TMP_FILE
-sed -i '/^## Table of Contents/d' $TMP_FILE
+sed -i'' -e '/## Table of Contents/,/##/{//!d;}' $TMP_FILE
+sed -i'' -e '/^## Table of Contents/d' $TMP_FILE
 # cleanup unecessary consecutive whitelines
-sed -i '/^[[:space:]]*$/N;/^[[:space:]]*\n[[:space:]]*$/D' $TMP_FILE
+sed -i'' -e '/^[[:space:]]*$/N;/^[[:space:]]*\n[[:space:]]*$/D' $TMP_FILE
+# remove empty line at the end of the file (required for macOS, doesn't harm on Linux)
+sed -i'' -e '${/^$/d;}' $TMP_FILE
+
+# Cleanup backup file that might be created by macOS sed:
+rm ${TMP_FILE}-e 2>/dev/null || true
 
 # add a frontmatter and a small introduction
 echo '---
@@ -43,4 +48,3 @@ The Tetragon API is an independant Go module that can be found in the Tetragon
 repository under [api](https://github.com/cilium/tetragon/tree/main/api). The
 version 1 of this API is defined in
 [github.com/cilium/tetragon/api/v1/tetragon](https://github.com/cilium/tetragon/tree/main/api/v1/tetragon).' | cat - $TMP_FILE > $1
-
