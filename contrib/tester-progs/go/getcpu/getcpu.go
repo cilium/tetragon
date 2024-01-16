9c75f7b113c7004bd6e8af05c461148727701f96
Kornilios Kourtis <kornilios@isovalent.com>
2024-01-16 14:46:20 +0100

    contrib/tester-progs: add getcpu prog
    Add a getcpu program that just performs the getcpu syscall and exits
    with the error value. Will be used in the next patch.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ contrib/tester-progs/go/getcpu/getcpu.go
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package main
+
+import (
+	"os"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	var cpu, node int
+	_, _, err := unix.Syscall(
+		unix.SYS_GETCPU,
+		uintptr(unsafe.Pointer(&cpu)),
+		uintptr(unsafe.Pointer(&node)),
+		0,
+	)
+	os.Exit(int(err))
+}
