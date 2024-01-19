e62c1885e020434b64162b049e9d5e6662bf206c
Jiri Olsa <jolsa@kernel.org>
2024-01-19 12:26:01 +0000

    tetragon: Add direct-write-tester tester prog
    Adding direct-write-tester tester prog to test the bypass of sigkill
    action in pwrite. It's originally written by Anastasios.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ contrib/tester-progs/direct-write-tester.c
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+// Copyright Authors of Tetragon
+
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define BLOCKSIZE 4096
+#define O_DIRECT  00040000
+
+int main(int argc, char **argv)
+{
+	char *avd = "NEW_MESSAGE\n";
+	void *buffer;
+	int fd;
+
+	if (argc != 2)
+		exit(-1);
+
+	fd = open(argv[1], O_WRONLY | O_DIRECT | O_CREAT, S_IRWXU | S_IRWXG);
+	if (fd == -1) {
+		perror("open");
+		exit(-1);
+	}
+
+	posix_memalign(&buffer, BLOCKSIZE, BLOCKSIZE);
+	memset(buffer, 0, BLOCKSIZE);
+	memcpy(buffer, avd, strlen(avd) + 1);
+
+	ssize_t ret = pwrite(fd, buffer, BLOCKSIZE, 0);
+	if (ret == -1) {
+		perror("write");
+		exit(-1);
+	}
+
+	close(fd);
+	free(buffer);
+	return 0;
+}
