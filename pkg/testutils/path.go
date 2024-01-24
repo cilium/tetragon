c1d6f115ef2eb86d18cac09625f42bec09f0660a
Anastasios Papagiannis <tasos.papagiannnis@gmail.com>
2024-01-24 08:31:20 +0000

    Revert "testutils: Fix RepoRootPath when Tetragon is vendored"
    This reverts commit 5bcb468695cf6b8558c45af99288de171ebcbf97.
    
    Signed-off-by: Anastasios Papagiannis <tasos.papagiannnis@gmail.com>

--- pkg/testutils/path.go
+++ pkg/testutils/path.go
@@ -6,16 +6,10 @@ package testutils
 import (
 	"path/filepath"
 	"runtime"
-	"strings"
 )
 
 // RepoRootPath retrieves the repository root path (useful to find scripts and other files)
 func RepoRootPath(fname string) string {
 	_, testFname, _, _ := runtime.Caller(0)
-	// in the case where this file is vendored (i.e. under vendor directory)
-	// we have to skip the path on the right of vendor/
-	if index := strings.Index(testFname, "vendor"); index != -1 { // vendor found in path
-		return filepath.Join(testFname[0:index], fname)
-	}
 	return filepath.Join(filepath.Dir(testFname), "..", "..", fname)
 }
