20219652cba4e87705171ac03b2559e724434ec6
Jiri Olsa <jolsa@kernel.org>
2024-02-11 22:08:28 +0000

    killer: Add support to specify argument to killer-tester
    So we can set sys_prctl first argument value from following test.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- contrib/tester-progs/killer-tester.c
+++ contrib/tester-progs/killer-tester.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <sys/prctl.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	prctl(0xffff, 0, 0, 0, 0);
+	long long num = 0xffff;
+
+	if (argc == 2) {
+		num = strtoll(argv[1], NULL, 16);
+		if (num == LONG_MIN)
+			return -EINVAL;
+	}
+	prctl(num, 0, 0, 0, 0);
 	return errno;
 }
