d75dd297427b6633e7072bc9637b10085b3d562d
Jiri Olsa <jolsa@kernel.org>
2024-02-16 09:01:46 +0000

    tetragon: Rename killer bpf sensor to enforcer
    Renaming killer bpf sensor to enforcer including the
    file object names.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_enforcer.h
+++ bpf/process/bpf_enforcer.h
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
 /* Copyright Authors of Cilium */
 
-#ifndef __KILLER_H__
-#define __KILLER_H__
+#ifndef __ENFORCER_H__
+#define __ENFORCER_H__
 
 #include "vmlinux.h"
 #include "bpf_helpers.h"
 
-struct killer_data {
+struct enforcer_data {
 	__s16 error;
 	__s16 signal;
 };
@@ -16,25 +16,25 @@ struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
 	__uint(max_entries, 32768);
 	__type(key, __u64);
-	__type(value, struct killer_data);
-} killer_data SEC(".maps");
+	__type(value, struct enforcer_data);
+} enforcer_data SEC(".maps");
 
 static inline __attribute__((always_inline)) void
-do_killer_action(int error, int signal)
+do_enforcer_action(int error, int signal)
 {
 	__u64 id = get_current_pid_tgid();
-	struct killer_data *ptr, data = {
+	struct enforcer_data *ptr, data = {
 		.error = (__s16)error,
 		.signal = (__s16)signal,
 	};
 
-	ptr = map_lookup_elem(&killer_data, &id);
+	ptr = map_lookup_elem(&enforcer_data, &id);
 	if (ptr) {
 		ptr->error = (__s16)error;
 		ptr->signal = (__s16)signal;
 	} else {
-		map_update_elem(&killer_data, &id, &data, BPF_ANY);
+		map_update_elem(&enforcer_data, &id, &data, BPF_ANY);
 	}
 }
 
-#endif /* __KILLER_H__ */
+#endif /* __ENFORCER_H__ */
