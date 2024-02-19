b09361dc39ead7b2702956dd10e5e399aaf7a218
Anna Kapuscinska <anna@isovalent.com>
2024-02-19 23:07:34 +0000

    tetra: Add metrics-docs command
    This command utilizes github.com/isovalent/metricstool/pkg/metricsmd to
    generate a metrics reference.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

---
+++ cmd/tetra/metrics/print.go
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package metrics
+
+import (
+	"log/slog"
+
+	"github.com/isovalent/metricstool/pkg/metricsmd"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/spf13/cobra"
+
+	"github.com/cilium/tetragon/pkg/metrics/metricsconfig"
+)
+
+func New() *cobra.Command {
+	targets := map[string]string{
+		"tetragon": "Tetragon",
+	}
+
+	overrides := []metricsmd.LabelOverrides{
+		// Theses metrics takes VCS info into account supplied at build
+		// time, which changes every build, so override those.
+		{
+			Metric: "go_info",
+			Overrides: []metricsmd.LabelValues{
+				{
+					Label:  "version",
+					Values: []string{"go1.22.0"},
+				},
+			},
+		},
+		{
+			Metric: "tetragon_build_info",
+			Overrides: []metricsmd.LabelValues{
+				{
+					Label:  "commit",
+					Values: []string{"931b70f2c9878ba985ba6b589827bea17da6ec33"},
+				},
+				{
+					Label:  "go_version",
+					Values: []string{"go1.22.0"},
+				},
+				{
+					Label:  "modified",
+					Values: []string{"false"},
+				},
+				{
+					Label:  "time",
+					Values: []string{"2022-05-13T15:54:45Z"},
+				},
+			},
+		},
+	}
+
+	config := &metricsmd.Config{
+		Targets:        targets,
+		LabelOverrides: overrides,
+		InitMetrics:    initMetrics,
+		HeadingLevel:   1,
+	}
+
+	cmd, err := metricsmd.NewCmd(nil, nil, config)
+	if err != nil {
+		slog.Error("failed to create metrics-docs command", "error", err)
+	}
+	return cmd
+}
+
+func initMetrics(_ string, reg *prometheus.Registry, _ *slog.Logger) error {
+	metricsconfig.InitAllMetrics(reg)
+	return nil
+}
