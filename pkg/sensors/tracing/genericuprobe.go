d16d8517896aff5fbb69b45757a88676abd01b1f
Jiri Olsa <jolsa@kernel.org>
2024-02-25 08:57:54 +0000

    tetragon: Move genericuprobe sensor maps under new hierarchy
    Moving genericuprobe sensor maps under new hierarchy.
    
    per program maps:
    
      config_map
      uprobe_calls
      filter_map
      tg_mb_sel_opts
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/genericuprobe.go
+++ pkg/sensors/tracing/genericuprobe.go
@@ -288,9 +288,10 @@ func createGenericUprobeSensor(
 	}
 
 	return &sensors.Sensor{
-		Name:  name,
-		Progs: progs,
-		Maps:  maps,
+		Name:   name,
+		Progs:  progs,
+		Maps:   maps,
+		Policy: policyName,
 	}, nil
 }
 
@@ -415,9 +416,9 @@ func createMultiUprobeSensor(sensorPath string, multiIDs []idtable.EntryID) ([]*
 
 	progs = append(progs, load)
 
-	configMap := program.MapBuilderPin("config_map", sensors.PathJoin(pinPath, "config_map"), load)
-	tailCalls := program.MapBuilderPin("uprobe_calls", sensors.PathJoin(pinPath, "up_calls"), load)
-	filterMap := program.MapBuilderPin("filter_map", sensors.PathJoin(pinPath, "filter_map"), load)
+	configMap := program.MapBuilderProgram("config_map", load)
+	tailCalls := program.MapBuilderProgram("uprobe_calls", load)
+	filterMap := program.MapBuilderProgram("filter_map", load)
 
 	maps = append(maps, configMap, tailCalls, filterMap)
 
@@ -472,10 +473,10 @@ func createUprobeSensorFromEntry(uprobeEntry *genericUprobe,
 
 	progs = append(progs, load)
 
-	configMap := program.MapBuilderPin("config_map", sensors.PathJoin(pinPath, "config_map"), load)
-	tailCalls := program.MapBuilderPin("uprobe_calls", sensors.PathJoin(pinPath, "up_calls"), load)
-	filterMap := program.MapBuilderPin("filter_map", sensors.PathJoin(pinPath, "filter_map"), load)
-	selMatchBinariesMap := program.MapBuilderPin("tg_mb_sel_opts", sensors.PathJoin(pinPath, "tg_mb_sel_opts"), load)
+	configMap := program.MapBuilderProgram("config_map", load)
+	tailCalls := program.MapBuilderProgram("uprobe_calls", load)
+	filterMap := program.MapBuilderProgram("filter_map", load)
+	selMatchBinariesMap := program.MapBuilderProgram("tg_mb_sel_opts", load)
 	maps = append(maps, configMap, tailCalls, filterMap, selMatchBinariesMap)
 	load.SetTailCall("uprobe", tailCalls)
 	return progs, maps
