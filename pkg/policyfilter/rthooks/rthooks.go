982835803b29fccd78ecd25bd0cbd18307db9028
Anna Kapuscinska <anna@isovalent.com>
2024-02-27 00:30:05 +0000

    policyfiltermetrics: Define subsys label values
    Introduce Subsys type and constants representing possible values of subsys label
    on tetragon_policyfilter_metrics_total metric.
    
    This is done to improve how metrics with labels are initialized and maintained.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/policyfilter/rthooks/rthooks.go
+++ pkg/policyfilter/rthooks/rthooks.go
@@ -103,7 +103,7 @@ func createContainerHook(_ context.Context, arg *rthooks.CreateContainerArg) err
 	if err := pfState.AddPodContainer(policyfilter.PodID(podID), namespace, pod.Labels, containerID, cgid); err != nil {
 		log.WithError(err).Warn("failed to update policy filter, aborting hook.")
 	}
-	policyfiltermetrics.OpInc("rthooks", "add-container", err)
+	policyfiltermetrics.OpInc(policyfiltermetrics.RTHooksSubsys, "add-container", err)
 
 	return nil
 }
