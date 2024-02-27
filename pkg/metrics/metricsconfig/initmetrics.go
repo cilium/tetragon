5f452d2c0371dcb86f72854576af1b470079fb08
Anna Kapuscinska <anna@isovalent.com>
2024-02-27 23:35:39 +0000

    observer: Initialize custom collectors for docs
    observer.bpfCollector doesn't report any metrics if Tetragon isn't fully
    initialized - so also when we generate metrics docs with tetra. To include
    observer BPF metrics in metrics docs, let's define a "fake" collector -
    observer.bpfZeroCollector - reporting same metrics as observer.bpfCollector
    but with hardcoded zero values.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/metrics/metricsconfig/initmetrics.go
+++ pkg/metrics/metricsconfig/initmetrics.go
@@ -62,6 +62,9 @@ func InitHealthMetricsForDocs(registry *prometheus.Registry) {
 
 	kprobemetrics.InitMetricsForDocs(registry)
 	policystatemetrics.InitMetricsForDocs(registry)
+
+	// register custom zero collectors
+	registry.MustRegister(observer.NewBPFZeroCollector())
 }
 
 func initResourcesMetrics(registry *prometheus.Registry) {
