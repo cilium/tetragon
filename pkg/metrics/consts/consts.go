692b3821d19306500540230df0bce25944aa69a1
Anna Kapuscinska <anna@isovalent.com>
2024-02-28 15:08:44 +0000

    eventmetrics: Initialize metrics with example labels for docs
    Initialize tetragon_events_total with all possible event types and example
    process metadata.
    
    Initialize tetragon_policy_events_total with example policy and kprobe.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/metrics/consts/consts.go
+++ pkg/metrics/consts/consts.go
@@ -6,3 +6,9 @@ package consts
 const MetricsNamespace = "tetragon"
 
 var KnownMetricLabelFilters = []string{"namespace", "workload", "pod", "binary"}
+
+var (
+	ExamplePolicyLabel   = "example-tracingpolicy"
+	ExampleKprobeLabel   = "example_kprobe"
+	ExampleProcessLabels = []string{"example-namespace", "example-workload", "example-pod", "example-binary"}
+)
