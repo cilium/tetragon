f84198fffc57a6ae078902a96cc5f963af5197cb
William Findlay <will@isovalent.com>
2024-02-29 09:51:15 -0500

    loader: remove dead code
    There was some dead code in the sensor loader referencing old maps that we no longer need.
    Remove it here.
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- pkg/sensors/program/loader.go
+++ pkg/sensors/program/loader.go
@@ -642,15 +642,6 @@ func installTailCalls(bpfDir string, spec *ebpf.CollectionSpec, coll *ebpf.Colle
 		return nil
 	}
 
-	if err := install("http1_calls", "sk_msg"); err != nil {
-		return err
-	}
-	if err := install("http1_calls_skb", "sk_skb/stream_verdict"); err != nil {
-		return err
-	}
-	if err := install("tls_calls", "classifier"); err != nil {
-		return err
-	}
 	if ci != nil {
 		if err := install(ci.mapName, ci.secPrefix); err != nil {
 			return err
