c3d9138220830fccf99291ff71238f1f527c202f
Djalal Harouni <tixxdz@gmail.com>
2024-03-12 21:27:05 +0100

    tests: validate CRD tags
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- pkg/sensors/tracing/kprobe_validation_test.go
+++ pkg/sensors/tracing/kprobe_validation_test.go
@@ -313,3 +313,51 @@ spec:
 		assert.Error(t, err)
 	}
 }
+
+// Test that tracing policy max tags
+func TestTracingPolicyTagsMax(t *testing.T) {
+	// Ensure that CRD fail if tags > 16
+	crd1 := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "file-install"
+spec:
+  kprobes:
+  - call: "fd_install"
+    syscall: false
+    tags: [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17" ]
+ `
+
+	crd2 := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "file-install"
+spec:
+  kprobes:
+  - call: "fd_install"
+    syscall: false
+    tags: [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16" ]
+ `
+
+	crd3 := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "file-install"
+spec:
+  kprobes:
+  - call: "fd_install"
+    syscall: false
+ `
+
+	_, err := tracingpolicy.FromYAML(crd1)
+	assert.Error(t, err)
+
+	_, err = tracingpolicy.FromYAML(crd2)
+	assert.NoError(t, err)
+
+	_, err = tracingpolicy.FromYAML(crd3)
+	assert.NoError(t, err)
+}
