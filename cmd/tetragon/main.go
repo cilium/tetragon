e04f63e015491e36d588a415e8bd70a986fc0e19
Djalal Harouni <tixxdz@gmail.com>
2024-03-28 11:11:13 +0100

    tetragon: log current security context if any at startup
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

--- cmd/tetragon/main.go
+++ cmd/tetragon/main.go
@@ -44,6 +44,7 @@ import (
 	"github.com/cilium/tetragon/pkg/process"
 	"github.com/cilium/tetragon/pkg/ratelimit"
 	"github.com/cilium/tetragon/pkg/reader/namespace"
+	"github.com/cilium/tetragon/pkg/reader/proc"
 	"github.com/cilium/tetragon/pkg/rthooks"
 	"github.com/cilium/tetragon/pkg/sensors/base"
 	"github.com/cilium/tetragon/pkg/sensors/program"
@@ -175,6 +176,9 @@ func tetragonExecute() error {
 
 	// Create run dir early
 	os.MkdirAll(defaults.DefaultRunDir, 0755)
+
+	// Log early security context in case something fails
+	proc.LogCurrentSecurityContext()
 
 	// When an instance terminates or restarts it may cleanup bpf programs,
 	// having a check here to see if another instance is already running, can
