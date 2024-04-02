c0fe8ab6b1b79b5f790a20476d2f4d731f21265f
Kornilios Kourtis <kornilios@isovalent.com>
2024-04-02 14:12:23 +0200

    policyfilter: implement ErrorLabel() for metrics
    This patch implements ErrorLabel() for generating error labels for
    metrics. This fixes an issue with the previous implementation where the
    cardinality of the error_type label was potentially unbounded.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/policyfilter/error_test.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package policyfilter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrorLabel(t *testing.T) {
+	var err error = &podNamespaceConflictErr{PodID{}, "foo", "lala"}
+	require.Equal(t, "", ErrorLabel(nil))
+	require.Equal(t, "pod-namespace-conflict", ErrorLabel(err))
+}
