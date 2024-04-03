11f83c114f8040ef162349c3b3f46bcdfed32ea6
Andrei Fedotov <anfedotoff@yandex-team.ru>
2024-04-03 18:59:31 +0300

    Added TestKprobeUserStacktrace unit test
    Signed-off-by: Andrey Fedotov <anfedotoff@yandex-team.ru>

---
+++ contrib/tester-progs/go/user-stacktrace/user-stacktrace.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package main
+
+import (
+	"os"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+//nolint:all
+func main() {
+	// This test has endless loop for stable stack trace collection.
+	// It must be terminated from tests.
+	var cpu, node int
+	_, _, err := unix.Syscall(
+		unix.SYS_GETCPU,
+		uintptr(unsafe.Pointer(&cpu)),
+		uintptr(unsafe.Pointer(&node)),
+		0,
+	)
+	if err != 0 {
+		os.Exit(int(err))
+	}
+	for {
+		time.Sleep(time.Minute)
+	}
+}
