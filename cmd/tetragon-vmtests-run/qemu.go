2b733b70d9bfd8f295412a1e8a14019ab0817d47
Mahe Tardy <mahe.tardy@gmail.com>
2024-04-15 13:34:34 +0000

    vmtests: use LVH as a library to build qemu args
    Deduplicate some codes shared between vmtests and LVH:
    - inject LVH runner.RunConf into vmtests RunConf and reuse existing
      LVH configuration fields.
    - use LVH runner.BuildQemuArgs along with custom vmtests logic.
    - removes the support for HVF, we would need to put it in LVH to get
      this again but I don't think this is used nowadays.
    
    This is mainly so that we benefit from the arm64 support as LVH was
    recently updated to support it.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- cmd/tetragon-vmtests-run/qemu.go
+++ cmd/tetragon-vmtests-run/qemu.go
@@ -5,80 +5,36 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"path/filepath"
 
+	"github.com/cilium/little-vm-helper/pkg/runner"
 	"github.com/sirupsen/logrus"
 )
 
-func buildQemuArgs(log *logrus.Logger, rcnf *RunConf) ([]string, error) {
-	qemuArgs := []string{
-		// no need for all the default devices
-		"-nodefaults",
-		// no need display (-nographics seems a bit slower)
-		"-display", "none",
-		// don't reboot, just exit
-		"-no-reboot",
-		// cpus, memory
-		"-smp", "2", "-m", "4G",
-	}
-
-	if rcnf.enableHVF {
-		log.Info("HVF enabled")
-		qemuArgs = append(qemuArgs, "-accel", "hvf")
-	} else if !rcnf.disableKVM {
-		// quick-and-dirty kvm detection
-		if f, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0755); err == nil {
-			qemuArgs = append(qemuArgs, "-enable-kvm", "-cpu", "kvm64")
-			f.Close()
-		} else {
-			log.Infof("KVM disabled (%v)", err)
-		}
-	}
-
-	var kernelRoot string
-	switch rcnf.rootDev {
-	case "hda":
-		qemuArgs = append(qemuArgs, "-hda", rcnf.testImageFname())
-		kernelRoot = "/dev/sda"
-	case "vda":
-		qemuArgs = append(qemuArgs, "-drive", fmt.Sprintf("file=%s,if=virtio,index=0,media=disk", rcnf.testImageFname()))
-		kernelRoot = "/dev/vda"
-	default:
-		return nil, fmt.Errorf("invalid root device: %s", rcnf.rootDev)
-	}
-
-	if rcnf.kernelFname != "" {
-		appendArgs := []string{
-			fmt.Sprintf("root=%s", kernelRoot),
-			"console=ttyS0",
-			"earlyprintk=ttyS0",
-			"panic=-1",
-		}
+// buildQemuArgs is a wrapper around LVH's runner.BuildQemuArgs and also handles
+// custom configuration from vmtests
+func buildQemuArgs(log *logrus.Logger, rcnf RunConf) ([]string, error) {
+	if rcnf.KernelFname != "" {
 		if rcnf.disableUnifiedCgroups {
-			appendArgs = append(appendArgs, "systemd.unified_cgroup_hierarchy=0")
+			rcnf.KernelAppendArgs = append(rcnf.KernelAppendArgs, "systemd.unified_cgroup_hierarchy=0")
 		}
 		if rcnf.useTetragonTesterInit {
-			appendArgs = append(appendArgs, fmt.Sprintf("init=%s", TetragonTesterBin))
+			rcnf.KernelAppendArgs = append(rcnf.KernelAppendArgs, fmt.Sprintf("init=%s", TetragonTesterBin))
 		}
-		qemuArgs = append(qemuArgs,
-			"-kernel", rcnf.kernelFname,
-			"-append", strings.Join(appendArgs, " "),
-		)
 	}
 
-	// NB: not sure what the best option is here, this is from trial-and-error
-	qemuArgs = append(qemuArgs,
-		"-serial", "mon:stdio",
-		"-device", "virtio-serial-pci",
-	)
+	rcnf.CPU = 2
+	rcnf.Mem = "4G"
+	// the new image is in the base image folder
+	rcnf.Image = filepath.Join(filepath.Dir(rcnf.baseImageFilename), rcnf.testImageFilename())
+
+	qemuArgs, err := runner.BuildQemuArgs(log, &rcnf.RunConf)
+	if err != nil {
+		return nil, err
+	}
 
 	for _, fs := range rcnf.filesystems {
 		qemuArgs = append(qemuArgs, fs.qemuArgs()...)
-	}
-
-	if len(rcnf.portForwards) > 0 {
-		qemuArgs = append(qemuArgs, rcnf.portForwards.QemuArgs()...)
 	}
 
 	return qemuArgs, nil
