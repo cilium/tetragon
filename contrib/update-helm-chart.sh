6aeca70b719ce597184aedb1f69f30c5ca265197
Mahe Tardy <mahe.tardy@gmail.com>
2024-04-19 18:31:15 +0200

    contrib: use renovate to update yq image
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- contrib/update-helm-chart.sh
+++ contrib/update-helm-chart.sh
@@ -3,7 +3,9 @@
 set -ex
 
 shopt -s expand_aliases
-alias yq='docker run --rm -v "${PWD}":/workdir --user "$(id -u):$(id -g)" mikefarah/yq:4.27.3'
+# renovate: datasource=docker
+YQ_IMAGE=mikefarah/yq:4.27.3@sha256:495c1e1db2d653cce61a06da52cfca0c7d68d6249cc6e61b2a134d92c609c016
+alias yq="docker run --rm -v \"${PWD}\":/workdir --user \"$(id -u):$(id -g)\" $YQ_IMAGE"
 
 if [ -z "$1" ] || [[ ! $1 =~ ^v[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
   echo "USAGE: ./contrib/update-helm-chart.sh vX.Y.Z"
