fbacb1b1413208d2f1e7a4aa59e7d9f78c93d8c4
Anna Kapuscinska <anna@isovalent.com>
2024-04-25 19:05:13 +0100

    metrics: Return a copy of LabelFilter in WithEnabledLabels(nil)
    LabelFilter.WithEnabledLabels behaved in the same way when passed nil or an
    empty slice - it disabled all labels. However, it's convenient to have a "no-op"
    option and just return a copy of the original filter. Let's do that when the
    passed slice is nil.
    
    This change doesn't affect users. Currently the method is always called with
    a non-nil slice.
    
    While here, I documented LabelFilter.WithEnabledLabels with edge cases.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/metrics/labelfilter.go
+++ pkg/metrics/labelfilter.go
@@ -3,10 +3,22 @@
 
 package metrics
 
+import (
+	"maps"
+)
+
 type LabelFilter map[string]bool
 
+// WithEnabledLabels returns a new LabelFilter with only the labels in enabledLabels enabled.
+// If enabledLabels is nil, a copy of the original LabelFilter is returned.
+// If enabledLabels is empty, all labels are disabled.
+// If enabledLabels contains labels that are not in the original LabelFilter, they are ignored.
 func (f LabelFilter) WithEnabledLabels(enabledLabels []string) LabelFilter {
-	labelFilter := make(LabelFilter)
+	labelFilter := maps.Clone(f)
+	if enabledLabels == nil {
+		return labelFilter
+	}
+
 	// disable all configurable labels
 	for l := range f {
 		labelFilter[l] = false
