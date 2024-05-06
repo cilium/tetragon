zyjj1/cilium-tetragon

tetragon: Add cgroup rate config option
Adding --cgroup-rate=options to configure exec/fork/exit allowed
cgroup rates.

The option is in following format:

  'events,interval'

Example:

  --cgroup-rate="1000,1s"      # 1000/second rate
  --cgroup-rate="10000,5s"     # 10000/5 seconds rate

Signed-off-by: Jiri Olsa <jolsa@kernel.org>

pkg/option/flags.go
@@ -5,6 +5,8 @@ package option
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/cilium/tetragon/pkg/defaults"
@@ -94,6 +96,8 @@ const (
 	KeyExposeKernelAddresses = "expose-kernel-addresses"
 
 	KeyGenerateDocs = "generate-docs"
+
+	KeyCgroupRate = "cgroup-rate"
 )
 
 func ReadAndSetFlags() error {
@@ -182,7 +186,52 @@ func ReadAndSetFlags() error {
 		Config.ExposeStackAddresses = viper.GetBool(KeyExposeStackAddresses)
 	}
 
+	Config.CgroupRate = ParseCgroupRate(viper.GetString(KeyCgroupRate))
 	return nil
+}
+
+type CgroupRate struct {
+	Events   uint64
+	Interval uint64
+}
+
+func ParseCgroupRate(rate string) CgroupRate {
+	empty := CgroupRate{}
+
+	if rate == "" {
+		return empty
+	}
+
+	s := strings.Split(rate, ",")
+	if len(s) != 2 {
+		logger.GetLogger().Warnf("failed to parse cgroup rate '%s'", rate)
+		return empty
+	}
+
+	var interval time.Duration
+	var events int
+	var err error
+
+	if len(s[0]) > 0 {
+		events, err = strconv.Atoi(s[0])
+		if err != nil {
+			logger.GetLogger().Warnf("failed to parse cgroup rate '%s' : %w", rate, err)
+			return empty
+		}
+	}
+
+	if len(s[1]) > 0 {
+		interval, err = time.ParseDuration(s[1])
+		if err != nil {
+			logger.GetLogger().Warnf("failed to parse cgroup rate '%s' : %w", rate, err)
+			return empty
+		}
+	}
+
+	return CgroupRate{
+		Events:   uint64(events),
+		Interval: uint64(interval),
+	}
 }
 
 func AddFlags(flags *pflag.FlagSet) {
@@ -286,4 +335,5 @@ func AddFlags(flags *pflag.FlagSet) {
 
 	flags.Bool(KeyGenerateDocs, false, "Generate documentation in YAML format to stdout")
 
+	flags.String(KeyCgroupRate, "", "Base sensor events cgroup rate <events,interval> disabled by default ('1000/1s' means rate 1000 events per second")
 }

cilium/tetragon

pkg/option/flags.go
@@ -5,6 +5,8 @@ package option
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/cilium/tetragon/pkg/defaults"
@@ -94,6 +96,8 @@ const (
 	KeyExposeKernelAddresses = "expose-kernel-addresses"
 
 	KeyGenerateDocs = "generate-docs"
+
+	KeyCgroupRate = "cgroup-rate"
 )
 
 func ReadAndSetFlags() error {
@@ -182,7 +186,52 @@ func ReadAndSetFlags() error {
 		Config.ExposeStackAddresses = viper.GetBool(KeyExposeStackAddresses)
 	}
 
+	Config.CgroupRate = ParseCgroupRate(viper.GetString(KeyCgroupRate))
 	return nil
+}
+
+type CgroupRate struct {
+	Events   uint64
+	Interval uint64
+}
+
+func ParseCgroupRate(rate string) CgroupRate {
+	empty := CgroupRate{}
+
+	if rate == "" {
+		return empty
+	}
+
+	s := strings.Split(rate, ",")
+	if len(s) != 2 {
+		logger.GetLogger().Warnf("failed to parse cgroup rate '%s'", rate)
+		return empty
+	}
+
+	var interval time.Duration
+	var events int
+	var err error
+
+	if len(s[0]) > 0 {
+		events, err = strconv.Atoi(s[0])
+		if err != nil {
+			logger.GetLogger().Warnf("failed to parse cgroup rate '%s' : %s", rate, err)
+			return empty
+		}
+	}
+
+	if len(s[1]) > 0 {
+		interval, err = time.ParseDuration(s[1])
+		if err != nil {
+			logger.GetLogger().Warnf("failed to parse cgroup rate '%s' : %s", rate, err)
+			return empty
+		}
+	}
+
+	return CgroupRate{
+		Events:   uint64(events),
+		Interval: uint64(interval),
+	}
 }
 
 func AddFlags(flags *pflag.FlagSet) {
@@ -286,4 +335,5 @@ func AddFlags(flags *pflag.FlagSet) {
 
 	flags.Bool(KeyGenerateDocs, false, "Generate documentation in YAML format to stdout")
 
+	flags.String(KeyCgroupRate, "", "Base sensor events cgroup rate <events,interval> disabled by default ('1000/1s' means rate 1000 events per second")
 }