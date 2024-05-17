a9e6882697de7586d4aa5a52aa50e32d041ea64c
Jiri Olsa <jolsa@kernel.org>
2024-05-17 12:17:04 +0200

    tetragon: Add __V61_BPF_PROG version of FUNC macros
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/include/func.h
+++ bpf/include/func.h
@@ -4,6 +4,13 @@
 #ifndef __BPF_FUNC_H__
 #define __BPF_FUNC_H__
 
+#ifdef __V61_BPF_PROG
+#define FUNC_LOCAL  static __attribute__((noinline)) __attribute__((__unused__))
 #define FUNC_INLINE static inline __attribute__((always_inline))
+#else
+/* Older kernels have all functions inlined.  */
+#define FUNC_LOCAL  static inline __attribute__((always_inline))
+#define FUNC_INLINE static inline __attribute__((always_inline))
+#endif
 
 #endif /* __BPF_FUNC_H__ */
