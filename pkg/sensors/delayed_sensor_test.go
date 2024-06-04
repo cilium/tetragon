d744b363d28f8a4c9414567c060e93b8acd1b298
Kornilios Kourtis <kornilios@isovalent.com>
2024-06-04 14:27:07 +0200

    sensors: test intermediate states ({,un}loading)
    This patch tests the intermediate states that were introduced in the
    previous patches. Specifically, it tests whether ListTracingPolicies
    call observes loading/unloading states for the corresponding sensors. It
    does so by a TestDelayedSensor that implements the SensorIface
    interface.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/sensors/delayed_sensor_test.go
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package sensors
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// A sensor to test the intermediate policy states (loading / unloading)
+
+func makeTestDelayedSensor(t *testing.T) *TestDelayedSensor {
+	s := &TestDelayedSensor{
+		name:   "test-delayed-sensor",
+		loaded: false,
+		ch:     make(chan struct{}),
+	}
+	RegisterPolicyHandlerAtInit("dummy-policyhandler", &dummyHandler{s: s})
+	t.Cleanup(func() {
+		delete(registeredPolicyHandlers, "dummy-policyhandler")
+	})
+
+	return s
+}
+
+type TestDelayedSensor struct {
+	name   string
+	loaded bool
+	ch     chan struct{}
+}
+
+func (tds *TestDelayedSensor) GetName() string {
+	return tds.name
+}
+
+func (tds *TestDelayedSensor) IsLoaded() bool {
+	return tds.loaded
+}
+
+func (tds *TestDelayedSensor) Load(_ string) error {
+	select {
+	case <-tds.ch:
+	case <-time.After(10 * time.Second):
+		return fmt.Errorf("TestDelayedSensor/Load timeout when waiting for unblocking")
+	}
+	tds.loaded = true
+	return nil
+}
+
+func (tds *TestDelayedSensor) Unload() error {
+	select {
+	case <-tds.ch:
+	case <-time.After(10 * time.Second):
+		return fmt.Errorf("TestDelayedSensor/Unload timeout when waiting for unblocking")
+	}
+	tds.loaded = false
+	return nil
+}
+
+func (tds *TestDelayedSensor) Destroy() {
+	tds.loaded = false
+}
+
+func (tds *TestDelayedSensor) unblock(t *testing.T) {
+	select {
+	case tds.ch <- struct{}{}:
+	default:
+		t.Fatalf("unblocked failed: channel does not seem to be empty")
+	}
+
+}
