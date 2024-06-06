zyjj1/cilium-tetragon

autochore: make crds for RuntimeSecurityPolicy
Generate the k8s files for the newly added RuntimeSecurityPolicy CRD.

Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

pkg/k8s/apis/cilium.io/v1alpha1/zz_generated.deepcopy.go
@@ -114,6 +114,29 @@ func (in *EnforcerSpec) DeepCopy() *EnforcerSpec {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExecutableSelector) DeepCopyInto(out *ExecutableSelector) {
+	*out = *in
+	if in.MatchPaths != nil {
+		in, out := &in.MatchPaths, &out.MatchPaths
+		*out = make([]MatchPathSelector, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExecutableSelector.
+func (in *ExecutableSelector) DeepCopy() *ExecutableSelector {
+	if in == nil {
+		return nil
+	}
+	out := new(ExecutableSelector)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *KProbeArg) DeepCopyInto(out *KProbeArg) {
 	*out = *in
 	return
@@ -271,6 +294,27 @@ func (in *ListSpec) DeepCopy() *ListSpec {
 		return nil
 	}
 	out := new(ListSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *MatchPathSelector) DeepCopyInto(out *MatchPathSelector) {
+	*out = *in
+	if in.Paths != nil {
+		in, out := &in.Paths, &out.Paths
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new MatchPathSelector.
+func (in *MatchPathSelector) DeepCopy() *MatchPathSelector {
+	if in == nil {
+		return nil
+	}
+	out := new(MatchPathSelector)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -466,6 +510,157 @@ func (in *PodInfoStatus) DeepCopy() *PodInfoStatus {
 		return nil
 	}
 	out := new(PodInfoStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuleExecutionConfig) DeepCopyInto(out *RuleExecutionConfig) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuleExecutionConfig.
+func (in *RuleExecutionConfig) DeepCopy() *RuleExecutionConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(RuleExecutionConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicy) DeepCopyInto(out *RuntimeSecurityPolicy) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicy.
+func (in *RuntimeSecurityPolicy) DeepCopy() *RuntimeSecurityPolicy {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicy)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *RuntimeSecurityPolicy) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicyList) DeepCopyInto(out *RuntimeSecurityPolicyList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]RuntimeSecurityPolicy, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicyList.
+func (in *RuntimeSecurityPolicyList) DeepCopy() *RuntimeSecurityPolicyList {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicyList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *RuntimeSecurityPolicyList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicyRule) DeepCopyInto(out *RuntimeSecurityPolicyRule) {
+	*out = *in
+	if in.ExecutionConfig != nil {
+		in, out := &in.ExecutionConfig, &out.ExecutionConfig
+		*out = new(RuleExecutionConfig)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicyRule.
+func (in *RuntimeSecurityPolicyRule) DeepCopy() *RuntimeSecurityPolicyRule {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicyRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicySelector) DeepCopyInto(out *RuntimeSecurityPolicySelector) {
+	*out = *in
+	if in.PodSelector != nil {
+		in, out := &in.PodSelector, &out.PodSelector
+		*out = new(v1.LabelSelector)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.ExecutableSelector != nil {
+		in, out := &in.ExecutableSelector, &out.ExecutableSelector
+		*out = new(ExecutableSelector)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicySelector.
+func (in *RuntimeSecurityPolicySelector) DeepCopy() *RuntimeSecurityPolicySelector {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicySelector)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicySpec) DeepCopyInto(out *RuntimeSecurityPolicySpec) {
+	*out = *in
+	if in.Selectors != nil {
+		in, out := &in.Selectors, &out.Selectors
+		*out = new(RuntimeSecurityPolicySelector)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Rules != nil {
+		in, out := &in.Rules, &out.Rules
+		*out = make([]RuntimeSecurityPolicyRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicySpec.
+func (in *RuntimeSecurityPolicySpec) DeepCopy() *RuntimeSecurityPolicySpec {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicySpec)
 	in.DeepCopyInto(out)
 	return out
 }

cilium/tetragon

pkg/k8s/apis/cilium.io/v1alpha1/zz_generated.deepcopy.go
@@ -114,6 +114,29 @@ func (in *EnforcerSpec) DeepCopy() *EnforcerSpec {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExecutableSelector) DeepCopyInto(out *ExecutableSelector) {
+	*out = *in
+	if in.MatchPaths != nil {
+		in, out := &in.MatchPaths, &out.MatchPaths
+		*out = make([]MatchPathsSelector, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExecutableSelector.
+func (in *ExecutableSelector) DeepCopy() *ExecutableSelector {
+	if in == nil {
+		return nil
+	}
+	out := new(ExecutableSelector)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *KProbeArg) DeepCopyInto(out *KProbeArg) {
 	*out = *in
 	return
@@ -271,6 +294,27 @@ func (in *ListSpec) DeepCopy() *ListSpec {
 		return nil
 	}
 	out := new(ListSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *MatchPathsSelector) DeepCopyInto(out *MatchPathsSelector) {
+	*out = *in
+	if in.Values != nil {
+		in, out := &in.Values, &out.Values
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new MatchPathsSelector.
+func (in *MatchPathsSelector) DeepCopy() *MatchPathsSelector {
+	if in == nil {
+		return nil
+	}
+	out := new(MatchPathsSelector)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -466,6 +510,157 @@ func (in *PodInfoStatus) DeepCopy() *PodInfoStatus {
 		return nil
 	}
 	out := new(PodInfoStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuleExecutionConfig) DeepCopyInto(out *RuleExecutionConfig) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuleExecutionConfig.
+func (in *RuleExecutionConfig) DeepCopy() *RuleExecutionConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(RuleExecutionConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicy) DeepCopyInto(out *RuntimeSecurityPolicy) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicy.
+func (in *RuntimeSecurityPolicy) DeepCopy() *RuntimeSecurityPolicy {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicy)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *RuntimeSecurityPolicy) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicyList) DeepCopyInto(out *RuntimeSecurityPolicyList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]RuntimeSecurityPolicy, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicyList.
+func (in *RuntimeSecurityPolicyList) DeepCopy() *RuntimeSecurityPolicyList {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicyList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *RuntimeSecurityPolicyList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicyRule) DeepCopyInto(out *RuntimeSecurityPolicyRule) {
+	*out = *in
+	if in.ExecutionConfig != nil {
+		in, out := &in.ExecutionConfig, &out.ExecutionConfig
+		*out = new(RuleExecutionConfig)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicyRule.
+func (in *RuntimeSecurityPolicyRule) DeepCopy() *RuntimeSecurityPolicyRule {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicyRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicySelector) DeepCopyInto(out *RuntimeSecurityPolicySelector) {
+	*out = *in
+	if in.PodSelector != nil {
+		in, out := &in.PodSelector, &out.PodSelector
+		*out = new(v1.LabelSelector)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.ExecutableSelector != nil {
+		in, out := &in.ExecutableSelector, &out.ExecutableSelector
+		*out = new(ExecutableSelector)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicySelector.
+func (in *RuntimeSecurityPolicySelector) DeepCopy() *RuntimeSecurityPolicySelector {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicySelector)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RuntimeSecurityPolicySpec) DeepCopyInto(out *RuntimeSecurityPolicySpec) {
+	*out = *in
+	if in.Selectors != nil {
+		in, out := &in.Selectors, &out.Selectors
+		*out = new(RuntimeSecurityPolicySelector)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Rules != nil {
+		in, out := &in.Rules, &out.Rules
+		*out = make([]RuntimeSecurityPolicyRule, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RuntimeSecurityPolicySpec.
+func (in *RuntimeSecurityPolicySpec) DeepCopy() *RuntimeSecurityPolicySpec {
+	if in == nil {
+		return nil
+	}
+	out := new(RuntimeSecurityPolicySpec)
 	in.DeepCopyInto(out)
 	return out
 }