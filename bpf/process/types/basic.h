e83ac152f9e1c3ed0273b2caccdbb53d861d36a6
Mahe Tardy <mahe.tardy@gmail.com>
2024-06-13 16:53:12 +0200

    bpf: remove rate limit maps and structs for kernel <5.3
    [ upstream commit ed824b3ae42515feb5fdd044a4b6cafde9af4b66 ]
    
    Since the rate limit feature is only available for LARGE_BPF_PROG, let's
    remove the unnecessary map and the struct from the small BPF progs.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- bpf/process/types/basic.h
+++ bpf/process/types/basic.h
@@ -2016,6 +2016,7 @@ do_action_signal(int signal)
  */
 #define KEY_BYTES_PER_ARG 40
 
+#ifdef __LARGE_BPF_PROG
 /* Rate limit scope. */
 #define ACTION_RATE_LIMIT_SCOPE_THREAD	0
 #define ACTION_RATE_LIMIT_SCOPE_PROCESS 1
@@ -2057,7 +2058,6 @@ struct {
 	__type(value, __u8[sizeof(struct ratelimit_key) + 128]);
 } ratelimit_ro_heap SEC(".maps");
 
-#ifdef __LARGE_BPF_PROG
 static inline __attribute__((always_inline)) bool
 rate_limit(__u64 ratelimit_interval, __u64 ratelimit_scope, struct msg_generic_kprobe *e)
 {
