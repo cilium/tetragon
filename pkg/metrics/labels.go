4cebe0f4cd3feb745839c2241c565d9f15e385dd
Anna Kapuscinska <anna@isovalent.com>
2024-06-17 16:11:22 +0100

    Extend metrics library
    This is a general rewrite of pkg/metrics library, but it a fully backwards
    compatible way. Goals and key components are described in doc.go, so I won't
    repeat them here.
    
    Fixes: #2376
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/metrics/labels.go
+++ pkg/metrics/labels.go
@@ -3,31 +3,38 @@
 
 package metrics
 
-type FilteredLabels interface {
-	Keys() []string
-	Values() []string
+import "github.com/prometheus/client_golang/prometheus"
+
+// ConstrainedLabel represents a label with constrained cardinality.
+// Values is a list of all possible values of the label.
+type ConstrainedLabel struct {
+	Name   string
+	Values []string
 }
 
-type ProcessLabels struct {
-	Namespace string
-	Workload  string
-	Pod       string
-	Binary    string
+// UnconstrainedLabel represents a label with unconstrained cardinality.
+// ExampleValue is an example value of the label used for documentation.
+type UnconstrainedLabel struct {
+	Name         string
+	ExampleValue string
 }
 
-func NewProcessLabels(namespace, workload, pod, binary string) *ProcessLabels {
-	return &ProcessLabels{
-		Namespace: namespace,
-		Workload:  workload,
-		Pod:       pod,
-		Binary:    binary,
+func stringToUnconstrained(labels []string) []UnconstrainedLabel {
+	unconstrained := make([]UnconstrainedLabel, len(labels))
+	for i, label := range labels {
+		unconstrained[i] = UnconstrainedLabel{
+			Name:         label,
+			ExampleValue: "example",
+		}
 	}
+	return unconstrained
 }
 
-func (l ProcessLabels) Keys() []string {
-	return []string{"namespace", "workload", "pod", "binary"}
-}
-
-func (l ProcessLabels) Values() []string {
-	return []string{l.Namespace, l.Workload, l.Pod, l.Binary}
+func promContainsLabel(labels prometheus.ConstrainedLabels, label string) bool {
+	for _, l := range labels {
+		if l.Name == label {
+			return true
+		}
+	}
+	return false
 }
