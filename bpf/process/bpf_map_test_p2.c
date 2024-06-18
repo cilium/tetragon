56b8922ede012e4f80b5d567970c3cb0a62ba934
Jiri Olsa <jolsa@kernel.org>
2024-06-18 08:15:58 +0000

    tetragon: Add tests for map builders
    Add tests for the map builders, will be likely extended.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ bpf/process/bpf_map_test_p2.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include "vmlinux.h"
+#include "api.h"
+#include "bpf_tracing.h"
+#include "bpf_helpers.h"
+#include "compiler.h"
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, int);
+} m1 SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, int);
+} m2 SEC(".maps");
+
+__attribute__((section("kprobe/wake_up_new_task"), used)) int
+BPF_KPROBE(p2)
+{
+	map_lookup_elem(&m1, &(int){ 0 });
+	map_lookup_elem(&m2, &(int){ 0 });
+	return 0;
+}
