e2159b9c741ae4cbf2c28b834134f8579b8a1c10
Djalal Harouni <tixxdz@gmail.com>
2024-06-20 16:22:02 +0100

    tetragon: resolve uid->username for processes from /proc
    Right now we resolve uid->username only for processes that
    start after tetragon. To also handle the ones that start before,
    we need to resolve the username during /proc fs scanning.
    
    This patch adds userinfo module part of sensors/exec and call it
    for both bpf exec sensors and proc_reader procfs scanning.
    
    We keep same semantics if a process is not in mount and user host
    namespaces we do not resolve its username.
    
    Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

---
+++ pkg/sensors/exec/userinfo/userinfo.go
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package userinfo
+
+import (
+	"errors"
+
+	"github.com/cilium/tetragon/pkg/api/processapi"
+	"github.com/cilium/tetragon/pkg/grpc/exec"
+	"github.com/cilium/tetragon/pkg/option"
+	"github.com/cilium/tetragon/pkg/reader/namespace"
+	"github.com/cilium/tetragon/pkg/reader/userdb"
+)
+
+// Errors
+var (
+	ErrNotInHostNs = errors.New("process is not in host namespaces")
+)
+
+func getAccountUnix(uid uint32, ns *processapi.MsgNamespaces) (string, error) {
+	inHost, err := namespace.IsMsgNsInHostMntUser(ns)
+	if err != nil {
+		return "", err
+	}
+	if inHost {
+		username, err := userdb.UsersCache.LookupUser(uid)
+		if err != nil {
+			return "", err
+		}
+		return username, nil
+	}
+	return "", ErrNotInHostNs
+}
+
+func MsgToExecveAccountUnix(m *exec.MsgExecveEventUnix) error {
+	if option.Config.UsernameMetadata == int(option.USERNAME_METADATA_UNIX) {
+		username, err := getAccountUnix(m.Unix.Process.UID, &m.Unix.Msg.Namespaces)
+		if err != nil {
+			return err
+		}
+
+		m.Unix.Process.User.Name = username
+	}
+	return nil
+}
