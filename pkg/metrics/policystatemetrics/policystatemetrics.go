f568f4ee9cdb7719a2a23e07713630fe88419d8a
Anna Kapuscinska <anna@isovalent.com>
2024-06-23 21:22:30 +0100

    policystatemetrics: Refactor metrics collector
    Use new helpers from pkg/metrics to define custom metrics and collector.
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/metrics/policystatemetrics/policystatemetrics.go
+++ pkg/metrics/policystatemetrics/policystatemetrics.go
@@ -10,41 +10,41 @@ import (
 
 	"github.com/cilium/tetragon/api/v1/tetragon"
 	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/metrics"
 	"github.com/cilium/tetragon/pkg/metrics/consts"
 	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-type policyStateCollector struct {
-	descriptor *prometheus.Desc
+var stateLabel = metrics.ConstrainedLabel{
+	Name: "state",
+	Values: []string{
+		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_LOAD_ERROR.String()), "tp_state_"),
+		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ERROR.String()), "tp_state_"),
+		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_DISABLED.String()), "tp_state_"),
+		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ENABLED.String()), "tp_state_"),
+	},
 }
 
-func InitMetrics(registry *prometheus.Registry) {
-	registry.MustRegister(newPolicyStateCollector())
-}
-
-func InitMetricsForDocs(registry *prometheus.Registry) {
-	registry.MustRegister(newPolicyStateZeroCollector())
-}
+var policyState = metrics.MustNewCustomGauge(metrics.NewOpts(
+	consts.MetricsNamespace, "", "tracingpolicy_loaded",
+	"The number of loaded tracing policy by state.",
+	nil, []metrics.ConstrainedLabel{stateLabel}, nil,
+))
 
 // This metric collector converts the output of ListTracingPolicies into a few
 // gauges metrics on collection. Thus, it needs a sensor manager to query.
-func newPolicyStateCollector() *policyStateCollector {
-	return &policyStateCollector{
-		descriptor: prometheus.NewDesc(
-			prometheus.BuildFQName(consts.MetricsNamespace, "", "tracingpolicy_loaded"),
-			"The number of loaded tracing policy by state.",
-			[]string{"state"}, nil,
-		),
-	}
+func NewPolicyStateCollector() metrics.CollectorWithInit {
+	return metrics.NewCustomCollector(
+		metrics.CustomMetrics{
+			policyState,
+		},
+		collect,
+		collectForDocs,
+	)
 }
 
-func (c *policyStateCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.descriptor
-}
-
-func (c *policyStateCollector) Collect(ch chan<- prometheus.Metric) {
-
+func collect(ch chan<- prometheus.Metric) {
 	sm := observer.GetSensorManager()
 	if sm == nil {
 		logger.GetLogger().Debug("failed retrieving the sensor manager: manager is nil")
@@ -65,72 +65,26 @@ func (c *policyStateCollector) Collect(ch chan<- prometheus.Metric) {
 		counters[state]++
 	}
 
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
+	ch <- policyState.MustMetric(
 		float64(counters[tetragon.TracingPolicyState_TP_STATE_LOAD_ERROR]),
 		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_LOAD_ERROR.String()), "tp_state_"),
 	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
+	ch <- policyState.MustMetric(
 		float64(counters[tetragon.TracingPolicyState_TP_STATE_ERROR]),
 		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ERROR.String()), "tp_state_"),
 	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
+	ch <- policyState.MustMetric(
 		float64(counters[tetragon.TracingPolicyState_TP_STATE_DISABLED]),
 		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_DISABLED.String()), "tp_state_"),
 	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
+	ch <- policyState.MustMetric(
 		float64(counters[tetragon.TracingPolicyState_TP_STATE_ENABLED]),
 		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ENABLED.String()), "tp_state_"),
 	)
 }
 
-// policyStateZeroCollector implements prometheus.Collector. It collects "zero"
-// metrics. It's intended to be used when the sensor manager doesn't exist, but
-// we still want Prometheus metrics to be exposed.
-type policyStateZeroCollector struct {
-	policyStateCollector
-}
-
-func newPolicyStateZeroCollector() prometheus.Collector {
-	return &policyStateZeroCollector{
-		policyStateCollector: *newPolicyStateCollector(),
+func collectForDocs(ch chan<- prometheus.Metric) {
+	for _, state := range stateLabel.Values {
+		ch <- policyState.MustMetric(0, state)
 	}
-}
-
-func (c *policyStateZeroCollector) Describe(ch chan<- *prometheus.Desc) {
-	c.policyStateCollector.Describe(ch)
-}
-
-func (c *policyStateZeroCollector) Collect(ch chan<- prometheus.Metric) {
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
-		0,
-		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_LOAD_ERROR.String()), "tp_state_"),
-	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
-		0,
-		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ERROR.String()), "tp_state_"),
-	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
-		0,
-		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_DISABLED.String()), "tp_state_"),
-	)
-	ch <- prometheus.MustNewConstMetric(
-		c.descriptor,
-		prometheus.GaugeValue,
-		0,
-		strings.TrimPrefix(strings.ToLower(tetragon.TracingPolicyState_TP_STATE_ENABLED.String()), "tp_state_"),
-	)
 }
