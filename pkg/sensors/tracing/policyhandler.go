cilium/tetragon

commit 8eb13e8abf2caddae083f09979adf5cebdc32e11

tetragon: Add generic LSM sensor
Adding generic LSM sensor that reads LSM crd config
and setups LSM programs.
The LSM is configured with hook, like:

  spec:
    lsmhooks:
    - hook: "bprm_check_security"

Signed-off-by: Andrei Fedotov <anfedotoff@yandex-team.ru>

pkg/sensors/tracing/policyhandler.go
@@ -44,5 +44,9 @@ func (h policyHandler) PolicyHandler(
 		name := fmt.Sprintf("gtp-sensor-%d", atomic.AddUint64(&sensorCounter, 1))
 		return createGenericTracepointSensor(spec, name, policyID, policyName, handler)
 	}
+	if len(spec.LsmHooks) > 0 {
+		name := fmt.Sprintf("glsm-sensor-%d", atomic.AddUint64(&sensorCounter, 1))
+		return createGenericLsmSensor(spec, name, policyID, policyName)
+	}
 	return nil, nil
 }

pkg/api/ops/ops.go
@@ -23,6 +23,7 @@ const (
 	MSG_OP_GENERIC_KPROBE     = 13
 	MSG_OP_GENERIC_TRACEPOINT = 14
 	MSG_OP_GENERIC_UPROBE     = 15
+	MSG_OP_GENERIC_LSM        = 16
 
 	// MSG_OP_CLONE notifies user-space that a clone() event has occurred.
 	MSG_OP_CLONE = 23

pkg/grpc/tracing/tracing.go
@@ -815,6 +815,117 @@ func (msg *MsgGenericUprobeUnix) Cast(o interface{}) notify.Message {
 	return &t
 }
 
+type MsgGenericLsmUnix struct {
+	Msg        *tracingapi.MsgGenericKprobe
+	Hook       string
+	Args       []tracingapi.MsgGenericKprobeArg
+	PolicyName string
+	Message    string
+	Tags       []string
+}
+
+func (msg *MsgGenericLsmUnix) Notify() bool {
+	return true
+}
+
+func (msg *MsgGenericLsmUnix) RetryInternal(ev notify.Event, timestamp uint64) (*process.ProcessInternal, error) {
+	return eventcache.HandleGenericInternal(ev, msg.Msg.ProcessKey.Pid, &msg.Msg.Tid, timestamp)
+}
+
+func (msg *MsgGenericLsmUnix) Retry(internal *process.ProcessInternal, ev notify.Event) error {
+	return eventcache.HandleGenericEvent(internal, ev, &msg.Msg.Tid)
+}
+
+func (msg *MsgGenericLsmUnix) HandleMessage() *tetragon.GetEventsResponse {
+	k := GetProcessLsm(msg)
+	if k == nil {
+		return nil
+	}
+	return &tetragon.GetEventsResponse{
+		Event:    &tetragon.GetEventsResponse_ProcessLsm{ProcessLsm: k},
+		NodeName: nodeName,
+		Time:     ktime.ToProto(msg.Msg.Common.Ktime),
+	}
+}
+
+func (msg *MsgGenericLsmUnix) Cast(o interface{}) notify.Message {
+	t := o.(MsgGenericLsmUnix)
+	return &t
+}
+
+func (msg *MsgGenericLsmUnix) PolicyInfo() tracingpolicy.PolicyInfo {
+	return tracingpolicy.PolicyInfo{
+		Name: msg.PolicyName,
+		Hook: fmt.Sprintf("lsm:%s", msg.Hook),
+	}
+}
+
+func GetProcessLsm(event *MsgGenericLsmUnix) *tetragon.ProcessLsm {
+	var tetragonParent, tetragonProcess *tetragon.Process
+	var tetragonArgs []*tetragon.KprobeArgument
+
+	proc, parent := process.GetParentProcessInternal(event.Msg.ProcessKey.Pid, event.Msg.ProcessKey.Ktime)
+	if proc == nil {
+		tetragonProcess = &tetragon.Process{
+			Pid:       &wrapperspb.UInt32Value{Value: event.Msg.ProcessKey.Pid},
+			StartTime: ktime.ToProto(event.Msg.ProcessKey.Ktime),
+		}
+	} else {
+		tetragonProcess = proc.UnsafeGetProcess()
+		if err := proc.AnnotateProcess(option.Config.EnableProcessCred, option.Config.EnableProcessNs); err != nil {
+			logger.GetLogger().WithError(err).WithField("processId", tetragonProcess.Pid).Debugf("Failed to annotate process with capabilities and namespaces info")
+		}
+	}
+	if parent != nil {
+		tetragonParent = parent.UnsafeGetProcess()
+	}
+
+	for _, arg := range event.Args {
+		a := getKprobeArgument(arg)
+		tetragonArgs = append(tetragonArgs, a)
+	}
+
+	tetragonEvent := &tetragon.ProcessLsm{
+		Process:      tetragonProcess,
+		Parent:       tetragonParent,
+		FunctionName: event.Hook,
+		Args:         tetragonArgs,
+		Action:       kprobeAction(event.Msg.ActionId),
+		PolicyName:   event.PolicyName,
+		Message:      event.Message,
+		Tags:         event.Tags,
+	}
+
+	if tetragonProcess.Pid == nil {
+		eventcachemetrics.EventCacheError(eventcachemetrics.NilProcessPid, notify.EventType(tetragonEvent)).Inc()
+		return nil
+	}
+
+	if ec := eventcache.Get(); ec != nil &&
+		(ec.Needed(tetragonProcess) ||
+			(tetragonProcess.Pid.Value > 1 && ec.Needed(tetragonParent))) {
+		ec.Add(nil, tetragonEvent, event.Msg.Common.Ktime, event.Msg.ProcessKey.Ktime, event)
+		return nil
+	}
+
+	if proc != nil {
+		// At kprobes we report the per thread fields, so take a copy
+		// of the thread leader from the cache then update the corresponding
+		// per thread fields.
+		//
+		// The cost to get this is relatively high because it requires a
+		// deep copy of all the fields of the thread leader from the cache in
+		// order to safely modify them, to not corrupt gRPC streams.
+		tetragonEvent.Process = proc.GetProcessCopy()
+		process.UpdateEventProcessTid(tetragonEvent.Process, &event.Msg.Tid)
+	}
+	if parent != nil {
+		tetragonEvent.Parent = tetragonParent
+	}
+
+	return tetragonEvent
+}
+
 type MsgProcessThrottleUnix struct {
 	Type   tetragon.ThrottleType
 	Cgroup string

pkg/k8s/apis/cilium.io/v1alpha1/tracing_policy_types.go
@@ -85,6 +85,9 @@ type TracingPolicySpec struct {
 	// +kubebuilder:validation:Optional
 	// A list of uprobe specs.
 	UProbes []UProbeSpec `json:"uprobes,omitempty"`
+	// +kubebuilder:validation:Optional
+	// A list of uprobe specs.
+	LsmHooks []LsmHookSpec `json:"lsmhooks,omitempty"`
 
 	// +kubebuilder:validation:Optional
 	// PodSelector selects pods that this policy applies to

pkg/k8s/apis/cilium.io/v1alpha1/types.go
@@ -285,6 +285,26 @@ type UProbeSpec struct {
 	Tags []string `json:"tags,omitempty"`
 }
 
+type LsmHookSpec struct {
+	// Name of the function to apply the kprobe spec to.
+	Hook string `json:"hook"`
+	// +kubebuilder:validation:Optional
+	// A short message of 256 characters max that will be included
+	// in the event output to inform users what is going on.
+	Message string `json:"message"`
+	// +kubebuilder:validation:Optional
+	// A list of function arguments to include in the trace output.
+	Args []KProbeArg `json:"args,omitempty"`
+	// +kubebuilder:validation:Optional
+	// Selectors to apply before producing trace output. Selectors are ORed.
+	Selectors []KProbeSelector `json:"selectors,omitempty"`
+	// +kubebuilder:validation:optional
+	// +kubebuilder:validation:MaxItems=16
+	// Tags to categorize the event, will be include in the event output.
+	// Maximum of 16 Tags are supported.
+	Tags []string `json:"tags,omitempty"`
+}
+
 type ListSpec struct {
 	// Name of the list
 	Name string `json:"name"`

pkg/k8s/apis/cilium.io/v1alpha1/version.go
@@ -7,4 +7,4 @@ package v1alpha1
 // Used to determine if CRD needs to be updated in cluster
 //
 // Developers: Bump patch for each change in the CRD schema.
-const CustomResourceDefinitionSchemaVersion = "1.2.0"
+const CustomResourceDefinitionSchemaVersion = "1.2.1"

commit 1225f7aeb210eb4d37f297f7ec0881b50894ec2e

tracing: allow only one of kprobes/tracepoints/lsm
For historic reasons, the tracing sensor has three different aspects:
kprobes, tracepoints, and (recently) lsm hooks.

Also for historic reasons, we did not allow tracepoints and kprobes in
the same policy.

With the addition of the LSM sensor
(8eb13e8abf2caddae083f09979adf5cebdc32e11), if a policy includes an lsm
section together with either a kprobe section or a tracepoint section,
the lsm section will be ignored.

This patch rejects policies that have more than one section of kprobes,
tracepoints, and lsm hooks in the policy.

A better solution would be to decouple the tracing sensor, and create
one sensor for kprobes, one for tracepoints, and one for lsm sensors.
See: https://github.com/cilium/tetragon/issues/2706

Fixes: 8eb13e8abf2caddae083f09979adf5cebdc32e11

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/sensors/tracing/policyhandler.go
@@ -27,8 +27,19 @@ func (h policyHandler) PolicyHandler(
 
 	policyName := policy.TpName()
 	spec := policy.TpSpec()
-	if len(spec.KProbes) > 0 && len(spec.Tracepoints) > 0 {
-		return nil, errors.New("tracing policies with both kprobes and tracepoints are not currently supported")
+
+	sections := 0
+	if len(spec.KProbes) > 0 {
+		sections++
+	}
+	if len(spec.Tracepoints) > 0 {
+		sections++
+	}
+	if len(spec.LsmHooks) > 0 {
+		sections++
+	}
+	if sections > 1 {
+		return nil, errors.New("tracing policies with multiple sections of kprobes, tracepoints, or lsm hooks are currently not supported")
 	}
 
 	handler := eventhandler.GetCustomEventhandler(policy)