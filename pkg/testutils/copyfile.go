449c05cda599f61ba200a5590325bb9ff62a3ec9
Kornilios Kourtis <kornilios@isovalent.com>
2024-07-24 11:01:02 +0200

    testutils: add a CopyFileToTmp function
    CopyFileToTmp copies a file to /tmp and returns its name.
    It also chmods the destination file, so it can be used to copy binaries.
    It's going to be used the next patch.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/testutils/copyfile.go
+++ pkg/testutils/copyfile.go
@@ -4,8 +4,10 @@
 package testutils
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path"
 	"syscall"
 )
 
@@ -27,4 +29,39 @@ func CopyFile(toPath, fromPath string, perm os.FileMode) error {
 		syscall.Unlink(toPath)
 	}
 	return err
+}
+
+func CopyFileToTmp(fname string) (string, error) {
+	bname := path.Base(fname)
+	pattern := fmt.Sprintf("%s.*", bname)
+	outf, err := os.CreateTemp("", pattern)
+	if err != nil {
+		return "", err
+	}
+	defer outf.Close()
+
+	inf, err := os.Open(fname)
+	if err != nil {
+		os.Remove(outf.Name())
+		return "", err
+	}
+	defer inf.Close()
+
+	info, err := inf.Stat()
+	if err != nil {
+		os.Remove(outf.Name())
+		return "", err
+	}
+	err = os.Chmod(outf.Name(), info.Mode())
+	if err != nil {
+		os.Remove(outf.Name())
+		return "", err
+	}
+
+	if _, err := io.Copy(outf, inf); err != nil {
+		os.Remove(outf.Name())
+		return "", err
+	}
+
+	return outf.Name(), nil
 }
