2943ad274a96762091eb714480b23b2ac3eb199e
Jiri Olsa <jolsa@kernel.org>
2024-07-26 22:00:17 +0000

    tetragon: Clean up cgroup_rate_map map periodically
    Making sure we evict inactive values from cgroup_rate_map map
    once per minute.
    
    We do have cleanup hook on cgroup_rmdir tracepoint, but we can
    still end up with leftover records in cgroup_rate_map for quiet
    cgroups.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/metrics/cgroupratemetrics/cgroupratemetrics.go
+++ pkg/metrics/cgroupratemetrics/cgroupratemetrics.go
@@ -16,8 +16,10 @@ const (
 	ThrottleStop
 	LookupFail
 	UpdateFail
+	DeleteFail
 	Check
 	Process
+	Delete
 )
 
 var totalLabelValues = map[CgroupRateType]string{
@@ -25,8 +27,10 @@ var totalLabelValues = map[CgroupRateType]string{
 	ThrottleStop:  "throttle_stop",
 	LookupFail:    "lookup_fail",
 	UpdateFail:    "update_fail",
+	DeleteFail:    "delete_fail",
 	Check:         "check",
 	Process:       "process",
+	Delete:        "delete",
 }
 
 func (e CgroupRateType) String() string {
