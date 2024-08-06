90ad10ae2a27e4d2ab80d1bcc2df3c33f24fbfce
Kornilios Kourtis <kornilios@isovalent.com>
2024-08-06 14:54:55 +0200

    rthooks: fix comments
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/rthooks/rthooks.go
+++ pkg/rthooks/rthooks.go
@@ -2,8 +2,25 @@
 // Copyright Authors of Tetragon
 
 // Package rthooks contains code for managing run-time hooks
+//
 // Runtime hooks are hooks for (synchronously) notifying the agent for runtime
 // events such as the creation of a container.
+//
+// Different parts of the agent can register callbacks that will run sequentially when a
+// RuntimeHookRequest is issued.
+//
+// Specifically:
+//  - sensors can register their callbacks at init() using RegisterCallbacksAtInit
+//    which registers in these hooks in globalRunner.
+//  - after init(), GlobalRunner() can be used to retrieve this runner and pass
+//    it to the gRPC server code so that it can execute these callbacks when a
+//    RuntimeHookRequest is issued.
+//  - some of these hooks need access to pkg/watcher, so before passing the
+//    runner to gRPC server, we add the watcher as well. Hooks can access the
+//    watcher via the argument passed in the executed callback.
+//  - all callbacks are executed, i.e., if a callback returns an error execution of callbacks does
+//    not stop
+
 package rthooks
 
 import (
@@ -16,19 +33,6 @@ import (
 var (
 	globalRunner = &Runner{}
 )
-
-// rthooks  provides a way for the gRPC server code to trigger sensor-specific
-// code when a RuntimeHookRequest is issued.
-//
-// Specifically:
-//  - sensors can register their callbacks at init() using RegisterCallbacksAtInit
-//    which registers in these hooks in globalRunner.
-//  - after init(), GlobalRunner() can be used to retrieve this runner and pass
-//    it to the gRPC server code so that it can execute these callbacks when a
-//    RuntimeHookRequest is issued.
-//  - some of these hooks need access to pkg/watcher, so before passing the
-//    runner to gRPC server, we add the watcher as well. Hooks can access the
-//    watcher via the argument passed in the executed callback.
 
 // RegisterCallbacksAtInit registers callbacks (should be called at init())
 func RegisterCallbacksAtInit(cbs Callbacks) {
