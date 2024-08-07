f703accd12ac257ba68a6fa13fe341da183be118
Kornilios Kourtis <kornilios@isovalent.com>
2024-08-07 17:21:05 +0200

    rthooks: ContainerID() support crio paths
    crio paths add a prefix to the container path. Remove it when we return
    the container id. Add a test.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/rthooks/args_test.go
+++ pkg/rthooks/args_test.go
@@ -26,3 +26,21 @@ func TestPodIDFromCgroupPath(t *testing.T) {
 		assert.Equal(t, tc.id, podIDFromCgroupPath(tc.path))
 	}
 }
+
+func TestContainerIDFromCgroupPath(t *testing.T) {
+	type test struct {
+		id, path string
+	}
+
+	ts := []test{{
+		path: "/kubepods/besteffort/pod05e102bf-8744-4942-a241-9b6f07983a53/f52a212505a606972cf8614c3cb856539e71b77ecae33436c5ac442232fbacf8",
+		id:   "f52a212505a606972cf8614c3cb856539e71b77ecae33436c5ac442232fbacf8",
+	}, {
+		path: "/kubepods/besteffort/pod897277d4-5e6f-4999-a976-b8340e8d075e/crio-a4d6b686848a610472a2eed3ae20d4d64b6b4819feb9fdfc7fd7854deaf59ef3",
+		id:   "a4d6b686848a610472a2eed3ae20d4d64b6b4819feb9fdfc7fd7854deaf59ef3",
+	}}
+
+	for _, tc := range ts {
+		assert.Equal(t, tc.id, containerIDFromCgroupPath(tc.path))
+	}
+}
