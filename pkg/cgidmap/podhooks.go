d6a0cd1aaf85f992fd28a14e318e6361f59cf1e9
Kornilios Kourtis <kornilios@isovalent.com>
2024-08-09 09:59:24 +0200

    cgidmap: map for improved pod association
    Pod association is attaching pod information to events.
    
    Previously, pod association happened using the cgroup name under the
    assumption that each container is executed directly on a cgroup with a
    name that contains the container id of the k8s pod. Hence, by having
    events (bpf and proc) include the cgroup name, it was possible to
    retrieve the container id and from that the pod.
    
    This approach, however, has some issues. One example is that crun, an
    OCI runner used by cri-o, may (and by default will) execute containers
    in a cgroup where the name does include the container id.
    
    For example:
    kubepods-besteffort.slice/kubepods-besteffort-pod42b7841b_bb27_49e2_81bc_3d3a372dd231.slice/crio-6727930f5d4e9b799d58732af6882826591eb476f680b6499d8919d1ec515eee.scope/container/
    
    In this case, the pod association scheme based on the cgroup name does
    not work.
    
    To address the above and other issues, this patch introduces cgidmap,
    which allows doing pod association by cgroup id (rather than cgroup name).
    
    The general idea is that:
     - events  (bpf and proc) will include the cgroup id, which can be used
       to query the map for the container id. Once we have the container id,
       pod association can proceed as previously.
    
     - mappings of cgroup ids to container ids will be added by the runtime
       hooks, before a container starts.
    
     - changes in pods (pod removal, container removal) will be handled via
       the K8s API server pod watcher.
    
    One issue left for subsequent patches is dealing with cases where the
    runtime hook is not running, which is required for pod association for
    pods started before tetragon.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/cgidmap/podhooks.go
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package cgidmap
+
+import (
+	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/podhelpers"
+	"github.com/cilium/tetragon/pkg/podhooks"
+
+	"github.com/google/uuid"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+func init() {
+	podhooks.RegisterCallbacksAtInit(podhooks.Callbacks{
+		PodCallbacks: func(podInformer cache.SharedIndexInformer) {
+			registerPodCallbacks(podInformer)
+		},
+	})
+}
+
+func registerPodCallbacks(podInformer cache.SharedIndexInformer) {
+
+	m, err := GlobalMap()
+	if err != nil {
+		logger.GetLogger().WithError(err).Warn("failed to retrieve cgidmap, not registering rthook")
+		return
+	}
+
+	podInformer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				pod, ok := obj.(*v1.Pod)
+				if !ok {
+					logger.GetLogger().Warn("cgidmap, add-pod handler: unexpected object type: %T", pod)
+					return
+				}
+				updatePodHandler(m, pod)
+			},
+			UpdateFunc: func(_, newObj interface{}) {
+				pod, ok := newObj.(*v1.Pod)
+				if !ok {
+					logger.GetLogger().Warn("cgidmap, update-pod handler: unexpected object type(s): new:%T", pod)
+					return
+				}
+				updatePodHandler(m, pod)
+			},
+			DeleteFunc: func(obj interface{}) {
+				var pod *corev1.Pod
+				switch concreteObj := obj.(type) {
+				case *corev1.Pod:
+					pod = concreteObj
+				case cache.DeletedFinalStateUnknown:
+					// Handle the case when the watcher missed the deletion event
+					// (e.g. due to a lost apiserver connection).
+					deletedObj, ok := concreteObj.Obj.(*corev1.Pod)
+					if !ok {
+						return
+					}
+					pod = deletedObj
+				default:
+					return
+				}
+				deletePodHandler(m, pod)
+
+			},
+		},
+	)
+}
+
+func deletePodHandler(m Map, pod *corev1.Pod) {
+	podID, err := uuid.Parse(string(pod.UID))
+	if err != nil {
+		logger.GetLogger().WithError(err).WithField("pod-id", pod.UID).Warn("cgidmap, podDeleted: failed to parse pod id")
+		return
+	}
+	m.Update(podID, nil)
+}
+
+func updatePodHandler(m Map, pod *corev1.Pod) {
+	podID, err := uuid.Parse(string(pod.UID))
+	if err != nil {
+		logger.GetLogger().WithError(err).WithField("pod-id", pod.UID).Warn("cgidmap, podDeleted: failed to parse pod id")
+		return
+	}
+	m.Update(podID, podhelpers.PodContainersIDs(pod))
+}
