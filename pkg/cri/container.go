f6da71e9ed17a71a94b88c7834095a14442da5d1
Kornilios Kourtis <kornilios@isovalent.com>
2024-08-09 12:19:16 +0200

    cri: add a function to retrieve the cgroupPath
    We use https://github.com/tidwall/gjson to get the field, and, also, add
    some code to parse the cgroup path if it's in a weird systemd format.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/cri/container.go
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package cri
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/tidwall/gjson"
+	criapi "k8s.io/cri-api/pkg/apis/runtime/v1"
+)
+
+// NB(kkourt): taken from github.com/opencontainers/runc/libcontainer/cgroups/systemd
+// which does not work due to a ebpf incomaptibility:
+// # github.com/opencontainers/runc/libcontainer/cgroups/ebpf
+// vendor/github.com/opencontainers/runc/libcontainer/cgroups/ebpf/ebpf_linux.go:190:3: unknown field Replace in struct literal of type link.RawAttachProgramOptions
+//
+// systemd represents slice hierarchy using `-`, so we need to follow suit when
+// generating the path of slice. Essentially, test-a-b.slice becomes
+// /test.slice/test-a.slice/test-a-b.slice.
+func systemdExpandSlice(slice string) (string, error) {
+	suffix := ".slice"
+	// Name has to end with ".slice", but can't be just ".slice".
+	if len(slice) < len(suffix) || !strings.HasSuffix(slice, suffix) {
+		return "", fmt.Errorf("invalid slice name: %s", slice)
+	}
+
+	// Path-separators are not allowed.
+	if strings.Contains(slice, "/") {
+		return "", fmt.Errorf("invalid slice name: %s", slice)
+	}
+
+	var path, prefix string
+	sliceName := strings.TrimSuffix(slice, suffix)
+	// if input was -.slice, we should just return root now
+	if sliceName == "-" {
+		return "/", nil
+	}
+	for _, component := range strings.Split(sliceName, "-") {
+		// test--a.slice isn't permitted, nor is -test.slice.
+		if component == "" {
+			return "", fmt.Errorf("invalid slice name: %s", slice)
+		}
+
+		// Append the component to the path and to the prefix.
+		path += "/" + prefix + component + suffix
+		prefix += component + "-"
+	}
+	return path, nil
+}
+
+func ParseCgroupsPath(cgroupPath string) (string, error) {
+	if strings.Contains(cgroupPath, "/") {
+		return cgroupPath, nil
+	}
+
+	// There are some cases where CgroupsPath  is specified as "slice:prefix:name"
+	// From runc --help
+	//   --systemd-cgroup    enable systemd cgroup support, expects cgroupsPath to be of form "slice:prefix:name"
+	//                       for e.g. "system.slice:runc:434234"
+	//
+	// https://github.com/opencontainers/runc/blob/5cf9bb229feed19a767cbfdf9702f6487341e29e/libcontainer/specconv/spec_linux.go#L655-L663
+	parts := strings.Split(cgroupPath, ":")
+	if len(parts) == 3 {
+		var err error
+		slice, scope, name := parts[0], parts[1], parts[2]
+		slice, err = systemdExpandSlice(slice)
+		if err != nil {
+			return "", fmt.Errorf("failed to parse cgroup path: %s (%s does not seem to be a slice)", cgroupPath, slice)
+		}
+		// https://github.com/opencontainers/runc/blob/5cf9bb229feed19a767cbfdf9702f6487341e29e/libcontainer/cgroups/systemd/common.go#L95-L101
+		if !strings.HasSuffix(name, ".slice") {
+			name = scope + "-" + name + ".scope"
+		}
+		return filepath.Join(slice, name), nil
+	}
+
+	return "", fmt.Errorf("Unknown cgroup path: %s", cgroupPath)
+}
+
+func CgroupPath(ctx context.Context, cli criapi.RuntimeServiceClient, containerID string) (string, error) {
+	req := criapi.ContainerStatusRequest{
+		ContainerId: containerID,
+		Verbose:     true,
+	}
+	res, err := cli.ContainerStatus(ctx, &req)
+	if err != nil {
+		return "", err
+	}
+
+	info := res.GetInfo()
+	if info == nil {
+		return "", errors.New("no container info")
+	}
+
+	var path, json string
+	if infoJson, ok := info["info"]; ok {
+		json = infoJson
+		path = "runtimeSpec.linux.cgroupsPath"
+	} else {
+		return "", errors.New("could not find info")
+	}
+
+	ret := gjson.Get(json, path).String()
+	if ret == "" {
+		return "", errors.New("failed to find cgroupsPath in json")
+	}
+
+	return ParseCgroupsPath(ret)
+}
