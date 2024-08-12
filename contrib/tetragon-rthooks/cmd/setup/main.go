cilium/tetragon

commit f9913836ec9e73bc3b2726fab34b3baea898c3f8

tetragon-oci-hook-setup: prepare for nri interface
Refactor code so that we can implement the NRI interface in
tetragon-oci-hook-setup. Specifically, move everything under the
ociHooksInstall function.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

contrib/tetragon-rthooks/cmd/setup/main.go
@@ -62,7 +62,16 @@ func ociHooksConfig(binFname string, binArgs ...string) *ociHooks.Hook {
 }
 
 func (i *Install) ociHooksInstall(log *slog.Logger) {
+	var sigChan chan os.Signal
+	if i.Daemonize {
+		sigChan = make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	}
 
+	// copy the binary to the host
+	i.copyBinary(log)
+
+	// add .json file to oci hooks dir
 	_, binBaseName := path.Split(i.LocalBinary)
 	binFname := filepath.Join(i.HostInstallDir, binBaseName)
 
@@ -83,24 +92,8 @@ func (i *Install) ociHooksInstall(log *slog.Logger) {
 	}
 
 	log.Info("written conf", "conf-dst-path", confDst)
-}
 
-func (i *Install) Run(log *slog.Logger) error {
-	var sigChan chan os.Signal
-	if i.Daemonize {
-		sigChan = make(chan os.Signal, 1)
-		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	}
-
-	i.copyBinary(log)
-	switch i.Interface {
-	case "oci-hooks":
-		i.ociHooksInstall(log)
-	default:
-		log.Error("unknown interface", "interface", i.Interface)
-		os.Exit(1)
-	}
-
+	// if --daemonize is set, wait until we receive a signal, and then uninstall hook.
 	if i.Daemonize {
 		<-sigChan
 		u := Uninstall{
@@ -113,6 +106,19 @@ func (i *Install) Run(log *slog.Logger) error {
 			log.Error("uninstall failed", "err", err)
 		}
 	}
+}
+
+func (i *Install) Run(log *slog.Logger) error {
+
+	i.copyBinary(log)
+	switch i.Interface {
+	case "oci-hooks":
+		i.ociHooksInstall(log)
+		return nil
+	}
+
+	log.Error("unknown interface", "interface", i.Interface)
+	os.Exit(1)
 
 	return nil
 }

commit 2020a548c8aa32c524aa5218b9faccc1d79d8ab8

rthooks: setup do not copy binary twice
For oci-hooks, the binary is copied twice. No need for that, remove the
second copy.

Fixes: f9913836ec

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

contrib/tetragon-rthooks/cmd/setup/main.go
@@ -73,9 +73,6 @@ func (i *Install) ociHooksInstall(log *slog.Logger) {
 		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	}
 
-	// copy the binary to the host
-	i.copyBinary(log)
-
 	// add .json file to oci hooks dir
 	_, binBaseName := path.Split(i.LocalBinary)
 	binFname := filepath.Join(i.HostInstallDir, binBaseName)
@@ -115,6 +112,7 @@ func (i *Install) ociHooksInstall(log *slog.Logger) {
 
 func (i *Install) Run(log *slog.Logger) error {
 
+	// copy the binary to the host
 	i.copyBinary(log)
 	switch i.Interface {
 	case "oci-hooks":