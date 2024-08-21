cilium/tetragon

commit 0227f5d0b3c69744fece03900f664d3bdf69c08b

process: resolve pods with cgidmap
This commit adds functionality (disabled by default) to resolve Pods
with cgroup ids, instead of cgroup names.

The motivation is to have more robust pod association and deal with
idiosyncrasies of certain runtimes (specifically, crun; See next patch).

To enable this feature users need to set:
 --enable-cgidmap
 --enable-cri

It is also highly recommended to use runtime hooks so that the cgroup id
mappings are set before the container starts.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/cgidmap/cgidmap.go
@@ -8,6 +8,7 @@
 package cgidmap
 
 import (
+	"errors"
 	"sync"
 
 	"github.com/cilium/tetragon/pkg/logger"
@@ -15,12 +16,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-)
-
-var (
-	// TODO: turn these into config options
-	enabled = true
-	debug   = true
 )
 
 // convinience types to make APIs more readable
@@ -90,7 +85,7 @@ func newMap() (*cgidm, error) {
 		cgMap:       make(map[CgroupID]int),
 		contMap:     make(map[ContainerID]int),
 		log:         log,
-		DebugLogger: logger.NewDebugLogger(log, debug),
+		DebugLogger: logger.NewDebugLogger(log, option.Config.EnableCgIDmapDebug),
 	}
 
 	var criResolver *criResolver
@@ -253,8 +248,18 @@ var (
 // GetState returns the global map
 func GlobalMap() (Map, error) {
 	setGlMap.Do(func() {
+		if !option.Config.EnableCgIDmap {
+			glMap = nil
+			glError = errors.New("cgidmap disabled")
+			return
+		}
+
 		glMap, glError = newMap()
-		glMap.log.Info("cgidmap initialized (err=%v)", glError)
+		if glError != nil {
+			glMap.log.Info("cgidmap initialized")
+		} else {
+			glMap.log.WithError(glError).Warn("cgidmap initialization failed")
+		}
 	})
 	return glMap, glError
 }

docs/data/tetragon_flags.yaml
@@ -18,6 +18,8 @@ options:
       usage: Configuration directory that contains a file for each option
     - name: cpuprofile
       usage: Store CPU profile into provided file
+    - name: cri-endpoint
+      usage: CRI endpoint
     - name: data-cache-size
       default_value: "1024"
       usage: Size of the data events cache
@@ -28,6 +30,15 @@ options:
     - name: disable-kprobe-multi
       default_value: "false"
       usage: Allow to disable kprobe multi interface
+    - name: enable-cgidmap
+      default_value: "false"
+      usage: enable pod resolution via cgroup ids
+    - name: enable-cgidmap-debug
+      default_value: "false"
+      usage: enable cgidmap debugging info
+    - name: enable-cri
+      default_value: "false"
+      usage: enable CRI client for tetragon
     - name: enable-export-aggregation
       default_value: "false"
       usage: Enable JSON export aggregation

pkg/api/processapi/processapi.go
@@ -91,6 +91,7 @@ type MsgK8s struct {
 
 type MsgK8sUnix struct {
 	Docker string
+	Cgrpid uint64
 }
 
 type MsgGenericCred struct {

pkg/cgidmap/rthooks.go
@@ -7,20 +7,23 @@ import (
 	"context"
 
 	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/option"
 	"github.com/cilium/tetragon/pkg/rthooks"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	if enabled {
-		rthooks.RegisterCallbacksAtInit(rthooks.Callbacks{
-			CreateContainer: createContainerHook,
-		})
-	}
+	rthooks.RegisterCallbacksAtInit(rthooks.Callbacks{
+		CreateContainer: createContainerHook,
+	})
 }
 
 func createContainerHook(_ context.Context, arg *rthooks.CreateContainerArg) error {
+	if !option.Config.EnableCgIDmap {
+		return nil
+	}
+
 	log := logger.GetLogger().WithFields(logrus.Fields{
 		"rthook":  true,
 		"cgidmap": true,

pkg/grpc/exec/exec.go
@@ -189,7 +189,8 @@ func (msg *MsgExecveEventUnix) Retry(internal *process.ProcessInternal, ev notif
 	nspid := msg.Unix.Process.NSPID
 
 	if option.Config.EnableK8s && containerId != "" {
-		podInfo = process.GetPodInfo(containerId, filename, args, nspid)
+		cgroupID := msg.Unix.Kube.Cgrpid
+		podInfo = process.GetPodInfo(cgroupID, containerId, filename, args, nspid)
 		if podInfo == nil {
 			eventcachemetrics.EventCacheRetries(eventcachemetrics.PodInfo).Inc()
 			return eventcache.ErrFailedToGetPodInfo

pkg/grpc/process_manager_test.go
@@ -64,9 +64,9 @@ func TestProcessManager_getPodInfo(t *testing.T) {
 	err := process.InitCache(watcher.NewFakeK8sWatcher(pods), 10)
 	assert.NoError(t, err)
 	defer process.FreeCache()
-	pod := process.GetPodInfo("container-id-not-found", "", "", 0)
+	pod := process.GetPodInfo(0, "container-id-not-found", "", "", 0)
 	assert.Nil(t, pod)
-	pod = process.GetPodInfo("aaaaaaa", "", "", 1234)
+	pod = process.GetPodInfo(0, "aaaaaaa", "", "", 1234)
 	assert.Equal(t,
 		&tetragon.Pod{
 			Namespace: podA.Namespace,
@@ -129,7 +129,7 @@ func TestProcessManager_getPodInfoMaybeExecProbe(t *testing.T) {
 	err := process.InitCache(watcher.NewFakeK8sWatcher(pods), 10)
 	assert.NoError(t, err)
 	defer process.FreeCache()
-	pod := process.GetPodInfo("aaaaaaa", "/bin/command", "arg-a arg-b", 1234)
+	pod := process.GetPodInfo(0, "aaaaaaa", "/bin/command", "arg-a arg-b", 1234)
 	assert.Equal(t,
 		&tetragon.Pod{
 			Namespace: podA.Namespace,

pkg/option/config.go
@@ -97,6 +97,9 @@ type config struct {
 
 	EnableCRI   bool
 	CRIEndpoint string
+
+	EnableCgIDmap      bool
+	EnableCgIDmapDebug bool
 }
 
 var (

pkg/option/flags.go
@@ -108,6 +108,9 @@ const (
 
 	KeyEnableCRI   = "enable-cri"
 	KeyCRIEndpoint = "cri-endpoint"
+
+	KeyEnableCgIDmap      = "enable-cgidmap"
+	KeyEnableCgIDmapDebug = "enable-cgidmap-debug"
 )
 
 type UsernameMetadaCode int
@@ -228,6 +231,9 @@ func ReadAndSetFlags() error {
 
 	Config.EnableCRI = viper.GetBool(KeyEnableCRI)
 	Config.CRIEndpoint = viper.GetString(KeyCRIEndpoint)
+
+	Config.EnableCgIDmap = viper.GetBool(KeyEnableCgIDmap)
+	Config.EnableCgIDmapDebug = viper.GetBool(KeyEnableCgIDmapDebug)
 	return nil
 }
 
@@ -387,4 +393,7 @@ func AddFlags(flags *pflag.FlagSet) {
 
 	flags.Bool(KeyEnableCRI, false, "enable CRI client for tetragon")
 	flags.String(KeyCRIEndpoint, "", "CRI endpoint")
+
+	flags.Bool(KeyEnableCgIDmap, false, "enable pod resolution via cgroup ids")
+	flags.Bool(KeyEnableCgIDmapDebug, false, "enable cgidmap debugging info")
 }

commit f73fb4676edb2b33b026a15fb1b59fb3b653cf7f

cgidmap: fix initialization bug
Fixes: 0227f5d0b3c6 ("process: resolve pods with cgidmap")

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/cgidmap/cgidmap.go
@@ -255,7 +255,7 @@ func GlobalMap() (Map, error) {
 		}
 
 		glMap, glError = newMap()
-		if glError != nil {
+		if glError == nil {
 			glMap.log.Info("cgidmap initialized")
 		} else {
 			glMap.log.WithError(glError).Warn("cgidmap initialization failed")