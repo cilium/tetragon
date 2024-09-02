6aa16460eb78ef7646b6c4da974d6e34dfe893e5
Anna Kapuscinska <anna@isovalent.com>
2024-09-02 02:41:35 +0200

    eventcache: Remove tetragon_event_cache_<entry_type>_errors_total metrics
    They're now replaced by more intuitive metrics
    tetragon_event_cache_fetch_failures_total{entry_type="<entry_type>"}
    
    Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

--- pkg/eventcache/metrics.go
+++ pkg/eventcache/metrics.go
@@ -56,18 +56,6 @@ var (
 )
 
 var (
-	processInfoErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace:   consts.MetricsNamespace,
-		Name:        "event_cache_process_info_errors_total",
-		Help:        "The total of times we failed to fetch cached process info for a given event type.",
-		ConstLabels: nil,
-	}, []string{"event_type"})
-	podInfoErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace:   consts.MetricsNamespace,
-		Name:        "event_cache_pod_info_errors_total",
-		Help:        "The total of times we failed to fetch cached pod info for a given event type.",
-		ConstLabels: nil,
-	}, []string{"event_type"})
 	EventCacheCount = prometheus.NewCounter(prometheus.CounterOpts{
 		Namespace:   consts.MetricsNamespace,
 		Name:        "event_cache_accesses_total",
@@ -85,12 +73,6 @@ var (
 		Name:      "event_cache_retries_total",
 		Help:      "The total number of retries for event caching per entry type.",
 	}, []string{"entry_type"})
-	parentInfoErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace:   consts.MetricsNamespace,
-		Name:        "event_cache_parent_info_errors_total",
-		Help:        "The total of times we failed to fetch cached parent info for a given event type.",
-		ConstLabels: nil,
-	}, []string{"event_type"})
 	cacheSize = metrics.MustNewCustomGauge(metrics.NewOpts(
 		consts.MetricsNamespace, "", "event_cache_entries",
 		"The number of entries in the event cache.",
@@ -118,12 +100,9 @@ func newCacheCollector() prometheus.Collector {
 }
 
 func RegisterMetrics(group metrics.Group) {
-	group.MustRegister(processInfoErrors)
-	group.MustRegister(podInfoErrors)
 	group.MustRegister(EventCacheCount)
 	group.MustRegister(eventCacheErrorsTotal)
 	group.MustRegister(eventCacheRetriesTotal)
-	group.MustRegister(parentInfoErrors)
 	group.MustRegister(
 		newCacheCollector(),
 		failedFetches,
@@ -137,24 +116,11 @@ func InitMetrics() {
 	}
 	for ev := range tetragon.EventType_name {
 		if tetragon.EventType(ev) != tetragon.EventType_UNDEF && tetragon.EventType(ev) != tetragon.EventType_TEST {
-			ProcessInfoError(tetragon.EventType(ev)).Add(0)
-			PodInfoError(tetragon.EventType(ev)).Add(0)
-			ParentInfoError(tetragon.EventType(ev)).Add(0)
 			for er := range cacheErrorLabelValues {
 				EventCacheError(er, tetragon.EventType(ev)).Add(0)
 			}
 		}
 	}
-}
-
-// Get a new handle on a processInfoErrors metric for an eventType
-func ProcessInfoError(eventType tetragon.EventType) prometheus.Counter {
-	return processInfoErrors.WithLabelValues(eventType.String())
-}
-
-// Get a new handle on a podInfoErrors metric for an eventType
-func PodInfoError(eventType tetragon.EventType) prometheus.Counter {
-	return podInfoErrors.WithLabelValues(eventType.String())
 }
 
 // Get a new handle on an eventCacheErrorsTotal metric for an error
@@ -165,9 +131,4 @@ func EventCacheError(er CacheError, eventType tetragon.EventType) prometheus.Cou
 // Get a new handle on an eventCacheRetriesTotal metric for an entryType
 func EventCacheRetries(entryType CacheEntryType) prometheus.Counter {
 	return eventCacheRetriesTotal.WithLabelValues(entryType.String())
-}
-
-// Get a new handle on an processInfoErrors metric for an eventType
-func ParentInfoError(eventType tetragon.EventType) prometheus.Counter {
-	return parentInfoErrors.WithLabelValues(eventType.String())
 }
