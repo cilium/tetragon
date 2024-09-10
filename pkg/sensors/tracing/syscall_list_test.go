dc3ce82aadc56e759f7389173a15c26d16cfa518
Kornilios Kourtis <kornilios@isovalent.com>
2024-09-10 16:24:28 +0200

    SyscallVal: add some simple tests
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/sensors/tracing/syscall_list_test.go
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package tracing
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func defABI(t *testing.T) string {
+	abi, err := defaultABI()
+	assert.Nil(t, err)
+	return abi
+}
+
+func defSysCallPrefix(t *testing.T) string {
+	switch a := runtime.GOARCH; a {
+	case "amd64":
+		return "__x64_"
+	case "arm64":
+		return "__arm64_"
+	default:
+		t.Fatalf("unsupported arch: %s", a)
+		return ""
+	}
+}
+
+func TestParseSyscallValue(t *testing.T) {
+	type testCase struct {
+		val             string
+		expError        bool
+		expABI, expName string
+	}
+
+	tcs := []testCase{
+		{val: "sys_dup", expError: false, expABI: defABI(t), expName: "sys_dup"},
+		{val: "i386/sys_dup", expError: false, expABI: "i386", expName: "sys_dup"},
+		{val: "__ia32_sys_dup", expError: false, expABI: "i386", expName: "sys_dup"},
+		{val: "x64/sys_dup", expError: false, expABI: "x64", expName: "sys_dup"},
+		{val: "__x64_sys_dup", expError: false, expABI: "x64", expName: "sys_dup"},
+		{val: "__foo_sys_dup", expError: false, expABI: defABI(t), expName: "__foo_sys_dup"},
+	}
+
+	for _, tc := range tcs {
+		abi, name, err := parseSyscallValue(SyscallVal(tc.val))
+		if tc.expError {
+			require.Error(t, err, fmt.Sprintf("tc: %+v", tc))
+		} else {
+			require.Nil(t, err)
+			require.Equal(t, tc.expABI, abi)
+			require.Equal(t, tc.expName, name)
+		}
+	}
+}
+
+func TestSyscallValSymbol(t *testing.T) {
+	type testCase struct {
+		val         string
+		symExpError bool
+		symExpVal   string
+	}
+
+	tcs := []testCase{
+		{val: "sys_dup", symExpError: false, symExpVal: defSysCallPrefix(t) + "sys_dup"},
+		{val: "i386/sys_dup", symExpError: false, symExpVal: "__ia32_sys_dup"},
+		{val: "__ia32_sys_dup", symExpError: false, symExpVal: "__ia32_sys_dup"},
+		{val: "x64/sys_dup", symExpError: false, symExpVal: "__x64_sys_dup"},
+		{val: "__foo_dup", symExpError: true},
+		{val: "arm64/sys_dup", symExpError: false, symExpVal: "__arm64_sys_dup"},
+	}
+
+	for _, tc := range tcs {
+		v := SyscallVal(tc.val)
+		sym, err := v.Symbol()
+		if tc.symExpError {
+			require.Error(t, err)
+		} else {
+			require.Nil(t, err)
+			require.Equal(t, tc.symExpVal, sym)
+		}
+	}
+}
