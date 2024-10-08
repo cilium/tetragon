56729f52cdd8e257d4dac1e6f43f998ffb360ba6
Kornilios Kourtis <kornilios@isovalent.com>
2024-10-08 13:17:22 +0200

    autochore: make protogen
    Generate code for SyscallID.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- vendor/github.com/cilium/tetragon/api/v1/tetragon/tetragon.pb.json.go
+++ vendor/github.com/cilium/tetragon/api/v1/tetragon/tetragon.pb.json.go
@@ -456,6 +456,22 @@ func (msg *KprobeBpfMap) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *SyscallId) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{
+		UseEnumNumbers:  false,
+		EmitUnpopulated: false,
+		UseProtoNames:   true,
+	}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *SyscallId) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+	}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *KprobeArgument) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
