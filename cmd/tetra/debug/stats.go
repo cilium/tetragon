41e5d6d5e5cd7bb1731ddb96c199584ee17604fe
Jiri Olsa <jolsa@kernel.org>
2024-10-24 12:45:30 +0000

    tetra: Add enable-stat debug command
    Adding enable-stat debug command that enabled bpf program
    stats for as long as the command is running, like:
    
      # ./tetra debug enable-stats
      ^C
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ cmd/tetra/debug/stats.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package debug
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cilium/ebpf"
+	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
+)
+
+func NewEnableStatsCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "enable-stats",
+		Short: "Enable BPF stats",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			// We enable BPF stats system wide keep it enabled as long as
+			// the stats descriptor is open - app is running
+			_, err := ebpf.EnableStats(uint32(unix.BPF_STATS_RUN_TIME))
+			if err != nil {
+				return fmt.Errorf("failed to enable stats: %v", err)
+			}
+
+			// BPF stats are enabled..
+
+			for {
+				time.Sleep(time.Hour)
+			}
+		},
+	}
+
+	return &cmd
+}
