cilium/tetragon

commit a5d7f0ee69bfc8eed1e5e2b4f6c3370c93de0281

tetragon: Load initial sensor via sensor manager
Loading initial sensor via sensor manager so it stores all the
loaded sensors/programs. So far we kept it separated but having
it in one place is handy for metrics requesting this data.

Moving the load before the server is started so we don't need to
"wait" for that. This way we can remove the sensorMgWait channel.

Signed-off-by: Jiri Olsa <jolsa@kernel.org>

cmd/tetragon/main.go
@@ -189,6 +189,18 @@ func deleteOldBpfDir(path string) {
 	log.Infof("Removed bpf instance: %s", path)
 }
 
+func loadInitialSensor(ctx context.Context) error {
+	base.ConfigCgroupRate(&option.Config.CgroupRate)
+
+	mgr := observer.GetSensorManager()
+	initialSensor := base.GetInitialSensor()
+
+	if err := mgr.AddSensor(ctx, initialSensor.Name, initialSensor); err != nil {
+		return err
+	}
+	return mgr.EnableSensor(ctx, initialSensor.Name)
+}
+
 func tetragonExecute() error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -359,20 +371,7 @@ func tetragonExecute() error {
 		cancel()
 	}()
 
-	// start sensor manager, and have it wait on sensorMgWait until we load
-	// the base sensor. note that this means that calling methods on the
-	// manager will block so they will have to either be executed in a
-	// goroutine or after we close the sensorMgWait channel to avoid
-	// deadlock.
-	sensorMgWait := make(chan struct{})
-	defer func() {
-		// if we fail before closing the channel, close it so that
-		// the sensor manager routine is unblocked.
-		if sensorMgWait != nil {
-			close(sensorMgWait)
-		}
-	}()
-	if err := obs.InitSensorManager(sensorMgWait); err != nil {
+	if err := obs.InitSensorManager(); err != nil {
 		return err
 	}
 
@@ -447,6 +446,16 @@ func tetragonExecute() error {
 		return err
 	}
 
+	// Load initial sensor before we start the server,
+	// so it's there before we allow to load policies.
+	if err = loadInitialSensor(ctx); err != nil {
+		return err
+	}
+	observer.GetSensorManager().LogSensorsAndProbes(ctx)
+	defer func() {
+		observer.RemoveSensors(ctx)
+	}()
+
 	pm, err := tetragonGrpc.NewProcessManager(
 		ctx,
 		&cleanupWg,
@@ -477,27 +486,8 @@ func tetragonExecute() error {
 
 	obs.LogPinnedBpf(observerDir)
 
-	base.ConfigCgroupRate(&option.Config.CgroupRate)
-
-	// load base sensor
-	initialSensor := base.GetInitialSensor()
-	if err := initialSensor.Load(observerDir); err != nil {
-		return err
-	}
-	defer func() {
-		initialSensor.Unload()
-	}()
-
 	cgrouprate.NewCgroupRate(ctx, pm, base.CgroupRateMap, &option.Config.CgroupRate)
 	cgrouprate.Config(base.CgroupRateOptionsMap)
-
-	// now that the base sensor was loaded, we can start the sensor manager
-	close(sensorMgWait)
-	sensorMgWait = nil
-	observer.GetSensorManager().LogSensorsAndProbes(ctx)
-	defer func() {
-		observer.RemoveSensors(ctx)
-	}()
 
 	err = loadTpFromDir(ctx, option.Config.TracingPolicyDir)
 	if err != nil {

pkg/observer/observertesthelper/observer_test_helper.go
@@ -358,7 +358,7 @@ func loadExporter(tb testing.TB, ctx context.Context, obs *observer.Observer, op
 	processCacheSize := 32768
 	dataCacheSize := 1024
 
-	if err := obs.InitSensorManager(nil); err != nil {
+	if err := obs.InitSensorManager(); err != nil {
 		return err
 	}
 

pkg/testutils/sensors/sensors.go
@@ -67,7 +67,7 @@ func getTestSensorManager(ctx context.Context, t *testing.T, pfState policyfilte
 	}
 
 	path := bpf.MapPrefixPath()
-	mgr, err = sensors.StartSensorManagerWithPF(path, nil, pfState)
+	mgr, err = sensors.StartSensorManagerWithPF(path, pfState)
 	if err != nil {
 		t.Fatalf("StartSensorManagerWithPF failed: %s", err)
 	}

pkg/sensors/tracing/generickprobe_test.go
@@ -144,7 +144,7 @@ func Test_DisableEnablePolicy_Kprobe(t *testing.T) {
 
 	tus.LoadSensor(t, base.GetInitialSensor())
 	path := bpf.MapPrefixPath()
-	mgr, err := sensors.StartSensorManager(path, nil)
+	mgr, err := sensors.StartSensorManager(path)
 	assert.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -190,7 +190,7 @@ func Test_DisableEnablePolicy_KernelMemoryBytes(t *testing.T) {
 
 	tus.LoadSensor(t, base.GetInitialSensor())
 	path := bpf.MapPrefixPath()
-	mgr, err := sensors.StartSensorManager(path, nil)
+	mgr, err := sensors.StartSensorManager(path)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {

pkg/bench/bench.go
@@ -87,7 +87,7 @@ func runTetragon(ctx context.Context, configFile string, args *Arguments, summar
 	option.Config.BpfDir = bpf.MapPrefixPath()
 	obs := observer.NewObserver()
 
-	if err := obs.InitSensorManager(nil); err != nil {
+	if err := obs.InitSensorManager(); err != nil {
 		logger.GetLogger().Fatalf("InitSensorManager failed: %v", err)
 	}
 

pkg/observer/observer.go
@@ -341,8 +341,8 @@ func (k *Observer) Start(ctx context.Context) error {
 }
 
 // InitSensorManager starts the sensor controller
-func (k *Observer) InitSensorManager(waitChan chan struct{}) error {
-	mgr, err := sensors.StartSensorManager(option.Config.BpfDir, waitChan)
+func (k *Observer) InitSensorManager() error {
+	mgr, err := sensors.StartSensorManager(option.Config.BpfDir)
 	if err != nil {
 		return err
 	}

pkg/sensors/manager.go
@@ -27,24 +27,18 @@ type SensorStatus struct {
 // The purpose of this goroutine is to serialize loading and unloading of
 // sensors as requested from different goroutines (e.g., different GRPC
 // clients).
-//
-// if waitChan is not nil, the serving of sensor requests will block until
-// something is received. The intention of this is to allow the main function
-// to first load the base sensor before the sensor manager starts loading other sensors.
 func StartSensorManager(
 	bpfDir string,
-	waitChan chan struct{},
 ) (*Manager, error) {
 	pfState, err := policyfilter.GetState()
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize policy filter state: %w", err)
 	}
-	return StartSensorManagerWithPF(bpfDir, waitChan, pfState)
+	return StartSensorManagerWithPF(bpfDir, pfState)
 }
 
 func StartSensorManagerWithPF(
 	bpfDir string,
-	waitChan chan struct{},
 	pfState policyfilter.State,
 ) (*Manager, error) {
 	colMap := newCollectionMap()

pkg/sensors/manager_test.go
@@ -43,7 +43,7 @@ func TestAddPolicy(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	assert.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -71,7 +71,7 @@ func TestAddPolicies(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	assert.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -102,7 +102,7 @@ func TestAddPolicySpecError(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	assert.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -134,7 +134,7 @@ func TestAddPolicyLoadError(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	assert.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -157,7 +157,7 @@ func TestPolicyFilterDisabled(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	mgr, err := StartSensorManagerWithPF("", nil, policyfilter.DisabledState())
+	mgr, err := StartSensorManagerWithPF("", policyfilter.DisabledState())
 	assert.NoError(t, err)
 	defer mgr.StopSensorManager(ctx)
 
@@ -209,7 +209,7 @@ func TestPolicyStates(t *testing.T) {
 		})
 
 		policy := v1alpha1.TracingPolicy{}
-		mgr, err := StartSensorManager("", nil)
+		mgr, err := StartSensorManager("")
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			if err := mgr.StopSensorManager(ctx); err != nil {
@@ -234,7 +234,7 @@ func TestPolicyStates(t *testing.T) {
 		})
 
 		policy := v1alpha1.TracingPolicy{}
-		mgr, err := StartSensorManager("", nil)
+		mgr, err := StartSensorManager("")
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			if err := mgr.StopSensorManager(ctx); err != nil {
@@ -274,7 +274,7 @@ func TestPolicyLoadErrorOverride(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -313,7 +313,7 @@ func TestPolicyListingWhileLoadUnload(t *testing.T) {
 	polName := "test-policy"
 	testSensor := makeTestDelayedSensor(t)
 
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {
@@ -440,7 +440,7 @@ func TestPolicyKernelMemoryBytes(t *testing.T) {
 	})
 
 	policy := v1alpha1.TracingPolicy{}
-	mgr, err := StartSensorManager("", nil)
+	mgr, err := StartSensorManager("")
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		if err := mgr.StopSensorManager(ctx); err != nil {

commit 82281e2960c811168515671134c2a0084bed41ff

tetragon: Move procevents.GetRunningProcs call from base sensor load
We need to call procevents.GetRunningProcs after server is started
observer listeners are configured.

Currently it's called within base sensor load, but it's now loaded
before server is started, so we don't get any initial exec events.

Fixes: a5d7f0ee69bf ("tetragon: Load initial sensor via sensor manager")
Signed-off-by: Jiri Olsa <jolsa@kernel.org>

cmd/tetragon/main.go
@@ -48,6 +48,7 @@ import (
 	"github.com/cilium/tetragon/pkg/reader/proc"
 	"github.com/cilium/tetragon/pkg/rthooks"
 	"github.com/cilium/tetragon/pkg/sensors/base"
+	"github.com/cilium/tetragon/pkg/sensors/exec/procevents"
 	"github.com/cilium/tetragon/pkg/sensors/program"
 	"github.com/cilium/tetragon/pkg/server"
 	"github.com/cilium/tetragon/pkg/tracingpolicy"
@@ -485,6 +486,10 @@ func tetragonExecute() error {
 	}
 
 	obs.LogPinnedBpf(observerDir)
+
+	if err = procevents.GetRunningProcs(); err != nil {
+		return err
+	}
 
 	cgrouprate.NewCgroupRate(ctx, pm, base.CgroupRateMap, &option.Config.CgroupRate)
 	cgrouprate.Config(base.CgroupRateOptionsMap)

pkg/observer/observertesthelper/observer_test_helper.go
@@ -40,6 +40,7 @@ import (
 	"github.com/cilium/tetragon/pkg/rthooks"
 	"github.com/cilium/tetragon/pkg/sensors"
 	"github.com/cilium/tetragon/pkg/sensors/base"
+	"github.com/cilium/tetragon/pkg/sensors/exec/procevents"
 	"github.com/cilium/tetragon/pkg/testutils"
 	"github.com/cilium/tetragon/pkg/watcher"
 	"github.com/cilium/tetragon/pkg/watcher/crd"
@@ -437,6 +438,10 @@ func loadObserver(tb testing.TB, ctx context.Context, base *sensors.Sensor,
 		base.Unload()
 	})
 
+	if err := procevents.GetRunningProcs(); err != nil {
+		return err
+	}
+
 	if tp != nil {
 		if err := observer.GetSensorManager().AddTracingPolicy(ctx, tp); err != nil {
 			tb.Fatalf("SensorManager.AddTracingPolicy error: %s\n", err)
@@ -452,6 +457,10 @@ func loadObserver(tb testing.TB, ctx context.Context, base *sensors.Sensor,
 func loadSensors(tb testing.TB, base sensors.SensorIface, sens []sensors.SensorIface) error {
 	if err := base.Load(option.Config.BpfDir); err != nil {
 		tb.Fatalf("Load base error: %s\n", err)
+	}
+
+	if err := procevents.GetRunningProcs(); err != nil {
+		tb.Fatalf("procevents.GetRunningProcs: %s", err)
 	}
 
 	for _, s := range sens {

pkg/sensors/exec/exec.go
@@ -250,11 +250,7 @@ func handleThrottleEvent(r *bytes.Reader) ([]observer.Event, error) {
 type execProbe struct{}
 
 func (e *execProbe) LoadProbe(args sensors.LoadProbeArgs) error {
-	err := program.LoadTracepointProgram(args.BPFDir, args.Load, args.Verbose)
-	if err == nil {
-		err = procevents.GetRunningProcs()
-	}
-	return err
+	return program.LoadTracepointProgram(args.BPFDir, args.Load, args.Verbose)
 }
 
 func init() {

pkg/testutils/sensors/sensors.go
@@ -11,6 +11,8 @@ import (
 	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/cilium/tetragon/pkg/policyfilter"
 	"github.com/cilium/tetragon/pkg/sensors"
+	"github.com/cilium/tetragon/pkg/sensors/base"
+	"github.com/cilium/tetragon/pkg/sensors/exec/procevents"
 )
 
 // LoadSensor is a helper for loading a sensor in tests
@@ -31,6 +33,14 @@ func LoadSensor(t *testing.T, sensori sensors.SensorIface) {
 	t.Cleanup(func() {
 		sensor.Unload()
 	})
+}
+
+func LoadInitialSensor(t *testing.T) {
+	LoadSensor(t, base.GetInitialSensor())
+
+	if err := procevents.GetRunningProcs(); err != nil {
+		t.Fatalf("procevents.GetRunningProcs: %s", err)
+	}
 }
 
 // TestSensorManager sensor manager used in tests

pkg/bugtool/maps_test.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
 	"github.com/stretchr/testify/assert"
 
@@ -30,7 +29,7 @@ func TestFindMaps(t *testing.T) {
 	})
 
 	t.Run("BaseSensorMemlock", func(t *testing.T) {
-		tus.LoadSensor(t, base.GetInitialSensor())
+		tus.LoadInitialSensor(t)
 
 		const path = "/sys/fs/bpf/testSensorBugtool"
 		pinnedMaps, err := FindPinnedMaps(path)

pkg/sensors/exec/cgroups_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/cilium/tetragon/pkg/option"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	"github.com/cilium/tetragon/pkg/sensors/cgroup/cgrouptrackmap"
 	"github.com/cilium/tetragon/pkg/sensors/config/confmap"
 	"github.com/cilium/tetragon/pkg/sensors/exec/procevents"
@@ -605,7 +604,7 @@ func TestLoadCgroupsPrograms(t *testing.T) {
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	tus.LoadSensor(t, testsensor.GetCgroupSensor())
 }
@@ -617,7 +616,7 @@ func TestTgRuntimeConf(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	val, err := testutils.GetTgRuntimeConf()
 	assert.NoError(t, err)
@@ -648,7 +647,7 @@ func TestCgroupNoEvents(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	testManager := setupObserver(t)
 
@@ -700,7 +699,7 @@ func TestCgroupEventMkdirRmdir(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	testManager := setupObserver(t)
 
@@ -1051,7 +1050,7 @@ func TestCgroupv2K8sHierarchyInUnified(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	// Probe full environment detection
 	setupTgRuntimeConf(t, invalidValue, invalidValue, invalidValue, invalidValue)
@@ -1073,7 +1072,7 @@ func TestCgroupv2K8sHierarchyInHybrid(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	// Probe full environment detection
 	setupTgRuntimeConf(t, invalidValue, invalidValue, invalidValue, invalidValue)
@@ -1093,7 +1092,7 @@ func testCgroupv1K8sHierarchyInHybrid(t *testing.T, withExec bool, selectedContr
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	testManager := setupObserver(t)
 
@@ -1350,7 +1349,7 @@ func TestCgroupv2ExecK8sHierarchyInUnified(t *testing.T) {
 	option.Config.HubbleLib = tus.Conf().TetragonLib
 	option.Config.Verbosity = 5
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	// Probe full environment detection
 	_, err := testutils.GetTgRuntimeConf()

pkg/sensors/exec/exec_test.go
@@ -679,7 +679,7 @@ func TestExecPerfring(t *testing.T) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 
 	ops := func() {

pkg/sensors/exec/threads_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/cilium/tetragon/pkg/observer/observertesthelper"
 	"github.com/cilium/tetragon/pkg/option"
-	"github.com/cilium/tetragon/pkg/sensors/base"
+	"github.com/cilium/tetragon/pkg/sensors/exec/procevents"
 	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
 	"github.com/cilium/tetragon/pkg/testutils"
 	"github.com/cilium/tetragon/pkg/testutils/perfring"
@@ -98,7 +98,12 @@ func TestMatchCloneThreadsIDs(t *testing.T) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
+
+	if err := procevents.GetRunningProcs(); err != nil {
+		t.Fatalf("procevents.GetRunningProcs: %s", err)
+	}
+
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 
 	testBinPath := "contrib/tester-progs/threads-tester"

pkg/sensors/tracing/enforcer_test.go
@@ -26,7 +26,6 @@ import (
 	"github.com/cilium/tetragon/pkg/option"
 	"github.com/cilium/tetragon/pkg/policyfilter"
 	"github.com/cilium/tetragon/pkg/sensors"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	"github.com/cilium/tetragon/pkg/syscallinfo"
 	"github.com/cilium/tetragon/pkg/testutils"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
@@ -624,7 +623,7 @@ spec:
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 
 	sensor1, err := gEnforcerPolicy.PolicyHandler(policy1, policyfilter.NoFilterID)
 	assert.NoError(t, err)

pkg/sensors/tracing/generickprobe_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/cilium/tetragon/pkg/idtable"
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 	"github.com/cilium/tetragon/pkg/sensors"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -142,7 +141,7 @@ func Test_DisableEnablePolicy_Kprobe(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	path := bpf.MapPrefixPath()
 	mgr, err := sensors.StartSensorManager(path)
 	assert.NoError(t, err)
@@ -183,7 +182,7 @@ func Test_DisableEnablePolicy_KernelMemoryBytes(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	path := bpf.MapPrefixPath()
 	mgr, err := sensors.StartSensorManager(path)
 	require.NoError(t, err)

pkg/sensors/tracing/kprobe_test.go
@@ -3973,7 +3973,7 @@ func matchBinariesPerfringTest(t *testing.T, operator string, values []string) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	sm := tus.GetTestSensorManager(t)
 
@@ -4087,7 +4087,7 @@ func TestKprobeMatchBinariesEarlyExec(t *testing.T) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	sm := tus.GetTestSensorManager(t)
 
@@ -4154,7 +4154,7 @@ func TestKprobeMatchBinariesPrefixMatchArgs(t *testing.T) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	sm := tus.GetTestSensorManager(t)
 
@@ -6092,7 +6092,7 @@ func TestLinuxBinprmExtractPath(t *testing.T) {
 	}
 
 	option.Config.HubbleLib = tus.Conf().TetragonLib
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	sm := tus.GetTestSensorManager(t)
 

pkg/sensors/tracing/policyfilter_test.go
@@ -24,7 +24,6 @@ import (
 	"github.com/cilium/tetragon/pkg/option"
 	"github.com/cilium/tetragon/pkg/policyfilter"
 	"github.com/cilium/tetragon/pkg/reader/notify"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	"github.com/cilium/tetragon/pkg/sensors/config/confmap"
 	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
 	"github.com/cilium/tetragon/pkg/testutils"
@@ -114,7 +113,7 @@ func TestNamespacedPolicies(t *testing.T) {
 
 	policyfilter.TestingEnableAndReset(t)
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 	sm := tus.GetTestSensorManager(t)
 

pkg/sensors/tracing/selectors_test.go
@@ -24,7 +24,6 @@ import (
 	"github.com/cilium/tetragon/pkg/policyfilter"
 	"github.com/cilium/tetragon/pkg/reader/notify"
 	"github.com/cilium/tetragon/pkg/sensors"
-	"github.com/cilium/tetragon/pkg/sensors/base"
 	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
 	"github.com/cilium/tetragon/pkg/testutils"
 	"github.com/cilium/tetragon/pkg/testutils/perfring"
@@ -47,7 +46,7 @@ func loadGenericSensorTest(t *testing.T, spec *v1alpha1.TracingPolicySpec) *sens
 		Spec:     *spec,
 	}
 
-	tus.LoadSensor(t, base.GetInitialSensor())
+	tus.LoadInitialSensor(t)
 	tus.LoadSensor(t, testsensor.GetTestSensor())
 
 	ret, err := sensors.SensorsFromPolicy(tp, policyfilter.NoFilterID)