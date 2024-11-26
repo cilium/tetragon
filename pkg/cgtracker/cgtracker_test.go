6a4c54da901f1aaa691b81e7ad995296d090b8ac
Kornilios Kourtis <kornilios@isovalent.com>
2024-11-26 11:32:06 +0100

    cgtracker: add test for execve event
    This commit adds a test for cgtracker and the execve events which tests
    that if we crate a cgroup child the tracker id is what we expect.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/cgtracker/cgtracker_test.go
+++ pkg/cgtracker/cgtracker_test.go
@@ -4,7 +4,7 @@
 package cgtracker
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -16,13 +16,22 @@ import (
 	"time"
 
 	"github.com/cilium/tetragon/pkg/bpf"
-	"github.com/cilium/tetragon/pkg/btf"
 	"github.com/cilium/tetragon/pkg/cgroups"
 	"github.com/cilium/tetragon/pkg/defaults"
+	grpcexec "github.com/cilium/tetragon/pkg/grpc/exec"
+	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/observer"
 	"github.com/cilium/tetragon/pkg/option"
 	"github.com/cilium/tetragon/pkg/sensors"
+	"github.com/cilium/tetragon/pkg/sensors/config/confmap"
+	_ "github.com/cilium/tetragon/pkg/sensors/exec" // NB: needed so that the exec sensor can load the execve probe on its init
+	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
+	"github.com/cilium/tetragon/pkg/testutils"
+	"github.com/cilium/tetragon/pkg/testutils/perfring"
+	testprogs "github.com/cilium/tetragon/pkg/testutils/progs"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/unix"
@@ -49,32 +58,7 @@ func initBpffs() string {
 }
 
 func TestMain(m *testing.M) {
-	flag.StringVar(&option.Config.HubbleLib,
-		"bpf-lib", filepath.Join(repoRootPath(), "bpf", "objs"),
-		"tetragon lib directory (location of btf file and bpf objs).")
-	flag.Parse()
-
-	if envLib := os.Getenv("TETRAGON_LIB"); envLib != "" {
-		option.Config.HubbleLib = envLib
-	}
-
-	err := btf.InitCachedBTF(defaults.DefaultTetragonLib, "")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error initializing BTF file: %s", err)
-		os.Exit(1)
-	}
-
-	// setup a custom bpffs path to pin objects
-	dirPath := initBpffs()
-	defer func() {
-		// cleanup bpffs path
-		if dirPath != "" {
-			os.RemoveAll(dirPath)
-		}
-	}()
-
-	ec := m.Run()
-
+	ec := tus.TestSensorsRun(m, "cgtracker-test")
 	os.Exit(ec)
 }
 
@@ -99,7 +83,7 @@ func loadCgTrackerSensor(t *testing.T) *sensors.Sensor {
 	return s
 }
 
-func doMapTest(t *testing.T, cgfsPath string) {
+func cgfsMkdirTemp(t *testing.T, cgfsPath string) string {
 	var st syscall.Statfs_t
 	if err := syscall.Statfs(cgfsPath, &st); err != nil {
 		t.Fatalf("error accessing cgroup path '%s': %s", cgfsPath, err)
@@ -108,6 +92,8 @@ func doMapTest(t *testing.T, cgfsPath string) {
 		t.Logf("'%s' is cgroup v2", cgfsPath)
 	} else if st.Type == unix.TMPFS_MAGIC {
 		t.Logf("'%s' is cgroup v1", cgfsPath)
+	} else {
+		t.Fatalf("'%s' not a cgroup fs", cgfsPath)
 	}
 
 	// create tempdir
@@ -121,6 +107,13 @@ func doMapTest(t *testing.T, cgfsPath string) {
 			t.Logf("failed to remove '%s': %s", dir, err)
 		}
 	})
+	return dir
+}
+
+func doMapTest(t *testing.T, cgfsPath string) {
+	testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+
+	dir := cgfsMkdirTemp(t, cgfsPath)
 	t.Logf("created cgroup dir '%s'", dir)
 
 	cgPaths := []string{"untracked", "untracked/a", "tracked", "tracked/a", "tracked/a/x1", "tracked/a/x2", "tracked/b"}
@@ -193,4 +186,126 @@ func doMapTest(t *testing.T, cgfsPath string) {
 func TestCgTrackerMap(t *testing.T) {
 	loadCgTrackerSensor(t)
 	doMapTest(t, "/sys/fs/cgroup")
+}
+
+func loadExecSensorWithCgTracker(t *testing.T) {
+	oldVal := option.Config.EnableCgTrackerID
+	option.Config.EnableCgTrackerID = true
+	t.Cleanup(func() {
+		option.Config.EnableCgTrackerID = oldVal
+	})
+	tus.LoadInitialSensor(t)
+	tus.LoadSensor(t, testsensor.GetTestSensor())
+}
+
+func TestCgTrackerEvents(t *testing.T) {
+	doProgTest(t, "/sys/fs/cgroup")
+}
+
+func doProgTest(t *testing.T, cgfsPath string) {
+	testutils.CaptureLog(t, logger.GetLogger().(*logrus.Logger))
+
+	dir := cgfsMkdirTemp(t, cgfsPath)
+	t.Logf("created cgroup dir '%s'", dir)
+
+	if err := observer.InitDataCache(1024); err != nil {
+		t.Fatalf("observertesthelper.InitDataCache: %s", err)
+	}
+	err := confmap.UpdateTgRuntimeConf(bpf.MapPrefixPath(), os.Getpid())
+	require.NoError(t, err)
+
+	loadExecSensorWithCgTracker(t)
+
+	fname := filepath.Join(bpf.MapPrefixPath(), MapName)
+	m, err := OpenMap(fname)
+	if err != nil {
+		t.Fatalf("failed to open cgtracker map '%s': %s", fname, err)
+	}
+	defer m.Close()
+
+	cgPaths := []string{"a", "b", "a/x"}
+	cgIDs := map[string]uint64{}
+	fullPath := map[string]string{}
+	for _, path := range cgPaths {
+		d := filepath.Join(dir, path)
+		if err := os.Mkdir(d, 0700); err != nil {
+			t.Fatalf("failed to create '%s': %s", d, err)
+		}
+		if path == "a" {
+			err = m.AddCgroupTrackerPath(d)
+			require.NoError(t, err)
+		}
+
+		cgID, err := cgroups.GetCgroupIdFromPath(d)
+		require.NoError(t, err)
+		cgIDs[path] = cgID
+		t.Logf("cgid for %s is %d", path, cgID)
+		fullPath[path] = d
+	}
+	t.Cleanup(func() {
+		for _, path := range slices.Backward(cgPaths) {
+			cgPath := filepath.Join(dir, path)
+			i := 0
+			for {
+				if err := os.Remove(cgPath); err == nil {
+					break
+				} else if i == 5 {
+					t.Logf("failed to unlink '%s': %s", cgPath, err)
+					break
+				}
+				time.Sleep(10 * time.Millisecond)
+				i++
+			}
+		}
+	})
+
+	// copy command /bin/true to /tmp so that we can effectively filter based on binary
+	tmpTrue := testutils.CopyExecToTemp(t, "true")
+	progTester := testprogs.StartTester(t, context.Background())
+
+	type testCase struct {
+		cgPath         string // cgroup path
+		expCgID        uint64 // expected cgroup id
+		expCgTrackerID uint64 // expetect cgroup tracker id
+	}
+
+	tcs := []testCase{
+		{cgPath: "a", expCgID: cgIDs["a"], expCgTrackerID: cgIDs["a"]},
+		{cgPath: "a/x", expCgID: cgIDs["a/x"], expCgTrackerID: cgIDs["a"]},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	for _, tc := range tcs {
+		// add tester to the specified cgroup
+		progTester.AddToCgroup(t, fullPath[tc.cgPath])
+
+		// print cgroup to logs
+		out, err := progTester.Command("exec /usr/bin/cat /proc/self/cgroup")
+		require.NoError(t, err, out)
+		t.Logf("%s", out)
+
+		// for the test operation, we execute a copy of "true" to generate an exec event
+		ops := func() {
+			if out, err := progTester.Exec(tmpTrue); err != nil {
+				t.Logf("command failed: %s", err)
+			} else {
+				t.Logf("ops out: %s", out)
+			}
+		}
+
+		// run the op and check the events
+		events := perfring.RunTestEvents(t, ctx, ops)
+		for _, ev := range events {
+			if exec, ok := ev.(*grpcexec.MsgExecveEventUnix); ok {
+				if exec.Unix.Process.Filename == tmpTrue {
+					require.Equal(t, tc.expCgID, exec.Unix.Kube.Cgrpid, "cgroup id does not match")
+					require.Equal(t, tc.expCgTrackerID, exec.Unix.Kube.CgrpTrackerID, "cgroup tracker id does not match")
+				}
+			}
+		}
+	}
+
+	progTester.Stop()
 }
