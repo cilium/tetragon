79f2d730cc2665ae2fa225e12131bf141ef0ca7d
Kevin Sheldrake <kevin.sheldrake@isovalent.com>
2024-12-03 10:33:39 +0000

    ProcessCache: Disable interval test in VM
    The TestProcessCacheInterval test is flaky in VMs, probably because it
    relies on timing. This commit disables the test in the vmtests.
    
    Signed-off-by: Kevin Sheldrake <kevin.sheldrake@isovalent.com>

--- pkg/vmtests/skip.go
+++ pkg/vmtests/skip.go
@@ -19,6 +19,7 @@ type skipRule struct {
 var rules = []skipRule{
 	skipRule{TestNameRe: "pkg.sensors.tracing.TestLoader", KernelRe: "(6\\.6|6\\.1)"},
 	skipRule{TestNameRe: "pkg.tracepoint.TestTracepointLoadFormat", KernelRe: "(6\\.1|bpf-next)"},
+	skipRule{TestNameRe: "pkg.sensors.exec.TestProcessCacheInterval", KernelRe: ""},
 }
 
 func init() {
