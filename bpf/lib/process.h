cilium/tetragon

commit e73718ccb739ed45c4495cc82ace07c33f171b47

exec: track matchbinary children
This commit adds an mb_bitset field in the binary structure.
The filed is set in the exec hook using the parent bitset
and the contents of the tg_mbset_map.

Note that in bpf_fork, we memcpy the old binary so the bitset will be
propageted to children.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

bpf/lib/process.h
@@ -305,6 +305,8 @@ struct msg_execve_event {
 #endif
 }; // All fields aligned so no 'packed' attribute.
 
+typedef __u64 mbset_t;
+
 // This structure stores the binary path that was recorded on execve.
 // Technically PATH_MAX is 4096 but we limit the length we store since we have
 // limits on the length of the string to compare:
@@ -318,6 +320,8 @@ struct binary {
 	__s64 path_length;
 	// BINARY_PATH_MAX_LEN first bytes of the path
 	char path[BINARY_PATH_MAX_LEN];
+	// matchBinary bitset for binary
+	mbset_t mb_bitset;
 }; // All fields aligned so no 'packed' attribute
 
 // The execve_map_value is tracked by the TGID of the thread group

bpf/process/bpf_execve_event.c
@@ -31,8 +31,6 @@ struct {
 	__type(value, struct msg_data);
 } data_heap SEC(".maps");
 
-typedef __u64 mbset_t;
-
 /* tg_mbset_map holds a mapping from (binary) paths to a bitset of ids that it matches. The map is
  * written by user-space and read in the exec hook to determine the bitset of ids of a binary that
  * is executed.
@@ -271,6 +269,29 @@ execve_rate(void *ctx)
 	return 0;
 }
 
+/* update bitset mark */
+FUNC_INLINE
+void update_mb_bitset(struct binary *bin)
+{
+	__u64 *bitsetp;
+	struct execve_map_value *parent;
+
+	parent = event_find_parent();
+	if (parent) {
+		/* ->mb_bitset is used to track matchBinary matches to children (followChildren), so
+		 * here we propagate the parent value to the child.
+		 */
+		bin->mb_bitset = parent->bin.mb_bitset;
+	}
+
+	/* check the map and see if the binary path matches a binary
+	 * NB: only the In operator is supported for now
+	 */
+	bitsetp = map_lookup_elem(&tg_mbset_map, bin->path);
+	if (bitsetp)
+		bin->mb_bitset |= *bitsetp;
+}
+
 /**
  * execve_send() sends the collected execve event data.
  *
@@ -357,6 +378,8 @@ execve_send(void *ctx)
 			curr->bin.path_length--;
 		}
 #endif
+
+		update_mb_bitset(&curr->bin);
 	}
 
 	event->common.flags = 0;

pkg/api/processapi/processapi.go
@@ -141,6 +141,7 @@ type MsgCapabilities struct {
 type Binary struct {
 	PathLength int64
 	Path       [BINARY_PATH_MAX_LEN]byte
+	MBSet      uint64
 }
 
 type MsgNamespaces struct {

commit 42837d5f172efeb8eced1556317f6c63a2a01bdf

exec: fix tracking of matchBinary children
If we executed a binary that directly executed in another binary, the
->mb_bitset field was zeroed out and the information was lost, which
meant that the matching would not work as expected.

Fix this by i) not zeroing out the ->mb_bitset field and, also,
or it with the parent.

Add a test.

Fixes: e73718ccb739 ("exec: track matchbinary children")

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

bpf/lib/process.h
@@ -323,8 +323,21 @@ struct binary {
 	// STRING_POSTFIX_MAX_LENGTH reversed last bytes of the path
 	char end_r[STRING_POSTFIX_MAX_LENGTH];
 	// matchBinary bitset for binary
+	// the struct.
+	// NB: keep this at as the last part of the struct, or modify binary_reset()
 	mbset_t mb_bitset;
 }; // All fields aligned so no 'packed' attribute
+
+FUNC_INLINE void
+binary_reset(struct binary *b)
+{
+	// buffer can be written at clone stage with parent's info, if previous path is longer than
+	// current, we can have leftovers at the end, so zero out bin structure.
+	//
+	// Do not zero the ->mb_bitset however, so that it can be inherited if exec() is called.
+	// This depends on ->mb_bitset being the last part of the struct.
+	memset(b, 0, sizeof(*b) - sizeof(b->mb_bitset));
+}
 
 // The execve_map_value is tracked by the TGID of the thread group
 // the msg_execve_key.pid. The thread IDs are recorded on the

bpf/process/bpf_execve_event.c
@@ -308,7 +308,7 @@ void update_mb_bitset(struct binary *bin)
 		/* ->mb_bitset is used to track matchBinary matches to children (followChildren), so
 		 * here we propagate the parent value to the child.
 		 */
-		bin->mb_bitset = parent->bin.mb_bitset;
+		bin->mb_bitset |= parent->bin.mb_bitset;
 	}
 
 	/* check the map and see if the binary path matches a binary
@@ -385,9 +385,8 @@ execve_send(void *ctx __arg_ctx)
 			curr->caps.inheritable = event->creds.caps.inheritable;
 		}
 #endif
-		// buffer can be written at clone stage with parent's info, if previous
-		// path is longer than current, we can have leftovers at the end.
-		memset(&curr->bin, 0, sizeof(curr->bin));
+		/* zero out previous paths in ->bin */
+		binary_reset(&curr->bin);
 #ifdef __LARGE_BPF_PROG
 		// read from proc exe stored at execve time
 		if (event->exe.len <= BINARY_PATH_MAX_LEN) {

pkg/sensors/tracing/matchbinaries_follow_children_test.go
@@ -64,7 +64,16 @@ func TestMatchBinariesFollowChildren(t *testing.T) {
 			t.Fatalf("failed to run command %s: %v", cmd, err)
 		}
 	}
-
 	perfring.RunTest(t, ctx, ops, eventFn)
-	require.Equal(t, 1, getcpuCnt)
+	require.Equal(t, 1, getcpuCnt, "single exec")
+
+	getcpuCnt = 0
+	ops2 := func() {
+		cmd := exec.Command(tmpShPath, "-c", fmt.Sprintf("exec sh -c %s", getcpuBin))
+		if err := cmd.Run(); err != nil {
+			t.Fatalf("failed to run command %s: %v", cmd, err)
+		}
+	}
+	perfring.RunTest(t, ctx, ops2, eventFn)
+	require.Equal(t, 1, getcpuCnt, "double exec")
 }