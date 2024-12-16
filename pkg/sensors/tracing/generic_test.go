9c8eb3a5669f2e674087a50fdae56d9cc95fe4ff
Tristan d'Audibert <tristan.daudibert@orange.com>
2024-12-16 17:57:23 +0100

    generic_test.go : Add unit test for resolveBtfArg with fake Kprobe policy
    `TestResolveBtfArgFromKprobePolicy` is introduced. It aims to test the
    access to the requested attribute with no errors and assert that the very
    last type corresponds to the requested policy type (string/int/etc).
    
    3 different cases are handled:
    - Test a classic hook
    - Test resolve from double pointer parameter
    - Assert an error is raised when the resolve path is too long
    
    Signed-off-by: Tristan d'Audibert <tristan.daudibert@orange.com>

---
+++ pkg/sensors/tracing/generic_test.go
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package tracing
+
+import (
+	"testing"
+
+	api "github.com/cilium/tetragon/pkg/api/tracingapi"
+	gt "github.com/cilium/tetragon/pkg/generictypes"
+	"github.com/cilium/tetragon/pkg/tracingpolicy"
+	"github.com/stretchr/testify/assert"
+)
+
+// This test attempt to test the Resolve flag in tracing policies.
+//   - The 2 first hooks assert no error occures when searching for the BTF config
+//   - The last test is used to assert an error occures when path length is
+//     higher than api.MaxBtfArgDepth
+func TestResolveBtfArgFromKprobePolicy(t *testing.T) {
+	rawPolicy := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "kprobes"
+spec:
+  kprobes:
+  - call: "security_bprm_check"
+    args:
+    - index: 0
+      type: "int"
+      resolve: 'mm.arg_start'
+  - call: "security_inode_copy_up"
+    args:
+    - index: 0
+      type: "uint32"
+      resolve: 'd_flags'
+    - index: 1
+      type: "int"
+      resolve: 'user.uid.val'
+  - call: "security_bprm_check"
+    args:
+    - index: 0
+      type: "string"
+      resolve: 'mm.owner.real_parent.real_parent.real_parent.real_parent.real_parent.real_parent.real_parent.real_parent.comm'
+  `
+	policy, err := tracingpolicy.FromYAML(rawPolicy)
+	assert.NoError(t, err, "FromYAML rawPolicy error %q", err)
+
+	successHook := policy.TpSpec().KProbes[:2]
+	for _, hook := range successHook {
+		for _, arg := range hook.Args {
+			lastBtfType, btfArg, err := resolveBtfArg(hook.Call, arg)
+
+			if err != nil {
+				t.Fatal(hook.Call, err)
+			}
+			assert.NotNil(t, lastBtfType)
+			assert.NotNil(t, btfArg)
+
+			argType := findTypeFromBtfType(arg, lastBtfType)
+			assert.NotEqual(t, gt.GenericInvalidType, argType, "Type %q is not supported", (*lastBtfType).TypeName())
+		}
+	}
+
+	failHook := policy.TpSpec().KProbes[2]
+	for _, arg := range failHook.Args {
+		_, _, err := resolveBtfArg(failHook.Call, arg)
+
+		if !assert.ErrorContains(t, err, "The maximum depth allowed is") {
+			t.Fatalf("The path %q must have len < %d", arg.Resolve, api.MaxBtfArgDepth)
+		}
+	}
+}
