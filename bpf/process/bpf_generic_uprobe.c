67953c17e3b1e92dd023efe844aac963c232808a
Jiri Olsa <jolsa@kernel.org>
2024-12-19 10:10:25 +0000

    tetragon: Use data_heap in generic functions
    Using data_heap in generic functions directly, so we don't
    need to pass it as argument.
    
    Passing map pointer through argument might end up storing and loading
    it to/from heap which makes it scalar value and breaks verifier.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_generic_uprobe.c
+++ bpf/process/bpf_generic_uprobe.c
@@ -64,16 +64,13 @@ generic_uprobe_event(struct pt_regs *ctx)
 __attribute__((section("uprobe"), used)) int
 generic_uprobe_setup_event(void *ctx)
 {
-	return generic_process_event_and_setup(
-		ctx,
-		(struct bpf_map_def *)&uprobe_calls, 0);
+	return generic_process_event_and_setup(ctx, (struct bpf_map_def *)&uprobe_calls);
 }
 
 __attribute__((section("uprobe"), used)) int
 generic_uprobe_process_event(void *ctx)
 {
-	return generic_process_event(ctx,
-				     (struct bpf_map_def *)&uprobe_calls, 0);
+	return generic_process_event(ctx, (struct bpf_map_def *)&uprobe_calls);
 }
 
 __attribute__((section("uprobe"), used)) int
