0118a0dfb1da51337b88de4fee919e70005ea4c9
Jiri Olsa <jolsa@kernel.org>
2024-12-20 08:44:59 +0000

    tetragon: Switch filter_read_arg to use maps directly
    Switching generic_start_process_filter to use maps directly and drop
    struct generic_maps.
    
    Keeping tail call map, because it's convenient in following changes.
    
    And rename it to generic_filter_arg.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_generic_lsm_core.c
+++ bpf/process/bpf_generic_lsm_core.c
@@ -78,11 +78,7 @@ generic_lsm_process_filter(void *ctx)
 __attribute__((section("lsm"), used)) int
 generic_lsm_filter_arg(void *ctx)
 {
-	return filter_read_arg(ctx, (struct bpf_map_def *)&process_call_heap,
-			       (struct bpf_map_def *)&filter_map,
-			       (struct bpf_map_def *)&lsm_calls,
-			       (struct bpf_map_def *)&config_map,
-			       true);
+	return generic_filter_arg(ctx, (struct bpf_map_def *)&lsm_calls, true);
 }
 
 __attribute__((section("lsm"), used)) int
