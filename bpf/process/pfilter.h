6ed124a93fb81adb2ce2409f2579772b71b0d1e8
Jiri Olsa <jolsa@kernel.org>
2025-01-10 11:26:45 +0000

    tetragon: Pass execve_map_value directly to match_binaries
    Passing execve_map_value directly to match_binaries to
    eliminate superfluous event_find_curr in it.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/pfilter.h
+++ bpf/process/pfilter.h
@@ -409,7 +409,7 @@ selector_process_filter(__u32 *f, __u32 index, struct execve_map_value *enter,
 	__u64 i;
 
 	/* Do binary filter first for selector index */
-	if (!match_binaries(index))
+	if (!match_binaries(index, enter))
 		return 0;
 
 	/* Find selector offset byte index */
