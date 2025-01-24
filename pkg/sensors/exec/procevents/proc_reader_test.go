503cab7b6552396f7b4695678fe4e23fbdb829a2
William Findlay <william.findlay@isovalent.com>
2025-01-24 11:38:23 -0500

    proc_reader: handle in_init_tree
    Recently we introduced the in_init_tree flag into execve map values to indicate whether
    a process is a member of the initial process tree for a container. This worked well for
    containers started after Tetragon, but broke for cases where the container was started
    before Tetragon, since our procfs walk did not account for the in_init_tree flag. Fix this
    behaviour by introducing logic in the procfs walk to account for this.
    
    Signed-off-by: William Findlay <william.findlay@isovalent.com>

--- pkg/sensors/exec/procevents/proc_reader_test.go
+++ pkg/sensors/exec/procevents/proc_reader_test.go
@@ -4,8 +4,15 @@
 package procevents
 
 import (
+	"os/exec"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/cilium/tetragon/pkg/api"
+	"github.com/cilium/tetragon/pkg/observer/observertesthelper/docker"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,5 +25,37 @@ func TestListRunningProcs(t *testing.T) {
 	for _, p := range procs {
 		require.NotZero(t, p.pid)
 		require.Equal(t, p.pid, p.tid)
+	}
+}
+
+func TestInInitTreeProcfs(t *testing.T) {
+	if err := exec.Command("docker", "version").Run(); err != nil {
+		t.Skipf("docker not available. skipping test: %s", err)
+	}
+
+	containerID := docker.Create(t, "--name", "procfs-in-init-tree-test", "bash", "bash", "-c", "sleep infinity")
+
+	docker.Start(t, "procfs-in-init-tree-test")
+	time.Sleep(1 * time.Second)
+
+	rootPidOutput, err := exec.Command("docker", "inspect", "-f", "{{.State.Pid}}", containerID).Output()
+	require.NoError(t, err, "root pid should fetch")
+	rootPid, err := strconv.Atoi(strings.TrimSpace(string(rootPidOutput)))
+	require.NoError(t, err, "root pid should parse")
+
+	procs, err := listRunningProcs("/proc")
+	require.NoError(t, err)
+	require.NotNil(t, procs)
+	require.NotEqual(t, 0, len(procs))
+
+	inInitTree := make(map[uint32]struct{})
+	for _, p := range procs {
+		require.NotZero(t, p.pid)
+		require.Equal(t, p.pid, p.tid)
+		_, v := procToKeyValue(p, inInitTree)
+		if v.Process.Pid == uint32(rootPid) || v.Parent.Pid == uint32(rootPid) {
+			isInInitTree := v.Flags&api.EventInInitTree == api.EventInInitTree
+			assert.True(t, isInInitTree)
+		}
 	}
 }
