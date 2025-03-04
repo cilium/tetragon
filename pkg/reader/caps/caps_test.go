9b015338ff8f93d9cf1d3941be8c3e983cdc34f9
Anadi Anadi <aanadi@cisco.com>
2025-03-04 22:11:42 +0000

    tetragon: Move specific unix constants to pkg/constants/constants_linux.go
    Some unix.CONSTANT constants are now moved to a separate constants package
    This allows platfrom independent go packages importing Linux constants to compile on Windows.
    
    Signed-off-by: Anadi Anadi <aanadi@cisco.com>

--- pkg/reader/caps/caps_test.go
+++ pkg/reader/caps/caps_test.go
@@ -6,39 +6,39 @@ package caps
 import (
 	"testing"
 
+	"github.com/cilium/tetragon/pkg/constants"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/sys/unix"
 )
 
 func TestIsCapValid(t *testing.T) {
-	valid := isCapValid(unix.CAP_CHOWN)
+	valid := isCapValid(constants.CAP_CHOWN)
 	assert.Equal(t, true, valid)
 
-	valid = isCapValid(unix.CAP_CHOWN - 1)
+	valid = isCapValid(constants.CAP_CHOWN - 1)
 	assert.Equal(t, false, valid)
 
-	valid = isCapValid(unix.CAP_LAST_CAP)
+	valid = isCapValid(constants.CAP_LAST_CAP)
 	assert.Equal(t, true, valid)
 
-	valid = isCapValid(unix.CAP_LAST_CAP + 1)
+	valid = isCapValid(constants.CAP_LAST_CAP + 1)
 	assert.Equal(t, false, valid)
 }
 
 func TestGetCapability(t *testing.T) {
 	// Test our caps package if it was updated and contains the last CAP_LAST_CAP from upstream
-	str, err := GetCapability(unix.CAP_LAST_CAP)
+	str, err := GetCapability(constants.CAP_LAST_CAP)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, str)
 
-	str, err = GetCapability(unix.CAP_CHOWN)
+	str, err = GetCapability(constants.CAP_CHOWN)
 	assert.NoError(t, err)
 	assert.Equal(t, "CAP_CHOWN", str)
 
-	str, err = GetCapability(unix.CAP_LAST_CAP + 1)
+	str, err = GetCapability(constants.CAP_LAST_CAP + 1)
 	assert.Error(t, err)
 	assert.Empty(t, str)
 
-	str, err = GetCapability(unix.CAP_CHOWN - 1)
+	str, err = GetCapability(constants.CAP_CHOWN - 1)
 	assert.Error(t, err)
 	assert.Empty(t, str)
 }
