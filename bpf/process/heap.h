ea6b6f3ed61d79a78c61d8227a7a2c26b1717bcd
Jiri Olsa <jolsa@kernel.org>
2025-03-13 12:50:26 +0000

    tetragon: Add heap_ro_zero map
    And use it instead of string_maps_ro_zero and ratelimit_ro_heap maps.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ bpf/process/heap.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#ifndef __HEAP_H__
+#define __HEAP_H__
+
+#include "ratelimit_maps.h"
+
+struct heap_ro_value {
+	union {
+		char string_maps_heap[STRING_MAPS_HEAP_SIZE];
+		char ratelimit_heap[sizeof(struct ratelimit_key) + 128];
+	};
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, struct heap_ro_value);
+} heap_ro_zero SEC(".maps");
+
+#endif // __HEAP_H__
