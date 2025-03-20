4c6a99b72d2c844faede9aa031376fbb5be06832
Anadi Sharma <aanadi@cisco.com>
2025-03-20 15:02:49 -0400

    tetragon/windows: Compile pkg/metricsconfig package on Windows
    Add windows specific stub-function: registerHealthMetricsEx in an otherwise platform independent metricsconfig package
    This enables compilation of pkg/metricsconfig on Windows
    
    Signed-off-by: Anadi Anadi <aanadi@cisco.com>

---
+++ pkg/metricsconfig/healthmetrics_windows.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package metricsconfig
+
+import "github.com/cilium/tetragon/pkg/metrics"
+
+func registerHealthMetricsEx(group metrics.Group) {
+
+}
