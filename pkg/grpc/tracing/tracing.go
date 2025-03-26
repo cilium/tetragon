2284ec8b0c2ef56465e042bc137b188279cadfc5
Jiri Olsa <jolsa@kernel.org>
2025-03-26 17:29:14 +0000

    tetragon/tracepoint: Add support for linux_binprm argument
    Adding support to retrieve linux_binprm argument.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/grpc/tracing/tracing.go
+++ pkg/grpc/tracing/tracing.go
@@ -575,6 +575,16 @@ func (msg *MsgGenericTracepointUnix) HandleMessage() *tetragon.GetEventsResponse
 					Abi: v.ABI,
 				},
 			}})
+		case tracingapi.MsgGenericKprobeArgLinuxBinprm:
+			bprm := &tetragon.KprobeLinuxBinprm{
+				Path:       v.Value,
+				Flags:      path.FilePathFlagsToStr(v.Flags),
+				Permission: path.FilePathModeToStr(v.Permission),
+			}
+
+			tetragonArgs = append(tetragonArgs, &tetragon.KprobeArgument{Arg: &tetragon.KprobeArgument_LinuxBinprmArg{
+				LinuxBinprmArg: bprm,
+			}})
 
 		default:
 			logger.GetLogger().Warnf("handleGenericTracepointMessage: unhandled value: %+v (%T)", arg, arg)
