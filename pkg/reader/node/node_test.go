cilium/tetragon

commit bcf9429a5cdc4454e8d69f02b1e5796f60b7df31

Watcher: fix NODE_NAME if missing
The pkg/reader/node package gets the value of the NODE_NAME environment
variable (if the HUBBLE_NODE_NAME is empty). If it is empty, it gets the
hostname and uses that. The pkg/watcher package just attempts to get the
NODE_NAME environment variable and warns that it will not work as
expected if it is empty or missing. This commit changes pkg/watcher so
that it uses pkg/reader/node's function instead.

Signed-off-by: Kevin Sheldrake <kevin.sheldrake@isovalent.com>

pkg/watcher/watcher.go
@@ -6,13 +6,13 @@ package watcher
 import (
 	"errors"
 	"fmt"
-	"os"
 	"reflect"
 	"strings"
 	"time"
 
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/cilium/tetragon/pkg/podhooks"
+	"github.com/cilium/tetragon/pkg/reader/node"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -121,7 +121,7 @@ func containerIndexFunc(obj interface{}) ([]string, error) {
 
 // NewK8sWatcher returns a pointer to an initialized K8sWatcher struct.
 func NewK8sWatcher(k8sClient kubernetes.Interface, stateSyncIntervalSec time.Duration) *K8sWatcher {
-	nodeName := os.Getenv("NODE_NAME")
+	nodeName := node.GetNodeNameForExport()
 	if nodeName == "" {
 		logger.GetLogger().Warn("env var NODE_NAME not specified, K8s watcher will not work as expected")
 	}
@@ -134,7 +134,7 @@ func NewK8sWatcher(k8sClient kubernetes.Interface, stateSyncIntervalSec time.Dur
 	k8sInformerFactory := informers.NewSharedInformerFactoryWithOptions(k8sClient, stateSyncIntervalSec,
 		informers.WithTweakListOptions(func(options *metav1.ListOptions) {
 			// Watch local pods only.
-			options.FieldSelector = "spec.nodeName=" + os.Getenv("NODE_NAME")
+			options.FieldSelector = "spec.nodeName=" + nodeName
 		}))
 	podInformer := k8sInformerFactory.Core().V1().Pods().Informer()
 	k8sWatcher.AddInformers(k8sInformerFactory, &InternalInformer{

commit 2152f4ddca58a8e8ab8449099c1cc3212d26524a

Fix discrepancy between export and k8s node name
The HUBBLE_NODE_NAME env var is useful to add extra metadata to the
"node_name" field for exported events. Often the Kubernetes node name is
not very descriptive, and additional information is useful. This can be
archived by eg. overriding the following helm values, where k8s will
replace the $(NODE_NAME) with the existing NODE_NAME env var in the helm
chart, populated from the actual node name.

tetragon.extraEnv=[{"name":"HUBBLE_NODE_NAME", "value":"$(NODE_NAME)-additional-info-here.domain.tld"}]

Prior to bcf9429 ("Watcher: fix NODE_NAME if missing") this worked
as expected where the export events keep this value, and the Kubernetes
pod watcher used the existing NODE_NAME. After that commit, the node
watcher stated using the HUBBLE_NODE_NAME, resulting in it not receiving any
pod events and pod attribution not working for exported events.

Fixes: bcf9429 ("Watcher: fix NODE_NAME if missing")
Signed-off-by: Odin Ugedal <ougedal@palantir.com>
Signed-off-by: Odin Ugedal <odin@uged.al>

pkg/reader/node/node.go
@@ -11,42 +11,73 @@ import (
 	"github.com/cilium/tetragon/pkg/option"
 )
 
+const (
+	hubbleNodeNameEnvVar = "HUBBLE_NODE_NAME"
+	nodeNameEnvVar       = "NODE_NAME"
+)
+
 var (
-	nodeName string
+	kubernetesNodeName string
+	exportNodeName     string
 )
 
 func init() {
-	SetNodeName()
+	SetExportNodeName()
+	SetKubernetesNodeName()
 }
 
-// SetNodeName initializes the nodeName variable. It's defined separately from
+// SetExportNodeName initializes the exportNodeName variable. It's defined separately from
 // init() so that it can be called from unit tests.
-func SetNodeName() {
+func SetExportNodeName() {
 	var err error
-	nodeName = os.Getenv("HUBBLE_NODE_NAME")
-	if nodeName == "" {
-		nodeName = os.Getenv("NODE_NAME")
+	if exportNodeName = os.Getenv(hubbleNodeNameEnvVar); exportNodeName != "" {
+		return
 	}
-	if nodeName == "" {
-		nodeName, err = os.Hostname()
-		if err != nil {
-			logger.GetLogger().WithError(err).Warn("failed to retrieve hostname")
-		}
+	if exportNodeName = os.Getenv(nodeNameEnvVar); exportNodeName != "" {
+		return
+	}
+	exportNodeName, err = os.Hostname()
+	if err != nil {
+		logger.GetLogger().WithError(err).Warn("failed to retrieve hostname")
 	}
 }
 
-// GetNodeNameForExport returns node name string for JSON export. It uses NODE_NAME
-// env variable by default, which is also used by k8s watcher to watch for local pods:
-//
-//	https://github.com/cilium/tetragon/blob/a7be620c9fecdc2b693e3633506aca35d46cd3b2/pkg/grpc/watcher.go#L32
-//
-// Set HUBBLE_NODE_NAME to override the node_name field for JSON export.
+// SetKubernetesNodeName initializes the kubernetesNodeName variable. It's defined separately from
+// init() so that it can be called from unit tests.
+func SetKubernetesNodeName() {
+	var err error
+	if kubernetesNodeName = os.Getenv(nodeNameEnvVar); kubernetesNodeName != "" {
+		return
+	}
+	if kubernetesNodeName = os.Getenv(hubbleNodeNameEnvVar); kubernetesNodeName != "" {
+		return
+	}
+	kubernetesNodeName, err = os.Hostname()
+	if err != nil {
+		logger.GetLogger().WithError(err).Warn("failed to retrieve hostname")
+	}
+}
+
+// GetNodeNameForExport returns node name string for JSON export. It uses the HUBBLE_NODE_NAME
+// env variable by default, and falls back to NODE_NAME if the former is missing. If both
+// are missing, it will use the host name reported by the kernel
 func GetNodeNameForExport() string {
-	return nodeName
+	return exportNodeName
+}
+
+// GetKubernetesNodeName returns node name string for the given node in Kubernetes. It uses the NODE_NAME
+// env variable by default, and falls back to HUBBLE_NODE_NAME if the former is missing. If both
+// are missing, it will use the host name reported by the kernel. This value is used when watching for
+// pods running on the node in Kubernetes.
+//
+// NOTE: This is different from the Export equivalent for cases where nodes in kubernetes are named different
+// from the desired node name in the JSON export.
+func GetKubernetesNodeName() string {
+	return kubernetesNodeName
 }
 
 // SetCommonFields set fields that are common in all the events.
 func SetCommonFields(ev *tetragon.GetEventsResponse) {
-	ev.NodeName = nodeName
+	ev.NodeName = exportNodeName
 	ev.ClusterName = option.Config.ClusterName
 }

pkg/watcher/watcher.go
@@ -56,7 +56,7 @@ func NewK8sWatcher(
 			k8sClient, stateSyncInterval, informers.WithTweakListOptions(
 				func(options *metav1.ListOptions) {
 					// watch local pods only
-					options.FieldSelector = "spec.nodeName=" + node.GetNodeNameForExport()
+					options.FieldSelector = "spec.nodeName=" + node.GetKubernetesNodeName()
 				}))
 	}
 	if crdClient != nil {

pkg/exporter/exporter_test.go
@@ -172,7 +172,7 @@ func Test_rateLimitExport(t *testing.T) {
 		}
 		defer os.Unsetenv(hubbleNodeNameEnv)
 	}
-	node.SetNodeName()
+	node.SetExportNodeName()
 
 	tests := []struct {
 		name              string

pkg/grpc/process_manager_test.go
@@ -214,7 +214,7 @@ func TestProcessManager_GetProcessExec(t *testing.T) {
 
 func TestProcessManager_GetProcessID(t *testing.T) {
 	assert.NoError(t, os.Setenv("NODE_NAME", "my-node"))
-	node.SetNodeName()
+	node.SetExportNodeName()
 
 	err := process.InitCache(watcher.NewFakeK8sWatcher([]interface{}{}), 10, defaults.DefaultProcessCacheGCInterval)
 	assert.NoError(t, err)

pkg/reader/node/node_test.go
@@ -15,10 +15,10 @@ import (
 func TestGetNodeNameForExport(t *testing.T) {
 	assert.NotEqual(t, "", GetNodeNameForExport()) // we should get the hostname here
 	assert.NoError(t, os.Setenv("NODE_NAME", "from-node-name"))
-	SetNodeName()
+	SetExportNodeName()
 	assert.Equal(t, "from-node-name", GetNodeNameForExport())
 	assert.NoError(t, os.Setenv("HUBBLE_NODE_NAME", "from-hubble-node-name"))
-	SetNodeName()
+	SetExportNodeName()
 	assert.Equal(t, "from-hubble-node-name", GetNodeNameForExport())
 	assert.NoError(t, os.Unsetenv("NODE_NAME"))
 	assert.NoError(t, os.Unsetenv("HUBBLE_NODE_NAME"))
@@ -30,10 +30,22 @@ func TestSetCommonFields(t *testing.T) {
 	assert.Empty(t, ev.ClusterName)
 	nodeName := "my-node-name"
 	assert.NoError(t, os.Setenv("NODE_NAME", nodeName))
-	SetNodeName()
+	SetExportNodeName()
 	option.Config.ClusterName = "my-cluster-name"
 	SetCommonFields(&ev)
 	assert.Equal(t, nodeName, ev.GetNodeName())
 	assert.Equal(t, option.Config.ClusterName, ev.GetClusterName())
 	assert.NoError(t, os.Unsetenv("NODE_NAME"))
+}
+
+func TestGetKubernetesNodeName(t *testing.T) {
+	assert.NotEqual(t, "", GetKubernetesNodeName()) // we should get the hostname here
+	assert.NoError(t, os.Setenv("NODE_NAME", "from-node-name"))
+	SetKubernetesNodeName()
+	assert.Equal(t, "from-node-name", GetKubernetesNodeName())
+	assert.NoError(t, os.Setenv("HUBBLE_NODE_NAME", "from-hubble-node-name"))
+	SetKubernetesNodeName()
+	assert.Equal(t, "from-node-name", GetKubernetesNodeName())
+	assert.NoError(t, os.Unsetenv("NODE_NAME"))
+	assert.NoError(t, os.Unsetenv("HUBBLE_NODE_NAME"))
 }