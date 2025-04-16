cilium/tetragon

commit c968ed4ed19e34970de8ce0dd8e4f736451c2aa6

watcher: Watch namespaces
This could be useful for selecting namespaces by labels.

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

pkg/watcher/namespace.go
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package watcher
+
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+const (
+	namespaceInformerName = "namespace"
+)
+
+func AddNamespaceInformer(w Watcher) error {
+	if w == nil {
+		return fmt.Errorf("k8s watcher not initialized")
+	}
+	factory := w.GetK8sInformerFactory()
+	if factory == nil {
+		return fmt.Errorf("k8s informer factory not initialized")
+	}
+	informer := factory.Core().V1().Namespaces().Informer()
+	w.AddInformer(namespaceInformerName, informer, map[string]cache.IndexFunc{})
+	return nil
+}
+
+func (watcher *K8sWatcher) GetNamespace(name string) (*corev1.Namespace, error) {
+	namespaceInformer := watcher.GetInformer(namespaceInformerName)
+	if namespaceInformer == nil {
+		return nil, fmt.Errorf("namespace informer not initialized")
+	}
+	obj, exists, err := namespaceInformer.GetStore().GetByKey(name)
+	if err != nil {
+		return nil, fmt.Errorf("namespace watcher returned: %w", err)
+	}
+	if !exists {
+		return nil, fmt.Errorf("namespace %s not found", name)
+	}
+	namespace, ok := obj.(*corev1.Namespace)
+	if !ok {
+		return nil, fmt.Errorf("unexpected type %t", obj)
+	}
+	return namespace, nil
+}

pkg/watcher/namespace_test.go
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package watcher
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestGetNamespace(t *testing.T) {
+	ctx := context.Background()
+	k8sClient := fake.NewSimpleClientset()
+	k8sWatcher := NewK8sWatcher(k8sClient, nil, 60*time.Second)
+	err := AddNamespaceInformer(k8sWatcher)
+	assert.NoError(t, err)
+	k8sWatcher.Start()
+	ns1 := v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{Name: "ns1"},
+	}
+	ns2 := v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{Name: "ns2"},
+	}
+	_, err = k8sClient.CoreV1().Namespaces().Create(ctx, &ns1, metav1.CreateOptions{})
+	assert.NoError(t, err)
+	_, err = k8sClient.CoreV1().Namespaces().Create(ctx, &ns2, metav1.CreateOptions{})
+	assert.NoError(t, err)
+	assert.Eventually(t, func() bool {
+		return len(k8sWatcher.GetInformer(namespaceInformerName).GetStore().List()) == 2
+	}, 10*time.Second, 1*time.Second)
+	res, err := k8sWatcher.GetNamespace("ns1")
+	assert.NoError(t, err)
+	assert.Equal(t, "ns1", res.Name)
+	res, err = k8sWatcher.GetNamespace("ns2")
+	assert.NoError(t, err)
+	assert.Equal(t, "ns2", res.Name)
+	_, err = k8sWatcher.GetNamespace("ns3")
+	assert.Error(t, err)
+}

cmd/tetragon/main_linux.go
@@ -429,6 +429,9 @@ func tetragonExecuteCtx(ctx context.Context, cancel context.CancelFunc, ready fu
 		if err != nil {
 			return err
 		}
+		if err := watcher.AddNamespaceInformer(k8sWatcher); err != nil {
+			return err
+		}
 	} else {
 		log.Info("Disabling Kubernetes API")
 		k8sWatcher = watcher.NewFakeK8sWatcher(nil)

install/kubernetes/tetragon/templates/clusterrole.yaml
@@ -9,6 +9,7 @@ rules:
   - apiGroups:
       - ""
     resources:
+      - namespaces
       - pods
       - services
     verbs:

commit 6d1fd9172b1509beb0b74819349b0efae749c728

Use controller-runtime manager to access namespaces
Use controller-runtime [^1] to access Kubernetes namespaces instead of
explicitly initializing an informer. It dynamically adds informers on
read, so we don't need to initialize informers upfront.

[^1]: https://pkg.go.dev/sigs.k8s.io/controller-runtime

Fixes: c968ed4ed19e ("watcher: Watch namespaces")
Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

cmd/tetragon/main.go
@@ -35,6 +35,7 @@ import (
 	tetragonGrpc "github.com/cilium/tetragon/pkg/grpc"
 	"github.com/cilium/tetragon/pkg/health"
 	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/manager"
 	"github.com/cilium/tetragon/pkg/metrics"
 	"github.com/cilium/tetragon/pkg/metricsconfig"
 	"github.com/cilium/tetragon/pkg/observer"
@@ -398,6 +399,9 @@ func tetragonExecuteCtx(ctx context.Context, cancel context.CancelFunc, ready fu
 	var k8sWatcher watcher.K8sResourceWatcher
 	if option.Config.EnableK8s {
 		log.Info("Enabling Kubernetes API")
+		// Start controller-runtime manager.
+		controllerManager := manager.Get()
+		controllerManager.Start(ctx)
 		// retrieve k8s clients
 		k8sClient, crdClient, err = watcher.GetK8sClients(waitCRDs)
 		if err != nil {
@@ -414,9 +418,6 @@ func tetragonExecuteCtx(ctx context.Context, cancel context.CancelFunc, ready fu
 		realK8sWatcher := k8sWatcher.(*watcher.K8sWatcher)
 		err = watcher.AddPodInformer(realK8sWatcher, true)
 		if err != nil {
-			return err
-		}
-		if err := watcher.AddNamespaceInformer(k8sWatcher); err != nil {
 			return err
 		}
 	} else {

pkg/manager/manager.go
@@ -1,0 +1,75 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+package manager
+
+import (
+	"context"
+	"sync"
+
+	"github.com/bombsimon/logrusr/v4"
+	"github.com/cilium/tetragon/pkg/logger"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlManager "sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var (
+	initOnce, startOnce sync.Once
+	manager             *ControllerManager
+)
+
+// ControllerManager is responsible for running controller-runtime controllers,
+// and interacting with Kubernetes API server in general. If you need to interact
+// with Kubernetes API server, this is the place to start.
+type ControllerManager struct {
+	manager ctrlManager.Manager
+}
+
+func Get() *ControllerManager {
+	initOnce.Do(func() {
+		ctrl.SetLogger(logrusr.New(logger.GetLogger()))
+		scheme := runtime.NewScheme()
+		utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+		controllerManager, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{Scheme: scheme})
+		if err != nil {
+			panic(err)
+		}
+		manager = &ControllerManager{
+			manager: controllerManager,
+		}
+	})
+	return manager
+}
+
+func (cm *ControllerManager) Start(ctx context.Context) {
+	startOnce.Do(func() {
+		go func() {
+			if err := cm.manager.Start(ctx); err != nil {
+				panic(err)
+			}
+		}()
+		cm.manager.GetCache().WaitForCacheSync(ctx)
+	})
+}
+
+func (cm *ControllerManager) GetNamespace(name string) (*corev1.Namespace, error) {
+	ns := corev1.Namespace{}
+	if err := cm.manager.GetCache().Get(context.Background(), types.NamespacedName{Name: name}, &ns); err != nil {
+		return nil, err
+	}
+	return &ns, nil
+}
+
+func (cm *ControllerManager) ListNamespaces() ([]corev1.Namespace, error) {
+	namespaceList := corev1.NamespaceList{}
+	if err := cm.manager.GetCache().List(context.Background(), &namespaceList, &client.ListOptions{}); err != nil {
+		return nil, err
+	}
+	return namespaceList.Items, nil
+}

pkg/watcher/namespace.go
@@ -1,48 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Tetragon
-
-package watcher
-
-import (
-	"errors"
-	"fmt"
-
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/tools/cache"
-)
-
-const (
-	namespaceInformerName = "namespace"
-)
-
-func AddNamespaceInformer(w Watcher) error {
-	if w == nil {
-		return errors.New("k8s watcher not initialized")
-	}
-	factory := w.GetK8sInformerFactory()
-	if factory == nil {
-		return errors.New("k8s informer factory not initialized")
-	}
-	informer := factory.Core().V1().Namespaces().Informer()
-	w.AddInformer(namespaceInformerName, informer, map[string]cache.IndexFunc{})
-	return nil
-}
-
-func (watcher *K8sWatcher) GetNamespace(name string) (*corev1.Namespace, error) {
-	namespaceInformer := watcher.GetInformer(namespaceInformerName)
-	if namespaceInformer == nil {
-		return nil, errors.New("namespace informer not initialized")
-	}
-	obj, exists, err := namespaceInformer.GetStore().GetByKey(name)
-	if err != nil {
-		return nil, fmt.Errorf("namespace watcher returned: %w", err)
-	}
-	if !exists {
-		return nil, fmt.Errorf("namespace %s not found", name)
-	}
-	namespace, ok := obj.(*corev1.Namespace)
-	if !ok {
-		return nil, fmt.Errorf("unexpected type %t", obj)
-	}
-	return namespace, nil
-}

.github/workflows/integration-test.yaml
@@ -1,4 +1,4 @@
-name: PodInfo Integration Test
+name: Integration Test
 on:
   pull_request:
     types:
@@ -33,6 +33,7 @@ jobs:
           SHA=${{ github.sha }}
         fi
         echo "sha=${SHA}" >> $GITHUB_OUTPUT
+        echo "agentImage=quay.io/cilium/tetragon-ci:${SHA}" >> $GITHUB_OUTPUT
         echo "operatorImage=quay.io/cilium/tetragon-operator-ci:${SHA}" >> $GITHUB_OUTPUT
 
     - name: Install Go
@@ -59,6 +60,7 @@ jobs:
       run: |
         helm upgrade --install tetragon ./install/kubernetes/tetragon -n kube-system \
           --set tetragonOperator.podInfo.enabled=true \
+          --set tetragon.image.override=${{ steps.vars.outputs.agentImage }} \
           --set tetragonOperator.image.override=${{ steps.vars.outputs.operatorImage }}
         kubectl rollout status -n kube-system deployment/tetragon-operator
-        go test --tags=integration -v ./operator/...
+        go test --tags=integration -v ./operator/... ./pkg/manager/...

pkg/manager/manager_integration_test.go
@@ -1,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build integration
+
+package manager
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+type ManagerTestSuite struct {
+	suite.Suite
+	testEnv *envtest.Environment
+	manager *ControllerManager
+}
+
+func (suite *ManagerTestSuite) SetupSuite() {
+	useExistingCluster := true
+	suite.testEnv = &envtest.Environment{
+		UseExistingCluster: &useExistingCluster,
+	}
+	_, err := suite.testEnv.Start()
+	assert.NoError(suite.T(), err)
+	suite.manager = Get()
+	suite.manager.Start(context.Background())
+}
+
+func (suite *ManagerTestSuite) TestListNamespaces() {
+	// List namespaces.
+	namespaces, err := suite.manager.ListNamespaces()
+	assert.NoError(suite.T(), err)
+	assert.NotEmpty(suite.T(), namespaces)
+
+	// Call GetNamespace on the first namespace in the list.
+	namespace, err := suite.manager.GetNamespace(namespaces[0].Name)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), namespaces[0].Name, namespace.Name)
+}
+
+func (suite *ManagerTestSuite) TearDownSuite() {
+	assert.NoError(suite.T(), suite.testEnv.Stop())
+}
+
+func TestControllerSuite(t *testing.T) {
+	suite.Run(t, new(ManagerTestSuite))
+}

pkg/watcher/namespace_test.go
@@ -1,45 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Tetragon
-
-package watcher
-
-import (
-	"context"
-	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/fake"
-)
-
-func TestGetNamespace(t *testing.T) {
-	ctx := context.Background()
-	k8sClient := fake.NewSimpleClientset()
-	k8sWatcher := NewK8sWatcher(k8sClient, nil, 60*time.Second)
-	err := AddNamespaceInformer(k8sWatcher)
-	assert.NoError(t, err)
-	k8sWatcher.Start()
-	ns1 := v1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{Name: "ns1"},
-	}
-	ns2 := v1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{Name: "ns2"},
-	}
-	_, err = k8sClient.CoreV1().Namespaces().Create(ctx, &ns1, metav1.CreateOptions{})
-	assert.NoError(t, err)
-	_, err = k8sClient.CoreV1().Namespaces().Create(ctx, &ns2, metav1.CreateOptions{})
-	assert.NoError(t, err)
-	assert.Eventually(t, func() bool {
-		return len(k8sWatcher.GetInformer(namespaceInformerName).GetStore().List()) == 2
-	}, 10*time.Second, 1*time.Second)
-	res, err := k8sWatcher.GetNamespace("ns1")
-	assert.NoError(t, err)
-	assert.Equal(t, "ns1", res.Name)
-	res, err = k8sWatcher.GetNamespace("ns2")
-	assert.NoError(t, err)
-	assert.Equal(t, "ns2", res.Name)
-	_, err = k8sWatcher.GetNamespace("ns3")
-	assert.Error(t, err)
-}