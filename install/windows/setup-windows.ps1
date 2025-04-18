6a15558bbe0a32329e05adb4cd9a4b26287d14f5
Anadi Sharma <aanadi@cisco.com>
2025-04-18 17:48:22 -0400

    tetragon/windows: Build windows bpf program and run smoke tests
    This CI action does the following actions:
    
    Build the process_monitor.sys bpf program and the ntosebpfext.sys driver it depends on. These programs are uploaded as artifacts.
    
    Build tetragon.exe and tetra,exe on a Windows worker and uploads them asartifacts.
    
    Downloads the artifacts and installs them. A file setup-windows.ps1 is added to facilitate this step.
    
    It then downloads the ebpf-for-windows build and installs it. Runs tetragon.exe and launches a process to see if an event is received.
    
    Signed-off-by: Anadi Anadi <aanadi@cisco.com>

---
+++ install/windows/setup-windows.ps1
@@ -0,0 +1,51 @@
+param (
+    [string]$tetragonBinariesZip, # Path to the first ZIP archive
+    [string]$ntosebpfextZip  # Path to the second ZIP archive
+)
+
+# Function to extract a zip file
+function Extract-ZipFile {
+    param (
+        [string]$zipPath
+    )
+    $destinationName =  [System.IO.Path]::GetFileNameWithoutExtension($zipPath)
+    $destinationPath = Join-Path (Split-Path -Parent $zipPath)$destinationName
+
+    if ( (Test-Path $destinationPath)) {
+        Remove-Item -Path $destinationPath -Recurse -Force
+    }
+
+    New-Item -ItemType Directory -Path $destinationPath
+    
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $destinationPath)
+}
+
+$buildZip = $ntosebpfextZip
+Write-Host "Extracting $buildZip"
+$ntosebpfextZipBinaries = Extract-ZipFile -zipPath $buildZip
+Write-Host "Ntosebpfext archive extracted to: $ntosebpfextZipBinaries"
+
+$tetragonZip = $tetragonBinariesZip
+Write-Host "Extracting $tetragonZip"
+$tetragonBinaries = Extract-ZipFile -zipPath $tetragonZip
+Write-Host "Tetragon archive extracted to: $tetragonBinaries"
+
+
+New-Item -ItemType Directory -Path "C:\Program Files\Tetragon\cmd" -Force
+New-Item -ItemType Directory -Path "C:\Program Files\Tetragon\BPF" -Force
+New-Item -ItemType Directory -Path "C:\Program Files\Tetragon\tetragon.tp.d" -Force
+
+Copy-Item -Path $tetragonBinaries\*.exe -destination "C:\Program Files\tetragon\cmd\" -Force
+
+Copy-Item -Path "$ntosebpfextZipBinaries\Release\process_monitor_km\process_monitor.sys" -destination "C:\Program Files\tetragon\BPF\" -Force
+
+
+Start-Process -FilePath "$ntosebpfextZipBinaries\Release\ntos_ebpf_ext_export_program_info.exe" -ArgumentList "--clear"
+Start-Process -FilePath "$ntosebpfextZipBinaries\Release\ntos_ebpf_ext_export_program_info.exe"
+
+sc.exe delete "ntosebpfext"
+
+sc.exe create ntosebpfext type= kernel start= demand binPath= "$ntosebpfextZipBinaries\Release\ntosebpfext\ntosebpfext.sys"
+
+sc.exe start ntosebpfext
