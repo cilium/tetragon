cilium/tetragon

commit 8ce47efeab7accb69a5e6b901d8b1904a72d4de7

sensors: cache spec when loading maps
For every map we load, we find its program, load its spec and then load
the map. This introduces unnecessary overhead when loading the maps.
This commit, caches the specs and only loads them if we have not seen
the program before.

This speeds up loading a sensor, but also tests.

For example, Without the patch, running:
  go test -exec sudo  ./pkg/sensors/tracing -bpf-lib $(pwd)/bpf/objs -test.run TestKprobeSelectors -count 1

three times, results in:
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  8.773s
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  8.703s
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  8.739s

With the patch, the same command results in:
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.419s
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.532s
  ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.491s

Which is a ~14% improvement.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/sensors/load_linux.go
@@ -33,8 +33,9 @@ func (s *Sensor) setMapPinPath(m *program.Map) {
 }
 
 func (s *Sensor) preLoadMaps(bpfDir string, loadedMaps []*program.Map) ([]*program.Map, error) {
+	loaderCache := newLoaderCache()
 	for _, m := range s.Maps {
-		if err := s.loadMap(bpfDir, m); err != nil {
+		if err := s.loadMap(bpfDir, loaderCache, m); err != nil {
 			return loadedMaps, fmt.Errorf("tetragon, aborting could not load sensor BPF maps: %w", err)
 		}
 		loadedMaps = append(loadedMaps, m)
@@ -43,7 +44,7 @@ func (s *Sensor) preLoadMaps(bpfDir string, loadedMaps []*program.Map) ([]*progr
 }
 
 // loadMap loads BPF map in the sensor.
-func (s *Sensor) loadMap(bpfDir string, m *program.Map) error {
+func (s *Sensor) loadMap(bpfDir string, loaderCache *loaderCache, m *program.Map) error {
 	l := logger.GetLogger()
 	if m.PinState.IsLoaded() {
 		l.WithFields(logrus.Fields{
@@ -54,7 +55,7 @@ func (s *Sensor) loadMap(bpfDir string, m *program.Map) error {
 		return nil
 	}
 
-	spec, err := ebpf.LoadCollectionSpec(m.Prog.Name)
+	spec, err := loaderCache.loadCollectionSpec(m.Prog.Name)
 	if err != nil {
 		return fmt.Errorf("failed to open collection '%s': %w", m.Prog.Name, err)
 	}
@@ -210,4 +211,27 @@ func flushKernelSpec() {
 
 func getCachedBTFFile() string {
 	return cachedbtf.GetCachedBTFFile()
+}
+
+type loaderCache struct {
+	specCache map[string]*ebpf.CollectionSpec
+}
+
+func newLoaderCache() *loaderCache {
+	return &loaderCache{
+		specCache: make(map[string]*ebpf.CollectionSpec),
+	}
+}
+
+func (c *loaderCache) loadCollectionSpec(n string) (*ebpf.CollectionSpec, error) {
+	ret, ok := c.specCache[n]
+	if ok {
+		return ret, nil
+	}
+
+	ret, err := ebpf.LoadCollectionSpec(n)
+	if err == nil {
+		c.specCache[n] = ret
+	}
+	return ret, err
 }

commit 93b05879a9cc2da5ae1b83de333d3b763b77126f

loaderCache: copy map spec before using it
Commit 8ce47efeab7a, introduced a cache for the program specs when
loading maps. The go tests did not run in the corresponding PR
(https://github.com/cilium/tetragon/pull/3685) and, as a result, we
ended up with the following failure in main for some (rhel8.9, 5.15,
5.10, 5.3) kernels for the TestKprobeOverrideMulti test:

    observer_test_helper.go:465: SensorManager.AddTracingPolicy error: sensor generic_kprobe from collection sys-openat-signal-override failed to load: failed prog /home/kkourt/src/tetragon/bpf/objs/bpf_generic_kprobe_v511.o kern_version 331700 loadInstance: opening collection '/home/kkourt/src/tetragon/bpf/objs/bpf_generic_kprobe_v511.o' failed: using replacement map override_tasks: MaxEntries: 32768 changed to 1: map spec is incompatible with existing map

The problem is that the map spec is updated and, because it is now
cached, it is reused across multiple instances of the map. These
instances have different pin paths and may have different
configurations. Specifically, in the TestKproveOverideMulti one instance
of the map is configured with a single entry, while all others are
configured with 32768 entries.

To address this problem, this commits makes a copy of the mapSpec before
modifying it. Compared to 8ce47efeab7a, this leads to worse performance
but still better than without the cache.

Running:
  go test -exec sudo  ./pkg/sensors/tracing -bpf-lib $(pwd)/bpf/objs -test.run TestKprobeSelectors -count 1

This commit:
ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.831s
ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.849s
ok      github.com/cilium/tetragon/pkg/sensors/tracing  7.781s

Without the cache (21b326c830d491b62816eedbe9c55d57afeaf9da):
ok      github.com/cilium/tetragon/pkg/sensors/tracing  9.078s
ok      github.com/cilium/tetragon/pkg/sensors/tracing  9.074s
ok      github.com/cilium/tetragon/pkg/sensors/tracing  9.068s

Which is a ~13.5% improvement.

pkg/sensors/load_linux.go
@@ -64,6 +64,10 @@ func (s *Sensor) loadMap(bpfDir string, loaderCache *loaderCache, m *program.Map
 		return fmt.Errorf("map '%s' not found from '%s'", m.Name, m.Prog.Name)
 	}
 
+	// code below will modify mapSpec. Crate a copy so that the original spec (which is reused
+	// via the loaderCache for different inscanes of the map) is not modified.
+	mapSpec = mapSpec.Copy()
+
 	s.setMapPinPath(m)
 	pinPath := filepath.Join(bpfDir, m.PinPath)
 