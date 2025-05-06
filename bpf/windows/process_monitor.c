31a6dc3a770af68ad82ddac1ed84cca270b5efca
Anadi Sharma <aanadi@cisco.com>
2025-05-06 17:00:51 -0400

    tetragon/windows: kernel Pinned Map in CreateProcess program
    This PR changes the CreateProcess bpf program to pin the ringbuffer map
    via Windows supported map pinning mechanism LIBBPF_PIN_BY_NAME. This
    allows the ringbuf map to be used by multiple programs via name.
    
    this lets tetragon observer listed for multiple events and dispatch them
    to sensors based on OP code.
    
    Signed-off-by: Anadi Anadi<aanadi@cisco.com>

--- bpf/windows/process_monitor.c
+++ bpf/windows/process_monitor.c
@@ -1,44 +1,51 @@
-
-// This BPF program listens for process events and logs the process id, parent process id, creating process id, creating
-// thread id, and operation to a ring buffer. It also logs the image path and command line of the process to LRU hash
-// maps.
-
 #include "bpf_helpers.h"
-#include "process_event.h"
+#include "ebpf_ntos_hooks.h"
 
 #define IMAGE_PATH_SIZE (1024)
 
-// 64k bytes is the max byte count that fits in a UNICODE_STRING (because Length is USHORT).  Exactly 64k seems
+// 64k bytes is the max byte count that fits in a UNICODE_STRING (because Length is a USHORT).  Exactly 64k seems
 // to be a little too high for eBPF, so we subtract a few bytes and the likelihood this actually truncates anything
 // important is pretty low.
 #define COMMAND_SCRATCH_SIZE ((64 * 1024) - 16)
 
+#define MSG_OP_EXECVE 5
+
+struct msg_common {
+	uint8_t op;
+	uint8_t flags; // internal flags not exported
+	uint8_t pad[2];
+	uint32_t size;
+	uint64_t ktime;
+};
+
 // Declare a per-CPU array to be used as scratch space.
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__type(key, uint64_t); // key is pid
+	__type(key, uint64_t); // key is pid_tgid
 	__uint(value_size, COMMAND_SCRATCH_SIZE);
 	__uint(max_entries, 1);
 } temp SEC(".maps");
 
 struct {
 	__uint(type, BPF_MAP_TYPE_LRU_HASH);
-	__type(key, uint64_t); // key is pid
+	__type(key, uint64_t); // key is pid_tgid
 	__uint(value_size, COMMAND_SCRATCH_SIZE);
 	__uint(max_entries, 1024);
 } scratch_space SEC(".maps");
 
-// The non variable fields from the process_event struct.
-struct {
+// The non variable fields from the process_md_t struct.
+// Note: this must be kept in sync with the C# version in process_monitor.Library's ProcessMonitorBPFLoader.cs
+struct process_info_t {
+	struct msg_common common;
 	uint32_t process_id;
 	uint32_t parent_process_id;
 	uint32_t creating_process_id;
 	uint32_t creating_thread_id;
-	uint64_t creation_time;
-	uint64_t exit_time;
+	uint64_t creation_time; ///< Process creation time.
+	uint64_t exit_time; ///< Process exit time.
 	uint32_t process_exit_code;
 	uint8_t operation;
-} process_event;
+} process_info_t;
 
 // LRU hash for storing the image path of a process.
 struct {
@@ -56,18 +63,23 @@ struct {
 	__uint(max_entries, 1024);
 } command_map SEC(".maps");
 
-// Ring-buffer for process_event.
+// Ring-buffer for process_info_t.
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
 	__uint(max_entries, 1024 * 64);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
 } process_ringbuf SEC(".maps");
+
+// The following line is optional, but is used to verify
+// that the ProcesMonitor prototype is correct or the compiler
+// would complain when the function is actually defined below.
+process_hook_t ProcessMonitor;
 
 inline __attribute__((always_inline)) void *
 get_scratch_space()
 {
 	uint64_t current_pid_tgid_key = bpf_get_current_pid_tgid();
 	void *scratch = bpf_map_lookup_elem(&temp, &current_pid_tgid_key);
-
 	if (!scratch) {
 		uint32_t temp_key = 0;
 		// Allocate scratch space for this CPU.
@@ -90,20 +102,19 @@ get_scratch_space()
 	return scratch;
 }
 
-char __ebpf_go_platform[] SEC(".ebpf_go_platform") = "windows";
-
 SEC("process")
-int process_monitor(process_event_t *ctx)
+int ProcessMonitor(process_md_t *ctx)
 {
-	process_event process_info;
+	struct process_info_t process_info;
 
 	memset(&process_info, 0, sizeof(process_info));
-
+	process_info.common.op = MSG_OP_EXECVE;
 	process_info.process_id = ctx->process_id;
 	process_info.parent_process_id = ctx->parent_process_id;
 	process_info.creating_process_id = ctx->creating_process_id;
 	process_info.creating_thread_id = ctx->creating_thread_id;
 	process_info.creation_time = ctx->creation_time;
+	process_info.common.ktime = bpf_ktime_get_ns();
 	process_info.exit_time = ctx->exit_time;
 	process_info.process_exit_code = ctx->process_exit_code;
 	process_info.operation = ctx->operation;
@@ -115,9 +126,8 @@ int process_monitor(process_event_t *ctx)
 			return 0;
 
 		int command_length = ctx->command_end - ctx->command_start;
-
 		if (command_length > COMMAND_SCRATCH_SIZE)
-			command_length = COMMAND_SCRATCH_SIZE; // Better to truncate than to get nothing
+			command_length = COMMAND_SCRATCH_SIZE;
 
 		// Use COMMAND_SCRATCH_SIZE -1 to ensure the last byte stays a 0 for null termination
 		memcpy_s(buffer, COMMAND_SCRATCH_SIZE - 1, ctx->command_start, command_length);
