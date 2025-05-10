3e8dd2a9dd1221a622266080a096d5b698f8f5b8
Jiri Olsa <jolsa@kernel.org>
2025-05-10 07:54:39 +0000

    tetragon: Add codegen and generate changes
    For previous usdt message/spec changes.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- vendor/github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker/yaml/yaml.pb.go
+++ vendor/github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker/yaml/yaml.pb.go
@@ -145,6 +145,7 @@ type eventCheckerHelper struct {
 	ProcessKprobe     *eventchecker.ProcessKprobeChecker     `json:"kprobe,omitempty"`
 	ProcessTracepoint *eventchecker.ProcessTracepointChecker `json:"tracepoint,omitempty"`
 	ProcessUprobe     *eventchecker.ProcessUprobeChecker     `json:"uprobe,omitempty"`
+	ProcessUsdt       *eventchecker.ProcessUsdtChecker       `json:"usdt,omitempty"`
 	ProcessLsm        *eventchecker.ProcessLsmChecker        `json:"lsm,omitempty"`
 	Test              *eventchecker.TestChecker              `json:"test,omitempty"`
 	ProcessLoader     *eventchecker.ProcessLoaderChecker     `json:"loader,omitempty"`
@@ -194,6 +195,12 @@ func (checker *EventChecker) UnmarshalJSON(b []byte) error {
 		}
 		eventChecker = helper.ProcessUprobe
 	}
+	if helper.ProcessUsdt != nil {
+		if eventChecker != nil {
+			return fmt.Errorf("EventChecker: cannot define more than one checker, got %T but already had %T", helper.ProcessUsdt, eventChecker)
+		}
+		eventChecker = helper.ProcessUsdt
+	}
 	if helper.ProcessLsm != nil {
 		if eventChecker != nil {
 			return fmt.Errorf("EventChecker: cannot define more than one checker, got %T but already had %T", helper.ProcessLsm, eventChecker)
@@ -242,6 +249,8 @@ func (checker EventChecker) MarshalJSON() ([]byte, error) {
 		helper.ProcessTracepoint = c
 	case *eventchecker.ProcessUprobeChecker:
 		helper.ProcessUprobe = c
+	case *eventchecker.ProcessUsdtChecker:
+		helper.ProcessUsdt = c
 	case *eventchecker.ProcessLsmChecker:
 		helper.ProcessLsm = c
 	case *eventchecker.TestChecker:
