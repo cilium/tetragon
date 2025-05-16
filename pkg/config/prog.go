c66c93870f9fd30b03de109e4aba523d28d701c0
Kornilios Kourtis <kornilios@isovalent.com>
2025-05-16 11:14:30 +0200

    tetragon: support loading compressed (gz) .o files
    This commit adds support for tetragon to load compressed object files
    with gzip.
    
    The intention is to save space on the container image since tetragon
    ships different object files for the same programs that correspond to
    different kernel versions.
    
    Why gzip? I did a small test[^1] using gzip, bzip2, and xz. In terms of
    compression ratio, xz is the best choice but the problem is that there
    is no support for this format in the go standard library. This would
    mean that we would have to rely another dependency (binary or external
    library) which I opted to avoid. Between bzip2 and gzip, gzip's
    decompression is much faster while the compression is about the same so
    gzip is the best choice.
    
     gzip:   95M->26M (comp: 2.86s decomp:  .41s)
    bzip2:   95M->27M (comp: 4.18s decomp: 1.71s)
       xz:   95M->18M (comp:17.50s decomp:  .95s)
    
    Another choice that I made was to first decompress the file, and then
    load it as in the normal case (as opposed to using the decompressed
    stream directly every time we load the program). This approach still
    provides significant savings in the space tetragon uses since many
    objects files that are meant for different kernel versions will never be
    decompressed. It depends, however, on the bpf object directory to be
    writable from the agent.
    
    [^1]:
    for cmd in gzip bzip2 xz
    do
            before=$(du -hcs bpf/objs/* | grep total | cut -f1 )
    
            t0=$(date +%s%N)
            $cmd bpf/objs/*
            t1=$(date +%s%N)
            diff0=$(echo "scale=2; $((t1-t0))/1000000000" | bc)
    
            after=$(du -hcs bpf/objs/* | grep total | cut -f1)
    
            t0=$(date +%s%N)
            $cmd --decompress bpf/objs/*
            t1=$(date +%s%N)
            diff1=$(echo "scale=2; $((t1-t0))/1000000000" | bc)
    
            printf "%5s: %5s->%5s (comp:%5ss decomp:%5ss)\n" $cmd ${before} ${after} ${diff0} ${diff1}
    done
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/config/prog.go
+++ pkg/config/prog.go
@@ -4,7 +4,9 @@
 package config
 
 import (
+	"compress/gzip"
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
@@ -13,18 +15,59 @@ import (
 	"github.com/cilium/tetragon/pkg/option"
 )
 
+func tryDecompressGz(gzFname, dst string) error {
+	gz, err := os.Open(gzFname)
+	if err != nil {
+		return err
+	}
+	defer gz.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	zr, err := gzip.NewReader(gz)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(out, zr)
+	if err != nil {
+		defer func() {
+			// if there was an error, remove the .o file after we close it
+			os.Remove(dst)
+		}()
+	}
+	return err
+}
+
+// FindProgramFileUnderLocations
+func FindProgramFileUnderLocations(name string, locations ...string) (string, error) {
+	var checkedPaths []string
+	for _, loc := range locations {
+		pathname := name
+		if len(loc) > 0 {
+			pathname = path.Join(loc, filepath.Base(name))
+		}
+
+		if _, err := os.Stat(pathname); err == nil {
+			return pathname, nil
+		}
+		checkedPaths = append(checkedPaths, pathname)
+
+		gzFname := pathname + ".gz"
+		if _, err := os.Stat(gzFname); err == nil && tryDecompressGz(gzFname, pathname) == nil {
+			return pathname, nil
+		}
+		checkedPaths = append(checkedPaths, gzFname)
+
+	}
+	return "", fmt.Errorf("program %q can not be found (checked paths: %s)", name, strings.Join(checkedPaths, ","))
+}
+
 // FindProgramFile attempts to find the program file based on its path
 func FindProgramFile(name string) (string, error) {
-	var checkedPaths []string
-	if _, err := os.Stat(name); err == nil {
-		return name, nil
-	}
-	checkedPaths = append(checkedPaths, name)
-
-	name = path.Join(option.Config.HubbleLib, filepath.Base(name))
-	if _, err := os.Stat(name); err == nil {
-		return name, nil
-	}
-	checkedPaths = append(checkedPaths, name)
-	return "", fmt.Errorf("program %q can not be found (checked paths: %s)", name, strings.Join(checkedPaths, ","))
+	return FindProgramFileUnderLocations(name, "", option.Config.HubbleLib)
 }
