ac61372af25821141613e59a16366021eccae45b
Mahe Tardy <mahe.tardy@gmail.com>
2025-05-26 17:51:41 +0200

    linters/testifylint: fix floating point comparison
    Fixes:
    	pkg/ratelimit/ratelimit_test.go:19:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:20:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:21:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:22:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:24:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:26:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:29:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    	pkg/ratelimit/ratelimit_test.go:30:2                   testifylint  float-compare: use assert.InEpsilon (or InDelta)
    
    Co-authored-by: Kevin Sheldrake <kevin.sheldrake@isovalent.com>
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/ratelimit/ratelimit_test.go
+++ pkg/ratelimit/ratelimit_test.go
@@ -16,18 +16,20 @@ import (
 )
 
 func Test_getLimit(t *testing.T) {
-	assert.Equal(t, rate.Limit(0), getLimit(0, time.Minute))
-	assert.Equal(t, rate.Limit(0), getLimit(0, 0))
-	assert.Equal(t, rate.Limit(1), getLimit(60, time.Minute))
-	assert.Equal(t, rate.Limit(10.0/60), getLimit(10, time.Minute))
+	eps := 1e-9
+
+	assert.InDelta(t, float64(rate.Limit(0)), float64(getLimit(0, time.Minute)), eps)
+	assert.InDelta(t, float64(rate.Limit(0)), float64(getLimit(0, 0)), eps)
+	assert.InEpsilon(t, float64(rate.Limit(1)), float64(getLimit(60, time.Minute)), eps)
+	assert.InEpsilon(t, float64(rate.Limit(10.0/60)), float64(getLimit(10, time.Minute)), eps)
 	// 1/ms => 1000/second
-	assert.Equal(t, rate.Limit(1000), getLimit(1, time.Millisecond))
+	assert.InEpsilon(t, float64(rate.Limit(1000)), float64(getLimit(1, time.Millisecond)), eps)
 	// 3600/hour => 1/second
-	assert.Equal(t, rate.Limit(1), getLimit(60*60, time.Hour))
+	assert.InEpsilon(t, float64(rate.Limit(1)), float64(getLimit(60*60, time.Hour)), eps)
 
 	// interval<=0 => infinite rate limit (allow all events)
-	assert.Equal(t, rate.Inf, getLimit(1, 0))
-	assert.Equal(t, rate.Inf, getLimit(1, -1))
+	assert.InEpsilon(t, float64(rate.Inf), float64(getLimit(1, 0)), eps)
+	assert.InEpsilon(t, float64(rate.Inf), float64(getLimit(1, -1)), eps)
 }
 
 func Test_rateLimitJSON(t *testing.T) {
