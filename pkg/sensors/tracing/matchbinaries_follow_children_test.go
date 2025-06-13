0783a3fe39b3c2797d52f8f98b33b69a89f0ac40
Jiri Olsa <jolsa@kernel.org>
2025-06-13 11:02:16 +0000

    tetragon: Add test for re-using mbset IDs
    The MatchBinaries.FollowChildren usability was limited up
    to 64 used IDs. Now when this limits is gone adding a test
    that adds tracing policy that uses 64 mbset IDs twice.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/matchbinaries_follow_children_test.go
+++ pkg/sensors/tracing/matchbinaries_follow_children_test.go
@@ -11,14 +11,22 @@ import (
 	"os/exec"
 	"testing"
 
+	"github.com/cilium/tetragon/pkg/bpf"
+	"github.com/cilium/tetragon/pkg/btf"
 	"github.com/cilium/tetragon/pkg/grpc/tracing"
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 	"github.com/cilium/tetragon/pkg/logger"
+	"github.com/cilium/tetragon/pkg/observer"
+	"github.com/cilium/tetragon/pkg/option"
 	"github.com/cilium/tetragon/pkg/reader/notify"
+	testsensor "github.com/cilium/tetragon/pkg/sensors/test"
 	"github.com/cilium/tetragon/pkg/testutils"
+	tuo "github.com/cilium/tetragon/pkg/testutils/observer"
 	"github.com/cilium/tetragon/pkg/testutils/perfring"
 	tus "github.com/cilium/tetragon/pkg/testutils/sensors"
+	"github.com/cilium/tetragon/pkg/tracingpolicy"
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestMatchBinariesFollowChildren(t *testing.T) {
@@ -77,4 +85,69 @@ func TestMatchBinariesFollowChildren(t *testing.T) {
 	}
 	perfring.RunTest(t, ctx, ops2, eventFn)
 	require.Equal(t, 1, getcpuCnt, "double exec")
+}
+
+func TestMatchBinariesFollowChildrenIDs(t *testing.T) {
+	// Limit this test to kprobe_multi systems, otherwise it'd take too long.
+	// The mbset functionality works the same for kprobe or kprobe_multi,
+	// so no harm done.
+
+	if !bpf.HasKprobeMulti() {
+		t.Skip("Test requires kprobe multi")
+	}
+
+	testutils.CaptureLog(t, logger.GetLogger())
+	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
+	defer cancel()
+
+	if err := observer.InitDataCache(1024); err != nil {
+		t.Fatalf("observertesthelper.InitDataCache: %s", err)
+	}
+
+	option.Config.HubbleLib = tus.Conf().TetragonLib
+	tus.LoadInitialSensor(t)
+	tus.LoadSensor(t, testsensor.GetTestSensor())
+	sm := tuo.GetTestSensorManager(t)
+
+	// Create tracing policy for 64 syscalls and each of them has MatchBinaries
+	// selector with FollowChildren, which uses 1 mbset ID.
+
+	sel := []v1alpha1.KProbeSelector{
+		{
+			MatchBinaries: []v1alpha1.BinarySelector{
+				{
+					Operator:       "In",
+					Values:         []string{"/usr/bin/tail"},
+					FollowChildren: true,
+				},
+			},
+		},
+	}
+
+	tp := tracingpolicy.GenericTracingPolicy{
+		Metadata: v1.ObjectMeta{
+			Name: "match-binaries",
+		},
+	}
+
+	syscalls, err := btf.GetSyscallsList()
+	require.NoError(t, err)
+
+	for _, sc := range syscalls[:64] {
+		kp := v1alpha1.KProbeSpec{
+			Call:      sc,
+			Syscall:   true,
+			Selectors: sel,
+		}
+		tp.Spec.KProbes = append(tp.Spec.KProbes, kp)
+	}
+
+	// Adding the tracing policy twice (1 addition uses 64 mbset IDs) will ensure
+	// that we recycle the mbset IDs properly on tracing policy removal.
+
+	for range 2 {
+		err := sm.Manager.AddTracingPolicy(ctx, &tp)
+		require.NoError(t, err)
+		sm.Manager.DeleteTracingPolicy(ctx, "match-binaries", "")
+	}
 }
