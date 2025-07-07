cilium/tetragon

commit d7a4f4f100a6ff1840cdb27a8ed0edec009031f8

bpf: support kernel_cap_t uint64
Support kernel_cap_t uint64 capabilities. Usually these are the
ones passed to capset() to security_capset().

Signed-off-by: Djalal Harouni <tixxdz@gmail.com>

pkg/btf/validation.go
@@ -259,6 +259,11 @@ func typesCompatible(specTy string, kernelTy string) bool {
 		case "struct sk_buff *":
 			return true
 		}
+	case "kernel_cap_t", "cap_inheritable", "cap_permitted", "cap_effective":
+		switch kernelTy {
+		case "struct kernel_cap_t *":
+			return true
+		}
 	}
 
 	return false

bpf/process/types/basic.h
@@ -68,6 +68,11 @@ enum {
 	s8_ty = 31,
 	u8_ty = 32,
 
+	kernel_cap_ty = 33,
+	cap_inh_ty = 34,
+	cap_prm_ty = 35,
+	cap_eff_ty = 36,
+
 	nop_s64_ty = -10,
 	nop_u64_ty = -11,
 	nop_u32_ty = -12,
@@ -1474,6 +1479,10 @@ static inline __attribute__((always_inline)) size_t type_to_min_size(int type,
 	case size_type:
 	case s64_ty:
 	case u64_ty:
+	case kernel_cap_ty:
+	case cap_inh_ty:
+	case cap_prm_ty:
+	case cap_eff_ty:
 		return 8;
 	case char_buf:
 	case char_iovec:
@@ -1687,6 +1696,10 @@ selector_arg_offset(__u8 *f, struct msg_generic_kprobe *e, __u32 selidx,
 			set32bit = e->sel.is32BitSyscall;
 		case s64_ty:
 		case u64_ty:
+		case kernel_cap_ty:
+		case cap_inh_ty:
+		case cap_prm_ty:
+		case cap_eff_ty:
 			pass &= filter_64ty(filter, args, set32bit);
 			break;
 		case size_type:
@@ -1698,6 +1711,7 @@ selector_arg_offset(__u8 *f, struct msg_generic_kprobe *e, __u32 selidx,
 		case skb_type:
 		case sock_type:
 			pass &= filter_inet(filter, args);
+			break;
 		default:
 			break;
 		}
@@ -2522,6 +2536,13 @@ read_call_arg(void *ctx, struct msg_generic_kprobe *e, int index, int type,
 		size = copy_kernel_module(args, arg);
 		break;
 	}
+	case kernel_cap_ty:
+	case cap_inh_ty:
+	case cap_prm_ty:
+	case cap_eff_ty:
+		probe_read(args, sizeof(__u64), (char *)arg);
+		size = sizeof(__u64);
+		break;
 	default:
 		size = 0;
 		break;

pkg/api/tracingapi/client_kprobe.go
@@ -309,6 +309,78 @@ func (m MsgGenericKprobeArgCapability) IsReturnArg() bool {
 	return m.Index == ReturnArgIndex
 }
 
+type MsgGenericKernelCapType struct {
+	Caps uint64
+}
+
+type MsgGenericKprobeArgKernelCapType struct {
+	Index uint64
+	Caps  uint64
+	Label string
+}
+
+func (m MsgGenericKprobeArgKernelCapType) GetIndex() uint64 {
+	return m.Index
+}
+
+func (m MsgGenericKprobeArgKernelCapType) IsReturnArg() bool {
+	return m.Index == ReturnArgIndex
+}
+
+type MsgGenericCapInheritable struct {
+	Caps uint64
+}
+
+type MsgGenericKprobeArgCapInheritable struct {
+	Index uint64
+	Caps  uint64
+	Label string
+}
+
+func (m MsgGenericKprobeArgCapInheritable) GetIndex() uint64 {
+	return m.Index
+}
+
+func (m MsgGenericKprobeArgCapInheritable) IsReturnArg() bool {
+	return m.Index == ReturnArgIndex
+}
+
+type MsgGenericCapPermitted struct {
+	Caps uint64
+}
+
+type MsgGenericKprobeArgCapPermitted struct {
+	Index uint64
+	Caps  uint64
+	Label string
+}
+
+func (m MsgGenericKprobeArgCapPermitted) GetIndex() uint64 {
+	return m.Index
+}
+
+func (m MsgGenericKprobeArgCapPermitted) IsReturnArg() bool {
+	return m.Index == ReturnArgIndex
+}
+
+type MsgGenericCapEffective struct {
+	Caps uint64
+}
+
+type MsgGenericKprobeArgCapEffective struct {
+	Index uint64
+	Caps  uint64
+	Label string
+}
+
+func (m MsgGenericKprobeArgCapEffective) GetIndex() uint64 {
+	return m.Index
+}
+
+func (m MsgGenericKprobeArgCapEffective) IsReturnArg() bool {
+	return m.Index == ReturnArgIndex
+}
+
 type MsgGenericUserNamespace struct {
 	Level  int32
 	Uid    uint32

pkg/generictypes/generictypes.go
@@ -44,6 +44,11 @@ const (
 	GenericS8Type  = 31
 	GenericU8Type  = 32
 
+	GenericKernelCap      = 33
+	GenericCapInheritable = 34
+	GenericCapPermitted   = 35
+	GenericCapEffective   = 36
+
 	GenericNopType     = -1
 	GenericInvalidType = -2
 )
@@ -114,6 +119,14 @@ func GenericTypeFromString(arg string) int {
 		return GenericS8Type
 	case "uint8":
 		return GenericU8Type
+	case "kernel_cap_t":
+		return GenericKernelCap
+	case "cap_inheritable":
+		return GenericCapInheritable
+	case "cap_permitted":
+		return GenericCapPermitted
+	case "cap_effective":
+		return GenericCapEffective
 	default:
 		return GenericInvalidType
 	}

pkg/sensors/tracing/args.go
@@ -439,6 +439,62 @@ func getArg(r *bytes.Reader, a argPrinter) tracingapi.MsgGenericKprobeArg {
 		arg.Value = int32(int8(output))
 		arg.Label = a.label
 		return arg
+	case gt.GenericKernelCap:
+		var output uint64
+		var arg api.MsgGenericKprobeArgKernelCapType
+
+		err := binary.Read(r, binary.LittleEndian, &output)
+		if err != nil {
+			logger.GetLogger().WithError(err).Warnf("kernel_cap_t type error")
+		} else {
+			arg.Caps = output
+		}
+
+		arg.Index = uint64(a.index)
+		arg.Label = a.label
+		return arg
+	case gt.GenericCapInheritable:
+		var output uint64
+		var arg api.MsgGenericKprobeArgCapInheritable
+
+		err := binary.Read(r, binary.LittleEndian, &output)
+		if err != nil {
+			logger.GetLogger().WithError(err).Warnf("kernel_cap_t cap_inheritable type error")
+		} else {
+			arg.Caps = output
+		}
+
+		arg.Index = uint64(a.index)
+		arg.Label = a.label
+		return arg
+	case gt.GenericCapPermitted:
+		var output uint64
+		var arg api.MsgGenericKprobeArgCapPermitted
+
+		err := binary.Read(r, binary.LittleEndian, &output)
+		if err != nil {
+			logger.GetLogger().WithError(err).Warnf("kernel_cap_t cap_permitted type error")
+		} else {
+			arg.Caps = output
+		}
+
+		arg.Index = uint64(a.index)
+		arg.Label = a.label
+		return arg
+	case gt.GenericCapEffective:
+		var output uint64
+		var arg api.MsgGenericKprobeArgCapEffective
+
+		err := binary.Read(r, binary.LittleEndian, &output)
+		if err != nil {
+			logger.GetLogger().WithError(err).Warnf("kernel_cap_t cap_effective type error")
+		} else {
+			arg.Caps = output
+		}
+
+		arg.Index = uint64(a.index)
+		arg.Label = a.label
+		return arg
 	default:
 		logger.GetLogger().WithError(err).WithField("event-type", a.ty).Warnf("Unknown event type")
 	}

commit fa32b6e0c737fa2d45770f165403293784c0ea60

btf validation: proper kernel type is kernel_cap_t *
There does not seem to be a "struct kernel_cap_t *" type. Change it to
"kernel_cap_t *".

Fixes: d7a4f4f100a6 ("bpf: support kernel_cap_t uint64")

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/btf/validation.go
@@ -401,7 +401,7 @@ func typesCompatible(specTy string, kernelTy string) bool {
 		}
 	case "kernel_cap_t", "cap_inheritable", "cap_permitted", "cap_effective":
 		switch kernelTy {
-		case "struct kernel_cap_t *":
+		case "kernel_cap_t *":
 			return true
 		}
 	case "dentry":