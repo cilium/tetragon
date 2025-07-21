716481434f5df00a73b09e0343d335b5f8d7add9
Jiri Olsa <jolsa@kernel.org>
2025-07-21 19:25:48 +0000

    tetragon: Load properly config_map in generic tracepoint
    We need config_map in generic tracepoint maps.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/generictracepoint.go
+++ pkg/sensors/tracing/generictracepoint.go
@@ -622,6 +622,9 @@ func createGenericTracepointSensor(
 		selMatchBinariesMap := program.MapBuilderProgram("tg_mb_sel_opts", prog0)
 		maps = append(maps, selMatchBinariesMap)
 
+		configMap := program.MapBuilderProgram("config_map", prog0)
+		maps = append(maps, configMap)
+
 		maps = append(maps, polInfo.policyConfMap(prog0))
 	}
 
