f654effcdd70240b3ca073be9c6226a049960edd
Jiri Olsa <jolsa@kernel.org>
2025-07-22 09:58:00 +0000

    tetragon: Drop map load from output and ima lsm programs
    It's not used.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/genericlsm.go
+++ pkg/sensors/tracing/genericlsm.go
@@ -102,16 +102,11 @@ func (k *observerLsmSensor) LoadProbe(args sensors.LoadProbeArgs) error {
 			},
 		}
 		args.Load.MapLoad = append(args.Load.MapLoad, config)
-
-		if err := program.LoadLSMProgram(args.BPFDir, args.Load, args.Maps, args.Verbose); err == nil {
-			logger.GetLogger().Info(fmt.Sprintf("Loaded generic LSM program: %s -> %s", args.Load.Name, args.Load.Attach))
-		} else {
-			return err
-		}
-	} else {
-		return fmt.Errorf("invalid loadData type: expecting idtable.EntryID/[] and got: %T (%v)",
-			args.Load.LoaderData, args.Load.LoaderData)
 	}
+	if err := program.LoadLSMProgram(args.BPFDir, args.Load, args.Maps, args.Verbose); err != nil {
+		return err
+	}
+	logger.GetLogger().Info(fmt.Sprintf("Loaded generic LSM program: %s -> %s", args.Load.Name, args.Load.Attach))
 	return nil
 }
 
@@ -451,7 +446,6 @@ func createLsmSensorFromEntry(polInfo *policyInfo, lsmEntry *genericLsm,
 		"lsm/generic_lsm_output",
 		lsmEntry.hook,
 		"generic_lsm").
-		SetLoaderData(lsmEntry.tableId).
 		SetPolicy(lsmEntry.policyName)
 	progs = append(progs, loadOutput)
 
@@ -475,7 +469,6 @@ func createLsmSensorFromEntry(polInfo *policyInfo, lsmEntry *genericLsm,
 				"lsm.s/generic_lsm_ima_"+loadProgImaType,
 				lsmEntry.hook,
 				"generic_lsm").
-				SetLoaderData(lsmEntry.tableId).
 				SetPolicy(lsmEntry.policyName)
 			progs = append(progs, loadIma)
 			imaHashMap := program.MapBuilderProgram("ima_hash_map", loadIma)
