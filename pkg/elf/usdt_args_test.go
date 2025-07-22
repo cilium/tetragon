c2b2a3cc0ab3aa1163e2526be8d6594534e90c87
Jiri Olsa <jolsa@kernel.org>
2025-07-22 13:50:13 +0000

    tetragon: Add usdt elf args test
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ pkg/elf/usdt_args_test.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build amd64 && linux
+// +build amd64,linux
+
+package elf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseArgs(t *testing.T) {
+	spec := UsdtSpec{
+		ArgsStr: "-4@-1372(%rbp) -8@(%rbp) 8@%rax -4@$-9",
+	}
+
+	err := parseArgs(&spec)
+
+	require.NoError(t, err)
+	assert.Equal(t, uint32(4), spec.ArgsCnt)
+	assert.Equal(t, USDT_ARG_TYPE_REG_DEREF, spec.Args[0].Type)
+	assert.Equal(t, uint16(0x80), spec.Args[0].RegOff)
+
+	assert.Equal(t, USDT_ARG_TYPE_REG_DEREF, spec.Args[1].Type)
+	assert.Equal(t, USDT_ARG_TYPE_REG, spec.Args[2].Type)
+	assert.Equal(t, USDT_ARG_TYPE_CONST, spec.Args[3].Type)
+}
