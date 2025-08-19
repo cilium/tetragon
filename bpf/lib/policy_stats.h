af1518edb18a3fd6813f0d3f7e5428ec0bd22822
Jiri Olsa <jolsa@kernel.org>
2025-08-19 13:26:44 +0000

    tetragon: Add fentry generic bpf object
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/lib/policy_stats.h
+++ bpf/lib/policy_stats.h
@@ -6,7 +6,16 @@
 
 #include "policy_conf.h"
 
-#if defined(GENERIC_KPROBE) || defined(GENERIC_TRACEPOINT) || defined(GENERIC_LSM) || defined(GENERIC_RAWTP) || defined(GENERIC_UPROBE) || defined(GENERIC_USDT) || defined(GENERIC_KRETPROBE) || defined(ALIGNCHECKER) || defined(GENERIC_URETPROBE)
+#if defined(GENERIC_KPROBE) ||         \
+	defined(GENERIC_TRACEPOINT) || \
+	defined(GENERIC_LSM) ||        \
+	defined(GENERIC_RAWTP) ||      \
+	defined(GENERIC_UPROBE) ||     \
+	defined(GENERIC_USDT) ||       \
+	defined(GENERIC_KRETPROBE) ||  \
+	defined(ALIGNCHECKER) ||       \
+	defined(GENERIC_URETPROBE) ||  \
+	defined(GENERIC_FENTRY)
 #define HAS_POLICY_STATS
 #endif
 
