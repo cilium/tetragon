4c24e20bc65446f67f528ec86e54304683f9c01a
Jiri Olsa <jolsa@kernel.org>
2025-08-21 13:36:30 +0000

    tetragon: Add support to offload path for usdt and uprobe sensors
    We are missing support to offload path retrieval in usdt and uprobe
    sensors, adding it.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_generic_usdt.c
+++ bpf/process/bpf_generic_usdt.c
@@ -21,6 +21,7 @@ int generic_usdt_process_filter(void *ctx);
 int generic_usdt_filter_arg(void *ctx);
 int generic_usdt_actions(void *ctx);
 int generic_usdt_output(void *ctx);
+int generic_usdt_path(void *ctx);
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
@@ -35,6 +36,9 @@ struct {
 		[TAIL_CALL_ARGS] = (void *)&generic_usdt_filter_arg,
 		[TAIL_CALL_ACTIONS] = (void *)&generic_usdt_actions,
 		[TAIL_CALL_SEND] = (void *)&generic_usdt_output,
+#ifndef __V61_BPF_PROG
+		[TAIL_CALL_PATH] = (void *)&generic_usdt_path,
+#endif
 	},
 };
 
@@ -99,3 +103,11 @@ generic_usdt_output(void *ctx)
 {
 	return generic_output(ctx, MSG_OP_GENERIC_USDT);
 }
+
+#ifndef __V61_BPF_PROG
+__attribute__((section("uprobe"), used)) int
+generic_usdt_path(void *ctx)
+{
+	return generic_path(ctx, (struct bpf_map_def *)&usdt_calls);
+}
+#endif
