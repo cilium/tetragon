998c4484f7db7a4a902e6b840cf484a0f34cfab2
Jiri Olsa <jolsa@kernel.org>
2025-08-28 12:25:28 +0000

    tetragon: Add support to filter on data fields
    Adding support to filter on data fields with matchData selector, like:
    
      kprobes:
      - call: "security_task_getscheduler"
        syscall: false
        data:
        - type: "string"
          source: "current_task"
          resolve: "comm"
        selectors:
        - matchData:
          - index: 0
            operator: "Equal"
            values:
            - "krava"
    
    The matchData is just syntax sugar on top of argument filtering,
    but in reality it just defines filter on specific argument value
    which happens to be taken from current task.
    
    Above means that both matchArgs and matchData are resolved/filtered
    with the same bpf code as if all the data were arguments.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/generictracepoint.go
+++ pkg/sensors/tracing/generictracepoint.go
@@ -686,7 +686,7 @@ func (tp *genericTracepoint) InitKernelSelectors(lists []v1alpha1.ListSpec) erro
 		}
 	}
 
-	selectors, err := selectors.InitKernelSelectorState(selSelectors, selArgs, &tp.actionArgs, &listReader{lists}, nil)
+	selectors, err := selectors.InitKernelSelectorState(selSelectors, selArgs, []v1alpha1.KProbeArg{}, &tp.actionArgs, &listReader{lists}, nil)
 	if err != nil {
 		return err
 	}
