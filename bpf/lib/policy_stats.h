b0e50ee7f3a1e4cd9d432705159a2869ec6fad62
Kornilios Kourtis <kornilios@isovalent.com>
2025-09-02 14:27:14 +0200

    bpf: policy stats
    Add a per-policy bpf map to maintain action statistics.
    
    For now, we keep counters for the following events:
     POLICY_POST: policy posted an event
     POLICY_SIGNAL: policy sent a signal
     POLICY_MONITOR_SIGNAL: policy did not sent a signal because it was in monitor mode
     POLICY_OVERRIDE: policy overrode a return value
     POLICY_MONITOR_OVERRIDE: policy did not overrode a return value because it was in monitor mode
     POLICY_NOTIFY_ENFORCER: policy notified the enforcer
     POLICY_MONITOR_NOTIFY_ENFORCER: policy did not notify the enforcer because it was in monitor mode
    
    Note that the enforcing actions have two variants: one where the
    enforcement action happened, and one when the enforcement did not happen
    because the policy was in monitor mode.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ bpf/lib/policy_stats.h
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Tetragon */
+
+#ifndef BPF_POLICYSTATS_H__
+#define BPF_POLICYSTATS_H__
+
+#include "policy_conf.h"
+
+enum policy_actions {
+	POLICY_INVALID_ACT_ = 0,
+	POLICY_POST = 1, /* policy posted an event */
+	POLICY_SIGNAL = 2, /* policy sent a signal */
+	POLICY_MONITOR_SIGNAL = 3, /* policy did not sent a signal because it was in monitor mode */
+	POLICY_OVERRIDE = 4, /* policy overrode a return value */
+	POLICY_MONITOR_OVERRIDE = 5, /* policy did not overrode a return value because it was in monitor mode */
+	POLICY_NOTIFY_ENFORCER = 6, /* policy notified the enforcer */
+	POLICY_MONITOR_NOTIFY_ENFORCER = 7, /* policy did not notify the enforcer because it was in monitor mode */
+	POLICY_NACTIONS_,
+};
+
+struct policy_stats {
+	u64 act_cnt[POLICY_NACTIONS_];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, struct policy_stats);
+} policy_stats SEC(".maps");
+
+#endif /* BPF_POLICYSTATS_H__ */
