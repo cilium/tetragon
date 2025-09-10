b3296678bb75e355165f176088f026a0f0219c8d
Federico Di Pierro <nierro92@gmail.com>
2025-09-10 14:29:47 +0200

    new(tests/e2e): introduce a metricschecker package for e2e tests.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

---
+++ tests/e2e/metricschecker/doc.go
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build !windows
+
+// This package provides a Tetragon Metrics Checker that
+// uses http GET requests to get metrics from all Tetragon pods.
+package metricschecker
