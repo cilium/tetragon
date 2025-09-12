cilium/tetragon

commit b0e50ee7f3a1e4cd9d432705159a2869ec6fad62

bpf: policy stats
Add a per-policy bpf map to maintain action statistics.

For now, we keep counters for the following events:
 POLICY_POST: policy posted an event
 POLICY_SIGNAL: policy sent a signal
 POLICY_MONITOR_SIGNAL: policy did not sent a signal because it was in monitor mode
 POLICY_OVERRIDE: policy overrode a return value
 POLICY_MONITOR_OVERRIDE: policy did not overrode a return value because it was in monitor mode
 POLICY_NOTIFY_ENFORCER: policy notified the enforcer
 POLICY_MONITOR_NOTIFY_ENFORCER: policy did not notify the enforcer because it was in monitor mode

Note that the enforcing actions have two variants: one where the
enforcement action happened, and one when the enforcement did not happen
because the policy was in monitor mode.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

bpf/lib/policy_stats.h
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Tetragon */
+
+#ifndef BPF_POLICYSTATS_H__
+#define BPF_POLICYSTATS_H__
+
+#include "policy_conf.h"
+
+enum policy_actions {
+	POLICY_INVALID_ACT_ = 0,
+	POLICY_POST = 1, /* policy posted an event */
+	POLICY_SIGNAL = 2, /* policy sent a signal */
+	POLICY_MONITOR_SIGNAL = 3, /* policy did not sent a signal because it was in monitor mode */
+	POLICY_OVERRIDE = 4, /* policy overrode a return value */
+	POLICY_MONITOR_OVERRIDE = 5, /* policy did not overrode a return value because it was in monitor mode */
+	POLICY_NOTIFY_ENFORCER = 6, /* policy notified the enforcer */
+	POLICY_MONITOR_NOTIFY_ENFORCER = 7, /* policy did not notify the enforcer because it was in monitor mode */
+	POLICY_NACTIONS_,
+};
+
+struct policy_stats {
+	u64 act_cnt[POLICY_NACTIONS_];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32);
+	__type(value, struct policy_stats);
+} policy_stats SEC(".maps");
+
+#endif /* BPF_POLICYSTATS_H__ */

bpf/lib/process.h
@@ -10,6 +10,7 @@
 #include "bpf_d_path.h"
 #include "../process/string_maps.h"
 #include "api.h"
+#include "policy_stats.h"
 
 /* Applying 'packed' attribute to structs causes clang to write to the
  * members byte-by-byte, as offsets may not be aligned. This is bad for
@@ -602,10 +603,18 @@ FUNC_INLINE void
 perf_event_output_metric(void *ctx, u8 msg_op, void *map, u64 flags, void *data, u64 size)
 {
 	long err;
+	u32 zero = 0;
+	struct policy_stats *pstats;
 
 	err = perf_event_output(ctx, map, flags, data, size);
-	if (err < 0)
+	if (err < 0) {
 		perf_event_output_update_error_metric(msg_op, err);
+		return;
+	}
+
+	pstats = map_lookup_elem(&policy_stats, &zero);
+	if (pstats)
+		lock_add(&pstats->act_cnt[POLICY_POST], 1);
 }
 
 /**

bpf/process/generic_calls.h
@@ -10,6 +10,7 @@
 #include "types/basic.h"
 #include "vmlinux.h"
 #include "policy_conf.h"
+#include "policy_stats.h"
 #include "generic_path.h"
 #include "bpf_ktime.h"
 
@@ -763,11 +764,14 @@ do_action(void *ctx, __u32 i, struct selector_action *actions, bool *post, bool 
 	int argi __maybe_unused;
 	int err = 0;
 	int zero = 0;
+	struct policy_stats *pstats;
+	u32 polacct;
 
 	e = map_lookup_elem(&process_call_heap, &zero);
 	if (!e)
 		return 0;
 
+	polacct = POLICY_INVALID_ACT_;
 	switch (action) {
 	case ACTION_NOPOST:
 		*post = false;
@@ -822,13 +826,21 @@ do_action(void *ctx, __u32 i, struct selector_action *actions, bool *post, bool 
 	case ACTION_SIGNAL:
 		signal = actions->act[++i];
 	case ACTION_SIGKILL:
-		if (enforce_mode)
+		if (enforce_mode) {
 			do_action_signal(signal);
+			polacct = POLICY_SIGNAL;
+		} else {
+			polacct = POLICY_MONITOR_SIGNAL;
+		}
 		break;
 	case ACTION_OVERRIDE:
 		error = actions->act[++i];
-		if (enforce_mode)
+		if (enforce_mode) {
 			do_override_action(error);
+			polacct = POLICY_OVERRIDE;
+		} else {
+			polacct = POLICY_MONITOR_OVERRIDE;
+		}
 		break;
 	case ACTION_GETURL:
 	case ACTION_DNSLOOKUP:
@@ -844,14 +856,25 @@ do_action(void *ctx, __u32 i, struct selector_action *actions, bool *post, bool 
 		error = actions->act[++i];
 		signal = actions->act[++i];
 		argi = actions->act[++i];
-		if (enforce_mode)
+		if (enforce_mode) {
 			do_action_notify_enforcer(e, error, signal, argi);
+			polacct = POLICY_NOTIFY_ENFORCER;
+		} else {
+			polacct = POLICY_MONITOR_NOTIFY_ENFORCER;
+		}
 		break;
 	case ACTION_CLEANUP_ENFORCER_NOTIFICATION:
 		do_enforcer_cleanup();
 	default:
 		break;
 	}
+
+	if (polacct != POLICY_INVALID_ACT_) {
+		pstats = map_lookup_elem(&policy_stats, &zero);
+		if (pstats)
+			lock_add(&pstats->act_cnt[polacct], 1);
+	}
+
 	if (!err) {
 		e->action = action;
 		return ++i;

commit 4b4ee3fd2135b0578f365d061baebb4d530d9f04

policy_stats: use the map only for policy sensors
perf_event_output_metric is defined in lib/process.h that is included
from the code in the base sensor as well.

As a result, some programs from the exec sensor end up having private
copies of the policy_stats map, which is just waster memory. Add some
ifdefs to only enable the policy_stats map for the tracing (generic)
sensors.

Fixes: b0e50ee7f3a1 ("bpf: policy stats")

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

bpf/lib/policy_stats.h
@@ -6,6 +6,10 @@
 
 #include "policy_conf.h"
 
+#if defined(GENERIC_KPROBE) || defined(GENERIC_TRACEPOINT) || defined(GENERIC_LSM) || defined(GENERIC_RAWTP) || defined(GENERIC_UPROBE) || defined(GENERIC_USDT) || defined(GENERIC_KRETPROBE) || defined(ALIGNCHECKER)
+#define HAS_POLICY_STATS
+#endif
+
 /* NB: if you are modifying this enum, you might want to change the proto descriptions for
  * TracingPolicyActionCounters.
  */
@@ -25,11 +29,29 @@ struct policy_stats {
 	u64 act_cnt[POLICY_NACTIONS_];
 };
 
+#if defined(HAS_POLICY_STATS)
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
 	__uint(max_entries, 1);
 	__type(key, __u32);
 	__type(value, struct policy_stats);
 } policy_stats SEC(".maps");
+
+FUNC_INLINE void
+policy_stats_update(int act)
+{
+	struct policy_stats *pstats;
+	u32 zero = 0;
+
+	pstats = map_lookup_elem(&policy_stats, &zero);
+	if (pstats)
+		lock_add(&pstats->act_cnt[act], 1);
+}
+#else
+FUNC_INLINE void
+policy_stats_update(int acct)
+{
+}
+#endif
 
 #endif /* BPF_POLICYSTATS_H__ */

bpf/lib/process.h
@@ -603,8 +603,6 @@ FUNC_INLINE void
 perf_event_output_metric(void *ctx, u8 msg_op, void *map, u64 flags, void *data, u64 size)
 {
 	long err;
-	u32 zero = 0;
-	struct policy_stats *pstats;
 
 	err = perf_event_output(ctx, map, flags, data, size);
 	if (err < 0) {
@@ -612,9 +610,7 @@ perf_event_output_metric(void *ctx, u8 msg_op, void *map, u64 flags, void *data,
 		return;
 	}
 
-	pstats = map_lookup_elem(&policy_stats, &zero);
-	if (pstats)
-		lock_add(&pstats->act_cnt[POLICY_POST], 1);
+	policy_stats_update(POLICY_POST);
 }
 
 /**

bpf/process/bpf_generic_tracepoint.c
@@ -4,11 +4,11 @@
 #include "vmlinux.h"
 #include "api.h"
 
+#define GENERIC_TRACEPOINT
+
 #include "compiler.h"
 #include "bpf_event.h"
 #include "bpf_task.h"
-
-#define GENERIC_TRACEPOINT
 
 #include "retprobe_map.h"
 #include "types/operations.h"

bpf/process/generic_calls.h
@@ -764,7 +764,6 @@ do_action(void *ctx, __u32 i, struct selector_action *actions, bool *post, bool 
 	int argi __maybe_unused;
 	int err = 0;
 	int zero = 0;
-	struct policy_stats *pstats;
 	u32 polacct;
 
 	e = map_lookup_elem(&process_call_heap, &zero);
@@ -870,9 +869,7 @@ do_action(void *ctx, __u32 i, struct selector_action *actions, bool *post, bool 
 	}
 
 	if (polacct != POLICY_INVALID_ACT_) {
-		pstats = map_lookup_elem(&policy_stats, &zero);
-		if (pstats)
-			lock_add(&pstats->act_cnt[polacct], 1);
+		policy_stats_update(polacct);
 	}
 
 	if (!err) {