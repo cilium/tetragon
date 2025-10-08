593e21846bec050418b9a38ff34a9410adc56ab6
Jiri Olsa <jolsa@kernel.org>
2025-10-08 23:50:25 +0200

    tetragon: Add Assignment parsing to asm object
    Adding Assignment parsing to asm object which will be used to
    carry simple register arithmetic from user space to bpf programs.
    
    At the moment we support following operations:
    
    - assigning constant value, like "rax=1"
    - assigning another register value, like "rsp=%rax"
    - assigning another register value plus offset, like "rsp=0x80%rax"
    - dereferencing register value with offset, like "-1372(%rbp)"
    
    Above operations will be set in user space in config and executed
    in bpf program over actual registers and user memory.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ pkg/asm/assignment_amd64_test.go
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build amd64 && linux
+
+package asm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAssignment(t *testing.T) {
+	var (
+		ass *Assignment
+		err error
+	)
+
+	// constants
+	ass, err = ParseAssignment("rax=1")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_CONST, ass.Type)
+	assert.Equal(t, uint64(1), ass.Off)
+	assert.Equal(t, uint16(0x50), ass.Dst)
+	assert.Equal(t, uint16(0x0), ass.Src)
+
+	ass, err = ParseAssignment("rbx=-1")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_CONST, ass.Type)
+	assert.Equal(t, uint64(0xffffffffffffffff), ass.Off)
+	assert.Equal(t, uint16(0x28), ass.Dst)
+	assert.Equal(t, uint16(0), ass.Src)
+
+	ass, err = ParseAssignment("rcx=0x123")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_CONST, ass.Type)
+	assert.Equal(t, uint64(0x123), ass.Off)
+	assert.Equal(t, uint16(0x58), ass.Dst)
+	assert.Equal(t, uint16(0), ass.Src)
+
+	// register
+	ass, err = ParseAssignment("rsp=%rax")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_REG, ass.Type)
+	assert.Equal(t, uint16(0x98), ass.Dst)
+	assert.Equal(t, uint16(0x50), ass.Src)
+	assert.Equal(t, uint64(0), ass.Off)
+
+	// register + offset
+	ass, err = ParseAssignment("rbp=128%rax")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_REG_OFF, ass.Type)
+	assert.Equal(t, uint16(0x20), ass.Dst)
+	assert.Equal(t, uint16(0x50), ass.Src)
+	assert.Equal(t, uint64(128), ass.Off)
+
+	ass, err = ParseAssignment("rax=0x80%rax")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_REG_OFF, ass.Type)
+	assert.Equal(t, uint16(0x50), ass.Dst)
+	assert.Equal(t, uint16(0x50), ass.Src)
+	assert.Equal(t, uint64(0x80), ass.Off)
+
+	// register deref
+	ass, err = ParseAssignment("rsp=-1372(%rbp)")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_REG_DEREF, ass.Type)
+	assert.Equal(t, uint16(0x98), ass.Dst)
+	assert.Equal(t, uint16(0x20), ass.Src)
+	assert.Equal(t, uint64(0xfffffffffffffaa4), ass.Off)
+
+	ass, err = ParseAssignment("rbp=0x20(%rsp)")
+	require.NoError(t, err)
+	assert.Equal(t, ASM_ASSIGNMENT_TYPE_REG_DEREF, ass.Type)
+	assert.Equal(t, uint16(0x20), ass.Dst)
+	assert.Equal(t, uint16(0x98), ass.Src)
+	assert.Equal(t, uint64(0x20), ass.Off)
+}
