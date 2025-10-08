0c39f4850470e311824c97e3486e369ecd9b80f2
Jiri Olsa <jolsa@kernel.org>
2025-10-08 15:37:29 +0000

    fix2
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_generic_uprobe.c
+++ bpf/process/bpf_generic_uprobe.c
@@ -46,9 +46,11 @@ struct {
 #include "generic_calls.h"
 
 #ifdef __MULTI_KPROBE
-#define MAIN "uprobe.multi/generic_uprobe"
+#define MAIN	"uprobe.multi/generic_uprobe"
+#define COMMON	"uprobe.multi"
 #else
-#define MAIN "uprobe/generic_uprobe"
+#define MAIN	"uprobe/generic_uprobe"
+#define COMMON	"uprobe"
 #endif
 
 __attribute__((section((MAIN)), used)) int
@@ -57,19 +59,19 @@ generic_uprobe_event(struct pt_regs *ctx)
 	return generic_start_process_filter(ctx, (struct bpf_map_def *)&uprobe_calls);
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_setup_event(void *ctx)
 {
 	return generic_process_event_and_setup(ctx, (struct bpf_map_def *)&uprobe_calls);
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_process_event(void *ctx)
 {
 	return generic_process_event(ctx, (struct bpf_map_def *)&uprobe_calls);
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_process_filter(void *ctx)
 {
 	int ret;
@@ -85,27 +87,27 @@ generic_uprobe_process_filter(void *ctx)
 	return PFILTER_REJECT;
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_filter_arg(void *ctx)
 {
 	return generic_filter_arg(ctx, (struct bpf_map_def *)&uprobe_calls, true);
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_actions(void *ctx)
 {
 	generic_actions(ctx, (struct bpf_map_def *)&uprobe_calls);
 	return 0;
 }
 
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_output(void *ctx)
 {
 	return generic_output(ctx, MSG_OP_GENERIC_UPROBE);
 }
 
 #ifndef __V61_BPF_PROG
-__attribute__((section("uprobe"), used)) int
+__attribute__((section(COMMON), used)) int
 generic_uprobe_path(void *ctx)
 {
 	return generic_path(ctx, (struct bpf_map_def *)&uprobe_calls);
