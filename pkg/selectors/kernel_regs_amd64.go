f2d98cef4fe578e2804516c2964df9a1131ac8cd
Jiri Olsa <jolsa@kernel.org>
2025-10-09 20:41:25 +0000

    tetragon: Add uprobe override regs action
    Adding support for uprobe override action that also allows
    generic register override via argRegs argument.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ pkg/selectors/kernel_regs_amd64.go
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build amd64 && linux
+
+package selectors
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/cilium/tetragon/pkg/api/processapi"
+	"github.com/cilium/tetragon/pkg/asm"
+)
+
+func parseOverrideRegs(k *KernelSelectorState, values []string, errValue uint64) (uint32, error) {
+	if len(k.regs) > 0 {
+		return uint32(0xffffffff), errors.New("only single instance of regs action is allowed")
+	}
+
+	regs := []processapi.RegAssignment{}
+
+	// If no registers were specified go with the default for override
+	// at the top of the user space function.
+	if len(values) == 0 {
+		values = []string{
+			fmt.Sprintf("rax=%d", errValue),
+			"rip=(%rsp)",
+			"rsp=8%rsp",
+		}
+	}
+
+	for _, val := range values {
+		ass, err := asm.ParseAssignment(val)
+		if err != nil {
+			return uint32(0xffffffff), err
+		}
+
+		regs = append(regs, processapi.RegAssignment{
+			Type: ass.Type,
+			Src:  ass.Src,
+			Dst:  ass.Dst,
+			Off:  ass.Off,
+		})
+	}
+
+	k.regs = regs
+	return uint32(0), nil
+}
