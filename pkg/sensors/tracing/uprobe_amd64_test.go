1397f425a06feeffb016655c2b001874ccc85cda
Jiri Olsa <jolsa@kernel.org>
2025-10-17 13:38:06 +0200

    tetragon: Add uprobe override action test with registers changes
    Adding uprobe override action test with registers changes that
    puts uprobe in test_1 function at:
    
        "push   %rbp\n"         /* +0  55             */
        "mov    %rsp,%rbp\n"    /* +1  48 89 e5       */
        "mov    $0x1,%eax\n"    /* +4  b8 01 00 00 00 */
    --> "mov    $0x3,%eax\n"    /* +9  b8 03 00 00 00 */
        "pop    %rbp\n"         /* +14 5d             */
        "ret\n"                 /* +15 c3             */
    
    and makes sure uprobe overrides test_1 return value (with 11)
    and the rest of the function is not executed.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/uprobe_amd64_test.go
+++ pkg/sensors/tracing/uprobe_amd64_test.go
@@ -81,3 +81,75 @@ spec:
 	err = jsonchecker.JsonTestCheck(t, checker)
 	require.NoError(t, err)
 }
+
+func TestUprobeOverrideRegsAction(t *testing.T) {
+	if !bpf.HasUprobeRegsChange() {
+		t.Skip("skipping regs override action test, regs override is not supported in kernel")
+	}
+
+	testBinary := testutils.RepoRootPath("contrib/tester-progs/regs-override")
+
+	// Put uprobe in test_1 function at:
+	//
+	//     "push   %rbp\n"         /* +0  55             */
+	//     "mov    %rsp,%rbp\n"    /* +1  48 89 e5       */
+	//     "mov    $0x1,%eax\n"    /* +4  b8 01 00 00 00 */
+	// --> "mov    $0x3,%eax\n"    /* +9  b8 03 00 00 00 */
+	//     "pop    %rbp\n"         /* +14 5d             */
+	//     "ret\n"                 /* +15 c3             */
+	//
+	// Make sure uprobe overrides test_1 return value (with 11)
+	// and the rest of the function is not executed.
+
+	pathHook := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "uprobe"
+spec:
+  uprobes:
+  - path: "` + testBinary + `"
+    symbols:
+    - "test_1+9"
+    selectors:
+    - matchActions:
+      - action: Override
+        argRegs:
+        - "rax=11"
+        - "rbp=(%rsp)"
+        - "rip=8(%rsp)"
+        - "rsp=8%rsp"
+`
+
+	pathConfigHook := []byte(pathHook)
+	err := os.WriteFile(testConfigFile, pathConfigHook, 0644)
+	if err != nil {
+		t.Fatalf("writeFile(%s): err %s", testConfigFile, err)
+	}
+
+	upChecker := ec.NewProcessUprobeChecker("UPROBE_BINARIES_MATCH").
+		WithProcess(ec.NewProcessChecker().
+			WithBinary(sm.Full(testBinary))).
+		WithSymbol(sm.Full("test_1+9"))
+	checker := ec.NewUnorderedEventChecker(upChecker)
+
+	var doneWG, readyWG sync.WaitGroup
+	defer doneWG.Wait()
+
+	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
+	defer cancel()
+
+	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
+	if err != nil {
+		t.Fatalf("GetDefaultObserverWithFile error: %s", err)
+	}
+	observertesthelper.LoopEvents(ctx, t, &doneWG, &readyWG, obs)
+	readyWG.Wait()
+
+	cmd := exec.Command(testBinary, "1")
+	require.Error(t, cmd.Run())
+	require.Equal(t, 11, cmd.ProcessState.ExitCode())
+
+	err = jsonchecker.JsonTestCheck(t, checker)
+	require.NoError(t, err)
+}
