c8018f3c361c0c516695a7540fc9546867c0c15f
Kornilios Kourtis <kornilios@isovalent.com>
2025-10-20 12:02:38 +0200

    tracing/usdt: improve test for resolve + override
    This commit improvbes testing by adding one more type (u32) as well as
    sub-struct fields.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- contrib/tester-progs/usdt-resolve.c
+++ contrib/tester-progs/usdt-resolve.c
@@ -2,25 +2,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 
 #include "usdt.h"
 
-struct mystruct {
-	unsigned long a, b, c;
+struct mysubstruct {
+	uint64_t v64;
+	uint8_t  v8;
+	uint32_t v32;
 };
+
+struct mystruct {
+	uint8_t  v8;
+	uint16_t v16;
+	uint32_t v32;
+	uint64_t v64;
+	struct mysubstruct sub;
+};
+
+void usage(char *argv0)
+{
+	fprintf(stderr, "Usage: %s <field> <val>\n", argv0);
+	fprintf(stderr, "field can be one of: v8, v16, v32, v64, sub.v32\n");
+}
 
 int main(int argc, char *argv[])
 {
 	volatile int ret = 0;
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s <int>\n", argv[0]);
+	if (argc < 3) {
+		usage(argv[0]);
 		exit(1);
 	}
-	struct mystruct s = {
-		.a = rand(),
-		.b = atoi(argv[1]),
-		.c = rand(),
-	};
+
+	char *field = argv[1];
+	long val = atol(argv[2]);
+	if (!val) {
+		usage(argv[0]);
+		exit(1);
+	}
+
+	struct mystruct s = {0};
+	if (!strcmp(field, "v8")) {
+		s.v8 = val;
+	} else if (!strcmp(field, "v16")) {
+		s.v16 = val;
+	} else if (!strcmp(field, "v32")) {
+		s.v32 = val;
+	} else if (!strcmp(field, "v64")) {
+		s.v64 = val;
+	} else if (!strcmp(field, "sub.v32")) {
+		s.sub.v32 = val;
+	} else {
+		usage(argv[0]);
+		exit(1);
+	}
+
 	USDT(tetragon, test, ret, &s);
 	printf("ret=%d\n", ret);
 	return ret;
