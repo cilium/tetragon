5c2e4bf921329e0e13f7ac5e195c1b1546ed475d
Federico Di Pierro <nierro92@gmail.com>
2025-10-27 17:19:02 +0100

    new(pkg): introduce `testutils.CheckKernelTracingExists`.
    [ upstream commit ef9cf56 ]
    
    Use it in tests to check that `tracing/events/syscalls` exist.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/testutils/path.go
+++ pkg/testutils/path.go
@@ -4,12 +4,26 @@
 package testutils
 
 import (
+	"os"
 	"path/filepath"
 	"runtime"
+
+	"github.com/cilium/tetragon/pkg/tracepoint"
 )
 
 // RepoRootPath retrieves the repository root path (useful to find scripts and other files)
 func RepoRootPath(fname string) string {
 	_, testFname, _, _ := runtime.Caller(0)
 	return filepath.Join(filepath.Dir(testFname), "..", "..", fname)
+}
+
+func CheckKernelTracingExists() bool {
+	traceFSDir, err := tracepoint.GetTraceFSPath()
+	if err != nil {
+		return false
+	}
+	if _, err = os.Stat(filepath.Join(traceFSDir, "events", "syscalls")); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
