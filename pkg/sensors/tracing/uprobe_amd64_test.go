11f3501f82e0dd11ad1a25902f8c11e7ce39df5d
Jiri Olsa <jolsa@kernel.org>
2025-11-05 20:39:32 +0000

    tetragon: Add test for retrieving uprobe regs data
    Testing that we retrieve the proper value from eax register.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/uprobe_amd64_test.go
+++ pkg/sensors/tracing/uprobe_amd64_test.go
@@ -349,3 +349,73 @@ func TestUprobeOverrideRegsAction_2bytes(t *testing.T) {
 func TestUprobeOverrideRegsAction_1byte(t *testing.T) {
 	testUprobeOverrideRegsActionSize(t, "al=0x12", "0xdeadbeefdeadbe12")
 }
+
+func TestUprobePtRegsData(t *testing.T) {
+	testBinary := testutils.RepoRootPath("contrib/tester-progs/regs-override")
+
+	// Put uprobe in test_1 function at:
+	//
+	//     "push   %rbp\n"         /* +0  55             */
+	//     "mov    %rsp,%rbp\n"    /* +1  48 89 e5       */
+	//     "mov    $0x1,%eax\n"    /* +4  b8 01 00 00 00 */
+	// --> "mov    $0x3,%eax\n"    /* +9  b8 03 00 00 00 */
+	//     "pop    %rbp\n"         /* +14 5d             */
+	//     "ret\n"                 /* +15 c3             */
+	//
+	// Make sure we retrieve data with eax value (1) as int argument.
+
+	pathHook := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "uprobe"
+spec:
+  uprobes:
+  - path: "` + testBinary + `"
+    symbols:
+    - "test_1+14"
+    data:
+    - index: 0
+      type: "int"
+      source: "pt_regs"
+      resolve: "eax"
+`
+
+	pathConfigHook := []byte(pathHook)
+	err := os.WriteFile(testConfigFile, pathConfigHook, 0644)
+	if err != nil {
+		t.Fatalf("writeFile(%s): err %s", testConfigFile, err)
+	}
+
+	upChecker := ec.NewProcessUprobeChecker("UPROBE_DATA_MATCH").
+		WithProcess(ec.NewProcessChecker().
+			WithBinary(sm.Full(testBinary))).
+		WithSymbol(sm.Full("test_1+14")).
+		WithData(ec.NewKprobeArgumentListMatcher().
+			WithOperator(lc.Ordered).
+			WithValues(
+				ec.NewKprobeArgumentChecker().WithIntArg(3),
+			))
+
+	checker := ec.NewUnorderedEventChecker(upChecker)
+
+	var doneWG, readyWG sync.WaitGroup
+	defer doneWG.Wait()
+
+	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
+	defer cancel()
+
+	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib)
+	if err != nil {
+		t.Fatalf("GetDefaultObserverWithFile error: %s", err)
+	}
+	observertesthelper.LoopEvents(ctx, t, &doneWG, &readyWG, obs)
+	readyWG.Wait()
+
+	cmd := exec.Command(testBinary, "1")
+	require.Error(t, cmd.Run())
+	require.Equal(t, 3, cmd.ProcessState.ExitCode())
+
+	err = jsonchecker.JsonTestCheck(t, checker)
+	require.NoError(t, err)
+}
