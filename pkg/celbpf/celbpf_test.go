971d572752d26256e69e0271e0e91439878ba08c
Kornilios Kourtis <kornilios@isovalent.com>
2025-11-12 13:20:02 +0100

    pkg/celbpf: simple tests
    Add tests that actually run the generated BPF program and compare what
    it returns against an expected value.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/celbpf/celbpf_test.go
+++ pkg/celbpf/celbpf_test.go
@@ -11,11 +11,32 @@ package celbpf
 import (
 	"testing"
 
+	"github.com/cilium/ebpf"
 	"github.com/stretchr/testify/require"
 )
 
-func TestTrue(t *testing.T) {
-	insts, err := Compile("true")
-	require.NoError(t, err)
-	t.Logf("insts[%d]:\n%s", len(insts), insts)
+func TestExprs(t *testing.T) {
+	testCase := []struct {
+		expr string
+		ret  uint32
+	}{
+		{"true", 1},
+		{"false", 0},
+	}
+
+	for _, tc := range testCase {
+		insts, err := Compile(tc.expr)
+		require.NoError(t, err)
+		// t.Logf("expr:%q\ninsts[%d]:\n%s", tc.expr, len(insts), insts)
+		prog, err := ebpf.NewProgramWithOptions(&ebpf.ProgramSpec{
+			Type:         ebpf.RawTracepoint,
+			Instructions: insts,
+			License:      "Dual BSD/GPL",
+		}, ebpf.ProgramOptions{LogLevel: ebpf.LogLevelInstruction})
+		require.NoError(t, err)
+		defer prog.Close()
+		val, err := prog.Run(&ebpf.RunOptions{})
+		require.NoError(t, err)
+		require.Equal(t, tc.ret, val, "result of %q was %d and not %d", tc.expr, val, tc.ret)
+	}
 }
