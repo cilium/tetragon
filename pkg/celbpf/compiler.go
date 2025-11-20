a622b44233b7adef72a8c7c0fbb883e91beb4b4e
Kornilios Kourtis <kornilios@isovalent.com>
2025-11-20 15:40:00 +0100

    pkg/celbpf: generate code for "10 == 10"
    Also for its unsigned variant: 10u == 10u
    
    This requires:
     - Adding the "==" operator
     - Adding branches, for which we need labels
     - Supporting int and unit literals
    
    This commit also introduces the idea of scratch registers (scratchRegs) that the
    compiler can keep track of and pass to the code generator to avoid
    conflicts.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/celbpf/compiler.go
+++ pkg/celbpf/compiler.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cilium/ebpf/asm"
 	cgCommon "github.com/google/cel-go/common"
 	cgAst "github.com/google/cel-go/common/ast"
+	cgOperators "github.com/google/cel-go/common/operators"
 	cgTypes "github.com/google/cel-go/common/types"
 	cgRef "github.com/google/cel-go/common/types/ref"
 )
@@ -22,27 +23,95 @@ type compiler struct {
 	cg  *codeGenerator
 }
 
-func newCompiler(ast *cgAst.AST, src cgCommon.Source) *compiler {
+func newCompiler(ast *cgAst.AST, src cgCommon.Source, labelPrefix string) *compiler {
 	return &compiler{
 		ast: ast,
 		src: src,
-		cg:  newCodeGenerator(),
+		cg:  newCodeGenerator(labelPrefix),
 	}
 }
 
 func (c *compiler) compileLiteral(lit cgRef.Val) error {
 	switch v := lit.(type) {
 	case cgTypes.Bool:
-		c.cg.emitPushBool(bool(v))
+		c.cg.emitPushBool(bool(v), scratchRegs[0])
+		return nil
+	case cgTypes.Int:
+		// NB: ctTypes.Int is int64
+		c.cg.emitPushInt64(int64(v), scratchRegs[0])
+		return nil
+	case cgTypes.Uint:
+		// NB: ctTypes.UInt is uint64
+		c.cg.emitPushInt64(int64(v), scratchRegs[0])
 		return nil
 	}
 	return fmt.Errorf("compileLiteral: does not support %T", lit)
+}
+
+func (c *compiler) compileCall(expr cgAst.Expr) error {
+
+	call := expr.AsCall()
+
+	// setup emit call
+	var emitCall func()
+	switch call.FunctionName() {
+	case cgOperators.Equals:
+		emitCall = func() {
+			c.cg.emitBranchEquals(scratchRegs[0], scratchRegs[1])
+		}
+	default:
+		return fmt.Errorf("compileCall: call %+v not supported", call)
+	}
+
+	// push argument
+	callArgs := call.Args()
+	argTypes := make([]*cgTypes.Type, 0, len(callArgs))
+	for _, arg := range callArgs {
+		ty := c.ast.GetType(arg.ID())
+		err := c.compileExpr(arg)
+		if err != nil {
+			return err
+		}
+		argTypes = append(argTypes, ty)
+	}
+
+	// pop arguments (reverse order)
+	for j := range callArgs {
+		i := len(callArgs) - j - 1
+		ty := argTypes[i]
+		switch ty.TypeName() {
+		case "int", "uint":
+			c.cg.emitPopInt64(scratchRegs[i])
+		case "bool":
+			c.cg.emitPopBool(scratchRegs[i])
+		default:
+			return fmt.Errorf("unsupported argument type: %s", ty.TypeName())
+		}
+	}
+
+	emitCall()
+	return nil
 }
 
 func (c *compiler) compileExpr(expr cgAst.Expr) error {
 	switch expr.Kind() {
 	case cgAst.LiteralKind:
 		return c.compileLiteral(expr.AsLiteral())
+	case cgAst.CallKind:
+		return c.compileCall(expr)
+	case cgAst.ComprehensionKind:
+		return errors.New("expression Kind 'ComprehensionKind' not supported")
+	case cgAst.IdentKind:
+		return errors.New("expression Kind 'IdentKind' not supported")
+	case cgAst.ListKind:
+		return errors.New("expression Kind 'ListKind' not supported")
+	case cgAst.MapKind:
+		return errors.New("expression Kind 'MapKind' not supported")
+	case cgAst.SelectKind:
+		return errors.New("expression Kind 'SelectKind' not supported")
+	case cgAst.StructKind:
+		return errors.New("expression Kind 'StructKind' not supported")
+
 	}
 	return fmt.Errorf("unsupported CEL expr: %d (%+v)", expr.Kind(), expr)
 }
@@ -53,7 +122,7 @@ func (c *compiler) compile() (asm.Instructions, error) {
 		return nil, errors.New("expecting CEL expression to return bool")
 	}
 	if err := c.compileExpr(expr); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to compile CEL expression: %w", err)
 	}
 	c.cg.emitPopBool(asm.R0)
 	c.cg.emitRaw(asm.Return())
