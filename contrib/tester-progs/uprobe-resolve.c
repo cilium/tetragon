35506bb4d79a9bfefe9308f49fec6b24ab4b782a
Tristan d'Audibert <tdaudibe@isovalent.com>
2025-12-06 16:19:00 +0100

    pkg/sensors : resolve array in Uprobe test
    Signed-off-by: Tristan d'Audibert <tdaudibe@isovalent.com>

--- contrib/tester-progs/uprobe-resolve.c
+++ contrib/tester-progs/uprobe-resolve.c
@@ -17,12 +17,14 @@ struct mystruct {
 	uint32_t v32;
 	uint64_t v64;
 	struct mysubstruct sub;
+	struct mysubstruct *arr[10];
+	struct mysubstruct **dyn;
 };
 
 void usage(char *argv0)
 {
 	fprintf(stderr, "Usage: %s <field> <val>\n", argv0);
-	fprintf(stderr, "field can be one of: v8, v16, v32, v64, sub.v32\n");
+	fprintf(stderr, "field can be one of: v8, v16, v32, v64, sub.v32 arr[idx].v64 dyn[idx].v64\n");
 }
 
 // without noinline, the symbol is found, but no event fires
@@ -58,6 +60,19 @@ int main(int argc, char *argv[])
 		s.v64 = val;
 	} else if (!strcmp(field, "sub.v32")) {
 		s.sub.v32 = val;
+	} else if ((field[0] == 'a' && field[1] == 'r' && field[2] == 'r')
+            || (field[0] == 'd' && field[1] == 'y' && field[2] == 'n')) {
+		long idx = atol(&field[4]);
+		if (!idx) {
+			usage(argv[0]);
+			exit(1);
+		}
+		struct mysubstruct s2 = {
+			.v64 = val,
+		};
+		s.arr[idx] = &s2;
+		s.dyn = s.arr;
+		printf("sub.v64:%lu\n", s2.v64); // It seems without this line, the compiler make optimization and the test fails.
 	} else {
 		usage(argv[0]);
 		exit(1);
