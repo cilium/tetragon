fedb4869440f36db02467aa673c6149a8b9943c5
Kobrin Ilay <ilayko3110@yandex.ru>
2025-12-17 15:44:30 +0300

    bpf: add logic for parent binaries filtering
    This commit adds bpf code for parent binaries filtering for new matchParentBinaries selector.
    Match binaries bpf maps sizes are increased to MAX_SELECTORS * 2 to store both selectors options.
    Parent filtering is processed with the same code, but key for matchParentBinaries has offset
    equal to MAX_SELECTORS.
    
    Signed-off-by: Kobrin Ilay <ilayko3110@yandex.ru>

--- bpf/process/bpf_execve_event.c
+++ bpf/process/bpf_execve_event.c
@@ -270,7 +270,7 @@ event_execve(struct exec_ctx_struct *ctx)
 	parent = event_find_parent();
 	if (parent) {
 		event->parent = parent->key;
-		update_mb_task(parent);
+		update_mb_task(parent, &parent->bin);
 	} else {
 		event_minimal_parent(event, task);
 	}
@@ -408,6 +408,24 @@ execve_send(struct exec_ctx_struct *ctx __arg_ctx)
 			curr->caps.inheritable = event->creds.caps.inheritable;
 		}
 #endif
+
+		__u32 zero = 0;
+		struct binary *bin = map_lookup_elem(&binary_heap_map, &zero);
+
+		if (bin) {
+			// use current binary as parent binary if cleanup pid is equal to current pid,
+			// i.e. exec call was invoked in the same process.
+			if (curr->key.pid == event->cleanup_key.pid) {
+				memcpy(bin, &curr->bin, sizeof(curr->bin));
+				map_update_elem(&parent_binaries_map, &curr->key.pid, bin, BPF_ANY);
+			} else {
+				struct execve_map_value *parent = event_find_parent();
+
+				if (parent)
+					map_update_elem(&parent_binaries_map, &curr->key.pid, &parent->bin, BPF_ANY);
+			}
+		}
+
 		/* zero out previous paths in ->bin */
 		binary_reset(&curr->bin);
 #ifdef __LARGE_BPF_PROG
