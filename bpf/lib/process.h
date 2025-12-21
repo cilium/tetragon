d650d40ef476cde4f051c15343655cca7e96a609
Jiri Olsa <jolsa@kernel.org>
2025-12-21 14:33:34 +0000

    tetragon: Move read_exe to execve sensor
    No point having it in lib.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/lib/process.h
+++ bpf/lib/process.h
@@ -7,7 +7,6 @@
 #include "bpf_event.h"
 #include "bpf_helpers.h"
 #include "bpf_cred.h"
-#include "bpf_d_path.h"
 #include "../process/string_maps.h"
 #include "api.h"
 #include "policy_stats.h"
@@ -670,50 +669,6 @@ FUNC_INLINE void
 event_output_metric(void *ctx, u8 msg_op, void *data, u64 size)
 {
 	perf_event_output_metric(ctx, msg_op, &tcpmon_map, BPF_F_CURRENT_CPU, data, size);
-}
-#endif
-
-/**
- * read_exe() Reads the path from the backing executable file of the current
- * process.
- *
- * The executable file of a process can change using the prctl() system call
- * and PR_SET_MM_EXE_FILE. Thus, this function should only be used under the
- * execve path since the executable file is locked and usually there is only
- * one remaining thread at its exit path.
- */
-#ifdef __LARGE_BPF_PROG
-FUNC_INLINE __u32
-read_exe(struct task_struct *task, struct heap_exe *exe)
-{
-	struct file *file = BPF_CORE_READ(task, mm, exe_file);
-	struct path *path = __builtin_preserve_access_index(&file->f_path);
-	__u64 offset = 0;
-	__u64 revlen = STRING_POSTFIX_MAX_LENGTH - 1;
-
-	// we need to walk the complete 4096 len dentry in order to have an accurate
-	// matching on the prefix operators, even if we only keep a subset of that
-	char *buffer;
-
-	buffer = d_path_local(path, (int *)&exe->len, (int *)&exe->error);
-	if (!buffer)
-		return 0;
-
-	if (exe->len > STRING_POSTFIX_MAX_LENGTH - 1)
-		offset = exe->len - (STRING_POSTFIX_MAX_LENGTH - 1);
-	else
-		revlen = exe->len;
-	// buffer used by d_path_local can contain up to MAX_BUF_LEN i.e. 4096 we
-	// only keep the first 255 chars for our needs (we sacrifice one char to the
-	// verifier for the > 0 check)
-	if (exe->len > BINARY_PATH_MAX_LEN - 1)
-		exe->len = BINARY_PATH_MAX_LEN - 1;
-	asm volatile("%[len] &= 0xff;\n"
-		     : [len] "+r"(exe->len));
-	with_errmetrics(probe_read, exe->buf, exe->len, buffer);
-	if (revlen < STRING_POSTFIX_MAX_LENGTH)
-		with_errmetrics(probe_read, exe->end, revlen, (char *)(buffer + offset));
-	return exe->len;
 }
 #endif
 #endif //_PROCESS__
