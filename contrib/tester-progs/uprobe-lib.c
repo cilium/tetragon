47c2ef5826693f1dbce12e3c53a3096432436f2c
Andy Strohman <astrohma@isovalent.com>
2026-01-16 18:32:53 -0800

    tester-progs: extend uprobe prog to pass string
    The following commit will use this change to extend unit tests.
    
    Pointers passed to functions hooked by uprobes may point to memory that
    has been paged out. This test forces this scenario to confirm that we
    will be able to reach a paged out argument.
    
    The problem being tested here is not specific to strings. This change
    uses a string as an example, and as a starting point. But, we need to
    cut over the reading of all pointer types to use a sleepable program
    in order to ensure that users will be able to access the pointer's data.
    
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- contrib/tester-progs/uprobe-lib.c
+++ contrib/tester-progs/uprobe-lib.c
@@ -26,12 +26,18 @@ int uprobe_test_lib_arg3(unsigned long a1, unsigned int a2, void *a3)
 
 int uprobe_test_lib_arg4(long a1, int a2, char a3, void *a4)
 {
-	printf("uprobe_test_lib_arg3 called\n");
+	printf("uprobe_test_lib_arg4 called\n");
 	return 0;
 }
 
 int uprobe_test_lib_arg5(int a1, char a2, unsigned long a3, short a4, void *a5)
 {
-	printf("uprobe_test_lib_arg3 called\n");
+	printf("uprobe_test_lib_arg5 called\n");
+	return 0;
+}
+
+int uprobe_test_lib_string_arg(char *str)
+{
+	printf("uprobe_test_lib_string_arg called\n");
 	return 0;
 }
