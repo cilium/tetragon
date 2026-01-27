243fb939c9aeb21b7754fd4dfd0a6678e4cfe1b5
Andy Strohman <astrohma@isovalent.com>
2026-01-27 01:32:15 +0000

    bpf: move extract_arg logic into new header
    This is preparation for a following change, so that it can be used
    outside of generic_calls.h.
    
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- bpf/process/generic_calls.h
+++ bpf/process/generic_calls.h
@@ -16,6 +16,7 @@
 #include "regs.h"
 #include "config.h"
 #include "uprobe_preload.h"
+#include "generic_arg.h"
 
 #define MAX_TOTAL 9000
 
@@ -466,59 +467,6 @@ read_arg(void *ctx, int index, int type, long orig_off, unsigned long arg, int a
 	else
 		return __read_arg_2(ctx, type, orig_off, arg, argm, args);
 }
-
-FUNC_INLINE int
-extract_arg_depth(u32 i, struct extract_arg_data *data)
-{
-	if (i >= MAX_BTF_ARG_DEPTH || !data->btf_config[i].is_initialized)
-		return 1;
-	*data->arg = *data->arg + data->btf_config[i].offset;
-	if (data->btf_config[i].is_pointer)
-		probe_read((void *)data->arg, sizeof(char *), (void *)*data->arg);
-	return 0;
-}
-
-#ifdef __LARGE_BPF_PROG
-FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned long *a)
-{
-	struct config_btf_arg *btf_config;
-
-	if (index >= EVENT_CONFIG_MAX_ARG)
-		return;
-
-	asm volatile("%[index] &= %1 ;\n"
-		     : [index] "+r"(index)
-		     : "i"(MAX_SELECTORS_MASK));
-	btf_config = config->btf_arg[index];
-	if (btf_config->is_initialized) {
-		struct extract_arg_data extract_data = {
-			.btf_config = btf_config,
-			.arg = a,
-		};
-		int i;
-
-		if (CONFIG(ITER_NUM)) {
-			bpf_for(i, 0, MAX_BTF_ARG_DEPTH)
-			{
-				if (extract_arg_depth(i, &extract_data))
-					break;
-			}
-		} else {
-#ifndef __V61_BPF_PROG
-#pragma unroll
-			for (i = 0; i < MAX_BTF_ARG_DEPTH; ++i) {
-				if (extract_arg_depth(i, &extract_data))
-					break;
-			}
-#else
-			loop(MAX_BTF_ARG_DEPTH, extract_arg_depth, &extract_data, 0);
-#endif /* __V61_BPF_PROG */
-		}
-	}
-}
-#else
-FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned long *a) {}
-#endif /* __LARGE_BPF_PROG */
 
 FUNC_INLINE int arg_idx(int index)
 {
