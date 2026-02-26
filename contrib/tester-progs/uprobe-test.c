b5de5079819b301c2d81565b6b615e669d7e429f
Jiri Olsa <jolsa@kernel.org>
2026-02-26 09:28:05 +0000

    tetragon: Add tests for matching uprobe empty string
    Adding tests for matching uprobe empty string.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- contrib/tester-progs/uprobe-test.c
+++ contrib/tester-progs/uprobe-test.c
@@ -14,6 +14,7 @@ int uprobe_test_lib_arg3(unsigned long a1, unsigned int a2, void *a3);
 int uprobe_test_lib_arg4(long a1, int a2, char a3, void *a4);
 int uprobe_test_lib_arg5(int a1, char a2, unsigned long a3, short a4, void *a5);
 int uprobe_test_lib_string_arg(char *str);
+int uprobe_test_lib_string_arg_empty(char *str);
 
 int main(void)
 {
@@ -26,4 +27,5 @@ int main(void)
 	uprobe_test_lib_arg4(-321, -2, 'b', (void *) 1);
 	uprobe_test_lib_arg5(1, 'c', 0xcafe, 1234, (void *) 2);
 	uprobe_test_lib_string_arg(pageout(str_arg, strlen(str_arg) + 1));
+	uprobe_test_lib_string_arg_empty("");
 }
