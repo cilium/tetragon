320a62fb33c27ad997251400cec18491d880e3c9
Jiri Olsa <jolsa@kernel.org>
2026-03-06 14:45:42 +0000

    tetragon: Add TestFentryObjectWrite* tests
    Adding TestFentryObjectWrite* tests that executes its kprobe
    counterparts with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/fentry_test.go
+++ pkg/sensors/tracing/fentry_test.go
@@ -29,3 +29,28 @@ func TestFentryLseek(t *testing.T) {
 		"Hook": "fentries",
 	})
 }
+
+func TestFentryObjectWriteReadHostNs(t *testing.T) {
+	checkFentry(t)
+	testKprobeObjectWriteReadHostNs(t, true)
+}
+
+func TestFentryObjectWriteRead(t *testing.T) {
+	checkFentry(t)
+	testKprobeObjectWriteRead(t, true)
+}
+
+func TestFentryObjectWriteCapsNotIn(t *testing.T) {
+	checkFentry(t)
+	testKprobeObjectWriteCapsNotIn(t, true)
+}
+
+func TestFentryObjectWriteReadNsOnly(t *testing.T) {
+	checkFentry(t)
+	testKprobeObjectWriteReadNsOnly(t, true)
+}
+
+func TestFentryObjectWriteReadPidOnly(t *testing.T) {
+	checkFentry(t)
+	testKprobeObjectWriteReadPidOnly(t, true)
+}
