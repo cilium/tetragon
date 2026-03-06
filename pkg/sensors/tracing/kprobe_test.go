b199a4b116276b0f03c1b4561b637c1bc165f313
Jiri Olsa <jolsa@kernel.org>
2026-03-06 21:09:59 +0000

    tetragon: Add TestFentryMatchArgsFdPostfix test
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -3822,7 +3822,7 @@ func TestKprobeMatchArgsFdEqual(t *testing.T) {
 	testKprobeMatchArgsFdEqual(t, false)
 }
 
-func TestKprobeMatchArgsFdPostfix(t *testing.T) {
+func testKprobeMatchArgsFdPostfix(t *testing.T, fentry bool) {
 	var doneWG, readyWG sync.WaitGroup
 	defer doneWG.Wait()
 
@@ -3838,7 +3838,7 @@ func TestKprobeMatchArgsFdPostfix(t *testing.T) {
 		argVals[3] = "shadow"
 	}
 
-	createCrdFile(t, getMatchArgsFdCrd("Postfix", argVals[:]))
+	createCrdFileFlag(t, getMatchArgsFdCrd("Postfix", argVals[:]), fentry)
 
 	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
 	if err != nil {
@@ -3856,6 +3856,10 @@ func TestKprobeMatchArgsFdPostfix(t *testing.T) {
 	checker := ec.NewUnorderedEventChecker(kpCheckers...)
 	err = jsonchecker.JsonTestCheck(t, checker)
 	require.NoError(t, err)
+}
+
+func TestKprobeMatchArgsFdPostfix(t *testing.T) {
+	testKprobeMatchArgsFdPostfix(t, false)
 }
 
 func TestKprobeMatchArgsFdPrefix(t *testing.T) {
