753db53de7a9bff7b1f162eb60604df4c372b3e4
Jiri Olsa <jolsa@kernel.org>
2026-03-06 21:04:43 +0000

    tetragon: Add TestFentryMatchArgsFilePrefix test
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -3738,7 +3738,7 @@ func TestKprobeMatchArgsFilePostfix(t *testing.T) {
 	testKprobeMatchArgsFilePostfix(t, false)
 }
 
-func TestKprobeMatchArgsFilePrefix(t *testing.T) {
+func testKprobeMatchArgsFilePrefix(t *testing.T, fentry bool) {
 	var doneWG, readyWG sync.WaitGroup
 	defer doneWG.Wait()
 
@@ -3754,7 +3754,7 @@ func TestKprobeMatchArgsFilePrefix(t *testing.T) {
 		argVals[3] = "/etc/s"
 	}
 
-	createCrdFile(t, getMatchArgsFileCrd("Prefix", argVals[:]))
+	createCrdFileFlag(t, getMatchArgsFileCrd("Prefix", argVals[:]), fentry)
 
 	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
 	if err != nil {
@@ -3776,6 +3776,10 @@ func TestKprobeMatchArgsFilePrefix(t *testing.T) {
 	checker := ec.NewUnorderedEventChecker(kpCheckers...)
 	err = jsonchecker.JsonTestCheck(t, checker)
 	require.NoError(t, err)
+}
+
+func TestKprobeMatchArgsFilePrefix(t *testing.T) {
+	testKprobeMatchArgsFilePrefix(t, false)
 }
 
 func TestKprobeMatchArgsFdEqual(t *testing.T) {
