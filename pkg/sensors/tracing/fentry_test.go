91b1bd4961b5d158456b5ba9b8be03c26f45dc24
Jiri Olsa <jolsa@kernel.org>
2026-03-06 21:17:46 +0000

    tetragon: Add TestFentryMatchArgsNonPrefix test
    Adding TestFentryMatchArgsNonPrefix test that executes its kprobe
    counterpart with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/fentry_test.go
+++ pkg/sensors/tracing/fentry_test.go
@@ -275,3 +275,8 @@ func TestFentrytMatchArgsFileMonitoringPrefix(t *testing.T) {
 	checkFentry(t)
 	testKprobeMatchArgsFileMonitoringPrefix(t, true)
 }
+
+func TestFentryMatchArgsNonPrefix(t *testing.T) {
+	checkFentry(t)
+	testKprobeMatchArgsNonPrefix(t, true)
+}
