046db0f1475ce12276f7534677dcb84a514ab2c2
Jiri Olsa <jolsa@kernel.org>
2026-03-06 21:07:23 +0000

    tetragon: Add TestFentryMatchArgsFdEqual test
    Adding TestFentryMatchArgsFdEqual test that executes its kprobe
    counterpart with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/fentry_test.go
+++ pkg/sensors/tracing/fentry_test.go
@@ -255,3 +255,8 @@ func TestFentryMatchArgsFilePrefix(t *testing.T) {
 	checkFentry(t)
 	testKprobeMatchArgsFilePrefix(t, true)
 }
+
+func TestFentryMatchArgsFdEqual(t *testing.T) {
+	checkFentry(t)
+	testKprobeMatchArgsFdEqual(t, true)
+}
