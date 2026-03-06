498a13602806bbe18269b20bf94a801b4ec2dbd6
Jiri Olsa <jolsa@kernel.org>
2026-03-06 21:30:01 +0000

    tetragon: Add TestFentryMatchBinariesLargePath test
    Adding TestFentryMatchBinariesLargePath test that executes its kprobe
    counterpart with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/fentry_test.go
+++ pkg/sensors/tracing/fentry_test.go
@@ -290,3 +290,8 @@ func TestFentryMatchBinaries(t *testing.T) {
 	checkFentry(t)
 	testKprobeMatchBinaries(t, true)
 }
+
+func TestFentryMatchBinariesLargePath(t *testing.T) {
+	checkFentry(t)
+	testKprobeMatchBinariesLargePath(t, true)
+}
