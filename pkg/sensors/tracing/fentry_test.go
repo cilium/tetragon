e7b5f71d6941a9a119beb6a855f08397feb391cf
Jiri Olsa <jolsa@kernel.org>
2026-03-07 12:30:24 +0000

    tetragon: Add TestFentryListSyscallDupsRange test
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/fentry_test.go
+++ pkg/sensors/tracing/fentry_test.go
@@ -337,3 +337,8 @@ func TestFentryRateLimit(t *testing.T) {
 	checkFentry(t)
 	testKprobeRateLimit(t, true, true)
 }
+
+func TestFentryListSyscallDupsRange(t *testing.T) {
+	checkFentry(t)
+	testKprobeListSyscallDupsRange(t, true)
+}
