99164be7cccf4c62026b9e78a6e2b66537a6ac19
Jiri Olsa <jolsa@kernel.org>
2026-03-07 13:15:29 +0000

    tetragon: Add TestFentryDentryPath test
    Adding TestFentryDentryPath test that executes its kprobe
    counterpart with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -6886,7 +6886,7 @@ func TestMaxPath(t *testing.T) {
 	testMaxPath(t, false)
 }
 
-func TestKprobeDentryPath(t *testing.T) {
+func testKprobeDentryPath(t *testing.T, fentry bool) {
 	var doneWG, readyWG sync.WaitGroup
 	defer doneWG.Wait()
 
@@ -6946,7 +6946,7 @@ spec:
         values:
         - "` + check1 + `"
 `
-	createCrdFile(t, hook)
+	createCrdFileFlag(t, hook, fentry)
 
 	t.Logf("Removing file 1 %s, check %s\n", file1.Name(), check1)
 	t.Logf("Removing file 2 %s, check %s\n", file2.Name(), check2)
@@ -6984,6 +6984,10 @@ spec:
 	// ... but not for file_2 (check_2).
 	err = jsonchecker.JsonTestCheckExpect(t, getChecker(check2), true)
 	require.NoError(t, err)
+}
+
+func TestKprobeDentryPath(t *testing.T) {
+	testKprobeDentryPath(t, false)
 }
 
 func TestKprobeResolvePid(t *testing.T) {
