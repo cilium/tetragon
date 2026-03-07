9007db76cdca7333621b508abf9b5c0712e7b5a1
Jiri Olsa <jolsa@kernel.org>
2026-03-07 12:42:55 +0000

    tetragon: Add TestFentryMultiMatcArgs test
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -5921,7 +5921,7 @@ func TestKprobeUserStackTrace(t *testing.T) {
 	testKprobeUserStackTrace(t, false)
 }
 
-func TestKprobeMultiMatcArgs(t *testing.T) {
+func testKprobeMultiMatcArgs(t *testing.T, fentry bool) {
 	if !config.EnableLargeProgs() {
 		t.Skip("Older kernels do not support matchArgs for more than one arguments")
 	}
@@ -5991,7 +5991,7 @@ spec:
 	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
 	defer cancel()
 
-	createCrdFile(t, tracingPolicy)
+	createCrdFileFlag(t, tracingPolicy, fentry)
 
 	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
 	if err != nil {
@@ -6031,6 +6031,10 @@ spec:
 	checker := ec.NewUnorderedEventChecker(kpCheckersRead, kpCheckersMmap)
 	err = jsonchecker.JsonTestCheck(t, checker)
 	require.NoError(t, err)
+}
+
+func TestKprobeMultiMatcArgs(t *testing.T) {
+	testKprobeMultiMatcArgs(t, false)
 }
 
 func trigger(t *testing.T) {
