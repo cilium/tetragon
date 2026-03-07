0b83a22e245df35b7f886988166438e04b2acb76
Jiri Olsa <jolsa@kernel.org>
2026-03-07 12:16:46 +0000

    tetragon: Add TestFentryRate* tests
    Adding TestFentryRate* tests that execute its kprobe
    counterparts with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -5206,7 +5206,7 @@ func TestKprobeWriteMaxDataFull(t *testing.T) {
 	testKprobeWriteMaxDataFull(t, false)
 }
 
-func testKprobeRateLimit(t *testing.T, rateLimit bool) {
+func testKprobeRateLimit(t *testing.T, rateLimit, fentry bool) {
 	testutils.CaptureLog(t, logger.GetLogger())
 	ctx, cancel := context.WithTimeout(context.Background(), tus.Conf().CmdWaitTime)
 	defer cancel()
@@ -5260,6 +5260,11 @@ spec:
 	tp, err := tracingpolicy.FromYAML(hook)
 	if err != nil {
 		t.Fatalf("failed to decode yaml: %s", err)
+	}
+
+	if fentry {
+		tp.TpSpec().Fentries = tp.TpSpec().KProbes
+		tp.TpSpec().KProbes = []v1alpha1.KProbeSpec{}
 	}
 
 	err = sm.Manager.AddTracingPolicy(ctx, tp)
@@ -5324,7 +5329,7 @@ func TestKprobeNoRateLimit(t *testing.T) {
 		t.Skip("Test requires kernel 5.4")
 	}
 
-	testKprobeRateLimit(t, false)
+	testKprobeRateLimit(t, false, false)
 }
 
 func TestKprobeRateLimit(t *testing.T) {
@@ -5332,7 +5337,7 @@ func TestKprobeRateLimit(t *testing.T) {
 		t.Skip("Test requires kernel 5.4")
 	}
 
-	testKprobeRateLimit(t, true)
+	testKprobeRateLimit(t, true, false)
 }
 
 func TestKprobeListSyscallDupsRange(t *testing.T) {
