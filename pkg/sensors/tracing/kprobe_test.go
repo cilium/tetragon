6e658a67864b137a4df63ca5544a3200b5528da5
Jiri Olsa <jolsa@kernel.org>
2026-03-07 12:48:04 +0000

    tetragon: Add TestFentryUserStackTrace test
    Adding TestFentryUserStackTrace test that executes its kprobe
    counterpart with fentries/kprobes swap.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -5793,7 +5793,7 @@ func TestKprobeKernelStackTrace(t *testing.T) {
 	testKprobeKernelStackTrace(t, false)
 }
 
-func TestKprobeUserStackTrace(t *testing.T) {
+func testKprobeUserStackTrace(t *testing.T, fentry bool) {
 	var doneWG, readyWG sync.WaitGroup
 	defer doneWG.Wait()
 
@@ -5824,7 +5824,7 @@ spec: ` + disableKprobeMulti + `
       - action: Post
         userStackTrace: true`
 
-	createCrdFile(t, tracingPolicy)
+	createCrdFileFlag(t, tracingPolicy, fentry)
 
 	obs, err := observertesthelper.GetDefaultObserverWithFile(t, ctx, testConfigFile, tus.Conf().TetragonLib, observertesthelper.WithMyPid())
 	if err != nil {
@@ -5867,6 +5867,10 @@ spec: ` + disableKprobeMulti + `
 	testCmd.Process.Kill()
 
 	require.NoError(t, err)
+}
+
+func TestKprobeUserStackTrace(t *testing.T) {
+	testKprobeUserStackTrace(t, false)
 }
 
 func TestKprobeMultiMatcArgs(t *testing.T) {
