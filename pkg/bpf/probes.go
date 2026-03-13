49a55b83c06202c8d1e47e8fd9d92398dd3ca1dd
Kornilios Kourtis <kornilios@isovalent.com>
2026-03-13 07:52:47 +0100

    bpf: introduce constants for the probes
    In policytests, we want to check for certain probes that are part of the
    info exported by agent. Let's use constants instead of strings to avoid
    errors.
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/bpf/probes.go
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+package bpf
+
+// probe constants
+const (
+	OverrideReturnProbe         = "override_return"
+	BuildIDProbe                = "buildid"
+	KprobeMultiProbe            = "kprobe_multi"
+	UprobeMultiProbe            = "uprobe_multi"
+	FmodRetProbe                = "fmodret"
+	FmodRetSyscallProbe         = "fmodret_syscall"
+	SignalHelperProbe           = "signal"
+	LargeProgsProbe             = "large"
+	LinkPinProbe                = "link_pin"
+	LsmProbe                    = "lsm"
+	MissedStatsKprobeMultiProbe = "missed_stats_kprobe_multi"
+	MissedStatsKprobeProbe      = "missed_stats_kprobe"
+	BatchUpdateProbe            = "batch_update"
+	UprobeRefCtrOffsetProbe     = "uprobe_refctroff"
+	AuditLoginUIDProbe          = "audit_loginuid"
+	ProbeWriteUserProbe         = "probe_write_user"
+	UprobeRegsChangeProbe       = "uprobe_regs_change"
+	MixBPFAndTailCallsProbe     = "mix_bpf_and_tail_calls"
+)
