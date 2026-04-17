0c557b5c090d47503c86e87b789ef819e386de8c
Andy Strohman <astrohma@isovalent.com>
2026-04-17 22:08:40 +0000

    kprobes: add test for NULL string args
    Previously, we had a bug where policies that configure string args would
    drop events when the argument was a NULL pointer. Later, with the
    addition of error arg types, this bug mutated so that events were sent,
    but corrupted.
    
    While working on fixing this bug, the author encountered another bug,
    where if the first argument ends up being an error arg type (ie NULL
    dereference) then this interfered with the reporting of returnArg.
    
    This test is meant to prevent either of these issues from returning.
    
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- tests/policytests/kprobes.go
+++ tests/policytests/kprobes.go
@@ -7,6 +7,7 @@ package tests
 
 import (
 	ec "github.com/cilium/tetragon/api/v1/tetragon/codegen/eventchecker"
+	lc "github.com/cilium/tetragon/pkg/matchers/listmatcher"
 	sm "github.com/cilium/tetragon/pkg/matchers/stringmatcher"
 	"github.com/cilium/tetragon/pkg/testutils/policytest"
 )
@@ -43,5 +44,38 @@ spec:
 		Name:         "execute lseek and check events",
 		Trigger:      policytest.NewCmdTrigger(lseek, "-1", "0", "4444"),
 		EventChecker: ec.NewUnorderedEventChecker(lseekChecker),
+	}
+}).RegisterAtInit()
+
+var _ = policytest.NewBuilder("kprobe-null-string").WithLabels("kprobes").WithPolicyTemplate(`
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "kprobe-null-string"
+spec:
+  kprobes:
+  - call: "security_sb_mount"
+    syscall: false
+    return: true
+    args:
+      - index: 0 # dev_name
+        type: "string"
+    returnArg:
+      index: 0
+      type: "int"
+`).AddScenario(func(c *policytest.Conf) *policytest.Scenario {
+	myBin := c.TestBinary("null-mount")
+	argChecker := ec.NewProcessKprobeChecker("kprobe-null-string").
+		WithProcess(ec.NewProcessChecker().
+			WithBinary(sm.Full(myBin))).WithArgs(ec.NewKprobeArgumentListMatcher().
+		WithOperator(lc.Ordered).
+		WithValues(
+			ec.NewKprobeArgumentChecker().WithErrorArg(ec.NewKprobeErrorChecker().WithMessage(sm.Full("Bad address"))),
+		)).WithReturn(ec.NewKprobeArgumentChecker().WithIntArg(0))
+
+	return &policytest.Scenario{
+		Name:         "check null pointer passed for string argument",
+		Trigger:      policytest.NewCmdTrigger(myBin).ExpectExitCode(0),
+		EventChecker: ec.NewUnorderedEventChecker(argChecker),
 	}
 }).RegisterAtInit()
