cilium/tetragon

commit 2aa4e930659e970edb8e2b7521192eac2ffe4c68

tetragon: handle resolving null pointer
We have been ignoring errors returned from bpf_probe_read() when
dereferencing during resolve. bpf_probe_read() returns a negative value
when it detects that a seg fault would occur. This is problematic both
in terms of exposing bogus argument values in the event and also when
preforming filtering on behalf of the matchArgs selector.

This change notes when we were unable to dereference so that arg
filtering does not happen against a bogus value.

Resolve may traverse multiple pointers, so we need a way to distinguish
which pointer could not be dereferenced.  The "depth" of the dereference
failure is recorded, and exposed to user space, so that we can report
the pointer that could not be dereferenced in the event.

For each arg, we set the depth at which the dereference failed in a
status header that precedes the argument value. If the status value is
0, then no error occurred and the argument's value follows. Any non-zero
status indicates failure reading the arg value. When user space
encounters this non-zero status, it will not attempt to read the arg's
value. This allows us to avoid transmitting bogus argument values from
the bpf program to user space.

Signed-off-by: Andy Strohman <astrohma@isovalent.com>

bpf/process/generic_calls.h
@@ -226,6 +226,14 @@ FUNC_INLINE bool is_read_arg_1(long type)
 	return false;
 }
 
+FUNC_INLINE long write_arg_status(struct msg_generic_kprobe *e, unsigned long offset,
+				  arg_status_t status)
+{
+	char *args = args_off(e, offset);
+	*(arg_status_t *)args = status;
+	return offset + sizeof(arg_status_t);
+}
+
 FUNC_INLINE long
 __read_arg_1(void *ctx, int type, long orig_off, unsigned long arg, int argm, char *args)
 {
@@ -336,7 +344,7 @@ __read_arg_1(void *ctx, int type, long orig_off, unsigned long arg, int argm, ch
 		size = 0;
 		break;
 	}
-	return size;
+	return size + sizeof(arg_status_t);
 }
 
 FUNC_INLINE long
@@ -414,7 +422,7 @@ __read_arg_2(void *ctx, int type, long orig_off, unsigned long arg, int argm, ch
 		size = 0;
 		break;
 	}
-	return size;
+	return size + sizeof(arg_status_t);
 }
 
 /**
@@ -433,7 +441,9 @@ __read_arg_2(void *ctx, int type, long orig_off, unsigned long arg, int argm, ch
 FUNC_INLINE long
 read_arg(void *ctx, int index, int type, long orig_off, unsigned long arg, int argm, int process)
 {
-	size_t min_size = type_to_min_size(type, argm);
+	// min size of type plus the size of the header which indicates arg read status
+	size_t min_size = type_to_min_size(type, argm) + sizeof(arg_status_t);
+
 	struct msg_generic_kprobe *e;
 	char *args;
 	const struct path *path_arg = 0;
@@ -447,16 +457,17 @@ read_arg(void *ctx, int index, int type, long orig_off, unsigned long arg, int a
 	if (orig_off >= 16383 - min_size)
 		return 0;
 
-	orig_off &= 16383;
-	args = args_off(e, orig_off);
-
+	orig_off = write_arg_status(e, orig_off, e->arg_status[index & MAX_SELECTORS_MASK]);
 	/* Cache args offset for filter use later */
 	e->argsoff[index & MAX_SELECTORS_MASK] = orig_off;
+	if (!is_arg_ok(e, index))
+		return sizeof(arg_status_t);
+
+	args = args_off(e, orig_off);
 
 	path_arg = get_path(type, arg, &path_buf);
 	if (path_arg)
-		return copy_path(args, path_arg);
-
+		return copy_path(args, path_arg) + sizeof(arg_status_t);
 	/*
 	 * Separate argument processing based on the process const
 	 * for 4.19 kernels..
@@ -514,12 +525,20 @@ FUNC_INLINE long get_pt_regs_arg(struct pt_regs *ctx, struct event_config *confi
 #endif /* __TARGET_ARCH_x86 && (GENERIC_KPROBE || GENERIC_UPROBE) */
 
 #if defined(GENERIC_UPROBE) || defined(GENERIC_USDT)
-FUNC_INLINE unsigned long get_preload_arg(struct pt_regs *ctx, long ty)
+FUNC_INLINE unsigned long get_preload_arg(struct pt_regs *ctx, long ty, arg_status_t *status)
 {
 	unsigned long arg = 0;
+	__u64 id = get_current_pid_tgid();
+
+	struct preload_data *data = map_lookup_elem(&sleepable_preload, &id);
+
+	if (data)
+		*status = data->status;
+	else
+		return arg;
 
 	if (ty == string_type) {
-		arg = preload_string_arg(ctx);
+		arg = (unsigned long)data->data;
 
 		// Make verifier to believe it's just an ordinary number and not
 		// a pointer to the map. The rest of the argument code might do
@@ -531,7 +550,7 @@ FUNC_INLINE unsigned long get_preload_arg(struct pt_regs *ctx, long ty)
 	return arg;
 }
 #else
-FUNC_INLINE long get_preload_arg(struct pt_regs *ctx, long ty)
+FUNC_INLINE long get_preload_arg(struct pt_regs *ctx, long ty, arg_status_t *status)
 {
 	return 0;
 }
@@ -559,6 +578,7 @@ FUNC_INLINE long generic_read_arg(void *ctx, int index, long off, struct bpf_map
 		     : "i"(MAX_SELECTORS_MASK));
 	ty = config->arg[index];
 	am = config->arm[index];
+	e->arg_status[index] = 0;
 
 #ifndef __LARGE_BPF_PROG
 #if defined(GENERIC_KPROBE) || defined(GENERIC_UPROBE) || defined(GENERIC_TRACEPOINT)
@@ -568,16 +588,16 @@ FUNC_INLINE long generic_read_arg(void *ctx, int index, long off, struct bpf_map
 #endif
 
 #if defined(GENERIC_TRACEPOINT) || defined(GENERIC_USDT)
+	asm volatile("%[index] &= %1 ;\n"
+		     : [index] "+r"(index)
+		     : "i"(MAX_SELECTORS_MASK));
+
 	a = (&e->a0)[index];
 	if (am & ARGM_PRELOAD)
-		a = get_preload_arg(ctx, ty);
+		a = get_preload_arg(ctx, ty, &e->arg_status[index]);
 	else
-		extract_arg(config, index, &a, false);
+		extract_arg(config, index, &a, false, &e->arg_status[index]);
 #else
-	arg_index = config->idx[index];
-	asm volatile("%[arg_index] &= %1 ;\n"
-		     : [arg_index] "+r"(arg_index)
-		     : "i"(MAX_SELECTORS_MASK));
 
 	/* Getting argument data based on the source attribute, which is encoded
 	 * in argument meta data, so far it's either:
@@ -588,15 +608,19 @@ FUNC_INLINE long generic_read_arg(void *ctx, int index, long off, struct bpf_map
 	 *   - real argument value
 	 */
 	if (am & ARGM_PRELOAD) {
-		a = get_preload_arg(ctx, ty);
+		a = get_preload_arg(ctx, ty, &e->arg_status[index & MAX_SELECTORS_MASK]);
 	} else {
+		asm volatile("%[index] &= %1 ;\n"
+			     : [index] "+r"(index)
+			     : "i"(MAX_SELECTORS_MASK));
+		arg_index = config->idx[index];
 		if (am & ARGM_PT_REGS)
 			a = get_pt_regs_arg(ctx, config, index);
 		else if (am & ARGM_CURRENT_TASK)
 			a = get_current_task();
 		else
-			a = (&e->a0)[arg_index];
-		extract_arg(config, index, &a, false);
+			a = (&e->a0)[arg_index & MAX_SELECTORS_MASK];
+		extract_arg(config, index, &a, false, &e->arg_status[index]);
 	}
 
 	if (should_offload_path(ty))
@@ -1198,9 +1222,11 @@ FUNC_INLINE int generic_retprobe(void *ctx, struct bpf_map_def *calls, unsigned 
 
 	switch (do_copy) {
 	case char_buf:
+		size = write_arg_status(e, size, 0);
 		size += __copy_char_buf(ctx, size, info.ptr, ret, false, e);
 		break;
 	case char_iovec:
+		size = write_arg_status(e, size, 0);
 		size += __copy_char_iovec(size, info.ptr, info.cnt, ret, e);
 		break;
 	default:

bpf/lib/generic.h
@@ -49,6 +49,8 @@ struct generic_path {
 	struct mount *mnt;
 };
 
+typedef __u32 arg_status_t;
+
 struct msg_generic_kprobe {
 	struct msg_common common;
 	struct msg_execve_key current;
@@ -65,6 +67,7 @@ struct msg_generic_kprobe {
 	char args[24000];
 	unsigned long a0, a1, a2, a3, a4;
 	long argsoff[MAX_POSSIBLE_ARGS];
+	arg_status_t arg_status[MAX_POSSIBLE_ARGS];
 	struct msg_selector_data sel;
 	__u32 idx; // attach cookie index
 	__u32 tailcall_index_process; // recursion index for generic_process_event
@@ -81,6 +84,11 @@ struct msg_generic_kprobe {
 	struct generic_path path;
 #endif
 };
+
+FUNC_INLINE bool is_arg_ok(struct msg_generic_kprobe *e, int idx)
+{
+	return !e->arg_status[idx & 0x7];
+}
 
 FUNC_INLINE size_t generic_kprobe_common_size(void)
 {

bpf/process/event_config.h
@@ -43,6 +43,7 @@ struct extract_arg_data {
 	struct config_btf_arg *btf_config;
 	unsigned long *arg;
 	bool can_sleep;
+	arg_status_t *arg_status;
 };
 
 #define MAX_BTF_ARG_DEPTH	  10

bpf/process/generic_arg.h
@@ -7,21 +7,29 @@
 FUNC_INLINE int
 extract_arg_depth(u32 i, struct extract_arg_data *data)
 {
+	int ret;
+
 	if (i >= MAX_BTF_ARG_DEPTH || !data->btf_config[i].is_initialized)
 		return 1;
+
 	*data->arg = *data->arg + data->btf_config[i].offset;
 	if (data->btf_config[i].is_pointer) {
 		if (data->can_sleep)
-			copy_from_user((void *)data->arg, sizeof(char *), (void *)*data->arg);
+			ret = copy_from_user((void *)data->arg, sizeof(char *), (void *)*data->arg);
 		else
-			probe_read((void *)data->arg, sizeof(char *), (void *)*data->arg);
+			ret = probe_read((void *)data->arg, sizeof(char *), (void *)*data->arg);
+
+		if (ret < 0) {
+			*data->arg_status = i + 1;
+			return 1;
+		}
 	}
 	return 0;
 }
 
 #ifdef __LARGE_BPF_PROG
 FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned long *a,
-			     bool can_sleep)
+			     bool can_sleep, arg_status_t *arg_status)
 {
 	struct config_btf_arg *btf_config;
 
@@ -31,12 +39,14 @@ FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned lo
 	asm volatile("%[index] &= %1 ;\n"
 		     : [index] "+r"(index)
 		     : "i"(MAX_SELECTORS_MASK));
+
 	btf_config = config->btf_arg[index];
 	if (btf_config->is_initialized) {
 		struct extract_arg_data extract_data = {
 			.btf_config = btf_config,
 			.arg = a,
 			.can_sleep = can_sleep,
+			.arg_status = arg_status,
 		};
 		int i;
 
@@ -61,7 +71,7 @@ FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned lo
 }
 #else
 FUNC_INLINE void extract_arg(struct event_config *config, int index, unsigned long *a,
-			     bool can_sleep)
+			     bool can_sleep, arg_status_t *arg_status)
 {
 }
 #endif /* __LARGE_BPF_PROG */

bpf/process/generic_path.h
@@ -140,6 +140,9 @@ FUNC_INLINE void generic_path_init(struct msg_generic_kprobe *e)
 	e->path.state = STATE_INIT;
 }
 
+FUNC_INLINE long write_arg_status(struct msg_generic_kprobe *e, unsigned long offset,
+				  arg_status_t status);
+
 /*
  * The path offload is plugged in roughly as follows:
  *
@@ -196,10 +199,15 @@ FUNC_INLINE long generic_path_offload(void *ctx, long ty, unsigned long arg,
 	if (!buffer)
 		return 0;
 
+	orig_off = write_arg_status(e, orig_off, e->arg_status[index & MAX_SELECTORS_MASK]);
 	e->argsoff[index & MAX_SELECTORS_MASK] = orig_off;
+
+	if (!is_arg_ok(e, index))
+		return sizeof(arg_status_t);
+
 	args = args_off(e, orig_off);
 	buf = get_buf(buffer, gp->off);
-	return store_path(args, buf, gp->path, MAX_BUF_LEN - gp->off - 1, 0);
+	return store_path(args, buf, gp->path, MAX_BUF_LEN - gp->off - 1, 0) + sizeof(arg_status_t);
 }
 
 FUNC_INLINE bool should_offload_path(long type)

bpf/process/types/basic.h
@@ -2007,6 +2007,9 @@ filter_arg_1(struct msg_generic_kprobe *e, struct selector_arg_filter *filter, c
 		if (filter->op == op_capabilities_gained) {
 			__u64 cap_old = *(__u64 *)args;
 			__u32 index2 = *((__u32 *)&filter->value);
+
+			if (!is_arg_ok(e, index2))
+				return 0;
 			__u64 cap_new = *(__u64 *)get_arg(e, index2);
 
 			return !!((cap_old ^ cap_new) & cap_new);
@@ -2168,6 +2171,9 @@ selector_arg_offset(void *ctx, struct bpf_map_def *tailcalls,
 		if (index >= 5)
 			return 0;
 
+		if (!is_arg_ok(e, index))
+			return 0;
+
 		args = get_arg(e, index);
 		if (!filter_arg(e, filter, args, arg))
 			return 0;
@@ -2220,6 +2226,10 @@ installfd(struct msg_generic_kprobe *e, int fd, int name, bool follow)
 	if (fd > 5) {
 		return 0;
 	}
+
+	if (!is_arg_ok(e, fd))
+		return 0;
+
 	fdoff = e->argsoff[fd];
 	asm volatile("%[fdoff] &= 0x7ff;\n"
 		     : [fdoff] "+r"(fdoff)
@@ -2236,6 +2246,10 @@ installfd(struct msg_generic_kprobe *e, int fd, int name, bool follow)
 			     :);
 		if (name > 5)
 			return 0;
+
+		if (!is_arg_ok(e, name))
+			return 0;
+
 		nameoff = e->argsoff[name];
 		asm volatile("%[nameoff] &= 0x7ff;\n"
 			     : [nameoff] "+r"(nameoff)
@@ -2264,6 +2278,10 @@ msg_generic_arg_value_u64(struct msg_generic_kprobe *e, unsigned int arg_id, __u
 
 	if (arg_id > MAX_POSSIBLE_ARGS)
 		return err_val;
+
+	if (!is_arg_ok(e, arg_id))
+		return err_val;
+
 	argoff = e->argsoff[arg_id];
 	argoff &= GENERIC_MSG_ARGS_MASK;
 	ret = (__u64 *)&e->args[argoff];
@@ -2283,6 +2301,8 @@ copyfd(struct msg_generic_kprobe *e, int oldfd, int newfd)
 		     :);
 	if (oldfd > 5)
 		return 0;
+	if (!is_arg_ok(e, oldfd))
+		return 0;
 	oldfdoff = e->argsoff[oldfd];
 	asm volatile("%[oldfdoff] &= 0x7ff;\n"
 		     : [oldfdoff] "+r"(oldfdoff)
@@ -2297,6 +2317,8 @@ copyfd(struct msg_generic_kprobe *e, int oldfd, int newfd)
 			     : [newfd] "+r"(newfd)
 			     :);
 		if (newfd > 5)
+			return 0;
+		if (!is_arg_ok(e, newfd))
 			return 0;
 		newfdoff = e->argsoff[newfd];
 		asm volatile("%[newfdoff] &= 0x7ff;\n"
@@ -2372,9 +2394,9 @@ rate_limit(__u64 ratelimit_interval, __u64 ratelimit_scope, struct msg_generic_k
 		if (i < MAX_POSSIBLE_ARGS - 1 && arg_idx(i + 1) != -1)
 			arg_size = e->argsoff[i + 1] - e->argsoff[i];
 		else
-			arg_size = e->common.size - e->argsoff[i];
+			arg_size = e->common.size - e->argsoff[i] + sizeof(arg_status_t);
 		if (arg_size > 0) {
-			key_index = e->argsoff[i] & 16383;
+			key_index = (e->argsoff[i] - sizeof(arg_status_t)) & 16383;
 			if (arg_size > KEY_BYTES_PER_ARG)
 				arg_size = KEY_BYTES_PER_ARG;
 			asm volatile("%[arg_size] &= 0x3f;\n" // ensure this mask is greater than KEY_BYTES_PER_ARG
@@ -2435,6 +2457,9 @@ tracksock(struct msg_generic_kprobe *e, int socki, bool track)
 		     : [socki] "+r"(socki)
 		     :);
 	if (socki > 5)
+		return 0;
+
+	if (!is_arg_ok(e, socki))
 		return 0;
 
 	sockoff = e->argsoff[socki];

bpf/process/user_preload.h
@@ -10,6 +10,7 @@
 #include "usdt_arg.h"
 
 struct preload_data {
+	arg_status_t status;
 	unsigned char data[4096];
 };
 
@@ -22,16 +23,9 @@ struct {
 
 #if defined(GENERIC_UPROBE) || defined(GENERIC_USDT)
 
-FUNC_INLINE unsigned long
-preload_string_arg(struct pt_regs *ctx)
-{
-	__u64 id = get_current_pid_tgid();
-
-	return (unsigned long)map_lookup_elem(&sleepable_preload, &id);
-}
-
 FUNC_INLINE int
-preload_string_type(struct pt_regs *ctx, struct event_config *config, unsigned long val)
+preload_string_type(struct pt_regs *ctx, struct event_config *config, unsigned long val,
+		    arg_status_t status)
 {
 	__u64 id = get_current_pid_tgid();
 	struct preload_data *data;
@@ -47,7 +41,9 @@ preload_string_type(struct pt_regs *ctx, struct event_config *config, unsigned l
 	if (!data)
 		return 0;
 
-	bpf_copy_from_user_str(data, sizeof(*data), (const void *)val, 0);
+	data->status = status;
+	if (!status)
+		bpf_copy_from_user_str(data->data, sizeof(data->data), (const void *)val, 0);
 	return 0;
 }
 
@@ -58,6 +54,7 @@ preload_pt_regs_arg(struct pt_regs *ctx, struct event_config *config, int index)
 	unsigned long val;
 	__u8 shift;
 	__s32 ty;
+	arg_status_t status = 0;
 
 	asm volatile("%[index] &= %1 ;\n"
 		     : [index] "+r"(index)
@@ -73,11 +70,11 @@ preload_pt_regs_arg(struct pt_regs *ctx, struct event_config *config, int index)
 	// found in registers via pt_regs source. So the following extract_arg
 	// call is not required, because config->btf_arg won't be populated
 	// for the pt_regs case.
-	extract_arg(config, index, &val, true);
+	extract_arg(config, index, &val, true, &status);
 
 	switch (ty) {
 	case string_type:
-		return preload_string_type(ctx, config, val);
+		return preload_string_type(ctx, config, val, status);
 	}
 
 	return 0;
@@ -88,6 +85,7 @@ preload_arg(struct pt_regs *ctx, struct event_config *config, int index)
 {
 	unsigned long a;
 	__s32 ty;
+	arg_status_t status = 0;
 
 #if defined(GENERIC_USDT)
 	a = read_usdt_arg(ctx, config, index, true);
@@ -116,7 +114,8 @@ preload_arg(struct pt_regs *ctx, struct event_config *config, int index)
 		break;
 	}
 #endif
-	extract_arg(config, index, &a, true);
+
+	extract_arg(config, index, &a, true, &status);
 
 	ty = config->arg[index];
 
@@ -124,7 +123,7 @@ preload_arg(struct pt_regs *ctx, struct event_config *config, int index)
 
 	switch (ty) {
 	case string_type:
-		return preload_string_type(ctx, config, a);
+		return preload_string_type(ctx, config, a, status);
 	}
 
 	return 0;

commit 54da889d8a147ffbea42618dbd7716cce0767dd5

bpf: initialize retprobe return arg status
Commit 2aa4e930659e ("tetragon: handle resolving null pointer") missed
this initialization in the return probe path. The offending commit did
properly initialize in the entry probe path.

The ramification of this bug was that unrelated, previous events which
detected arg read failures for the first arg could prevent reading the
return arg and then convey the unrelated error arg outcome for the
return arg.

Fixes: 2aa4e930659e ("tetragon: handle resolving null pointer")
Signed-off-by: Andy Strohman <astrohma@isovalent.com>

bpf/process/generic_calls.h
@@ -1240,6 +1240,7 @@ FUNC_INLINE int generic_retprobe(void *ctx, struct bpf_map_def *calls, unsigned 
 	ty_arg = config->argreturn;
 	do_copy = config->argreturncopy;
 	if (ty_arg) {
+		e->arg_status[0] = 0;
 		size += read_arg(ctx, 0, ty_arg, size, ret, 0, __READ_ARG_ALL);
 #if defined(__LARGE_BPF_PROG) && (defined(GENERIC_KRETPROBE) || defined(GENERIC_FEXIT))
 		struct socket_owner owner;