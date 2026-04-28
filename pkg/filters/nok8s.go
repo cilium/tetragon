cilium/tetragon

commit d833af47155e967cc6118173f58d135a58638aa0

nok8s: update pkg/filters
Disable label filters in pkg/filters for nonk8s builds.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/filters/nok8s.go
@@ -1,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Tetragon
+
+//go:build nok8s
+
+package filters
+
+import (
+	"context"
+	"errors"
+
+	"github.com/cilium/tetragon/api/v1/tetragon"
+)
+
+type LabelsFilter struct{}
+
+func (l *LabelsFilter) OnBuildFilter(_ context.Context, _ *tetragon.Filter) ([]FilterFunc, error) {
+	return nil, errors.New("label filters not supported in nok8s build")
+}

pkg/filters/labels.go
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Tetragon
 
+//go:build !nok8s
+
 package filters
 
 import (

commit 4ce02d7139b4efac1a14f4c86349a57dd6620a6b

nok8s: don't return an error for label filters
The current code results in:
> Error: failed to receive events: rpc error: code = Unknown desc = label filters not supported in nok8s build

When doing "tetra getevents" for the nok8s build.

Instead of return an error, just ignore the unsupported filters.

Fixes: d833af47155e ("nok8s: update pkg/filters")

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/filters/nok8s.go
@@ -7,7 +7,6 @@ package filters
 
 import (
 	"context"
-	"errors"
 
 	"github.com/cilium/tetragon/api/v1/tetragon"
 )
@@ -15,5 +14,5 @@ import (
 type LabelsFilter struct{}
 
 func (l *LabelsFilter) OnBuildFilter(_ context.Context, _ *tetragon.Filter) ([]FilterFunc, error) {
-	return nil, errors.New("label filters not supported in nok8s build")
+	return nil, nil
 }