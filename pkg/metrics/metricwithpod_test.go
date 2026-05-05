4ec4efbcbf81223bd672d4455bf9f9f67cec2536
Daniel Gomez <dangome3@cisco.com>
2026-05-05 11:22:43 -0500

    add unit tests for health and event metrics
    Unit tests for metrics config added in 'pkg/metrics/metricwithpod_test.go'
    Covered tests for health only metrics and halth+event metrics enabled.
    
    Signed-off-by: Daniel Gomez <dangome3@cisco.com>

--- pkg/metrics/metricwithpod_test.go
+++ pkg/metrics/metricwithpod_test.go
@@ -26,16 +26,59 @@ var sampleMsgGenericTracepointUnix = tracing.MsgGenericTracepointUnix{
 	PolicyName: "fake-policy",
 }
 
-func TestPodDelete(t *testing.T) {
+func TestMetricsWithPod(t *testing.T) {
+	eventMetrics := []string{"tetragon_events_total", "tetragon_policy_events_total", "tetragon_syscalls_total"}
+	healthMetrics := []string{"tetragon_build_info", "tetragon_data_events_total"}
+
 	reg := metricsconfig.GetRegistry()
-	metricsconfig.InitHealthMetrics(reg)
-	metricsconfig.InitEventsMetrics(reg)
+
+	// Only health metrics should be present
+	// * tetragon_build_info
+	// * tetragon_data_events_total
+	t.Run("TestPodDeleteHealthMetricsOnly", func(t *testing.T) {
+		metricsconfig.InitHealthMetrics(reg)
+
+		deletePod()
+		metricSeries := getMetricSeries(t, reg)
+
+		for _, metric := range healthMetrics {
+			require.NotNil(t, metricSeries[metric])
+		}
+
+		// Event metrics should be nil, even though the pod was deleted
+		for _, metric := range eventMetrics {
+			require.Nil(t, metricSeries[metric])
+		}
+
+	})
 
 	// Process four events, each one with different combination of pod/namespace.
 	// These events should be counted by multiple metrics with a "pod" label:
 	// * tetragon_events_total
 	// * tetragon_policy_events_total
 	// * tetragon_syscalls_total
+	t.Run("TestPodDeleteHealthAndEventMetrics", func(t *testing.T) {
+		metricsconfig.InitEventsMetrics(reg)
+
+		deletePod()
+		metricSeries := getMetricSeries(t, reg)
+		checkMetricSeriesCount(t, metricSeries, eventMetrics, 4)
+
+		// Exactly one timeseries should be deleted for each metric (matching both
+		// pod name and namespace).
+		metrics.DeleteMetricsForPod(&corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "fake-pod",
+				Namespace: "fake-namespace",
+			},
+		})
+
+		metricSeries = getMetricSeries(t, reg)
+		checkMetricSeriesCount(t, metricSeries, eventMetrics, 3)
+	})
+}
+
+func deletePod() {
 	for _, namespace := range []string{"fake-namespace", "other-namespace"} {
 		for _, pod := range []string{"fake-pod", "other-pod"} {
 			event := tetragon.GetEventsResponse{
@@ -62,20 +105,9 @@ func TestPodDelete(t *testing.T) {
 			eventmetrics.ProcessEvent(&sampleMsgGenericTracepointUnix, &event)
 		}
 	}
-	checkMetricSeriesCount(t, reg, 4)
-
-	// Exactly one timeseries should be deleted for each metric (matching both
-	// pod name and namespace).
-	metrics.DeleteMetricsForPod(&corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "fake-pod",
-			Namespace: "fake-namespace",
-		},
-	})
-	checkMetricSeriesCount(t, reg, 3)
 }
 
-func checkMetricSeriesCount(t *testing.T, registry *prometheus.Registry, seriesCount int) {
+func getMetricSeries(t *testing.T, registry *prometheus.Registry) map[string]*io_prometheus_client.MetricFamily {
 	metricFamilies, err := registry.Gather()
 	require.NoError(t, err)
 
@@ -83,8 +115,12 @@ func checkMetricSeriesCount(t *testing.T, registry *prometheus.Registry, seriesC
 	for _, metricFamily := range metricFamilies {
 		metricNameToSeries[*metricFamily.Name] = metricFamily
 	}
-	for _, metric := range []string{"tetragon_events_total", "tetragon_policy_events_total", "tetragon_syscalls_total"} {
-		metricFamily := metricNameToSeries[metric]
+	return metricNameToSeries
+}
+
+func checkMetricSeriesCount(t *testing.T, metricSeries map[string]*io_prometheus_client.MetricFamily, metrics []string, seriesCount int) {
+	for _, metric := range metrics {
+		metricFamily := metricSeries[metric]
 		require.NotNil(t, metricFamily)
 		assert.Len(t, metricFamily.Metric, seriesCount)
 	}
