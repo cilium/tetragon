aa4345d39dbda1626dac7ae99dbd8a8699c2613c
Anastasios Papagiannis <anastasios.papagiannis@isovalent.com>
2026-06-18 10:21:10 +0300

    Update tests
    Signed-off-by: Anastasios Papagiannis <anastasios.papagiannis@isovalent.com>

--- pkg/sensors/tracing/tracepoint_test.go
+++ pkg/sensors/tracing/tracepoint_test.go
@@ -454,7 +454,7 @@ func TestLoadTracepointSensor(t *testing.T) {
 		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "retprobe_map", Progs: []uint{1, 2}})
 
 		// all kprobe but generic_tracepoint_filter
-		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "config_map", Progs: []uint{0, 2, 4}})
+		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "config_map", Progs: []uint{0, 2, 4, 5}})
 
 		// shared with base sensor
 		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "execve_map", Progs: []uint{3, 4, 5}})
@@ -482,7 +482,7 @@ func TestLoadTracepointSensor(t *testing.T) {
 		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "retprobe_map", Progs: []uint{1, 2, 6, 7}})
 
 		// all kprobe but generic_tracepoint_filter
-		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "config_map", Progs: []uint{0, 2, 6}})
+		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "config_map", Progs: []uint{0, 2, 4, 5, 6}})
 
 		// shared with base sensor
 		sensorMaps = append(sensorMaps, tus.SensorMap{Name: "execve_map", Progs: []uint{3}})
