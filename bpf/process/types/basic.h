5ba56ec820594830921a17dcb7061b5b1688621d
Leonard Cohnen <leonard.cohnen@gmail.com>
2026-06-23 05:02:08 +0200

    bpf: set e->pass to the action of the selector
    Previously, we handed the offset into the passed arg filter to
    generic_action. In the future there may be different ways to get to the
    action stage, so the arg filter state doesn't always precede the action
    stage.
    
    Therefore, require the previous stage to already set the e->pass to the
    offset of the action in the matched selector. generic_action then just
    has to read the action at that offset.
    
    Signed-off-by: Leonard Cohnen <leonard.cohnen@gmail.com>

--- bpf/process/types/basic.h
+++ bpf/process/types/basic.h
@@ -2331,6 +2331,10 @@ filter_arg(struct msg_generic_kprobe *e, struct selector_arg_filter *filter, cha
 		return filter_arg_2(e, filter, args);
 }
 
+/* selector_arg_offset returns
+ * - the offset immediately after the argument filters, or
+ * - 0 if an argument does not match.
+ */
 FUNC_INLINE int
 selector_arg_offset(void *ctx, struct bpf_map_def *tailcalls,
 		    __u8 *f, struct msg_generic_kprobe *e, __u32 selidx,
@@ -2375,7 +2379,7 @@ selector_arg_offset(void *ctx, struct bpf_map_def *tailcalls,
 	filters = (struct selector_arg_filters *)&f[seloff];
 
 	if (filters->arglen <= sizeof(struct selector_arg_filters)) // no filters
-		return seloff;
+		return seloff + filters->arglen;
 
 #ifdef __LARGE_BPF_PROG
 #ifdef __V61_BPF_PROG
@@ -2389,7 +2393,7 @@ selector_arg_offset(void *ctx, struct bpf_map_def *tailcalls,
 			     : [argsoff] "+r"(argsoff));
 
 		if (argsoff <= 0)
-			return seloff;
+			return seloff + filters->arglen;
 
 		margsoff = (seloff + argsoff) & INDEX_MASK;
 		filter = (struct selector_arg_filter *)&f[margsoff];
@@ -2441,7 +2445,7 @@ selector_arg_offset(void *ctx, struct bpf_map_def *tailcalls,
 		if (!filter_arg(e, filter, args, arg))
 			return 0;
 	}
-	return seloff;
+	return seloff + filters->arglen;
 }
 
 FUNC_INLINE int filter_args_reject(u64 id)
