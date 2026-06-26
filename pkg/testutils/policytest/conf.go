08547c2289982f91b146f768a17dc63885b2b9af
Kornilios Kourtis <kornilios@isovalent.com>
2026-06-26 14:18:17 +0200

    policytest: introduce ParamVals and helper functions
    This commit introduces a dedicated type for parameter values, and a way
    to enumerate all parameters of a policytest using the recursive method
    allParamValues. This enables testing across the cross-product of all
    parameter values defined in a policytest (implemented in a subsequent
    commit).
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- pkg/testutils/policytest/conf.go
+++ pkg/testutils/policytest/conf.go
@@ -6,13 +6,29 @@
 package policytest
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 )
+
+type ParamVals map[string]any
+
+func (pvs ParamVals) String() string {
+	parts := make([]string, 0, len(pvs))
+	for k, v := range pvs {
+		parts = append(parts, fmt.Sprintf("%s=%v", k, v))
+	}
+	return strings.Join(parts, " ")
+}
+
+func newParamVals() ParamVals {
+	return ParamVals(make(map[string]any))
+}
 
 // TestConf is the runtime configuration for a single policy test
 type TestConf struct {
 	MonitorMode bool
-	ParamValues map[string]any
+	ParamValues ParamVals
 }
 
 // Conf is the runtime configuration for a sequence of policy tests
