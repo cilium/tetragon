22d92ea2ea570ce27202f47c65c0eb105b0027c5
Mahe Tardy <mahe.tardy@gmail.com>
2026-06-26 13:06:04 -0400

    tetra: propagate errors in status command
    Simplify and return actual errors on failure to propagate the exit code
    to the CLI caller.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- cmd/tetra/status/status.go
+++ cmd/tetra/status/status.go
@@ -4,7 +4,6 @@
 package status
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -13,26 +12,28 @@ import (
 	"github.com/cilium/tetragon/cmd/tetra/common"
 )
 
-func getStatus(ctx context.Context, client tetragon.FineGuidanceSensorsClient) {
-	response, err := client.GetHealth(ctx, &tetragon.GetHealthStatusRequest{})
-	if err != nil {
-		fmt.Printf("status error: %s\n", err)
-		return
-	}
-	healthStatus := response.GetHealthStatus()
-	if len(healthStatus) == 0 {
-		fmt.Println("Health Status: no status available")
-		return
-	}
-	fmt.Printf("Health Status: %s\n", healthStatus[0].Details)
-}
-
 func New() *cobra.Command {
 	return &cobra.Command{
 		Use:   "status",
 		Short: "Print health status",
-		Run: func(_ *cobra.Command, _ []string) {
-			common.CliRun(getStatus)
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			c, err := common.NewClientWithDefaultContextAndAddress()
+			if err != nil {
+				return fmt.Errorf("failed to create gRPC client: %w", err)
+			}
+			defer c.Close()
+
+			response, err := c.Client.GetHealth(c.Ctx, &tetragon.GetHealthStatusRequest{})
+			if err != nil {
+				return fmt.Errorf("failed to get health status: %w", err)
+			}
+			healthStatus := response.GetHealthStatus()
+			if len(healthStatus) == 0 {
+				cmd.Println("Health Status: no status available")
+				return nil
+			}
+			cmd.Printf("Health Status: %s\n", healthStatus[0].Details)
+			return nil
 		},
 	}
 }
