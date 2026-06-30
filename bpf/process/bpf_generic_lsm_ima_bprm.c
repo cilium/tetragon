c12d0d1156ecdd25eb3d49d597fb9f7160aaa675
Tam Mach <tam.mach@cilium.io>
2026-06-30 10:38:24 +1000

    bpf: Relocate bprm->file read in generic_lsm_ima_bprm
    This is highlighted by verifier, follow the similar pattern we have in
    process.h. Read it via __builtin_preserve_access_index so the correct
    field is used.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- bpf/process/bpf_generic_lsm_ima_bprm.c
+++ bpf/process/bpf_generic_lsm_ima_bprm.c
@@ -25,9 +25,9 @@ BPF_PROG(ima_bprm, struct linux_binprm *bprm)
 
 	if (dummy && dummy->state == 1) {
 		if (bpf_core_enum_value_exists(enum bpf_func_id, BPF_FUNC_ima_file_hash))
-			hash.algo = ima_file_hash(bprm->file, &hash.value, MAX_IMA_HASH_SIZE);
+			hash.algo = ima_file_hash(_(bprm->file), &hash.value, MAX_IMA_HASH_SIZE);
 		else
-			hash.algo = ima_inode_hash(bprm->file->f_inode, &hash.value, MAX_IMA_HASH_SIZE);
+			hash.algo = ima_inode_hash(_(bprm->file)->f_inode, &hash.value, MAX_IMA_HASH_SIZE);
 		hash.state = 2;
 		map_update_elem(&ima_hash_map, &pid_tgid, &hash, BPF_ANY);
 	}
