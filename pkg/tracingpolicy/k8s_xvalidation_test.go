20c0d2f491508df4df2e3383305cff2f581e46a4
Federico Di Pierro <nierro92@gmail.com>
2026-07-03 16:08:06 +0200

    new(pkg/tracingpolicy): added some new tests to test k8s validation.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/tracingpolicy/k8s_xvalidation_test.go
+++ pkg/tracingpolicy/k8s_xvalidation_test.go
@@ -33,7 +33,8 @@ metadata:
   name: "uprobe"
 spec:
   uprobes:
-  - path: "/usr/bin/test"`
+  - path: "/usr/bin/test"
+`
 	if withSymbol {
 		crd += "    symbols: [\"test_3\"]\n"
 	}
@@ -46,22 +47,25 @@ spec:
 
 	_, err := FromYAML(crd)
 	require.Error(t, err)
+	require.Contains(t, err.Error(), "symbols, addrs or offsets defined")
 }
 
 func TestUprobeValidationSymbolsAddrsOffsets(t *testing.T) {
-	testUprobeValidationSymbolsAddrsOffsets(t, true, true, true)
-}
+	t.Run("SymbolAddrOffset", func(t *testing.T) {
+		testUprobeValidationSymbolsAddrsOffsets(t, true, true, true)
+	})
 
-func TestUprobeValidationSymbolsAddrs(t *testing.T) {
-	testUprobeValidationSymbolsAddrsOffsets(t, true, true, false)
-}
+	t.Run("SymbolAddr", func(t *testing.T) {
+		testUprobeValidationSymbolsAddrsOffsets(t, true, true, false)
+	})
 
-func TestUprobeValidationSymbolsOffsets(t *testing.T) {
-	testUprobeValidationSymbolsAddrsOffsets(t, true, false, true)
-}
+	t.Run("SymbolOffset", func(t *testing.T) {
+		testUprobeValidationSymbolsAddrsOffsets(t, true, false, true)
+	})
 
-func TestUprobeValidationAddrsOffsets(t *testing.T) {
-	testUprobeValidationSymbolsAddrsOffsets(t, false, true, true)
+	t.Run("AddrOffset", func(t *testing.T) {
+		testUprobeValidationSymbolsAddrsOffsets(t, false, true, true)
+	})
 }
 
 func TestUprobeValidationReturnWithoutArg(t *testing.T) {
@@ -80,4 +84,52 @@ spec:
 
 	_, err := FromYAML(crd)
 	require.Error(t, err)
+}
+
+func testUprobeValidationOverrideArgNewSymbolAddrOffset(t *testing.T, withSymbol, withAdrr, withOff bool) {
+	crd := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "uprobe"
+spec:
+  uprobes:
+  - path: "/usr/bin/test"
+    symbols:
+    - "test_1"
+    selectors:
+    - matchActions:
+      - action: Override
+`
+	if withSymbol {
+		crd += "        argNewSymbol: \"test_3\"\n"
+	}
+	if withAdrr {
+		crd += "        argNewAddr: 0x985256\n"
+	}
+	if withOff {
+		crd += "        argNewOffset: 0x9156366\n"
+	}
+
+	_, err := FromYAML(crd)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "argNewSymbol, argNewAddr or argNewOffset defined")
+}
+
+func TestUprobeValidationOverrideArgNewSymbolAddrOffset(t *testing.T) {
+	t.Run("NewSymbolAddrOffset", func(t *testing.T) {
+		testUprobeValidationOverrideArgNewSymbolAddrOffset(t, true, true, true)
+	})
+
+	t.Run("NewSymbolAddr", func(t *testing.T) {
+		testUprobeValidationOverrideArgNewSymbolAddrOffset(t, true, true, false)
+	})
+
+	t.Run("NewSymbolOffset", func(t *testing.T) {
+		testUprobeValidationOverrideArgNewSymbolAddrOffset(t, true, false, true)
+	})
+
+	t.Run("NewAddrOffset", func(t *testing.T) {
+		testUprobeValidationOverrideArgNewSymbolAddrOffset(t, false, true, true)
+	})
 }
