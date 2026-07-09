3e7a3bde731bd267eba51b92a372790b1b289151
Jiri Olsa <jolsa@kernel.org>
2026-07-09 20:35:31 +0000

    tetragon: Remove copyLoadedCollection from windows build
    The collection is not used in windows tests.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/program/loader_windows.go
+++ pkg/sensors/program/loader_windows.go
@@ -122,7 +122,7 @@ func doLoadProgram(
 	load *Program,
 	loadOpts *LoadOpts,
 	_ int,
-	keepCollection bool,
+	_ bool,
 ) (*LoadedCollection, error) {
 
 	coll, err := bpf.GetCollectionByPath(load.Name)
@@ -248,9 +248,5 @@ func doLoadProgram(
 	// the memory from it.
 	load.KernelTypes = nil
 
-	// Copy the loaded collection before it's destroyed
-	if keepCollection {
-		return copyLoadedCollection(coll)
-	}
 	return nil, nil
 }
