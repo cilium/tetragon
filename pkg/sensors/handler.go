e9fb4d6a331ecf60b29e11ed7d717fcc86f22bd1
Andy Strohman <astrohma@isovalent.com>
2026-07-20 20:14:40 +0000

    sensors: add partially loaded state
    This signifies that the policy loaded, but not all hooks were attached.
    
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- pkg/sensors/handler.go
+++ pkg/sensors/handler.go
@@ -157,7 +157,7 @@ func (h *handler) addTracingPolicy(op *tracingPolicyAdd) error {
 		col.destroy(true)
 		return err
 	}
-	col.state = EnabledState
+	col.state = col.getEnabledStateType()
 	return nil
 }
 
@@ -187,7 +187,7 @@ func (h *handler) deleteTracingPolicy(op *tracingPolicyDelete) error {
 
 // should be called with h.collections.mu locked (for writing)
 func (h *handler) doDisableTracingPolicy(col *collection) error {
-	if col.state != EnabledState {
+	if !col.isEnabled() {
 		return fmt.Errorf("tracing policy %s is not enabled", col.name)
 	}
 
@@ -232,7 +232,7 @@ func (h *handler) doEnableTracingPolicy(col *collection) error {
 		return col.err
 	}
 
-	col.state = EnabledState
+	col.state = col.getEnabledStateType()
 	return nil
 }
 
@@ -435,7 +435,7 @@ func (h *handler) listPolicies(domain string) []*tetragon.TracingPolicyStatus {
 		pol := tetragon.TracingPolicyStatus{
 			Id:           col.tracingpolicyID,
 			Name:         ck.name,
-			Enabled:      col.state == EnabledState,
+			Enabled:      col.isEnabled(),
 			FilterId:     col.policyfilterID,
 			State:        col.state.ToTetragonState(),
 			Mode:         col.mode(),
