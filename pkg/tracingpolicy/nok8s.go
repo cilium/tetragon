7cec387f6c1df27e6a8aa44d612101118781c0ca
Federico Di Pierro <nierro92@gmail.com>
2026-07-22 15:57:09 +0200

    new(nok8s): validate policies using JSON schemas.
    Prior to this change, feeding a wrong-syntax policy spec
    to nok8s build would've accepted it just fine.
    After this change, syntax errors are caught and correctly thrown.
    Note that `x-kubernetes-validations` CEL expressions validation
    will come in the follow up commit.
    
    New dependency: github.com/santhosh-tekuri/jsonschema/v6.
    Impact of the change:
    * Binary size:
      * main: `49532K`
      * HEAD: `50752K`
    * Running memory:
      * main: `70252K`
      * HEAD: `73168`
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/tracingpolicy/nok8s.go
+++ pkg/tracingpolicy/nok8s.go
@@ -6,12 +6,44 @@
 package tracingpolicy
 
 import (
+	"bytes"
+	_ "embed"
 	"encoding/json"
 	"fmt"
+
+	"github.com/santhosh-tekuri/jsonschema/v6"
 
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 	"github.com/cilium/tetragon/pkg/nok8s"
 )
+
+const tpURL = "file:///tracingpolicy"
+
+var (
+	//go:embed schemas/tracingpolicy-cilium.io.json
+	tracingpolicyJSONSchema []byte
+	tpSchema                *jsonschema.Schema
+)
+
+type memLoader struct{}
+
+func (m *memLoader) Load(url string) (any, error) {
+	switch url {
+	case tpURL:
+		return bytesToMap(tracingpolicyJSONSchema)
+	}
+	return nil, fmt.Errorf("wrong loader url: %s", url)
+}
+
+func init() {
+	c := jsonschema.NewCompiler()
+	c.UseLoader(&memLoader{})
+	tpSchema = c.MustCompile(tpURL)
+}
+
+func bytesToMap(b []byte) (any, error) {
+	return jsonschema.UnmarshalJSON(bytes.NewReader(b))
+}
 
 // FromYAML parses a YAML string into a TracingPolicy -- !k8s version
 func FromYAML(data string) (TracingPolicy, error) {
@@ -26,6 +58,16 @@ func FromYAML(data string) (TracingPolicy, error) {
 		if err := json.Unmarshal(jsonBytes, &gtp); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal TracingPolicy: %w", err)
 		}
+
+		m, err := bytesToMap(jsonBytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert TracingPolicy to map: %w", err)
+		}
+
+		if err = tpSchema.Validate(m); err != nil {
+			return nil, fmt.Errorf("failed to validate TracingPolicy: %w", err)
+		}
+
 		return &gtp, nil
 	case v1alpha1.TPNamespacedKindDefinition:
 		return nil, fmt.Errorf("namespaced tracing policies not supported in non-k8s builds: %s", kind)
