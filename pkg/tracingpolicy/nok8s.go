0834ad56b705686d8d33d0c59e5361046daa926e
Federico Di Pierro <nierro92@gmail.com>
2026-07-22 15:57:09 +0200

    new(nok8s): validate policies using json schemas.
    Prior to this change, feeding a wrong-syntax policy spec
    to nok8s build would've accepted it just fine.
    After this change, syntax errors are caught and correctly thrown.
    Note that `x-kubernetes-validations` cel expressions are not validated.
    
    New dependency: github.com/santhosh-tekuri/jsonschema/v6.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/tracingpolicy/nok8s.go
+++ pkg/tracingpolicy/nok8s.go
@@ -6,12 +6,42 @@
 package tracingpolicy
 
 import (
+	"bytes"
+	_ "embed"
 	"encoding/json"
 	"fmt"
+
+	"github.com/santhosh-tekuri/jsonschema/v6"
 
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
 	"github.com/cilium/tetragon/pkg/nok8s"
 )
+
+const tpURL = "file:///tracingpolicy"
+
+var (
+	//go:embed schemas/tracingpolicy-cilium.io.json
+	tracingpolicyJsonSchema []byte
+	tpSchema                *jsonschema.Schema
+)
+
+var _ jsonschema.URLLoader = &MemLoader{}
+
+type MemLoader struct{}
+
+func (m *MemLoader) Load(url string) (any, error) {
+	switch url {
+	case tpURL:
+		return jsonschema.UnmarshalJSON(bytes.NewReader(tracingpolicyJsonSchema))
+	}
+	return nil, fmt.Errorf("wrong loader url: %s", url)
+}
+
+func init() {
+	c := jsonschema.NewCompiler()
+	c.UseLoader(&MemLoader{})
+	tpSchema = c.MustCompile(tpURL)
+}
 
 // FromYAML parses a YAML string into a TracingPolicy -- !k8s version
 func FromYAML(data string) (TracingPolicy, error) {
@@ -22,8 +52,15 @@ func FromYAML(data string) (TracingPolicy, error) {
 
 	switch kind {
 	case v1alpha1.TPKindDefinition:
+		inst, err := jsonschema.UnmarshalJSON(bytes.NewReader(jsonBytes))
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+		}
+		if err = tpSchema.Validate(inst); err != nil {
+			return nil, fmt.Errorf("failed to validate TracingPolicy: %w", err)
+		}
 		var gtp GenericTracingPolicy
-		if err := json.Unmarshal(jsonBytes, &gtp); err != nil {
+		if err = json.Unmarshal(jsonBytes, &gtp); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal TracingPolicy: %w", err)
 		}
 		return &gtp, nil
