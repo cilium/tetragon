1c44bddf62cd31531c32b299a329690f8aad6bc0
Federico Di Pierro <nierro92@gmail.com>
2026-07-23 17:02:59 +0200

    new: add new policytest to test the issue.
    [ upstream commit 6987ab007cc446c3133515d18601be14a61128ae ]
    
    The `uprobe-resolve` tester prog was also updated to allow us
    to resolve a secondary argument in its `func()` call.
    
    Finally, we had to add a probe (`HasCopyFromUserStr`) to check
    whether kernel supports `bpf_copy_from_user_str`.
    
    [ backporter's note ]: I only backported part of this commit in order to
    get CopyFromUserStr.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>
    Signed-off-by: Andy Strohman <astrohma@isovalent.com>

--- pkg/bpf/detect_linux.go
+++ pkg/bpf/detect_linux.go
@@ -660,6 +660,12 @@ func HasSubStringKfunc() bool {
 	return HasProgramLargeSize() && HasKfunc("bpf_strnstr")
 }
 
+// HasCopyFromUserStr returns true if the kernel supports the bpf_copy_from_user_str kfunc
+// which is required for many uprobe register-related tests.
+func HasCopyFromUserStr() bool {
+	return HasKfunc("bpf_copy_from_user_str")
+}
+
 func detectMixBpfAndTailCalls() bool {
 	// create a tail call map
 	tcMap, err := ebpf.NewMap(&ebpf.MapSpec{
@@ -791,4 +797,5 @@ var FeatureProbes = []FeatureProbe{
 	{Fentry, HasFentryProgram},
 	{GetFuncRet, HasGetFuncRetHelper},
 	{SubStringKfuncProbe, HasSubStringKfunc},
+	{CopyFromUserStr, HasCopyFromUserStr},
 }
