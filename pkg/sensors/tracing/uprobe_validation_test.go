b8f7fe7c5d125de3d02ace642e22f90bee5e7383
Federico Di Pierro <nierro92@gmail.com>
2026-07-24 08:28:43 +0200

    cleanup(pkg/sensors): drop TestUprobeValidationMultiplePreloadArguments.
    No need for it anymore since we now support multiple preload args.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/sensors/tracing/uprobe_validation_test.go
+++ pkg/sensors/tracing/uprobe_validation_test.go
@@ -11,38 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/tetragon/pkg/k8s/apis/cilium.io/v1alpha1"
-	"github.com/cilium/tetragon/pkg/testutils"
 )
-
-func TestUprobeValidationMultiplePreloadArguments(t *testing.T) {
-
-	// Using multiple preload arguments
-
-	uprobe := testutils.RepoRootPath("contrib/tester-progs/usdt-override")
-	crd := `
-apiVersion: cilium.io/v1alpha1
-kind: TracingPolicy
-metadata:
-  name: "uprobe"
-spec:
-  uprobes:
-  - path: "` + uprobe + `"
-    symbols:
-    - "test_3"
-    data:
-    - index: 0
-      type: "string"
-      source: "pt_regs"
-      resolve: "rdi"
-    - index: 1
-      type: "string"
-      source: "pt_regs"
-      resolve: "rsi"
-`
-
-	err := checkCrd(t, crd)
-	require.Error(t, err)
-}
 
 func TestUprobeEventConfigCarriesPolicyID(t *testing.T) {
 	var state uprobeConfigState
