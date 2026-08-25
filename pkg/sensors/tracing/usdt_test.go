c5744752c8506d9520c9dc90d7894fdab9c7d746
Federico Di Pierro <nierro92@gmail.com>
2026-08-25 09:23:34 +0200

    fix: fix all failing tests due to wrong-case keywords
    There were a bunch of tests using wrong-case keywords such as:
    * `apiversion` instead of `apiVersion`
    * `matchPids` instead of `matchPIDs`
    
    Fix all of them.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/sensors/tracing/usdt_test.go
+++ pkg/sensors/tracing/usdt_test.go
@@ -51,7 +51,7 @@ spec:
 `
 
 	noConfig := `
-apiversion: cilium.io/v1alpha1
+apiVersion: cilium.io/v1alpha1
 kind: TracingPolicy
 metadata:
   name: "noconfig"
@@ -91,7 +91,7 @@ spec:
 `
 
 	noConfig := `
-apiversion: cilium.io/v1alpha1
+apiVersion: cilium.io/v1alpha1
 kind: TracingPolicy
 metadata:
   name: "noconfig"
