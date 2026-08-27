46d683ac1b8700ffebce8f78568127847ee672a2
Mahe Tardy <mahe.tardy@gmail.com>
2026-08-27 13:16:03 +0200

    contrib/verify: simplify kernel version extraction
    This will make next commit easier, we will need to deal with 10.x kernel
    versions anyway, so I think it's okay to panic in that test helper.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- contrib/verify/helpers.go
+++ contrib/verify/helpers.go
@@ -6,20 +6,19 @@
 package verify
 
 import (
-	"fmt"
 	"strings"
 )
 
-func extractKernelVersion(fileName string) (string, error) {
+func extractKernelVersion(fileName string) string {
 	if idx := strings.LastIndex(fileName, "_v"); idx != -1 {
 		versionPart := strings.TrimSuffix(fileName[idx+2:], ".o")
 		if len(versionPart) >= 2 {
 			if versionPart[:1] == "1" {
-				return "", fmt.Errorf("version 10.x not supported (we will need to disambiguate notation): %s", versionPart)
+				panic("version 10.x not supported (we will need to disambiguate notation): " + versionPart)
 			}
-			return versionPart[:1] + "." + versionPart[1:], nil
+			return versionPart[:1] + "." + versionPart[1:]
 		}
 	}
 
-	return "", nil
+	return ""
 }
