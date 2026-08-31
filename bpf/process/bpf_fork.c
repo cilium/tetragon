2e18cc7bf792884d06b86285169fb8dc6398488e
Mahe Tardy <mahe.tardy@gmail.com>
2026-08-31 11:46:44 +0000

    bpf: copy args from parent at event_wake_up_new_task
    A child should inherit from its parent binary and cmd line arguments.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- bpf/process/bpf_fork.c
+++ bpf/process/bpf_fork.c
@@ -58,6 +58,7 @@ BPF_KPROBE(event_wake_up_new_task, struct task_struct *task)
 	curr->key.ktime = tg_get_ktime();
 	curr->nspid = get_task_pid_vnr_by_task(task);
 	memcpy(&curr->bin, &parent->bin, sizeof(curr->bin));
+	memcpy(&curr->args, &parent->args, sizeof(curr->args));
 	curr->pkey = parent->key;
 
 	/* Store the thread leader capabilities so we can check later
