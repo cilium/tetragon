77298f1b54dbac24a5dab68bf36b02246d95243b
Jiri Olsa <jolsa@kernel.org>
2026-08-31 12:14:22 +0000

    tetragon: Add uprobe-heap-size tracing policy option
    Add a new tracing policy option, uprobe-heap-size, that allows a spec
    file to configure the size of following maps:
    
      process_call_heap
      process_call_heap
      ratelimit_heap
      string_postfix_maps_heap
      string_prefix_maps_heap
      string_maps_heap
      buffer_heap_map
    
    used by uprobe and usdt sensors.
    
      options:
        - name: "uprobe-heap-size"
          value: "65536"
    
    With this option all heap maps are no longer shared (default) but dedicated
    to the sensor programs.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/genericusdt.go
+++ pkg/sensors/tracing/genericusdt.go
@@ -47,6 +47,7 @@ type observerUsdtSensor struct {
 type usdtHas struct {
 	sleepableOffload bool
 	sleepablePreload bool
+	uprobeHeapSize   int
 }
 
 var (
@@ -152,6 +153,9 @@ func createGenericUsdtSensor(
 		useMulti:   !polInfo.specOpts.DisableUprobeMulti && bpf.HasUprobeMulti(),
 	}
 
+	// user process_call_heap override
+	has.uprobeHeapSize = polInfo.specOpts.UprobeHeapSize
+
 	hasSetAction := false
 
 	defer func() {
@@ -244,12 +248,12 @@ func createMultiUsdtSensor(
 	filterMap := program.MapBuilderProgram("filter_map", load)
 	workloadsMap := program.MapBuilderProgram("workloads_map", load)
 
-	maps = append(maps, getUprobeHeapMap("process_call_heap", load))
-	maps = append(maps, getUprobeHeapMap("buffer_heap_map", load))
-	maps = append(maps, getUprobeHeapMap("string_maps_heap", load))
-	maps = append(maps, getUprobeHeapMap("string_prefix_maps_heap", load))
-	maps = append(maps, getUprobeHeapMap("string_postfix_maps_heap", load))
-	maps = append(maps, getUprobeHeapMap("ratelimit_heap", load))
+	maps = append(maps, getUprobeHeapMap("process_call_heap", has.uprobeHeapSize, load))
+	maps = append(maps, getUprobeHeapMap("buffer_heap_map", has.uprobeHeapSize, load))
+	maps = append(maps, getUprobeHeapMap("string_maps_heap", has.uprobeHeapSize, load))
+	maps = append(maps, getUprobeHeapMap("string_prefix_maps_heap", has.uprobeHeapSize, load))
+	maps = append(maps, getUprobeHeapMap("string_postfix_maps_heap", has.uprobeHeapSize, load))
+	maps = append(maps, getUprobeHeapMap("ratelimit_heap", has.uprobeHeapSize, load))
 	maps = append(maps, configMap, tailCalls, filterMap, workloadsMap)
 
 	filterMap.SetMaxEntries(len(multiIDs))
