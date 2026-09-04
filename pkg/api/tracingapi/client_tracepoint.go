6df2c8a7d5c5a856749de6415255b3261ae5ead1
Rafael Roquetto <rroquett@isovalent.com>
2026-09-04 17:56:27 -0600

    alignchecker: add msg_generic_kprobe
    Check msg_generic_tracepoint and its corresponding go structs,
    MsgGenericKprobe and MsgGenericTracepoint.
    
    Signed-off-by: Rafael Roquetto <rroquett@isovalent.com>

--- pkg/api/tracingapi/client_tracepoint.go
+++ pkg/api/tracingapi/client_tracepoint.go
@@ -8,15 +8,15 @@ import "github.com/cilium/tetragon/pkg/api/processapi"
 type MsgGenericTracepointArg any
 
 type MsgGenericTracepoint struct {
-	Common        processapi.MsgCommon
-	ProcessKey    processapi.MsgExecveKey
-	Namespaces    processapi.MsgNamespaces
-	Capabilities  processapi.MsgCapabilities
-	FuncId        int64
-	RetProbeId    uint64
-	ActionId      uint64
-	ActionArgId   uint32
-	Tid           uint32 // The recorded TID that triggered the event
-	KernelStackID int64
-	UserStackID   int64
+	Common        processapi.MsgCommon       `align:"common"`
+	ProcessKey    processapi.MsgExecveKey    `align:"current"`
+	Namespaces    processapi.MsgNamespaces   `align:"ns"`
+	Capabilities  processapi.MsgCapabilities `align:"caps"`
+	FuncId        int64                      `align:"func_id"`
+	RetProbeId    uint64                     `align:"retprobe_id"`
+	ActionId      uint64                     `align:"action"`
+	ActionArgId   uint32                     `align:"action_arg_id"`
+	Tid           uint32                     `align:"tid"` // The recorded TID that triggered the event
+	KernelStackID int64                      `align:"kernel_stack_id"`
+	UserStackID   int64                      `align:"user_stack_id"`
 }
