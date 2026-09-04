bd6c1bd32634abe61ebded9acebaea519f4603cb
Rafael Roquetto <rroquett@isovalent.com>
2026-09-04 17:21:29 -0600

    alignchecker: add msg_throttle_event
    Signed-off-by: Rafael Roquetto <rroquett@isovalent.com>

--- pkg/api/processapi/processapi.go
+++ pkg/api/processapi/processapi.go
@@ -258,8 +258,8 @@ type MsgCgroupEvent struct {
 }
 
 type MsgThrottleEvent struct {
-	Common MsgCommon
-	Kube   MsgK8s
+	Common MsgCommon `align:"common"`
+	Kube   MsgK8s    `align:"kube"`
 }
 
 type KernelStats struct {
