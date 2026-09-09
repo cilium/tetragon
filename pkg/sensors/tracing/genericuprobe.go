f6c6c56f2863d189114779abb3dbc79613c4cc68
Jiri Olsa <jolsa@kernel.org>
2026-09-09 08:30:51 +0000

    tetragon: Switch buffer_heap_map map to hash for uprobe/usdt
    The uprobe/usdt probes path in kernel do not disable preemption,
    we need to use hash instead of per-cpu heap.
    
    The uprobe and usdt sensor now create buffer_heap_map as hash
    map with BPF_F_NO_PREALLOC and use it as shared map with 32768
    entries.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/genericuprobe.go
+++ pkg/sensors/tracing/genericuprobe.go
@@ -1519,6 +1519,7 @@ func createMultiUprobeSensor(polInfo *policyInfo, sensorPath string, multiIDs []
 	retProbe := program.MapBuilderSensor("retprobe_map", load)
 
 	maps = append(maps, getUprobeHeapMap("process_call_heap", load))
+	maps = append(maps, getUprobeHeapMap("buffer_heap_map", load))
 	maps = append(maps, configMap, tailCalls, filterMap, retProbe)
 	maps = append(maps, createSelectorMaps(load, getUprobeProgramSelector(load, nil), substringMapEntries)...)
 
@@ -1572,6 +1573,7 @@ func createMultiUprobeSensor(polInfo *policyInfo, sensorPath string, multiIDs []
 		retFilterMap.SetMaxEntries(len(multiRetIDs))
 
 		maps = append(maps, getUprobeHeapMap("process_call_heap", loadret))
+		maps = append(maps, getUprobeHeapMap("buffer_heap_map", loadret))
 	}
 
 	return progs, maps, nil
