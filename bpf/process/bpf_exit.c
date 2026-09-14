e25b3d9570d8495ac2b188a550deb62f90af8820
Jiri Olsa <jolsa@kernel.org>
2026-09-14 11:36:35 +0000

    tetragon: Move bpf_exit.h into bpf_exit.c
    bpf_exit.h is a private header included only by bpf_exit.c, so fold
    its contents (exit_heap_map and event_exit_send) directly into
    bpf_exit.c and drop the header.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_exit.c
+++ bpf/process/bpf_exit.c
@@ -2,11 +2,80 @@
 /* Copyright Authors of Cilium */
 
 #include "vmlinux.h"
-#include "compiler.h"
-#include "bpf_exit.h"
+#include "api.h"
 #include "bpf_tracing.h"
 
+#include "compiler.h"
+#include "bpf_event.h"
+#include "bpf_task.h"
+#include "bpf_rate.h"
+#include "process.h"
+#include "bpf_process_event.h"
+#include "bpf_ktime.h"
+
 char _license[] __attribute__((section("license"), used)) = "Dual BSD/GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, struct msg_exit);
+} exit_heap_map SEC(".maps");
+
+FUNC_INLINE void event_exit_send(void *ctx, __u32 tgid)
+{
+	struct execve_map_value *enter;
+
+	/* It is safe to do a map_lookup_event() here because
+	 * we must have captured the execve case in order for an
+	 * exit to happen. Or in the FGS startup case we pre
+	 * populated it before loading BPF programs. At any rate
+	 * if the entry is _not_ in the execve_map the lookup
+	 * will create an empty entry, the ktime check below will
+	 * catch it and we will quickly delete the entry again.
+	 */
+	enter = execve_map_get_noinit(tgid);
+	if (!enter)
+		return;
+	if (enter->key.ktime) {
+		struct task_struct *task = (struct task_struct *)get_current_task();
+		size_t size = sizeof(struct msg_exit);
+		struct msg_exit *exit;
+		int zero = 0;
+
+		exit = map_lookup_elem(&exit_heap_map, &zero);
+		if (!exit)
+			return;
+
+		exit->common.op = MSG_OP_EXIT;
+		exit->common.flags = 0;
+		exit->common.pad[0] = 0;
+		exit->common.pad[1] = 0;
+		exit->common.size = size;
+		exit->common.ktime = tg_get_ktime();
+
+		exit->current.pid = tgid;
+		exit->current.pad[0] = 0;
+		exit->current.pad[1] = 0;
+		exit->current.pad[2] = 0;
+		exit->current.pad[3] = 0;
+		exit->current.ktime = enter->key.ktime;
+
+		/**
+		 * Per thread tracking rules TID == PID :
+		 *  We want the exit event to match the exec one, and since during exec
+		 *  we report the thread group leader, do same here as we read the exec
+		 *  entry from the execve_map anyway and explicitly set it to the to tgid.
+		 */
+		exit->info.tid = tgid;
+		with_errmetrics(probe_read, &exit->info.code, sizeof(exit->info.code),
+				_(&task->exit_code));
+
+		event_output_metric(ctx, MSG_OP_EXIT, exit, size);
+	}
+	execve_map_delete(tgid);
+	map_delete_elem(&tg_parents_bin, &enter->key.pid);
+}
 
 /*
  * Hooking on acct_process kernel function, which is called on the task's
