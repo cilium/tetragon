ea4888b36196048ff8cc30156b3c8833ecf77a31
Mark Stemm <mstemm@isovalent.com>
2026-09-15 17:38:52 -0700

    Look up pod sandboxes for pods and save in cgidmap
    When updating a pod, explicitly look up the pod sandbox for the pod
    and pass the pod sandbox id to cgidmap via UpdatePodSandbox().
    
    This ensures that cgidmap can look up pod sandboxes for cgroups
    associated with pod sandboxes.
    
    Signed-off-by: Mark Stemm <mstemm@isovalent.com>

--- pkg/cgidmap/register.go
+++ pkg/cgidmap/register.go
@@ -6,11 +6,14 @@
 package cgidmap
 
 import (
+	"context"
 	"fmt"
 	"uuid"
 
 	corev1 "k8s.io/api/core/v1"
+	criapi "k8s.io/cri-api/pkg/apis/runtime/v1"
 
+	"github.com/cilium/tetragon/pkg/cri"
 	"github.com/cilium/tetragon/pkg/logger"
 	"github.com/cilium/tetragon/pkg/logger/logfields"
 	"github.com/cilium/tetragon/pkg/manager/events"
@@ -48,6 +51,7 @@ func deletePodHandler(m Map, pod *corev1.Pod) {
 		return
 	}
 	m.Update(podID, nil)
+	m.UpdatePodSandbox(podID, "")
 }
 
 func updatePodHandler(m Map, pod *corev1.Pod) {
@@ -57,4 +61,28 @@ func updatePodHandler(m Map, pod *corev1.Pod) {
 		return
 	}
 	m.Update(podID, podhelpers.PodContainersIDs(pod))
+
+	var sandboxID string
+	ctx := context.Background()
+	if cli, err := cri.GetClient(ctx); err == nil {
+		req := criapi.ListPodSandboxRequest{
+			Filter: &criapi.PodSandboxFilter{
+				LabelSelector: map[string]string{
+					"io.kubernetes.pod.uid": string(pod.UID),
+				},
+			},
+		}
+		if res, err := cli.ListPodSandbox(ctx, &req); err != nil {
+			logger.GetLogger().Warn("cgidmap, podUpdated: failed to list pod sandbox", "pod-id", pod.UID, logfields.Error, err)
+		} else {
+			for _, item := range res.GetItems() {
+				if id := item.GetId(); id != "" {
+					sandboxID = id
+					break
+				}
+			}
+		}
+	}
+
+	m.UpdatePodSandbox(podID, sandboxID)
 }
